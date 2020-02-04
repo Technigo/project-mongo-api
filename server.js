bd006a8f31a4c8967660f20534b7c67c488e1373
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2020-02-04 20:44:19 +0100

    Add schema, database seeding and route logic

--- server.js
+++ server.js
@@ -1,38 +1,74 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import netflixData from './data/netflix-titles.json';
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/netflix';
+// mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+try {
+  mongoose.connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+} catch (error) {
+  console.log(error);
+}
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+mongoose.Promise = Promise;
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+// Show schema
+const Show = mongoose.model('Show', {
+  title: String,
+  director: String,
+  cast: String,
+  country: String,
+  date: String,
+  release_year: Number,
+  rating: String,
+  duration: String,
+  listed_in: String,
+  description: String,
+  type: String
+});
+
+// Delete content and prepare MongoDB with new data
+Show.deleteMany().then(() => {
+  netflixData.forEach(show => {
+    delete show.show_id;
+    new Show({ ...show }).save();
+  });
+});
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get('/shows', (req, res) => {
+  Show.find().then(shows => {
+    res.json(shows);
+  });
+});
+
+app.get('/shows/:id', (req, res) => {
+  Show.find({ _id: req.params.id }).then(show => {
+    if (show) {
+      res.json(show);
+    } else {
+      res.status(404).json({
+        error: 'Not found'
+      });
+    }
+  });
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
