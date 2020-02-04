429de7a1789459b329e44e5e8f111d35dca4aa18
Theresa Ulwahn <theresaulwahn@gmail.com>
2020-02-04 18:29:43 +0100

    pushing for the night. have asked on stack and slack for help to make the athletes show on the box

--- server.js
+++ server.js
@@ -5,15 +5,6 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import athletesData from './data/athletes-data.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
-
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
@@ -32,10 +23,10 @@ const Box = mongoose.model('Box', {
   affiliatename: String,
   countryoforiginname: String,
   athlete: {
-      //this object is relating to an object-ID from another model
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Athlete'
-      }
+    //this object is relating to an object-ID from another model
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Athlete'
+    }
 })
 
 if (process.env.RESET_DATABASE) {
@@ -75,17 +66,17 @@ app.get('/athletes', async (req, res) => {
   res.json(athletes)
 } )
 
-app.get('/athletes/:id', (req, res) => {
-  const showId = req.params.id 
-    console.log(showId)
-  const show = athletesData.filter(item => +item.competitorid === +showId)
-    console.log("id path parameter")
-  if (show) {
-    res.json(show)
-  } else {
-    res.status(404).json({ error: 'Athlete not found' })
-  }
-})
+// app.get('/athletes/:id', (req, res) => {
+//   const showId = req.params.id 
+//     console.log(showId)
+//   const show = athletesData.filter(item => +item.competitorid === +showId)
+//     console.log("id path parameter")
+//   if (show) {
+//     res.json(show)
+//   } else {
+//     res.status(404).json({ error: 'Athlete not found' })
+//   }
+// })
 
 app.get('/box', async (req, res) => {
   const box = await Box.find()
