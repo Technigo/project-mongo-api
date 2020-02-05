ab896a44bfb1676d1151691fd29ce4ea15b86448
nasimmhn <nasim.mahzoun@gmail.com>
2020-02-05 14:40:44 +0100

    init commit

--- server.js
+++ server.js
@@ -2,23 +2,49 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import data from './data/netflix-titles.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+
+//setup connection to mongodb
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/shows"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
+
+//to prevent reloading seeddatabase when server starts, we can wrap it in an environment variable//
+
+//setup of shows title
+const Show = mongoose.model('Show', {
+  // Properties defined here match the keys from the netflix-title.json file
+  show_id: Number,
+  title: String,
+  // director: String,
+  // cast: String,
+  // country: String,
+  // date_added: toString,
+  // release_year: Number,
+  // rating: String,
+  // duration: toString,
+  // listed_in: String,
+  // description: String,
+  // type: String
+})
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Show.deleteMany({})
+
+    data.forEach((showData) => {
+      new Show(showData).save()
+    })
+  }
+
+  seedDatabase()
+}
+
+
+
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
@@ -31,6 +57,14 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+
+//setup of GET route, client can fetch all the shows
+app.get("/shows", async (req, res) => {
+  const shows = await Show.find()
+  res.json(shows)
+})
+
+
 
 // Start the server
 app.listen(port, () => {
