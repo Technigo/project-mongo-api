caf5ce72cded977dae87e2abd74962e3661d00f3
Angelica Brodin <brodin.angelica@gmail.com>
2020-02-06 16:44:48 +0100

    changed the database to books in the datafile that already exists

--- server.js
+++ server.js
@@ -2,40 +2,52 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
-
 import booksData from './data/books.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/books"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-const Author = mongoose.model('Author', {
-  name: String,
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Author'
+const Book = mongoose.model('Book', {
+  bookID: {
+    type: Number
+  },
+  title: {
+    type: String
+  },
+  authors: {
+    type: String
+  },
+  average_rating: {
+    type: Number
+  },
+  isbn: {
+    unique: true,
+    type: String
+  },
+  isbn13: {
+    type: String
+  },
+  language_code: {
+    type: String
+  },
+  num_pages: {
+    type: Number
+  },
+  ratings_count: {
+    type: Number
+  },
+  text_reviews_count: {
+    type: Number
   }
-})
+});
 
-if (process.env.RESET_DATABASE) {
-  const seedDatabase = async () => {
-    await Author.deleteMany()
-    const tolkien = new Author({ name: 'J.R.R Tolkien' })
-    await tolkien.save()
-
-    const rowling = new Author({ name: 'J.K Rowling' })
-    await rowling.save()
-
-    await new Book({ title: "Harry Potter and the Philosepher's Stone", author: rowling }).save()
-    await new Book({ title: "Harry Potter and the Chamber of Secrets", author: rowling }).save()
-    await new Book({ title: "Harry Potter and the Prisoner of Azkaban", author: rowling }).save()
-    await new Book({ title: "Harry Potter and the Goblet of Fire", author: rowling }).save()
-    await new Book({ title: "Harry Potter and the Order of the Phoenix", author: rowling }).save()
-    await new Book({ title: "The Lord of the Rings", author: tolkien }).save()
-    await new Book({ title: "The Hobbit", author: tolkien }).save()
-  }
-  seedDatabase()
-}
+const addBooksToDatabase = () => {
+  booksData.forEach((book) => {
+    new Book(book).save();
+  });
+};
+addBooksToDatabase();
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -50,16 +62,6 @@ app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World')
-})
-
-app.get('/authors', async (req, res) => {
-  const authors = await Author.find()
-  res.json(authors)
-})
-
-app.get('/books', async (req, res) => {
-  const books = await Book.find().populate('author')
-  res.json(books)
 })
 
 // Start the server
