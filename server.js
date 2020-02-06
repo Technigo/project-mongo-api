09b73cf87d7724dd9b1423bd972939e8eccac8da
nyblad <nyblad@hotmail.com>
2020-02-06 10:28:14 +0100

    Added attending query and updated search query to name

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import guests from './data/guests.json'
 
-// Boiler plate for setting up mongoose
+// MONGOOSE SETUP
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/guest-list'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Model for guest
+// MODEL FOR GUEST
 const Guest = mongoose.model('Guest', {
   first_name: String,
   last_name: String,
@@ -22,6 +22,7 @@ const Guest = mongoose.model('Guest', {
   status_attending: Boolean,
 })
 
+// RESET DATABASE ON START
 if (process.env.RESET_DB) {
   console.log('Resetting database')
   const seedDatabase = async () => {
@@ -34,11 +35,11 @@ if (process.env.RESET_DB) {
   seedDatabase()
 }
 
-// Defines the port the app will run on. Defaults to 8000, can be overridden e.g PORT=9000 npm run dev
-const port = process.env.PORT || 8000
+// PORT & APP SETUP
+const port = process.env.PORT || 8000 // Default 8000, can be overridden e.g PORT=5000 npm run dev
 const app = express()
 
-// Middlewares
+// MIDDLEWARES
 app.use(cors())
 app.use(bodyParser.json())
 app.use((req, res, next) => {
@@ -49,24 +50,37 @@ app.use((req, res, next) => {
   }
 })
 
-// Start defining your routes here
+// ROUTES FOR GET
 app.get('/', (req, res) => {
   res.send('Mongo-project: Guest list')
 })
 
+// All guests
 app.get('/guests', async (req, res) => {
   const guests = await Guest.find()
+  const statusAttending = req.query.attending
   const page = req.query.page
-  const searchName = req.query.search
+  const searchName = req.query.name
   const PER_PAGE = 10
   let guestList = guests
 
+  // Attending true or false - Can I do this in a shorter way?
+  if (statusAttending === 'true') {
+    guestList = guestList.filter((item) => item.status_attending === true)
+  }
+  if (statusAttending === 'false') {
+    guestList = guestList.filter((item) => item.status_attending === false)
+  }
+
+  // Searching on first name or last name
   if (searchName) {
     guestList = guestList.filter((item) => {
-      const guestName = item.first_name.toLowerCase() || item.last_name.toLowerCase()
-      return guestName.includes(searchName.toLowerCase())
+      const firstName = item.first_name.toLowerCase()
+      const lastName = item.last_name.toLowerCase()
+      return (firstName.includes(searchName.toLowerCase()) || lastName.includes(searchName.toLowerCase()))
     })
   }
+  // Pagination
   if (page) {
     const startIndex = PER_PAGE * page
     guestList = guestList.slice(startIndex, startIndex + PER_PAGE)
@@ -77,8 +91,9 @@ app.get('/guests', async (req, res) => {
   })
 })
 
+// Specific guest id
 app.get('/guests/:id', async (req, res) => {
-  const guest = await Guest.findById(req.param.id) // Id for specific guest
+  const guest = await Guest.findById(req.param.id)
   if (guest) {
     res.json(guest)
   } else {
@@ -87,12 +102,15 @@ app.get('/guests/:id', async (req, res) => {
 })
 
 // Preparing for next step to use in form to add/update/delete guests
+// ROUTES FOR POST
 // app.post('/guests', (req, res) => {
 //   return res.send('Received a POST HTTP method')
 // })
+// ROUTES FOR PUT
 // app.put('/guests/:id', (req, res) => {
 //   return res.send(`PUT HTTP method on guest/${req.params.id} resource`)
 // })
+//ROUTES FOR DELETE
 // app.delete('/guests/:id', (req, res) => {
 //   return res.send(`DELETE HTTP method on guest/${req.params.id} resource`)
 // })
