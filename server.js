e47c32fe2f1cff02cce6b9e2dc807ea4384992e2
Jens_Nilsen <jensnilsen89@gmail.com>
2020-02-06 19:40:45 +0100

    code along done

--- server.js
+++ server.js
@@ -2,19 +2,37 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import { restart } from 'nodemon'
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 //
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
-import booksData from './data/books.json'
+// import booksData from './data/books.json'
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
-const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/books'
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/animals'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+
+const Animal = mongoose.model('Animal', {
+  name: String,
+  age: Number,
+  isFurry: Boolean
+})
+
+Animal.deleteMany().then(() => {
+  new Animal({ name: 'Alfons', age: 23, isFurry: true }).save()
+  new Animal({ name: 'hippo', age: 45, isFurry: false }).save()
+  new Animal({ name: 'Gogo', age: 43, isFurry: false }).save()
+  new Animal({
+    name: 'Alfonso the spanish Alfons',
+    age: 84,
+    isFurry: true
+  }).save()
+})
 
 // Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
@@ -27,10 +45,43 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: 'Service unavailable' })
+  }
+})
+
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send(booksData)
+  Animal.find().then((animals) => {
+    res.json(animals)
+  })
 })
+
+app.get('/:id', async (req, res) => {
+  try {
+    const animal = await Animal.findById(req.params.id)
+    if (animal) {
+      res.json(animal)
+    } else {
+      res.status(404).json({ error: 'not found' })
+    }
+  } catch (err) {
+    res.status(400).json({ error: 'Invalid id' })
+  }
+})
+
+// app.get('/:name', (req, res) => {
+//   Animal.findOne({ name: req.params.name }).then((animal) => {
+//     if (animal) {
+//       res.json(animal)
+//     } else {
+//       res.status(404).json({ error: 'not found' })
+//     }
+//   })
+// })
 
 // Start the server
 app.listen(port, () => {
