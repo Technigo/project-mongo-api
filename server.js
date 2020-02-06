bcbe0ba1e529b2e4c6f1594b3edf2ecb98d6d5e9
Michel Elmoznino Laufer <mitchel_85@hotmail.com>
2020-02-06 13:14:24 +0100

    Created mongoose model and paths for books, isbn and id

--- server.js
+++ server.js
@@ -2,17 +2,16 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import booksData from './data/books.json'
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
-// 
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project_mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
@@ -20,16 +19,82 @@ mongoose.Promise = Promise
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 9001
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
+// Mongoose model of books
+const Book = mongoose.model('Book', {
+  bookID: Number,
+  title: String,
+  authors: String,
+  average_rating: Number,
+  isbn: Number,
+  isbn13: Number,
+  language_code: String,
+  num_pages: Number,
+  ratings_count: Number,
+  text_reviews_count: Number
+})
+
+if(process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Book.deleteMany({})
+
+    booksData.forEach((bookData) => {
+      new Book(bookData).save()
+    })
+  }
+  seedDatabase()
+}
+
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Books API')
+})
+
+
+
+// Return the array with all the book objects, using query for title
+// For example, http://localhost:9001/books?title=Harry will return all the books with 'Harry' in the title
+// Also, the books are sorted by average rating from the highest to lowest
+app.get('/books', (req, res) => {
+  const queryString = req.query.title 
+  const queryRegex = new RegExp(queryString, "i")
+  Book.find({'title': queryRegex})
+    .sort({'average_rating': -1})
+    .then((results) => {
+      console.log('Found')
+      res.json(results)
+    }) .catch((err) => {
+       console.log('Error ' + err)
+       res.json({message: 'Cannot find book', err: err}) 
+    })
+})
+
+// Return an individual book object with a specific isbn-number
+app.get('/books/:isbn', (req, res) => {
+  const isbn = req.params.isbn 
+  Book.findOne({'isbn': isbn})
+    .then((results) => {
+      res.json(results)
+    }) .catch((err) => {
+      res.json({message: 'Cannot find the book', err: err})
+    })
+})
+
+// Return an individual book object with a specific id
+app.get('/books/_id/:_id', (req, res) => {
+  const _id = req.params._id
+  Book.findOne({'_id': _id})
+    .then((results) => {
+      res.json(results)
+    }) .catch((err) => {
+      res.json({message: 'Cannot find this book', err: err})
+    })
 })
 
 // Start the server
