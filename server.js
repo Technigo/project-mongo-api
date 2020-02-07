63d29db809b3a94273b05de43b4ba83c143a5922
Disa de Beau <Disan@MacBook-Air.local>
2020-02-07 12:33:56 +0100

    After wednesday codealong

--- server.js
+++ server.js
@@ -2,34 +2,87 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import booksData from './data/books.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
+
+const Book = mongoose.model('Book', {
+  bookID: {
+    type: String
+  },
+  title: {
+    type: String
+  },
+  authors: {
+    type: String
+  },
+  average_rating: {
+    type: Number
+  },
+  isbn: {
+    unique: true,
+    type: Number
+  },
+  isbn13: {
+    type: Number
+  },
+  language_code: {
+    type: String
+  },
+  num_pages: {
+    type: Number
+  },
+  ratings_count: {
+    type: Number
+  },
+  text_reviews_count: {
+    type: Number
+  }
+})
+
+const addBooksToDatabase = () => {
+  booksData.forEach((book) => {
+    new Book(book).save()
+  })
+}
+// addBooksToDatabase()
+
+const port = process.env.PORT || 9000
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+app.get('/books', (req, res) => {
+  const queryString = req.query.query
+  const queryRegex = new RegExp(queryString, "i")
+  Book.find({ 'title': queryRegex })
+    .sort({ 'average_rating': -1 })
+    .then((results) => {
+      //if it works
+      console.log('Found : ' + results)
+      res.json(results)
+    }).catch((err) => {
+      //if it doesn't work
+      console.log('Error ' + err)
+      res.json({ message: "Cannot find this book", err: err })
+    })
+})
+
+
+app.get('/books/:isbn', (req, res) => {
+  const isbn = req.params.isbn
+  Book.findOne({ 'isbn': isbn })
+    .then((results) => {
+      res.json(results)
+    }).catch((err) => {
+      res.json({ message: "Cannot find this book", err: err })
+    })
 })
 
 // Start the server
