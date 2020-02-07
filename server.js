19221db879429f59f25f7204226a59a5abc5cc66
Emmie Nilsson <emmie.nilsson@gmail.com>
2020-02-07 09:32:01 +0100

    Added model and function for seeding database for top music.

--- server.js
+++ server.js
@@ -3,29 +3,43 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import topMusicData from './data/top-music.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
+
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8082
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
+
+const Track = mongoose.model('Track', {
+  trackName: String,
+  artistName: String,
+  genre: String,
+  bpm: Number
+})
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Track.deleteMany({})
+
+    topMusicData.forEach((trackData) => {
+      new Track(trackData).save((err, trackData) => {
+        if (err) {
+          console.log(err)
+        }
+      })
+    })
+  }
+  seedDatabase()
+}
 
 // Start defining your routes here
 app.get('/', (req, res) => {
