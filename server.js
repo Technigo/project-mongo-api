b403f2d0498610255bae81a1a818bc19b98978f8
Jenfi <jennifer_fick@hotmail.com>
2020-02-08 16:50:15 +0100

    final

--- server.js
+++ server.js
@@ -2,17 +2,8 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
 import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
-// const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/books"
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
@@ -52,22 +43,33 @@ const Book = mongoose.model('Book', {
   },
 })
 
-// booksData.forEach((book) => { //This forEach-loop saves the bokks to the data base, so all the books from the json are put in the data base
-//   new Book(book).save();
-// })
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Book.deleteMany({})
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+    booksData.array.forEach((book) => {
+      new Book(book).save()
+    });
+  }
+  seedDatabase()
+}
+
+
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: 'Service unavailble' })
+  }
+})
+
+
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
@@ -77,22 +79,39 @@ app.get('/books', (req, res) => {
   const queryRegex = new RegExp(queryString, 'i');
   Book.find({ 'title': queryRegex })
     .then((results) => {
-      //Successfull
       console.log('Found ' + results);
       res.json(results);
     }).catch((err) => {
-      //Error/failure
       console.log('Error ' + err);
       res.json({ message: 'Cannot find this book' })
     })
 })
 
-app.get('/authors', async (req, res) => {
-  const authors = await Author.find()
-  res.json(authors)
+app.get('/books/:isbn', (req, res) => {
+  const isbn = req.params.isbn;
+  Book.findOne({ 'isbn': isbn })
+    .then((results) => {
+      res.json(results);
+    }).catch((err) => {
+      console.log({ message: 'Error ' + err })
+      res.json({ message: 'Cannot find this book' })
+    })
 })
 
-// Start the server
+app.get('/authors', async (req, res) => {
+  const queryString = req.query.author
+  const authorRegex = new RegExp(queryString, 'i')
+  Book.find({ 'authors': authorRegex })
+    .then((results) => {
+      console.log('Found ' + results);
+      res.json(results);
+    }).catch((err) => {
+      console.log('Error ' + err);
+      res.json({ message: 'Cannot find this author' })
+    })
+})
+
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
