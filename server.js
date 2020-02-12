3ece4ae0b748ee917b3a5bd7ab1551db54aa7f13
HannaCharlotteHolm <50885736+HannaCharlotteHolm@users.noreply.github.com>
2020-02-12 16:23:44 +0100

    Done

--- server.js
+++ server.js
@@ -2,26 +2,45 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+dotenv.config()
+
+import netflixData from './data/netflix-titles.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
+
+const Movie = mongoose.model('Movie', {
+  "show_id": Number,
+  "title": String,
+  "director": String,
+  "cast": String,
+  "country": String,
+  "date_added": String,
+  "release_year": Number,
+  "rating": String,
+  "duration": String,
+  "listed_in": String,
+  "description": String,
+  "type": String
+})
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Movie.deleteMany({})
+
+    netflixData.forEach((movie) => {
+      new Movie(movie).save()
+    })
+  }
+
+  seedDatabase()
+}
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
@@ -29,7 +48,35 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Check out /movies :D')
+})
+
+app.get('/movies', (req, res) => {
+  Movie.find()
+    .then((movies) => {
+      res.json(movies)
+    })
+})
+
+app.get('/movies/:id', (req, res) => {
+  Movie.findById(req.params.id)
+    .then((movie) => {
+      res.json(movie)
+    })
+})
+
+app.get('/movies/years/:year', (req, res) => {
+  Movie.find({ release_year: +req.params.year })
+    .then((movies) => {
+      res.json(movies)
+    })
+})
+
+app.get('/movies/types/:type', (req, res) => {
+  Movie.find({ type: req.params.type })
+    .then((movies) => {
+      res.json(movies)
+    })
 })
 
 // Start the server
