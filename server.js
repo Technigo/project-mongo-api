9883bfa2133e7291db0f706b83f549cee3ce363e
ucha3i <jchabik@gmail.com>
2020-02-18 21:15:47 +0100

    working project

--- server.js
+++ server.js
@@ -3,33 +3,62 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
+import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
 // import booksData from './data/books.json'
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/globes"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+// setup mangoose models
+const Category = mongoose.model('Category', {
+  name: String
+})
+
+const Movie = mongoose.model('Movie', {
+  title: String,
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category'
+  }
+})
+
+const seedDatabase = async () => {
+  await Category.deleteMany()
+  await Movie.deleteMany()
+
+  const cat = new Category({ name: 'Best movie of XX century' })
+  await cat.save()
+
+  const mov = new Movie({ title: 'Rejs (English title - The Cruise)', category: cat })
+  await mov.save()
+}
+
+seedDatabase()
+
+// ports
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
+// middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+// routes 
+
+app.get('/', (_, res) => {
+  res.send('Second backend project')
+})
+
+app.get('/categories', async (_, res) => {
+  res.json(await Category.find())
+})
+
+app.get('/titles', async (_, res) => {
+  res.json(await Movie.find().populate('category'))
 })
 
 // Start the server
