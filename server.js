6f61eb5577b6e493581b35418a82f702d27a5095
Camilla <camillalofroth@hotmail.com>
2020-03-09 10:28:54 +0100

    new asanas

--- server.js
+++ server.js
@@ -5,6 +5,29 @@ import mongoose from 'mongoose'
 import crypto from 'crypto'
 import bcrypt from 'bcrypt-nodejs'
 
+import cloudinary from 'cloudinary'
+import multer from 'multer'
+import cloudinaryStorage from 'multer-storage-cloudinary'
+
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+cloudinary.config({
+  cloud_name: 'dlhmccbpm', // this needs to be whatever you get from cloudinary
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+}) //Handles API keys and secrets fron cloudinary
+
+const storage = cloudinaryStorage({
+  cloudinary,
+  folder: 'pets',
+  allowedFormats: ['jpg', 'png'],
+  transformation: [{ width: 500, height: 500, crop: "limit" }]
+}) //storage
+
+process.env.CLOUDINARY_API_KEY
+
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/asana" //API
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUndefinedTopology: true })
 mongoose.Promise = Promise
@@ -18,7 +41,9 @@ const Chakra = mongoose.model('Chakra', {
 
 const Asana = mongoose.model('Asana', {
   name: String,
-  advanced: Boolean,
+  sanskritname: String,
+  description: String,
+  repeat: Boolean,
   chakra: {
   type: mongoose.Schema.Types.ObjectId,
   ref: 'Chakra'
@@ -71,65 +96,70 @@ const seedDatabase = async () => {
 
   //Rootchakra
 
-  await new Asana({ name: "Standing mountain pose", chakra: rootchakra, advanced: false }).save()
-  await new Asana({ name: "Table pose", chakra: rootchakra, advanced: false }).save()
-  await new Asana({ name: "Downward facing dog pose", chakra: rootchakra, advanced: false }).save()
-  await new Asana({ name: "Akward chair pose", chakra: rootchakra, advanced: false }).save()
-  await new Asana({ name: "High lunge pose", chakra: rootchakra, advanced: false }).save()
-  await new Asana({ name: "Tree pose", chakra: rootchakra, advanced: false }).save()
-  await new Asana({ name: "Cobra pose", chakra: rootchakra, advanced: false }).save()
-  await new Asana({ name: "Bridge pose", chakra: rootchakra, advanced: false }).save()
+  await new Asana({ name: "Childs pose", sanskritname: "Balasana", chakra: rootchakra, repeat: false }).save()//!
+  await new Asana({ name: "Downward facing dog pose", sanskritname: "Adho Muhka Svanasana", chakra: rootchakra, repeat: false }).save()//!
+  await new Asana({ name: "Akward chair pose", sanskritname: "Utkatasana", chakra: rootchakra, repeat: false }).save()//!
+  await new Asana({ name: "High lunge pose", sanskritname: "Utthita Ashwa Sanchalanasana", chakra: rootchakra, repeat: true }).save()//!
+  await new Asana({ name: "Tree pose", sanskritname: "Vrksasana", chakra: rootchakra, repeat: false }).save()//!
+  await new Asana({ name: "Bridge pose", sanskritname: "Setu Bandha Sarvangasana", chakra: rootchakra, repeat: false }).save()//!
 
   //Sacralchakra
 
-  await new Asana({ name: "Knee circles", chakra: sacralchakra, advanced: false }).save()
-  await new Asana({ name: "Butterfly pose", chakra: sacralchakra, advanced: false }).save()
-  await new Asana({ name: "Open-leg forward pose", chakra: sacralchakra, advanced: false }).save()
-  await new Asana({ name: "Happy baby pose", chakra: sacralchakra, advanced: false }).save()
-  await new Asana({ name: "Eye of the needle pose", chakra: sacralchakra, advanced: false }).save()
-  await new Asana({ name: "Deep Lounge", chakra: sacralchakra, advanced: false }).save()
-  await new Asana({ name: "Pigeon pose", chakra: sacralchakra, advanced: false }).save()
-  await new Asana({ name: "Savasana with open legs", chakra: sacralchakra, advanced: false }).save()
-  await new Asana({ name: "Fire log pose", chakra: sacralchakra, advanced: false }).save()
-  await new Asana({ name: "Standing forward fold", chakra: sacralchakra, advanced: false }).save()
+  await new Asana({ name: "Butterfly pose", sanskritname: "Supta Baddha Konasana", chakra: sacralchakra, repeat: false }).save()//!
+  await new Asana({ name: "Open-leg forward pose", sanskritname: "Upavistha Konasana", chakra: sacralchakra, repeat: false }).save()//!
+  await new Asana({ name: "Lizard pose", sanskritname: "Uttan Pristhasana", chakra: sacralchakra, repeat: true }).save()//!
+  await new Asana({ name: "Deep Lounge", sanskritname: "Anjaneyasana", chakra: sacralchakra, repeat: true }).save()//!
+  await new Asana({ name: "Pigeon pose", sanskritname: "Eka Pada Kapotasana", chakra: sacralchakra, repeat: true }).save()//!
+  await new Asana({ name: "Fire log pose", sanskritname: "Agnistambhasana", chakra: sacralchakra, repeat: false }).save()//!
+  await new Asana({ name: "Standing forward fold", sanskritname: "Uttanasana", chakra: sacralchakra, repeat: false }).save() //!
         
   //Solarplexuschakra
 
-  await new Asana({ name: "Warrior pose 1", chakra: solarplexuschakra, advanced: false }).save()
-  await new Asana({ name: "Warrior pose 2", chakra: solarplexuschakra, advanced: false }).save()
-  await new Asana({ name: "Warrior pose 3", chakra: solarplexuschakra, advanced: false }).save()
-  await new Asana({ name: "Triangle pose", chakra: solarplexuschakra, advanced: false }).save()
-  await new Asana({ name: "Reverse warrior pose", chakra: solarplexuschakra, advanced: false }).save()
-  await new Asana({ name: "Seated twist", chakra: solarplexuschakra, advanced: false }).save()
-  await new Asana({ name: "Standing half moon pose", chakra: solarplexuschakra, advanced: false }).save()
-  await new Asana({ name: "Boat pose", chakra: solarplexuschakra, advanced: false }).save()
-  await new Asana({ name: "Extended side angle pose", chakra: solarplexuschakra, advanced: false }).save()
-  await new Asana({ name: "Plank pose", chakra: solarplexuschakra, advanced: false }).save()
+  await new Asana({ name: "Warrior pose 1", sanskritname: "Virabhadrasana 1", chakra: solarplexuschakra, repeat: true }).save()//!
+  await new Asana({ name: "Warrior pose 2", sanskritname: "Virabhadrasana 2", chakra: solarplexuschakra, repeat: true }).save()//!
+  await new Asana({ name: "Warrior pose 3", sanskritname: "Virabhadrasana 3", chakra: solarplexuschakra, repeat: true }).save()//!
+  await new Asana({ name: "Triangle pose", sanskritname: "Trikonasana", chakra: solarplexuschakra, repeat: true }).save()//!
+  await new Asana({ name: "Reverse warrior pose", sanskritname: "Viparita Virabhadrasana", chakra: solarplexuschakra, repeat: true }).save()//!
+  await new Asana({ name: "Seated twist", sanskritname: "Ardha Matsyendrasana", chakra: solarplexuschakra, repeat: true }).save()//!
+  await new Asana({ name: "Standing half moon pose", sanskritname: "Ardha Chandrasana", chakra: solarplexuschakra, repeat: true }).save()//!
+  await new Asana({ name: "Boat pose", sanskritname: "Paripurna Navasana", chakra: solarplexuschakra, repeat: false }).save()//!
+  await new Asana({ name: "Bow pulling pose", sanskritname: "Dhanurasana", chakra: solarplexuschakra, repeat: false }).save()//!
+  await new Asana({ name: "Plank pose", sanskritname: "Phalakasana", chakra: solarplexuschakra, repeat: false }).save()//!
 
     //Heartchakra
 
-    await new Asana({ name: "Thread the needle twist", chakra: heartchakra, advanced: false }).save()
-    await new Asana({ name: "Cobra pose", chakra: heartchakra, advanced: false }).save()
-    await new Asana({ name: "Camel pose", chakra: heartchakra, advanced: false }).save()
-    await new Asana({ name: "Fish pose", chakra: heartchakra, advanced: false }).save()
-    await new Asana({ name: "Wheel pose", chakra: heartchakra, advanced: true }).save()
-    await new Asana({ name: "Half circle pose", chakra: heartchakra, advanced: false }).save()
-    await new Asana({ name: "Crocodile pose", chakra: heartchakra, advanced: false }).save()
-    await new Asana({ name: "Extended puppy pose", chakra: heartchakra, advanced: false }).save()
-    await new Asana({ name: "Handstand", chakra: heartchakra, advanced: true }).save()
-    await new Asana({ name: "Cat/Cow", chakra: heartchakra, advanced: false }).save()
-    await new Asana({ name: "Restorative Savasana", chakra: heartchakra, advanced: false }).save()
+    
+    await new Asana({ name: "Camel pose", sanskritname: "Utrasana", chakra: heartchakra, repeat: false }).save()//!
+    await new Asana({ name: "Fish pose", sanskritname: "Maysyasana", chakra: heartchakra, repeat: false }).save()//!
+    await new Asana({ name: "Wheel pose", sanskritname: "Urdvha Dhanurasana", chakra: heartchakra, repeat: false }).save()//!
+    await new Asana({ name: "Half circle pose", sanskritname: "Ardha Mandalasana", chakra: heartchakra, repeat: true }).save()//!
+    await new Asana({ name: "Extended puppy pose", sanskritname: "Anahatasana", chakra: heartchakra, repeat: false }).save()//!
+    await new Asana({ name: "Handstand", sanskritname: "Ardho Mukha Vrksasana", chakra: heartchakra, repeat: false }).save()//!
+    await new Asana({ name: "Wild thing", sanskritname: "Camatkarasana", chakra: heartchakra, repeat: true }).save()//!
+    await new Asana({ name: "Restorative Savasana", sanskritname: "Savasana", chakra: heartchakra, repeat: false }).save()//!
+    await new Asana({ name: "Cobra pose", sanskritname: "Bhujangasana", chakra: rootchakra, repeat: false }).save()//!
 
     //Throatchakra
 
-    await new Asana({ name: "Plow pose", chakra: heartchakra, advanced: false }).save()
-    await new Asana({ name: "Neck streches", chakra: heartchakra, advanced: false }).save()
-    await new Asana({ name: "Lateral shoulder streche", chakra: heartchakra, advanced: false }).save()
-    await new Asana({ name: "Crow pose", chakra: heartchakra, advanced: false }).save()
-    await new Asana({ name: "Shoulder stand", chakra: heartchakra, advanced: true }).save()
-    await new Asana({ name: "Rabbit pose", chakra: heartchakra, advanced: false }).save()
-    await new Asana({ name: "Shoulder shrugs", chakra: heartchakra, advanced: false }).save()
-    await new Asana({ name: "Seated yoga mudra", chakra: heartchakra, advanced: false }).save()
+    await new Asana({ name: "Head circles", chakra: heartchakra, description: "Move your head in a circle", repeat: false }).save()//!
+    await new Asana({ name: "Crane pose", sanskritname: "Kakasana", chakra: heartchakra, repeat: false }).save()//!
+    await new Asana({ name: "Shoulder stand", sanskritname: "Salamba Sarvangasana", chakra: heartchakra, repeat: false }).save()//!
+    await new Asana({ name: "Shoulder shrugs", chakra: heartchakra, repeat: false }).save()//!
+    await new Asana({ name: "Seated yoga mudra", chakra: heartchakra, repeat: false }).save()//!
+
+    //Thirdeyechakra
+
+    await new Asana({ name: "Drawing the line", sanskritname: "Tadasana", chakra: heartchakra, repeat: false }).save()//!
+    await new Asana({ name: "Eagle pose", sanskritname: "Garudasana", chakra: heartchakra, repeat: true }).save()//!
+    await new Asana({ name: "Standing side opener pose", sanskritname: "Katichakrasana", chakra: heartchakra, repeat: true }).save()//!
+
+    //Lotus pose
+   
+    await new Asana({ name: "Lotus pose", sanskritname: "Padmasana", chakra: rootchakra, repeat: false }).save()//!
+
+    //Savasana
+
+    await new Asana({ name: "Corpse pose", sanskritname: "Savasana", chakra: heartchakra, description: "Lay on your back and breathe slowly", repeat: false }).save()//!
 }
 seedDatabase()
 }
@@ -180,8 +210,8 @@ app.get('/', (req, res) => {
 
 app.post('/users', async (req, res) => {
   try {
-    const { name, password } = req.body
-    const user = new User({ name, password: bcrypt.hashSync(password) })
+    const { email, password } = req.body
+    const user = new User({ email, password: bcrypt.hashSync(password) })
     const saved = await user.save()
     res.status(201).json(saved)
   } catch (err) {
@@ -191,12 +221,12 @@ app.post('/users', async (req, res) => {
 
 app.post('/sessions', async (req, res) => {
   try {
-    const { name, password } = req.body
-    const user = await User.findOne({ name })
+    const { email, password } = req.body
+    const user = await User.findOne({ email })
     if (user && bcrypt.compareSync(password, user.password)) {
-      res.status(201).json({ useId: user._id, accessToken: user.accessToken })
+      res.status(201).json({ userId: user._id, accessToken: user.accessToken })
     } else {
-      res.json({ notFound: true })
+      res.json({ message: 'User not found' })
     }
   } catch (err) {
     res.status(400).json({ message: 'could not find user', errors: err.errors })
