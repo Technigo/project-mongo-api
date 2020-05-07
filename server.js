fd855558f784ec3cf2f3499b1f40a2ff9725d755
Your Name <malin.marie@hotmail.se>
2020-05-07 19:39:13 +0200

    added data to model/database

--- server.js
+++ server.js
@@ -2,37 +2,97 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import showData from './data/netflix-titles.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+const Shows = mongoose.model('Shows', {
+  "show_id": Number,
+  "title": String,
+  "director": String,
+  "cast": String,
+  "country": String,
+  "release_year": Number,
+  "description": String,
+  "type": String
+}, true)
+
+app.get('/shows', (req, res) => {
+  const page = +(req.query.page ?? 1);
+  const pageSize = +(req.query.pageSize ?? 20);
+   
+  Shows.find().then(show => {    
+    const startIndex = (page -1) * pageSize;
+    const endIndex = startIndex + pageSize ;
+    const shows = show.slice(startIndex, endIndex)
+    const returnObject = { 
+      pageSize: pageSize,
+      page: page,
+      maxPages: Math.ceil(showData.length/pageSize),
+      totalShows: showData.length,
+      results: shows
+    }
+
+    res.json(returnObject)
+  })
 })
 
-// Start the server
+app.get('/shows/:id', (req, res) => {
+  
+  Shows.findOne({ show_id: req.params.id }).then(show => {
+    if (show) {
+      res.json(show)
+    } else {
+      res.status(404).json({ error: `This show does not exist!`})
+    }
+  })
+})
+
+
+// app.get('/:name', (req, res) => {
+//   Animal.findOne({ name: req.params.name}).then(animal => {
+
+//     if (animal) {
+//       res.json(animal)
+//     }
+//     else {
+//       res.status(404).json({ error: `No animals with the name ${req.path.substring(1)}` })
+//     }
+//   })
+// })
+
+app.use((req, res, next) => { // if server
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: "Service unavailable"})
+  }
+})
+
+app.use((req,res,next) => {
+  res.status(404).json({error: `No path ${req.path}`})
+})
+
+if (process.env.RESET_DB) {
+	const seedDatabase = async () => {
+    await Shows.deleteMany({})
+
+		showData.forEach((show) => {
+			new Shows(show).save()
+		})
+  }
+
+  seedDatabase()
+}
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
