b610f8f32dd43d47057b5a4c38c9085f95e454a6
Your Name <malin.marie@hotmail.se>
2020-05-08 18:08:53 +0200

    model in separate file

---
+++ models.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+export const Shows = mongoose.model('Shows', {
+  "show_id": Number,
+  "title": String,
+  "director": [String],
+  "cast": [String],
+  "country": String,
+  "release_year": Number,
+  "description": String,
+  "type": String,
+})
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import showData from './data/netflix-titles.json'
+import { Shows } from './models'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -14,89 +15,88 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-const Shows = mongoose.model('Shows', {
-  "show_id": Number,
-  "title": String,
-  "director": String,
-  "cast": String,
-  "country": String,
-  "release_year": Number,
-  "description": String,
-  "type": String,
-}, true)
+app.get('/', (req, res) => {
+  res.json("Start page")
+})
 
-app.get('/shows', (req, res) => {
+app.get('/shows', async (req, res) => {   
+  const { year, country, actor } = req.query
+  let myFilter = {};
+  
+  if (year) {
+    myFilter['release_year'] = year;
+  }
+
+  if (country) {
+    myFilter['country'] = new RegExp(`${country}`, 'i')
+  }
+
+  if (actor) {
+    myFilter['cast'] = new RegExp(`${actor}`, 'i')
+  }
+  
+  const show = await Shows.find(myFilter).collation({ locale: 'en_US', strength: 1 });
+
   const page = +(req.query.page ?? 1);
-  const pageSize = +(req.query.pageSize ?? 20);
-   
-  Shows.find().then(show => {    
-    const startIndex = (page -1) * pageSize;
-    const endIndex = startIndex + pageSize ;
-    const shows = show.slice(startIndex, endIndex)
-    const returnObject = { 
-      pageSize: pageSize,
-      page: page,
-      maxPages: Math.ceil(showData.length/pageSize),
-      totalShows: showData.length,
-      results: shows
-    }
+  const pageSize = +(req.query.pageSize ?? 20); 
+  const startIndex = (page -1) * pageSize;
+  const endIndex = startIndex + pageSize ;
+  const filteredShows = show.slice(startIndex, endIndex)
+  const returnObject = { 
+    pageSize: pageSize,
+    page: page,
+    maxPages: Math.ceil(show.length/pageSize),
+    totalShows: show.length,
+    results: filteredShows
+  }
 
+  if (returnObject.results.length === 0) {
+    res.json("No results to show")
+  } else {
     res.json(returnObject)
-  })
+  }
 })
 
-app.get('/shows/:id', (req, res) => {
+app.get('/shows/:id', async (req, res) => {
   
-  Shows.findOne({ show_id: req.params.id }).then(show => {
-    if (show) {
-      res.json(show)
-    } else {
-      res.status(404).json({ error: `This show does not exist!`})
-    }
-  })
-})
-
-app.get('/categories', (req, res) => {
+  const show = await Shows.findOne({ show_id: req.params.id })
   
-  Shows.find().then(show => {
-    let categories = []
-
-    show.forEach((item) => {
-      if (!categories.includes(item.type)) {
-        categories.push(item.type)
-      }
-      else return
-    })
-
-    res.json(categories)
-  })
+  if (show) {
+    res.json(show)
+  } else {
+    res.status(404).json({ error: `This show does not exist!`})
+  }
 })
 
-app.get('/categories/:category', (req, res) => {
-  const category = req.params.category
+app.get('/categories', async (req, res) => {
+  let categories = []
 
-  Shows.find( { type: category }).collation({ locale: 'en_US', strength: 1 }).then (category => {
-    if (category.length === 0) (
-      res.json(`There is no category named ${req.params.category}`)
-    )
-    res.json(category)
+  const show = await Shows.find()
+  
+  await show.forEach((item) => {
+    if (!categories.includes(item.type)) {
+      categories.push(item.type)
+    } else return
   })
+
+  res.json(categories)
+
 })
 
+app.get('/categories/:category', async (req, res) => {
+  const { category } = req.params
 
-// app.get('/:name', (req, res) => {
-//   Animal.findOne({ name: req.params.name}).then(animal => {
+  const categoryFind = await Shows.find( { type: category } ).collation({ locale: 'en_US', strength: 1 })
+  
+  if (categoryFind.length === 0) {
+    res.json(`There is no category named ${req.params.category}`)
+  } else {
+    res.json(categoryFind)
+  }
+})
 
-//     if (animal) {
-//       res.json(animal)
-//     }
-//     else {
-//       res.status(404).json({ error: `No animals with the name ${req.path.substring(1)}` })
-//     }
-//   })
-// })
+app.use((req, res, next) => { // if server is unavailable
 
-app.use((req, res, next) => { // if server
   if (mongoose.connection.readyState === 1) {
     next()
   } else {
@@ -113,6 +113,16 @@ if (process.env.RESET_DB) {
     await Shows.deleteMany({})
 
 		showData.forEach((show) => {
+      const splitCast = show.cast.split(", ")
+      show.cast = splitCast
+
+      if (show.director === "") {
+        show.director = [];
+      } else {
+        const splitDirectors = show.director.split(", ")
+        show.director = splitDirectors
+      }
+  
 			new Shows(show).save()
 		})
   }
