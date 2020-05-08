7592a9f841fc1b8b12ac8b0d76d14a0fb2668442
palhamel <pal.hamel@gmail.com>
2020-05-08 19:18:43 +0200

    Setup mongoose model for Book

--- server.js
+++ server.js
@@ -5,16 +5,70 @@ import mongoose from 'mongoose'
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
-// 
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
+import booksData from './data/books.json'
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
+// Database setup:
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+
+// Mongoose model setup:
+  // Book Model
+const Book = mongoose.model('Book', {
+  bookId: {
+    type: Number
+  },
+  title: {
+    type: String
+  },
+  authors: {
+    type: String
+  },
+  average_rating: {
+    type: Number
+  },
+  isbn: {
+    type: String,
+  },
+  isbn13: {
+    type: String,
+  },
+  language_code: {
+    type: String
+  },
+  num_pages: {
+    type: Number
+  },
+  ratings_count: {
+    type: Number
+  },
+  text_reviews_count: {
+    type: Number
+  }
+})
+/*
+Tested for search via unique value
+  isbn: {
+    type: String,
+    // unique: true
+    // But give "Deprecation Warnings" in Node:
+    // https://mongoosejs.com/docs/deprecations.html
+  },
+*/
+
+
+
+
+
+
+
+
+
+
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -29,7 +83,7 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Hello world - Mongo API here! Get som books ready!')
 })
 
 // Start the server
