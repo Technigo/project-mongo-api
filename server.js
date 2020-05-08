0ea183de4c9b19883820173bff16ff7e46ef0d6f
William Jensen <william.riis93@gmail.com>
2020-05-08 10:08:35 +0200

    set up mongo database and books model

--- server.js
+++ server.js
@@ -2,24 +2,14 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import data from './data/books.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -27,9 +17,32 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+const Books = mongoose.model('Book', {
+  bookID: Number,
+  title: String,
+  authors: String,
+  average_rating: Number,
+  num_pages: Number,
+  ratings_count: Number,
+  text_reviews_count: Number
+})
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Books.deleteMany()
+
+    data.forEach((book) => {
+      new Books(book).save()
+    })
+  }
+  seedDatabase()
+}
+
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+app.get('/books', (req, res) => {
+  Books.find().then(books => {
+    res.json(books)
+  })
 })
 
 // Start the server
