c744ceb78d9950af42009b2ce7a75caf3b912d00
Ruterryter <hanna.ruter@gmail.com>
2020-05-08 11:52:54 +0200

    first set up

--- server.js
+++ server.js
@@ -2,25 +2,41 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import topMusicData from './data/top-music.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/tophits"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
+
+const Song = mongoose.model('Song', {
+  trackName: String,
+  artistName: String,
+  genre: String
+
+  // type: mongoose.Schema.Types.ObjectId,
+  // ref: 'Artist',
+
+})
+
+const Artist = mongoose.model('Artist', {
+  artistName: String
+})
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Song.deleteMany({})
+
+    topMusicData.forEach((songData) => {
+      new Song(songData).save()
+    })
+  }
+
+  seedDatabase()
+}
+
+const port = process.env.PORT || 8086
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
@@ -28,8 +44,15 @@ app.use(cors())
 app.use(bodyParser.json())
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+// app.get('/', (req, res) => {
+//   res.send('Hello world')
+// })
+
+//list all the tophits 
+app.get('/hits', async (req, res) => {
+  const songs = await Song.find()
+  res.json(songs)
+
 })
 
 // Start the server
