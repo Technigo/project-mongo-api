c9d51a7537541555e000332969e972d347c57ed5
Christina Persson <christina@MacBook-Pro.local>
2020-05-10 11:29:20 +0200

    Added data to database and can search for objects id

--- server.js
+++ server.js
@@ -1,38 +1,75 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import data from "./data/netflix-titles.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const Content = mongoose.model("Content", {
+  // show_id: Number,
+  title: String,
+  director: String,
+  cast: String,
+  country: String,
+  date_added: String,
+  release_year: Number,
+  rating: String,
+  duration: String,
+  listed_in: String,
+  description: String,
+  type: String,
+});
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Content.deleteMany();
+
+    data.forEach((contentData) => {
+      new Content(contentData).save();
+    });
+  };
+
+  seedDatabase();
+}
+
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/content", async (req, res) => {
+  const content = await Content.find();
+  res.json(content);
+});
+
+app.get("/content/:id", async (req, res) => {
+  const contentById = await Content.findById(req.params.id);
+
+  console.log(contentById);
+  if (contentById) {
+    res.json(contentById);
+  } else {
+    res
+      .status(404)
+      .json({ error: `Content with id number: ${contentById} not found` });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
