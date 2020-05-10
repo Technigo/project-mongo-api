03655182aeeeb265a4df12659beb87ab75d5c01f
Kajsa Bootin <kajsa@Kajsas-Air.zyxel.com>
2020-05-10 12:32:44 +0200

    code along

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 # Mongo API Project
 
-Replace this readme with your own information about your project. 
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+This project is built using the MongoDB database to store and retrieve data from, and having that data to produce a RESTful API. (Used Mongoose models)
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+## Learning objectives
+
+- How to model data in Mongoose
+- How to fetch items from a Mongo database using Mongoose
+- How to seed large amounts of data to a database
+
+## Tech
+
+- Express
+- Node.js
+- Mongoose
 
 ## View it live
-
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
--- server.js
+++ server.js
@@ -4,31 +4,48 @@ import cors from "cors";
 import mongoose from "mongoose";
 import topMusicData from "./data/top-music.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-//
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
-
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.Promise = Promise;
-
-const Song = mongoose.model("Song", {
-  artistName: String,
-  trackName: String,
-  length: Number,
-});
-
 // Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
+
+const Artist = mongoose.model("Artist", {
+  artistName: String,
+  genre: String,
+  popularity: Number,
+});
+
+const Track = mongoose.model("Track", {
+  trackName: String,
+  length: Number,
+  danceability: Number,
+});
+
+/*const Genre = mongoose.model("Genre", {
+  genre: String,
+});*/
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Artist.deleteMany({});
+    await Track.deleteMany({});
+    //await Genre.deleteMany({});
+
+    topMusicData.forEach((topMusicData) => {
+      new Artist(topMusicData).save();
+      new Track(topMusicData).save();
+      //new Genre(topMusicData).save();
+    });
+  };
+
+  seedDatabase();
+}
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
@@ -39,6 +56,12 @@ app.get("/", (req, res) => {
   // Displays all movies and shows
   res.json(topMusicData);
   //res.send("Hello Kajsa");
+});
+
+app.get("/artists", async (req, res) => {
+  const Artist = await Artist.find();
+  console.log(artists);
+  res.json(artists);
 });
 
 // Start the server
