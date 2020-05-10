a0e27c4e2f456a15b6bfcb712788fb84cf059b98
Tuliany <tuliany@mail.com>
2020-05-10 10:02:31 +0200

    reset database

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import { stringify } from 'querystring'
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -12,15 +13,44 @@ import mongoose from 'mongoose'
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/books"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
+const Author = mongoose.model('Author',{
+  name: String
+})
+
+const Book = mongoose.model('Book', {
+  title: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Author'
+  }
+})
+
+if (process.env.RESET_DATABASE) {
+console.log('reseting')
+
+  const seedDatabase = async () => {
+    await Author.deleteMany()
+
+    const coelho = new Author ({ name: 'Paulo Coelho'})
+    await coelho.save()
+
+    const echart = new Author ({name: 'Echart Tolle'})
+    await echart.save() 
+
+    await new Book({ title: "The Alchemist", author: Coelho}).save()
+    await new Book({ title: "The Power of Now", author: Tolle }).save()
+}
+seedDatabase()
+}
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 5000
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
@@ -30,6 +60,11 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/authors', async (req, res) => {
+  const authors = await Author.find()
+  res.json(authors)
 })
 
 // Start the server
