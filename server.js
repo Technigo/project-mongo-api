911b21ca8ed8d3c67bd1143dfd88e28a5a8da978
Ingrid Hammargren <ingrid.hammargren@gmail.com>
2020-05-11 15:56:58 +0200

    created mongoose model and the first route

--- server.js
+++ server.js
@@ -2,19 +2,37 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import topMusicData from './data/top-music.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+
+// Database model
+const Track = mongoose.model('Track', {
+  id: Number,
+  trackName: String,
+  artistName: String,
+  genre: String,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  loudness: Number,
+  liveness: Number,
+  valence: Number,
+  length: Number,
+  acousticness: Number,
+  speechiness: Number,
+  popularity: Number
+})
+
+// Putting the data in the database
+const seedDatabase = async () => {
+  await Track.deleteMany()
+  topMusicData.forEach((track) => new Track(track).save())
+}
+seedDatabase()
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -30,6 +48,12 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+// Endpoint to get all the tracks
+app.get('/tracks', async (req, res) => {
+  const tracks = await Track.find()
+  res.json(tracks)
 })
 
 // Start the server
