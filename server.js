52204b3c1e3b37099e69ecb00606a3856c686648
Ragna <ragnaulfars@gmail.com>
2020-05-11 15:15:45 +0200

    Have followed MongoDB intro. Will then clean up code to begin again.

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -11,10 +14,6 @@ import mongoose from 'mongoose'
 // import booksData from './data/books.json'
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
-
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -27,9 +26,49 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+  if(mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: 'Service unavailable'})
+  }
+})
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/animals"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.promise = Promise
+
+const Animal = mongoose.model("Animal", {
+  name: String,
+  age: Number,
+  isFurry: Boolean
+})
+
+Animal.deleteMany().then(() => {
+  new Animal({ name: 'Alfons', age: 3, isFurry: true }).save()
+  new Animal({ name: 'Lucy', age: 5, isFurry: true }).save()
+  new Animal({ name: 'Elín', age: 2, isFurry: false }).save()
+})
+
+app.get('/:name', (req, res) => {
+  try{
+    Animal.findOne({name: req.params.name}).then(animal => {
+      if(animal) {
+        res.json(animal)
+      } else {
+        res.status(404).json({error: 'Not found'})
+      }
+    })
+  } catch (err) {
+    res.status(400).json({ error: 'Invalid user id'})
+  }
+})
+
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  Animal.find().then(animals => {
+    res.json(animals)
+  })
 })
 
 // Start the server
