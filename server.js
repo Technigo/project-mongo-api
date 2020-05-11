52a3fbf43193539c63181a03da86285dd41492b1
palhamel <pal.hamel@gmail.com>
2020-05-11 14:43:26 +0200

    Change route isbn nr to use await async

--- server.js
+++ server.js
@@ -12,11 +12,6 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// TODO:
-/*
-
-*/
-
 // Mongoose model setup:
 // Book Model
 const Book = mongoose.model("Book", {
@@ -52,27 +47,19 @@ const Book = mongoose.model("Book", {
   },
 });
 
-
-// To reset database and the populate db:
+// To reset database and then populate db:
 // $ RESET_DATABASE=true npm run dev
 // Seed DATABASE using Async
 // forEach loop will put all Books from JSON into database
 if (process.env.RESET_DATABASE) {
-  console.log("Resetting database");
+  console.log("Message: Resetting database");
 
   const seedDatabase = async () => {
     await Book.deleteMany();
-    // new setup using await/async - but really no effect here:
     await booksData.forEach((book) => new Book(book).save());
-    // booksData.forEach((book) => {
-    //   new Book(book).save();
-    // });
   };
   seedDatabase();
 }
-// Dubbelkolla detta oven först innan SEED till MongoDB
-// se databas i Compass! project-mongo / books
-// project-mongo.books
 
 // Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
@@ -87,7 +74,7 @@ app.use(bodyParser.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("My API endpoints: /books ,  , plus handling error if no id match");
+  res.send("My API endpoints: /books ,/books/439554934 , plus handling error if no id match");
 });
 
 // test, test:
@@ -99,25 +86,21 @@ app.get("/all", async (req, res) => {
 // http://localhost:8080/books
 app.get("/books", async (req, res) => {
   const books = await Book.find();
-  console.log(`Found ${books.lenght} books`);
+  console.log(`Message: Found ${books.length} books`);
   res.json(books);
 });
 
-// a RESTful route to return ONE Book via ISBN nr:: AWAIT?????
+// a RESTful route to return ONE Book via ISBN nr, using async/await
 // http://localhost:8080/books/439785960
 app.get("/books/:isbn", async (req, res) => {
   const isbn = req.params.isbn;
-  Book.findOne({ isbn: isbn })
-    .then((results) => {
-      res.json(results);
-    })
-    .catch((err) => {
-      // res.json({ message: 'ISBN ot found', err: err })
-      res.status(404).json({ error: "ISBN not found" });
-    });
+  const book = await Book.findOne({ isbn: isbn });
+  if (book) {
+    res.json(book);
+  } else {
+    res.status(404).json({ error: `Book with ISBN nr ${isbn} not found` });
+  }
 });
-
-
 
 /*
   isbn search idea: {
@@ -131,14 +114,12 @@ app.get("/books/:isbn", async (req, res) => {
 /*
 TODO:
 
-More routes
-
-Handle errors
+- More routes
+- Handle errors
+- Deploy db cloud
 
 
 */
-
-
 
 // Start the server
 app.listen(port, () => {
