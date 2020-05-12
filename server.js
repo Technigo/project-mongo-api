881776d5bd29d26f33a56b10b3fe5348a89994ef
ebbabw <ebbabergholtz@gmail.com>
2020-05-12 21:22:27 +0200

    seedDatabase added

--- server.js
+++ server.js
@@ -10,63 +10,26 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-const Netflixtitle = mongoose.model('Netflixtitle', {
-  show_id: {
-    type: Number,
-  },
-  title: {
-    type: String,
-  },
-  director: {
-    type: String,
-  },
-  cast: {
-    type: String,
-  },
-  country: {
-    type: String,
-  },
-  date_added: {
-    type: String,
-  },
-  release_year: {
-    type: Number,
-  },
-  rating: {
-    type: String,
-  },
-  duration: {
-    type: String,
-  },
-  listed_in: {
-    type: String,
-  },
-  description: {
-    type: String,
-  },
-  type: {
-    type: String,
-  },
+const Title = mongoose.model('Title', {
+
+  title: String,
 
 });
 
-if (process.env.RESET_DATABASE) {
-console.log('Resettnig database...')
-
 const seedDatabase = async () => {
-  // clear out database
-  await Netflixtitle.deleteMany();
-  // save all of the title from netflixData.json to the database
-  await netflixData.forEach((title) => new Netflixtitle(title).save()) 
-};
-seedDatabase();
+
+  await Title.deleteMany()
+
+  const title1 = new Title({title: 'Homeland'})
+  await title1.save()
+
+  const title2 = new Title({title: 'Once apon a time'})
+  await title2.save()
 
 }
+seedDatabase()
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -74,12 +37,19 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+
+
 // Start defining your routes here
+
 app.get('/', (req, res) => {
-  netflixData.find().then(titles => {
-    res.json(titles)
-  })
+  res.send('Hello')
 })
+
+app.get('/titles', async (req, res) => {
+  const titles = await Title.find()
+  res.json(titles)
+})
+
 
 // Start the server
 app.listen(port, () => {
