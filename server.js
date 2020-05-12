495f6753e0968e8d9503c6e158242e88d77750fd
Christina Persson <christina@MacBook-Pro.local>
2020-05-12 17:18:29 +0200

    Probably done

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { query } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
@@ -15,7 +15,7 @@ const Show = mongoose.model("Show", {
   cast: { type: String },
   country: { type: String },
   date_added: { type: String },
-  release_year: { type: Number },
+  release_year: { type: String },
   rating: { type: String },
   duration: { type: String },
   listed_in: { type: String },
@@ -47,21 +47,45 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+const listEndpoints = require("express-list-endpoints");
+
 // Start defining your routes here
+
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send(listEndpoints(app));
 });
 
 app.get("/shows", async (req, res) => {
-  const show = await Show.find();
-  res.json(show);
+  const { page, title, director, cast, country, year, genre, type } = req.query;
+
+  const pageNo = +page || 1;
+  const perPage = 10;
+  const skip = perPage * (pageNo - 1);
+
+  const searchShow = await Show.find({
+    title: new RegExp(title, "i"),
+    director: new RegExp(director, "i"),
+    cast: new RegExp(cast, "i"),
+    country: new RegExp(country, "i"),
+    release_year: new RegExp(year, "i"),
+    listed_in: new RegExp(genre, "i"),
+    type: new RegExp(type, "i"),
+  })
+    .sort({ release_year: -1 })
+    .limit(perPage)
+    .skip(skip);
+
+  if (searchShow) {
+    res.json(searchShow);
+  } else {
+    res.status(404).json({ error: "Cannot find this search" });
+  }
 });
 
 app.get("/shows/:show_id", async (req, res) => {
   const { show_id } = req.params;
   const showById = await Show.findOne({ show_id: show_id });
 
-  console.log(showById);
   if (showById) {
     res.json(showById);
   } else {
