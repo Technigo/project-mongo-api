1ea876b5f039e024bc42d3bea9d213de104f8933
Lisas Sturegard <lisasturegard@hotmail.com>
2020-05-12 12:57:18 +0200

    Cleaned up code and made an entry  basic endpoint for all shows

--- server.js
+++ server.js
@@ -26,12 +26,9 @@ const NetflixShow = mongoose.model('NetflixShow', {
 })
 
 if (process.env.RESET_DB) {
-
   console.log('Resetting Database!')
-
   const seedDatabase = async () => {
     await NetflixShow.deleteMany({})
-
     netflixData.forEach((show) => {
       new NetflixShow(show).save()
     })
@@ -52,9 +49,10 @@ app.use((req, res, next) => {
   }
 })
 
-app.get('/', (req, res) => {
-  res.send('Netflix Shows')
-})
+app.get('/netflixshow', async (req, res) => {
+  const netflixShows = await NetflixShow.find();
+  res.json(netflixShows);
+});
  
 app.get('/netflixshow/type', async (req, res) => {
   const { type } = req.query
@@ -94,107 +92,3 @@ app.get('/netflixshow/title/:title', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
-
-/*
-
-const Author = mongoose.model('Author', {
-  name: String
-})
-
-const Book = mongoose.model('Book', {
-  title: String,
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Author'
-  }
-})
-
-if (process.env.RESET_DB) {
-  console.log('Resetting Database!')
-  const seedDatabase = async () => {
-    await Author.deleteMany()
-
-    const rowling = new Author({ name: 'J.K.  Rowling' })
-    await rowling.save()
-    const tolkien = new Author({ name: 'J.R.R. Tolkien' })
-    await tolkien.save()
-    const gaiman = new Author ({ name: 'Neil Gaiman'})
-    await gaiman.save()
-    const colfer = new Author({ name: 'Eoin Colfer'})
-    await colfer.save()
-
-    await Book.deleteMany()
-
-    await new Book({ title: "Harry Potter and the Philiospher's Stone", author: rowling }).save()
-    await new Book({ title: "Harry Potter and the Chamber of Secrets", author: rowling }).save()
-    await new Book({ title: "Harry Potter and the Prisoner of Azkaban", author: rowling }).save()
-    await new Book({ title: "Harry Potter and the Goblet of Fire", author: rowling }).save()
-    await new Book({ title: "Harry Potter and the Order of the Phoenix", author: rowling }).save()
-    await new Book({ title: "Harry Potter and the Order of the Phoenix", author: rowling }).save()
-    await new Book({ title: "Harry Potter and the Half Blood Pince", author: rowling }).save()
-    await new Book({ title: "Harry Potter and the Deathly Hallows", author: rowling }).save()
-    await new Book({ title: "The Hobbit", author: tolkien }).save()
-    await new Book({ title: "The Lord of the Rings", author: tolkien }).save()
-  }
-seedDatabase()
-}
-
-app.get('/authors', async (req, res) => {
-  const authors = await Author.find()
-  res.json(authors)
-})
-
-app.get('/authors/:id', async (req, res) => {
-  const author = await Author.findById(req.params.id)
-  if (author) {
-    res.json(author)
-  } else {
-    res.status(404).json({ error: 'Author not found' })
-  }
-
-})
-
-app.get('/authors/:id/books', async (req, res) => {
-  const author = await Author.findById(req.params.id)
-  if (author) {
-    const books = await Book.find({ author: mongoose.Types.ObjectId(author.id) })
-    res.json(books)
-  } else {
-    res.status(404).json({ error: 'Author not found' })
-  }
-})
-
-app.get('/books', async (req, res) => {
-  const books = await Book.find().populate('author')
-  res.json(books)
-}) */
-
-/*
-const Animal = mongoose.model('Animal', {
-  name: String,
-  age: Number,
-  isFurry: Boolean
-}) */
-
-/*
-Animal.deleteMany().then(() => {
-  new Animal({ name: 'Alfons', age: 4, isFurry: true }).save()
-  new Animal({ name: 'Kurt', age: 2, isFurry: true }).save()
-  new Animal({ name: 'Sara', age: 1, isFurry: false }).save()
-})
-
-app.get('/', (req, res) => {
-  Animal.find().then(animals => {
-    res.json(animals)
-  })
-})
-
-app.get('/:name', (req, res) => {
-  Animal.findOne({ name: req.params.name }).then(animal => {
-    if (animal) {
-      res.json(animal)
-    } else {
-      res.status(404).json({ error: 'Not found' })
-    }
-  })
-}) */
