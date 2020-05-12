3c4bc85073bc331438a82ccfb6f4ff2f45dcc519
emelie Lönnerheden <you@example.com>
2020-05-13 00:28:20 +0200

    hardcoded json for mongooseand status 404 for not found nominee

--- server.js
+++ server.js
@@ -2,24 +2,13 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import goldenGlobesData from './data/golden-globes.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
-
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo-api"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -27,9 +16,65 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+const Nominee = mongoose.model('Nominee', {
+  nominee: String,
+  year_award: Number,
+  year_film: Number,
+  category: String,
+  win: Boolean
+})
+
+new Nominee({ nominee: 'hej', year_film: 3, year_film: 4, win:true}).save()
+new Nominee({ nominee: 'hoj', year_film: 3, year_film: 4, win:true}).save()
+new Nominee({ nominee: 'hij', year_film: 3, year_film: 4, win:false}).save()
+
+/*
+const User = mongoose.model('User', {
+  username: {
+    type: String,
+    minlength: 3,
+    maxlength: 20
+  },
+  password: {
+    type: String,
+    minlength: 8
+  },
+  profilePicture: {
+    type: String,
+    default: ''
+  }
+}) */
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Nominee.deleteMany()
+
+    data.forEach((nominee) => {
+      new Nominee(nominee).save()
+    })
+  }
+  seedDatabase()
+}
+
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  Nominee.find().then(nominees => {
+    res.json(nominees)
+  })
+})
+
+app.get('/winners', (req, res) => {
+  res.send('Winner of Golden globes 2010-2019')
+})
+
+app.get('/:nominee', (req, res) => {
+  Nominee.findOne({nominee: req.params.nominee}).then(nominee => {
+   if (nominee) {
+      res.json(nominee)
+   } else {
+    res.status(404).json({error: 'Not found'})
+   }
+  })
 })
 
 // Start the server
