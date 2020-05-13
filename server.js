48c5981657fbba831ec0c1e437a817fc5f1faae5
Mathias Thorström <thisen@gmail.com>
2020-05-13 09:59:43 +0200

    Added dummy data and create first param based on region

--- server.js
+++ server.js
@@ -2,35 +2,115 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import booksData from './data/books.json'
+import itemData from './data/item-history.json'
+import michelinData from './data/michelin-restaurants.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
+// This can be exported to externa file instead
+const Michelin = mongoose.model('Michelin',{
+  name: String,
+  year: Number,
+  latitude: Number,
+  longitude: Number,
+  city: String,
+  region: String,
+  zipCode: Number,
+  cuisine: String,
+  price: String,
+  url: String,
+})
+
+
+//    BOOKS.JSON
+// bookID: Number,
+// title: String,
+// authors: String,
+// average_rating: Number,
+// isbn: Number,
+// isbn13: Number,
+// language_code: String,
+// num_pages: Number,
+// ratings_count: Number,
+// text_reviews_count: Number,
+
+
+//   ITEM-HISTORY.JSON
+// average: Number,
+// date: String,
+// highest: Number,
+// lowest: Number,
+// order_count: Number,
+// volume: Number,
+
+
+//   MICHELIN-RESTAURANTS
+// name: String,
+// year: Number,
+// latitude: Number,
+// longitude: Number,
+// city: String,
+// region: String,
+// zipCode: Number,
+// cuisine: String,
+// price: String,
+// url: String,
+
+
+if (process.env.RESET_DATABASE) {
+  console.log('Resetting database...');
+
+  const seedDatabase = async () => {
+      await Michelin.deleteMany();
+      await michelinData.forEach((restaurant) => new Michelin(restaurant).save());
+  };
+  seedDatabase();
+}
+
+
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+
+// Show all 50 restaurants
+app.get('/michelin', async (req, res) => {
+  const restaurant = await Michelin.find();
+  console.log(`Found ${restaurant.length} items..`);
+  res.json(restaurant);
+});
+
+// Region, filter by country michelin/:region
+app.get('/michelin/:region', ( req, res ) => {
+  const region = req.params.region
+
+  let michelinByRegion = michelinData.filter((restaurant) => restaurant.region === region)
+  console.log(`Number of restaurants found: ${michelinByRegion.length}`)
+  
+  if ( michelinByRegion.length > 0 ){
+    res.json(michelinByRegion)
+  } else {
+    res.status(404).json({ message: `Error, no restaurants found in ${region}` })
+  }
+})
+
+
+// Store all cuisines in a list an show results michelin/cuisine
+// Laster filter by cuisine michelin/cuisine?cuisine=modern
+
+// Filter on price michelin/price?price=$
+
+// Filter by year michelin/:year
+
+// Create a put method and delete method?
 
 // Start the server
 app.listen(port, () => {
