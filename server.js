99244f2ba6204bd08be193f5824df11e8a57ddf3
elsareutersward <elsa.reutersward@gmail.com>
2020-05-13 17:00:33 +0200

    Addition of endpoints

--- server.js
+++ server.js
@@ -2,37 +2,139 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import goldenGlobesData from './data/golden-globes.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const GoldenGlobes = mongoose.model('GoldenGlobes', {
+  year_film: {
+    type: Number,
+  },
+  year_award: {
+    type: Number,
+  },
+  ceremony: {
+    type: Number,
+  },
+  category: {
+    type: String,
+  },
+  nominee: {
+    type: String,
+  },
+  film: {
+    type: String,
+  },
+  win: {
+    type: Boolean,
+  },
+});
 
-// Add middlewares to enable cors and json body parsing
+// RESET_DATABASE=true npm run dev
+if (process.env.RESET_DATABASE) {
+  const seedDatabase = async () => {
+    await GoldenGlobes.deleteMany();
+    await goldenGlobesData.forEach((goldenGlobe) => new GoldenGlobes(goldenGlobe).save());
+  };
+  seedDatabase();
+}
+
+const port = process.env.PORT || 9000
+const app = express()
 app.use(cors())
 app.use(bodyParser.json())
-
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: 'Service unavailable'})
+  }
 })
 
-// Start the server
+app.get('/', async (req, res) => {
+  const goldenGlobes = await GoldenGlobes.find();
+  res.json(goldenGlobes);
+})
+
+app.get('/year/:year', async (req, res) => {
+  try {
+    const { year } = req.params;
+    const { won, category } = req.query;    
+    const mongoQuery = { year_award: year };
+    
+    if (won) {
+      mongoQuery.win = won
+    } 
+    
+    if (category) {
+      mongoQuery.category = new RegExp(category, 'i')
+    }
+
+    const goldenGlobesYear = await GoldenGlobes.find(mongoQuery);
+    if (goldenGlobesYear.length < 1) {
+      res.status(404).json({ error: 'Data not found' });
+    } else {
+      res.json(goldenGlobesYear);
+    }
+  } catch (err) {
+    res.status(400).json({ error: 'Invalid request'});
+  }
+})
+
+app.get('/category/:category', async (req, res) => {
+  try {
+    const { category } = req.params;
+    const { won } = req.query;    
+    const mongoQuery = { category: category };
+    
+    if (won) {
+      mongoQuery.win = won
+    }
+
+    const goldenGlobesCategory = await GoldenGlobes.find(mongoQuery);
+    if (goldenGlobesCategory.length < 1) {
+      res.status(404).json({ error: 'Data not found' });
+    } else {
+      res.json(goldenGlobesCategory);
+    }
+  } catch (err) {
+    res.status(400).json({ error: 'Invalid request'});
+  }
+})
+
+app.get('/search/nominee', async (req, res) => {
+  try {
+    const { nominee } = req.query;
+    const queryRegex = new RegExp(nominee, 'i');
+    const goldenGlobesNominee = await GoldenGlobes.find({ nominee: queryRegex });
+    if (goldenGlobesNominee.length < 1) {
+      res.status(404).json({ error: 'Nominee not found' });
+    } else {
+      res.json(goldenGlobesNominee);
+    }
+  } catch (err) {
+    res.status(400).json({ error: 'Invalid request'}); 
+  }
+})
+
+app.get('/nominee/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const goldenGlobesNominee = await GoldenGlobes.findOne({ _id: id });
+    
+    if (goldenGlobesNominee.length < 1) {
+      res.status(404).json({ error: 'Nominee not found' })
+    } else {
+      res.json(goldenGlobesNominee)
+    }
+  } catch (err) {
+    res.status(400).json({ error: 'Invalid request'})
+  }
+})
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
+
