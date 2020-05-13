559d357806edaab8d8b3f2814bf14cf5631dd076
camilla <camilla.gausland@gmail.com>
2020-05-13 17:49:46 +0200

    up to date

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ import mongoose from 'mongoose'
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
 // import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+//import netflixData from './data/netflix-titles.json'
+ import topMusicData from './data/top-music.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/music"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
@@ -27,10 +27,55 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+
+const Artist = mongoose.model('Artist', {
+  id: String,
+  trackName: String,
+  artistName: String,
+  genre: String,
+  danceability: Number
+})
+
+
+//if (process.env.RESET_DATABASE) {
+  console.log('resetting the database')
+const seedDatabase = async () => {
+  await Artist.deleteMany()
+  
+  topMusicData.forEach((artist) => {
+    new Artist(artist).save()
+  })
+}
+
+seedDatabase()
+//}
+
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+   res.send('Hello world')
+  
+  
 })
+
+app.get('/:artists', async (req, res) => {
+  const artists = await Artist.find()
+  res.json(artists)
+})
+
+app.get('/artists/:id', async (req, res) => {
+  try {
+    const artist = await Artist.findById(req.params.id)
+    if (artist) {
+      res.json(artist)
+    } else {
+      res.status(404).json({error: 'artist not found'})
+    }
+  } catch (err) {
+    res.status(400).json({error: 'invalid artist request'})
+  }
+})
+
+
 
 // Start the server
 app.listen(port, () => {
