3cc9abcd2add55637a65627b4442ab5b73514d04
Josephine <josephine@Josephines-Air.lan>
2020-05-13 22:31:41 +0200

    kept only one model and changed a path to use regExp

--- server.js
+++ server.js
@@ -2,62 +2,35 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-//
 import goldenGlobesData from './data/golden-globes.json';
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/nominations';
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// const Nominations = mongoose.model('Nominations', {
-//   year_film: Number,
-//   year_award: Number,
-//   ceremony: Number,
-//   category: String,
-//   nominee: String,
-//   film: String,
-//   win: Boolean,
-// });
-
 const Nominee = mongoose.model('Nominee', {
-  nominee: String,
+  nominee: {
+    type: String,
+  },
   category: {
     type: mongoose.Schema.Types.String,
     ref: 'Category',
   },
-  year_award: Number,
-  win: Boolean,
-});
-
-const Category = mongoose.model('Category', {
-  category: String,
-  nominee: String,
-  nominee: {
-    type: mongoose.Schema.Types.String,
-    ref: 'Nominee',
+  year_award: {
+    type: Number,
   },
-  year_award: Number,
-  win: Boolean,
+  win: {
+    type: Boolean,
+  },
 });
 
 if (process.env.RESET_DATABASE) {
   console.log('Resetting database!');
   const seedDatabase = async () => {
-    // await Nominations.deleteMany();
     await Nominee.deleteMany();
-    await Category.deleteMany();
 
     goldenGlobesData.forEach((nominationsData) => {
-      // new Nominations(nominationsData).save();
       new Nominee(nominationsData).save();
-      new Category(nominationsData).save();
     });
   };
   seedDatabase();
@@ -75,18 +48,7 @@ app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-// app.get('/nominations', async (req, res) => {
-//   const nominations = await Nominations.find();
-//   res.json(nominations);
-// });
-
-// shows all the nominees ever
-app.get('/nominees', async (req, res) => {
-  const nominees = await Nominee.find();
-  res.json(nominees);
-});
-
-// Shows a list of all the winners ever -works
+// Shows a list of all the winners ever
 app.get('/winners', async (req, res) => {
   const nominee = await Nominee.find();
   const winners = nominee.filter((item) => item.win);
@@ -97,7 +59,7 @@ app.get('/winners', async (req, res) => {
   }
 });
 
-// Shows a list of all the winners a certain year -works
+// Shows a list of all the winners a certain year
 app.get('/year/:year/winners', async (req, res) => {
   const year = req.params.year;
   const findYear = await Nominee.find();
@@ -110,7 +72,7 @@ app.get('/year/:year/winners', async (req, res) => {
   }
 });
 
-// Shows all the nominees of a certain category a specific year - works
+// Shows all the nominees of a certain category a specific year
 app.get('/year/:year/categories/:category', async (req, res) => {
   const year = req.params.year;
   const findYear = await Nominee.find();
@@ -128,32 +90,18 @@ app.get('/year/:year/categories/:category', async (req, res) => {
   }
 });
 
-// All nominations ever of a certain nominee - works
-app.get('/nominees/:nominee', async (req, res) => {
-  const nominee = req.params.nominee;
-  const foundNominee = await Nominee.find({ nominee: nominee });
+// All nominations ever of a certain nominee
+// with ie GET "/nominees?query=joaquin"
+app.get('/nominees', async (req, res) => {
+  const { query } = req.query;
+  const queryRegex = new RegExp(query, 'i');
+  const foundNominee = await Nominee.find({ nominee: queryRegex });
   if (foundNominee.length > 0) {
     res.json(foundNominee);
   } else {
     res.status(404).json({ error: 'Nominee not found' });
   }
 });
-
-app.get('/categories', async (req, res) => {
-  res.json(await Category.find().populate('Nominee'));
-});
-
-// app.get('/nominees/:nominee', async (req, res) => {
-//   const nominee = req.params.nominee;
-//   if (nominee) {
-//     await Category.find({
-//       nominee: mongoose.Types.String(nominee.nominee),
-//     });
-//     res.json(nominee);
-//   } else {
-//     res.status(404).json({ error: 'Nominee not found' });
-//   }
-// });
 
 // Start the server
 app.listen(port, () => {
