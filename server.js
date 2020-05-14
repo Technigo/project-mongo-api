8bda39ea655d6f7ae1c75fd22709598999075b32
Ragna <ragnaulfars@gmail.com>
2020-05-14 11:13:15 +0200

    The whole project

--- server.js
+++ server.js
@@ -2,18 +2,33 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
-import dotenv from 'dotenv'
+import data from './data/golden-globes.json'
 
-dotenv.config()
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/nominations"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.promise = Promise
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+const Nomination = mongoose.model('Nomination', {
+  year_film: Number,
+  year_award: Number,
+  ceremony: Number,
+  category: String,
+  nominee: String,
+  film: String,
+  win: Boolean
+})
+
+if (process.env.RESET_DB) {
+  console.log('Resetting database!')
+  const seedDatabase = async () => {
+    await Nomination.deleteMany()
+
+    data.forEach((nominationData) => {
+      new Nomination(nominationData).save()
+    })
+  }
+  seedDatabase()
+}
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -27,48 +42,90 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-  if(mongoose.connection.readyState === 1) {
+  if(mongoose.connection.readyState === 1 || 2) {
     next()
   } else {
     res.status(503).json({ error: 'Service unavailable'})
   }
 })
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/animals"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.promise = Promise
-
-const Animal = mongoose.model("Animal", {
-  name: String,
-  age: Number,
-  isFurry: Boolean
+//Start defining your routes here
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-Animal.deleteMany().then(() => {
-  new Animal({ name: 'Alfons', age: 3, isFurry: true }).save()
-  new Animal({ name: 'Lucy', age: 5, isFurry: true }).save()
-  new Animal({ name: 'Elín', age: 2, isFurry: false }).save()
+app.get('/nominations', async (req, res) => {
+  let nominations = await Nomination.find()
+
+  const nominee = req.query.nominee
+  // You can look for a specific nominee: http://localhost:8080/nominations?nominee=Homeland
+
+  if(nominee) {
+    nominations = nominations.filter((item) => item.nominee === nominee)
+  }
+    res.json(nominations)
 })
 
-app.get('/:name', (req, res) => {
-  try{
-    Animal.findOne({name: req.params.name}).then(animal => {
-      if(animal) {
-        res.json(animal)
-      } else {
-        res.status(404).json({error: 'Not found'})
-      }
-    })
-  } catch (err) {
-    res.status(400).json({ error: 'Invalid user id'})
+app.get('/nominations/id/:id', async (req, res) => {
+  const nominations = await Nomination.findById(req.params.id)
+  res.json(nominations)
+})
+
+app.get('/nominations/:year_award', async (req, res) => {
+  const { year_award } = req.params
+  let yearNominations = await Nomination.find({ year_award: year_award })
+
+  const showWin = req.query.win
+
+  if(showWin && yearNominations) {
+    yearNominations = yearNominations.filter((item) => item.win === true)
+  }
+  res.json(yearNominations)
+
+  if (yearNominations) {
+    res.json(yearNominations)
+  } else {
+    res.status(404).json({ error: 'Not found'})
   }
 })
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  Animal.find().then(animals => {
-    res.json(animals)
-  })
+app.get('/nominations/category/:category', async (req, res) => {
+  const nominations = await Nomination.find()
+  const nominationCategory = req.params.category
+  const showCategoryWin = req.query.win
+
+  let categories = nominations.filter((item) => item.category.includes(nominationCategory))
+
+  if (showCategoryWin && categories) {
+    categories = categories.filter((item) => item.win === true)
+  }
+  res.json(categories)
+
+  if (categories) {
+    res.json(categories)
+  } else {
+    res.status(404).json({ error: 'Not found'})
+  }
+})
+
+app.get('/nominations/category/:category/year/:year', async (req, res) => {
+  const nominations = await Nomination.find()
+  const nominationCategory = req.params.category
+  const year = req.params.year
+  const showWins = req.query.win
+
+  let categories = nominations.filter((item) => item.category.includes(nominationCategory))
+
+  if (showWins && categories && year) {
+    let wonCategories = categories.filter((item) => item.win === true)
+    wonCategories = wonCategories.filter((item) => item.year_award === +year)
+    res.json(wonCategories)
+  } else if (categories && year) {
+    const yearCategories = categories.filter((item) => item.year_award === +year)
+    res.json(yearCategories)
+  } else {
+    res.status(404).json({ error: 'Not found'})
+  }
 })
 
 // Start the server
