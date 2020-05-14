908a734000f74d9e8a7116e5c5e249cf0a6990cf
ebbabw <ebbabergholtz@gmail.com>
2020-05-14 23:21:57 +0200

    added directors route

--- server.js
+++ server.js
@@ -6,49 +6,30 @@ import mongoose from 'mongoose'
 //API input
 import netflixTitles from './data/netflix-titles.json'
 
+
+const Key = 'mongodb+srv://ebbabw:gs4m4g4U6pC6ejLK@cluster0-yp7wy.mongodb.net/project-mongo?retryWrites=true&w=majority'
+
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(Key, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
 const Netflixdata = mongoose.model('Netflixdata', {
 
-  show_id: {
-    type: Number
-  },
-  title: {
-    type: String
-  }, 
-  director: {
-    type: String
-  },
-  cast:  {
-    type: String
-  },
-  country: {
-    type: String
-  },
-  date_added: {
-    type: String
-  },
-  release_year: {
-    type: Number
-  },
-  rating: {
-    type: String
-  },
-  duration: {
-    type: String
-  },
-  listed_in: {
-    type: String
-  },
-  description: {
-    type: String
-  },
-  type: {
-    type: String
-  }
+  title: { type: String }, 
+  cast:  { type: String },
+  country: { type: String },
+  date_added: { type: String },
+  release_year: { type: Number },
+  rating: { type: String },
+  duration: { type: String },
+  listed_in: { type: String },
+  description: { type: String },
+  type: { type: String }
+})
 
+const Director = mongoose.model('Director', {
+
+  director: { type: String },
 
 })
 
@@ -56,8 +37,11 @@ if (process.env.RESET_DATABASE) {
   console.log("Resetting database...");
   const seedDatabase = async () => {
     await Netflixdata.deleteMany();
-    
+    await Director.deleteMany();
+
     netflixTitles.forEach((netflixdata) => new Netflixdata(netflixdata).save());
+    netflixTitles.forEach((netflixdata) => new Director(netflixdata).save());
+
   };
   seedDatabase();
 }
@@ -80,14 +64,69 @@ app.use((req, res, next) => {
 // Start defining your routes here
 
 app.get('/', (req, res) => {
-  res.send('Hello! use these routes /nominees')
+  res.send('Hello! use these routes /netflixtitles')
 })
 
 
-app.get("/nominees", async (req, res) => {
+app.get("/netflixtitles", async (req, res) => {
   const titles = await Netflixdata.find();
   res.json(titles);
 });
+
+
+app.get("/netflixtitles/:directors", async (req, res) => {
+  const director = await Director.find();
+  res.json(director);
+});
+
+
+app.get('/netflixtitles/:id', async (req, res) => {
+
+  try {
+    const netflixID = await Netflixdata.findById(req.params.id)
+
+    if(netflixID) {
+      res.json(netflixID)
+    } else {
+      res.status(404).json({error: 'Id not found'})
+    }
+
+  } catch(err) {
+    res.status(400).json({error: 'Something is Invalid'})
+  }
+
+})
+
+// app.get('/netflixtitles/:country', async (req, res) => {
+
+
+//     const netflixCountry = await Netflixdata.find(req.params.country)
+//     const filterByCountry =  netflixCountry.filter((item) => item.country).toLowerCase().includes(country).toLowerCase()
+
+//     if(filterByCountry) {
+//       res.json(filterByCountry)
+//     } else {
+//       res.status(404).json({error: 'Country not found'})
+//     }
+
+// })
+
+// app.get("/netflixtitles/:country", async (req, res) => {
+
+//   const country = await Netflixdata.find(req.params.country)
+//   const filterByCountry = titlesbycountry.filter((title) => title.country.toString().toLowerCase().includes(country.toLowerCase())) 
+//   res.json(filterByCountry);
+// });
+
+// app.get('netflixtitles/:country', async (req, res) => {
+//   const { country } = req.params;
+//   const bycountry = await Netflixdata.findOne({ country: country });
+//   if (bycountry) {
+//     res.json(bycountry);
+//   } else {
+//     res.status(404).json({ error: 'Could not find' });
+//   }
+// });
 
 
 // Start the server
