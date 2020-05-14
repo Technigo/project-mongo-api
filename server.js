237134f57192829b05991e1ae95308d45140b65e
ebbabw <ebbabergholtz@gmail.com>
2020-05-14 11:08:33 +0200

    added Api Golden Globes

--- server.js
+++ server.js
@@ -3,48 +3,39 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
+//API input
+import { GoldenGlobeData } from './data/golden-globes.json'
 
-import netflixData from './data/netflix-titles.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/titles"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-const Title = mongoose.model('Title', {
+const Nominee = mongoose.model('Nominee', {
 
-  title: String,
+  year_film: Number,
+  year_award: Number,
+  ceremony: Number,
+  category: String,
+  nominee: String,
+  film: String,
+  win: Boolean,
 
-});
+})
 
-const Netflixtitle = mongoose.model('Netflixtitle', {
+ if (process.env.RESET_DATABASE === true) {
+     console.log('Resettnig database...');
+  
+    const seedDatabase = async () => {
+      await Nominee.deleteMany();
 
-  director: String,
-
-  title: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Title'
+      await GoldenGlobeData.forEach((nominee) => new Nominee(nominee).save());
+    
   }
 
-});
-
-if (process.env.RESET_DATABASE) {
-
-const seedDatabase = async () => {
-
-  await Title.deleteMany()
-  await Netflixtitle.deleteMany()
-
-  const homeland = new Title({title: 'Homeland'})
-  await homeland.save()
-
-  const onceapon = new Title({title: 'Once apon a time'})
-  await onceapon.save()
-
-  await new Netflixtitle({ director: "Myers", title: homeland}).save()
-
+  seedDatabase()
 }
-seedDatabase()
-}
+
 
 const port = process.env.PORT || 8080
 const app = express()
@@ -53,26 +44,42 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: 'Service unavailable' })
+  }
+})
 
 // Start defining your routes here
 
 app.get('/', (req, res) => {
-  res.send('Hello')
+  res.send('Hello! use these routes')
 })
 
-app.get('/titles', async (req, res) => {
-  const titles = await Title.find()
-  res.json(titles)
+app.get('/nominees', async (req, res) => {
+  const goldenGlobenom = await Nominee.find()
+  res.json(goldenGlobenom)
 })
 
-app.get('/netflixtitles', async (req, res) => {
- const Netflixtitles = await Netflixtitle.find().populate('title')
- res.json(Netflixtitles)
-})
+// app.get('/netflixtitles', async (req, res) => {
+//  const Netflixtitles = await Netflixtitle.find().populate('title')
+//  res.json(Netflixtitles)
+// })
 
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
+
+
+
+
+// Method	Endpoints	Notes
+// GET	/product	Get all products
+// GET	/product/:id	Get single product
+// POST	/product	Add product
+// PUT	/product/:id	Update product
+// DELETE	/product/:id	Delete product
