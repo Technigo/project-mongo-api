bc58eadc0aa7c9976e607a5b6b2f23450cf662cd
annesophiegendron <annesophie.gendron@gmail.com>
2020-05-14 11:27:28 +0200

    seedDatabase function set up

--- server.js
+++ server.js
@@ -2,23 +2,27 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import netflixData from './data/netflix-titles.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
+const Show = mongoose.model('Show', {
+  title: String
+})
+
+if (process.env.RESET_DATABASE) {
+  const seedDatabase = async () => {
+    await Show.deleteMany()
+
+    netflixData.forEach((showData) => {
+      new Show(showData).save()
+    })
+  }
+  seedDatabase()
+}
+
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
@@ -30,6 +34,11 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/shows', async (req, res) => {
+  const shows = await Show.find()
+  res.json(shows)
 })
 
 // Start the server
