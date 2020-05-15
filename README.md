bd9f30b90ba7988cf78d57b8736844830a581807
Jihyun An <jihyunan42@gmail.com>
2020-05-15 11:14:07 +0200

    cleaned up the code and changed ReadMe

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # Mongo API Project
 
-Replace this readme with your own information about your project. 
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+This week I've produced Golden Globes nominations API using mongoose model and Mongo database as a weekly project for Technigo bootcamp.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+One of the problems was how to have several queries working under one single endpoint. This was solved by having if else statement so that if there is a query, the await function can filter the API results based on the query. 
+
+Another problem was to filter the nominations that won with query. Here I couldn't use RegExp because the value of this query(win) is boolean and I couldn't figure out how to pass in boolean value in ReqExp. So I skipped using RegExp and instead used .where() to filter the results based on the key 'win'.
+
+I had problem deploying the app on Heroku where it would return an empty array. This was caused because the database wasn't seeded yet. Seeding the database is dependent on resetting the database and this was set as an if statement. Therefore I needed to add the key RESET_DATABASE and its value as true in the configuration variables in my Heroku settings. 
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+https://mongo-backed-api.herokuapp.com/
+
+You can use query "nominee", "category", "win" to filter the API results. You can also use query "page" to set the page number, and you will see 20 nominations per page. If you want to see the nominations by year, use the endpoint /nominations/years/:year. ex. /nominations/years/2019
--- server.js
+++ server.js
@@ -24,35 +24,43 @@ app.use(bodyParser.json())
 if (process.env.RESET_DATABASE) {
   console.log('Resetting database ...')
   const seedDatabase = async () => {
-    // Clear our database
     await Nomination.deleteMany()
-    // Save all of the books from books.json to the database
     await goldenGlobesData.forEach((nomination) => new Nomination(nomination).save())
   }
   seedDatabase()
 }
 
-/* new Nominations({
-  "year_film": 2009,
-  "year_award": 2010,
-  "ceremony": 67,
-  "category": "Best Motion Picture - Drama",
-  "nominee": "Avatar",
-  "film": "",
-  "win": true
-}).save() */
 
-// Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Welcome to the golden globes nominations API.')
 })
 
 app.get('/nominations', async (req, res) => {
   const { nominee } = req.query
+  const { category } = req.query
+  const { win } = req.query
+  const { page } = req.query || 1
+  const PAGE_SIZE = 20
   if (nominee) {
     const queryRegex = new RegExp(nominee, 'i')
     const nominations = await Nomination.find({ nominee: queryRegex })
-    console.log(`found ${nominations.length} nominations...`)
+    console.log(`found ${nominations.length} nominations with the nominee...`)
+    res.json(nominations)
+  } else if (category) {
+    const queryRegex = new RegExp(category, 'i')
+    const nominations = await Nomination.find({ category: queryRegex })
+    console.log(`found ${nominations.length} nominations within the category...`)
+    res.json(nominations)
+  } else if (win) {
+    // const queryRegex = new RegExp('\\b' + win + '\\b', 'i')
+    console.log(win)
+    const nominations = await Nomination.find().where({ 'win': win })
+    console.log(`found ${nominations.length} nominations that won...`)
+    res.json(nominations)
+  } else if (page) {
+    console.log(`showing ${PAGE_SIZE} nominations on page number ${page}...`)
+    // const queryRegex = new RegExp(page)
+    const nominations = await Nomination.find().limit(PAGE_SIZE).skip(PAGE_SIZE * page - PAGE_SIZE)
     res.json(nominations)
   } else {
     const nominations = await Nomination.find()
@@ -60,8 +68,6 @@ app.get('/nominations', async (req, res) => {
     res.json(nominations)
   }
 })
-
-
 
 app.get('/nominations/years/:year', async (req, res) => {
   const { year } = req.params
@@ -72,7 +78,6 @@ app.get('/nominations/years/:year', async (req, res) => {
     res.status(404).json({ error: `Could not find nominations with id ${year}` })
   }
 })
-
 
 // Start the server
 app.listen(port, () => {
