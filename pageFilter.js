c90ad602860e539a306099c464c7127e7b524064
Your Name <malin.marie@hotmail.se>
2020-05-19 21:45:12 +0200

    filter

--- pageFilter.js
+++ pageFilter.js
@@ -2,13 +2,9 @@ import mongoose from 'mongoose'
 
 export const pageFilter = async ( Shows, page = 1, pageSize = 20, filter) => {
   
-  // if (filter === undefined) {
-  //   filter = { type: type }
-  // }
-
   const startIndex = (page -1) * pageSize
   const totalResults = await Shows.find( filter ).countDocuments()
-  const resultsFind = await Shows.find( filter ).skip(startIndex).limit(pageSize).collation({ locale: 'en_US', strength: 1 })
+  const resultsFind = await Shows.find( filter ).collation({ locale: 'en_US', strength: 1 }).skip(startIndex).limit(pageSize)
   
   return { 
     pageSize: pageSize,
