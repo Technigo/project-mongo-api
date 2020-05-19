7241275239b09949cc7abd385e61c752cf63ffaa
Your Name <malin.marie@hotmail.se>
2020-05-19 22:04:53 +0200

    filter

--- pageFilter.js
+++ pageFilter.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose'
 
 export const pageFilter = async ( Shows, page = 1, pageSize = 20, filter) => {
+
+  const collation = { "locale": 'en_US', "strength": 1  };
   
   const startIndex = (page -1) * pageSize
-  const totalResults = await Shows.find( filter ).countDocuments()
-  const resultsFind = await Shows.find( filter ).collation({ locale: 'en_US', strength: 1 }).skip(startIndex).limit(pageSize)
+  const totalResults = await Shows.find(filter).collation(collation).countDocuments()
+  const resultsFind = await Shows.find(filter).collation(collation).skip(startIndex).limit(pageSize)
   
   return { 
     pageSize: pageSize,
