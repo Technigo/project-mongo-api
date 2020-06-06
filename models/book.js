90f42e082e7ea67d242ca0785d1a04b928ef8d05
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-06-06 20:43:33 +0200

    added default settings

---
+++ models/book.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose'
+
+const Book = mongoose.model('Book', {
+  bookID: {
+    type: Number,
+  },
+  title: {
+    type: String,
+  },
+  authors: {
+    type: String,
+  },
+  average_rating: {
+    type: Number,
+  },
+  isbn: {
+    type: Number,
+  },
+  isbn13: {
+    type: Number,
+  },
+  language_code: {
+    type: String,
+  },
+  num_pages: {
+    type: Number,
+  },
+  ratings_count: {
+    type: Number,
+  },
+  text_reviews_count: {
+    type: Number,
+  }
+})
+
+export default Book
+
+// author: {
+//   type: mongoose.Schema.Types.ObjectId,
+//   ref: 'Author'
+// }
--- server.js
+++ server.js
@@ -1,39 +1,46 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-import mongoose, { mongo } from 'mongoose'
+import mongoose from 'mongoose'
 import booksData from './data/books.json'
+import Book from './models/book'
+import Author from './models/author'
 
 // DATABASE
-
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-mongo'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-const Author = mongoose.model('Author', {
-  name: String
-})
 
-const Book = mongoose.model('Book', {
-  title: String,
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Author'
+// RESET DATABASE
+if (process.env.RESET_DATABASE) {
+  const seedDatabase = async () => {
+    await Book.deleteMany()
+    await Author.deleteMany()
+    // await booksData.forEach((book) => new Book(book).save());
   }
-})
+  seedDatabase()
+}
+
 
 // SERVER
 // Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+// overridden when starting the server. For example: PORT=9000 npm start
 const port = process.env.PORT || 8080
+
+
+// INSTANCE
+// Creates an instance of express which gives access to all functions from express to create a server
 const app = express()
 
+
+// MIDDLEWARES
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
+
+// SERVER CONNECTION CHECK
 // Error handling when the server is not connected
 app.use((req, res, next) => {
   if (mongoose.connection.readyState === 1) {
@@ -43,7 +50,8 @@ app.use((req, res, next) => {
   }
 })
 
-// ROUTES
+
+// API ENDPOINTS
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
@@ -58,8 +66,17 @@ app.get('/books', async (req, res) => {
   res.json(books)
 })
 
+app.get('/books', async (req, res) => {
+  const { query } = req.query;
+  const queryRegex = new RegExp(query, 'i');
+  const books = await Book.find({ title: queryRegex }).sort({
+    average_rating: -1,
+  });
+  res.json(books);
+});
 
-// START
+
+// START SERVER
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
