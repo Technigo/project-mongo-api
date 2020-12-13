ab8f4c57c42b894c9d2c697aa9c423cb2cc9454f
Marjaana Tienaho <marjaanatienaho@Marjaanas-Air.lan>
2020-12-13 20:52:13 +0100

    Added 2 routes

--- server.js
+++ server.js
@@ -9,13 +9,30 @@ import mongoose from 'mongoose'
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
 // import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
+import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/NetflixItem"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
+const NetflixItem = mongoose.model('NetflixItem', {
+  title: String,
+  release_year: Number,
+  country: String,
+  show_id: Number,
+  type: String
+})
+if (process.env.RESET_DB) {
+	const seedDatabase = async () => {
+    await NetflixItem.deleteMany({})
+
+		netflixData.forEach((item) => {
+			new NetflixItem(item).save()
+		})
+  }
+  seedDatabase()
+}
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
@@ -26,10 +43,30 @@ const app = express()
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
+app.use((req, res, next) => {
+  if(mongoose.connection.readyState === 1){
+    next()
+  } else {
+    res.status(503).json({ error: 'Service unavailable'})
+  }
+})
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  NetflixItem.find().then(movies => {
+    res.json(movies)
+  })
+})
+
+app.get('/releaseyear/:release_year', async (req, res) => {
+  
+   const item = await NetflixItem.findById(req.params.release_year)
+      if (item) {
+        res.json(item)
+      } else {
+        res.status(404).json({error: 'Release year not found'})
+      }
+    
 })
 
 // Start the server
