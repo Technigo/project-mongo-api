b9ced0db8c379c116a80a14c21798b8f32fd410d
Corneliaryas <hello@corneliaryas.com>
2020-12-14 15:11:43 +0100

    Adding one item to Mongo Database

--- server.js
+++ server.js
@@ -3,25 +3,53 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import netflixData from './data/netflix-titles.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
+
+const Show = mongoose.model('Show', {
+  show_id: Number,
+  title: String,
+  director: String,
+  cast: String,
+  country: String,
+  date_added: String,
+  release_year: Number,
+  rating: String,
+  duration: String,
+  listed_in: String,
+  description: String,
+  type: String
+})
+
+if (process.env.RESET_DATABASE){
+  const seedDatabase = async () => {
+    console.log("Resetting database")
+    await Show.deleteMany()
+
+    const chocolate = new Show({
+      "title": "Chocolate",
+      "director": "",
+      "cast": "Ha Ji-won, Yoon Kye-sang, Jang Seung-jo, Kang Bu-ja, Lee Jae-ryong, Min Jin-woong, Kim Won-hae, Yoo Teo",
+      "country": "South Korea",
+      "date_added": "November 30, 2019",
+      "release_year": 2019,
+      "rating": "TV-14",
+      "duration": "1 Season",
+      "listed_in": "International TV Shows, Korean TV Shows, Romantic TV Shows",
+      "description": "Brought together by meaningful meals in the past and present, a doctor and a chef are reacquainted when they begin working at a hospice ward.",
+      "type": "TV Show"})
+
+    await chocolate.save()
+  }
+
+ seedDatabase()
+}
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
@@ -30,6 +58,11 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/shows', async (req, res) => {
+  const shows = await Show.find()
+  res.json(shows)
 })
 
 // Start the server
