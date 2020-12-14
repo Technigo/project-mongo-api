2e398a69464c728d5f2f5dc80328c287ebdd6f05
Andrea <andrea88@live.se>
2020-12-14 16:46:32 +0100

    started creating model and endpoint - something does not work.. will correct tomorrow :)

--- server.js
+++ server.js
@@ -3,36 +3,79 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import booksData from './data/books.json'
 
+// CODE FOR DATABASE
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+// AUTHOR MODEL
+const Author = mongoose.model('Author', {
+  authors: String
+})
+
+// BOOK MODEL
+const Book = mongoose.model('Book', {
+  bookID: Number,
+  title: String,
+  author: String,
+  average_rating: Number,
+  isbn: String,
+  isbn13: Number,
+  language_code: String,
+  num_pages: Number,
+  ratings_count: Number,
+  text_reviews_count: Number
+}) 
+
+// RESET DATABASE - Deleting existing data to prevent dublicates
+if (process.env.RESET_DATABASE) {
+  const seedDatabase = async () => {
+    await Book.deleteMany()
+    await booksData.forEach(book => new Book(book).save())
+  }
+  seedDatabase()
+}
+
+// SERVER
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
+// MIDDLEWARES  
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
+// ROUTES
 app.get('/', (req, res) => {
-  res.send('Hello world')
+	res.send('This is an API with books')
 })
 
-// Start the server
+// ENDPOINT BOOKS
+app.get('/books', async (req, res) => {
+  const allBooks = await Book.find()
+  res.json(allBooks)
+})
+
+// ENDPOINT SINGLE BOOK
+app.get('/books:id', async (req, res) => {
+  const singleBook = await Book.findById(req.params.id)
+
+  if(singleBook) {
+    res.json(singleBook)
+  } else {
+    res.status(404).json({ error: 'Book not found '})
+  }
+})
+
+// ENDPOINT AUTHOR
+app.get('/authors', async (req, res) => {
+  const authors = await Author.find()
+  res.json(authors)
+})
+
+// START SERVER
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
+
