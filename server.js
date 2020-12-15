3e111d718e9da4c10db676234c7e8ef4744475c1
Annika Melin <melin.anki>
2020-12-15 16:30:21 +0100

    improved readability and removed reused code

--- server.js
+++ server.js
@@ -41,7 +41,6 @@ if (process.env.RESET_DATABASE) {
   populateDatabase()
 }
 
-// didn't work
 // app.use((request, response, next) => {
 //   if (connection.readyState === 1) {
 //     next()
@@ -55,149 +54,156 @@ app.get('/', (request, response) => {
   response.send("Welcome to Annika's book API, this time with MongoDB")
 })
 
+// limit all routes to show 20 results
+// add endpoint documentation
+// move out error messages and do those conditionals more precise, or more general
+
 app.get('/books', async (request, response) => {
 
   const search = request.query.search
   const language = request.query.language
   const sort = request.query.sort
 
-  if (search && language && sort === 'rating-desc') {
-    filteredBooks = await Book.find({
+  const findTitle = { $regex: ("\\b" + search + "\\b"), $options: "i" }
+  const findAuthor = { $regex: ("\\b" + search + "\\b"), $options: "i" }
+  const findLanguage = { $regex: language, $options: "i" }
+  const filteredFields = 'title authors language_code average_rating num_pages'
+  const sortRating = { average_rating: -1 }
+  const sortPages = { num_pages: 1 }
+
+  if (search && language && sort) {
+    let sortBooks
+    if (sort === 'rating-desc') {
+      sortBooks = sortRating
+    }
+    else if (sort === 'pages-asc') {
+      sortBooks = sortPages
+    }
+    else if (sort !== 'rating-desc' || sort !== 'pages-asc') {
+      return response.status(404).json(
+        { error: "No such sorting" }
+      )
+    }
+    const filteredBooks = await Book.find({
       $and: [{
         $or: [
-          { authors: { $regex: ("\\b" + search + "\\b"), $options: "i" } },
-          { title: { $regex: ("\\b" + search + "\\b"), $options: "i" } }
+          { authors: findAuthor },
+          { title: findTitle }
         ]
       },
-      { language_code: { $regex: language, $options: "i" } }
+      { language_code: findLanguage }
       ]
     },
-      'title authors language_code average_rating num_pages'
+      filteredFields
     ).sort(
-      { average_rating: -1 }
+      sortBooks
     ).exec()
     if (filteredBooks.length > 0) {
       return response.json(filteredBooks)
     } else {
-      return response.status(404).json({ error: "No such combination" })
+      return response.status(404).json(
+        { error: "No such author/title and language combination" }
+      )
     }
   }
-
-  if (search && language && sort === 'pages-asc') {
-    filteredBooks = await Book.find({
-      $and: [{
-        $or: [
-          { authors: { $regex: ("\\b" + search + "\\b"), $options: "i" } },
-          { title: { $regex: ("\\b" + search + "\\b"), $options: "i" } }
-        ]
-      },
-      { language_code: { $regex: language, $options: "i" } }
-      ]
-    },
-      'title authors language_code average_rating num_pages'
-    ).sort(
-      { num_pages: 1 }
-    ).exec()
-    if (filteredBooks.length > 0) {
-      return response.json(filteredBooks)
-    } else {
-      return response.status(404).json({ error: "No such combination" })
-    }
-  }
-
-  // if (sort !== 'rating-desc' || sort !== 'pages-asc') {
-  //   return response.status(404).json({ error: "No such sorting" })
-  // }
 
   if (search && language) {
     const filteredBooks = await Book.find({
       $and: [{
         $or: [
-          { authors: { $regex: ("\\b" + search + "\\b"), $options: "i" } },
-          { title: { $regex: ("\\b" + search + "\\b"), $options: "i" } }
+          { authors: findAuthor },
+          { title: findTitle }
         ]
       },
-      { language_code: { $regex: language, $options: "i" } }
+      { language_code: findLanguage }
       ]
     },
-      'title authors language_code average_rating num_pages'
+      filteredFields
     ).exec()
     if (filteredBooks.length > 0) {
       return response.json(filteredBooks)
     } else {
-      return response.status(404).json({ error: "No such combination" })
+      return response.status(404).json(
+        { error: "No such combination" }
+      )
     }
   }
 
-  if (search && sort === 'rating-desc') {
-    const filteredBooks = await Book.find({
-      $or: [
-        { authors: { $regex: ("\\b" + search + "\\b"), $options: "i" } },
-        { title: { $regex: ("\\b" + search + "\\b"), $options: "i" } }
-      ]
-    },
-      'title authors language_code average_rating num_pages',
-    ).sort(
-      { average_rating: -1 }
-    ).exec()
-    if (filteredBooks.length > 0) {
-      return response.json(filteredBooks)
-    } else {
-      return response.status(404).json({ error: "No such combination" })
+  if (search && sort) {
+    let sortBooks
+    if (sort === 'rating-desc') {
+      sortBooks = sortRating
+    } else if (sort === 'pages-asc') {
+      sortBooks = sortPages
+    } else if (sort !== 'rating-desc' || sort !== 'pages-asc') {
+      return response.status(404).json(
+        { error: "No such sorting" }
+      )
     }
-  }
-
-  if (search && sort === 'pages-asc') {
     const filteredBooks = await Book.find({
       $or: [
-        { authors: { $regex: ("\\b" + search + "\\b"), $options: "i" } },
-        { title: { $regex: ("\\b" + search + "\\b"), $options: "i" } }
+        { authors: findAuthor },
+        { title: findTitle }
       ]
     },
-      'title authors language_code average_rating num_pages',
+      filteredFields
     ).sort(
-      { num_pages: 1 }
+      sortBooks
     ).exec()
     if (filteredBooks.length > 0) {
       return response.json(filteredBooks)
     } else {
-      return response.status(404).json({ error: "No such combination" })
+      return response.status(404).json(
+        { error: "No title or author match" }
+      )
     }
   }
 
   if (search) {
     const filteredBooks = await Book.find({
       $or: [
-        { authors: { $regex: ("\\b" + search + "\\b"), $options: "i" } },
-        { title: { $regex: ("\\b" + search + "\\b"), $options: "i" } }
+        { authors: findAuthor },
+        { title: findTitle }
       ]
     },
-      'title authors language_code average_rating num_pages'
+      filteredFields
     ).exec()
     if (filteredBooks.length > 0) {
       return response.json(filteredBooks)
     } else {
-      return response.status(404).json({ error: "No title match" })
+      return response.status(404).json(
+        { error: "No title or author match" }
+      )
     }
   }
 
   else {
-    const books = await Book.find({}, 'title authors language_code average_rating num_pages').exec()
+    const books = await Book.find(
+      {},
+      filteredFields
+    ).exec()
     response.json(books)
   }
 })
 
 app.get('/books/:id', async (request, response) => {
   try {
-    const book = await Book.findById({ _id: request.params.id }, 'title authors language_code average_rating num_pages').exec()
+    const book = await Book.findById(
+      { _id: request.params.id },
+      filteredFields
+    ).exec()
     if (book) {
       response.json(book)
     } else {
-      response.status(404).json({ error: "Book not found" })
+      response.status(404).json(
+        { error: "Book not found" }
+      )
     }
   }
   catch (error) {
-    response.status(400).json({ error: "Invalid book id" })
+    response.status(400).json(
+      { error: "Invalid book id" }
+    )
   }
 })
 
