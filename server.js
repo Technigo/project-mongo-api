4862017efacafbe82a27a9fda9b15da5f51f0124
Annika Melin <melin.anki>
2020-12-15 22:04:43 +0100

    removed duplicated code and created conditionals instead

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ import mongoose, { connection } from 'mongoose'
 import books from './data/books.json'
 
 // Connects to mongodb
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo" // project-mongo will be the name of our database, we decide what to name it here
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-const port = process.env.PORT || 8082
+const port = process.env.PORT || 8081
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
@@ -33,7 +33,7 @@ const Book = mongoose.model('Book', {
 
 if (process.env.RESET_DATABASE) {
   const populateDatabase = async () => {
-    await Book.deleteMany() // await makes it wait until this is finnished, as this takes some time, until you continue to the next line
+    await Book.deleteMany()
     books.forEach(async item => {
       await new Book(item).save()
     })
@@ -41,22 +41,20 @@ if (process.env.RESET_DATABASE) {
   populateDatabase()
 }
 
-// app.use((request, response, next) => {
-//   if (connection.readyState === 1) {
-//     next()
-//   } else {
-//     response.status(503).json({ error: 'Database unavailable' })
-//   }
-// })
+app.use((request, response, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    response.status(503).json({ error: 'Database unavailable' })
+  }
+})
+
+const filteredFields = 'title authors language_code average_rating num_pages'
 
 // Routes are defined here
 app.get('/', (request, response) => {
   response.send("Welcome to Annika's book API, this time with MongoDB")
 })
-
-// limit all routes to show 20 results
-// add endpoint documentation
-// move out error messages and do those conditionals more precise, or more general
 
 app.get('/books', async (request, response) => {
 
@@ -64,127 +62,93 @@ app.get('/books', async (request, response) => {
   const language = request.query.language
   const sort = request.query.sort
 
-  const findTitle = { $regex: ("\\b" + search + "\\b"), $options: "i" }
-  const findAuthor = { $regex: ("\\b" + search + "\\b"), $options: "i" }
-  const findLanguage = { $regex: language, $options: "i" }
-  const filteredFields = 'title authors language_code average_rating num_pages'
-  const sortRating = { average_rating: -1 }
-  const sortPages = { num_pages: 1 }
-
-  if (search && language && sort) {
-    let sortBooks
-    if (sort === 'rating-desc') {
-      sortBooks = sortRating
-    }
-    else if (sort === 'pages-asc') {
-      sortBooks = sortPages
-    }
-    else if (sort !== 'rating-desc' || sort !== 'pages-asc') {
-      return response.status(404).json(
-        { error: "No such sorting" }
-      )
-    }
-    const filteredBooks = await Book.find({
-      $and: [{
-        $or: [
-          { authors: findAuthor },
-          { title: findTitle }
-        ]
-      },
-      { language_code: findLanguage }
-      ]
-    },
-      filteredFields
-    ).sort(
-      sortBooks
-    ).exec()
-    if (filteredBooks.length > 0) {
-      return response.json(filteredBooks)
-    } else {
-      return response.status(404).json(
-        { error: "No such author/title and language combination" }
-      )
-    }
-  }
-
-  if (search && language) {
-    const filteredBooks = await Book.find({
-      $and: [{
-        $or: [
-          { authors: findAuthor },
-          { title: findTitle }
-        ]
-      },
-      { language_code: findLanguage }
-      ]
-    },
-      filteredFields
-    ).exec()
-    if (filteredBooks.length > 0) {
-      return response.json(filteredBooks)
-    } else {
-      return response.status(404).json(
-        { error: "No such combination" }
-      )
-    }
-  }
-
-  if (search && sort) {
-    let sortBooks
-    if (sort === 'rating-desc') {
-      sortBooks = sortRating
-    } else if (sort === 'pages-asc') {
-      sortBooks = sortPages
-    } else if (sort !== 'rating-desc' || sort !== 'pages-asc') {
-      return response.status(404).json(
-        { error: "No such sorting" }
-      )
-    }
-    const filteredBooks = await Book.find({
-      $or: [
-        { authors: findAuthor },
-        { title: findTitle }
-      ]
-    },
-      filteredFields
-    ).sort(
-      sortBooks
-    ).exec()
-    if (filteredBooks.length > 0) {
-      return response.json(filteredBooks)
-    } else {
-      return response.status(404).json(
-        { error: "No title or author match" }
-      )
-    }
-  }
+  let books = []
+  let findTitle = null
+  let findAuthor = null
+  let findLanguage = null
+  let sortBooks = null
 
   if (search) {
-    const filteredBooks = await Book.find({
-      $or: [
-        { authors: findAuthor },
-        { title: findTitle }
+    findTitle = { $regex: ("\\b" + search + "\\b"), $options: "i" }
+    findAuthor = { $regex: ("\\b" + search + "\\b"), $options: "i" }
+  } else {
+    null
+  }
+
+  if (language) {
+    findLanguage = { $regex: language, $options: "i" }
+  } else {
+    null
+  }
+
+  if (sort) {
+    if (sort === 'pages-asc') {
+      sortBooks = { num_pages: 1 }
+    } else if (sort === 'rating-desc') {
+      sortBooks = { average_rating: -1 }
+    } else {
+      return response.status(404).json(
+        { error: "Wrong sort input" }
+      )
+    }
+  } else {
+    null
+  }
+
+  if (search && language && sort || search && language || search & sort || language && sort) {
+    books = await Book.find({
+      $and: [{
+        $or: [
+          { authors: findAuthor },
+          { title: findTitle }
+        ]
+      },
+      { language_code: findLanguage }
       ]
     },
       filteredFields
+    ).sort(
+      sortBooks
     ).exec()
-    if (filteredBooks.length > 0) {
-      return response.json(filteredBooks)
-    } else {
-      return response.status(404).json(
-        { error: "No title or author match" }
-      )
-    }
   }
 
-  else {
-    const books = await Book.find(
+  if (search && !language || language && !search) {
+    books = await Book.find({
+      $or: [{
+        $or: [
+          { authors: findAuthor },
+          { title: findTitle }
+        ]
+      },
+      { language_code: findLanguage }
+      ]
+    },
+      filteredFields
+    ).sort(
+      sortBooks
+    ).exec()
+  } else {
+    null
+  }
+
+  if (sort && !search && !language || !sort && !search && !language) {
+    books = await Book.find(
       {},
       filteredFields
+    ).sort(
+      sortBooks
     ).exec()
-    response.json(books)
+  }
+
+  if (books.length > 0) {
+    return response.json(books)
+  } else {
+    return response.status(404).json(
+      { error: "Not found" }
+    )
   }
 })
+
 
 app.get('/books/:id', async (request, response) => {
   try {
