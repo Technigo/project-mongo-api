c921e8611b40a9bf78ad2954df2b35ecca7033c6
Therese Lindén <theresemlinden@gmail.com>
2020-12-15 11:15:32 +0100

    updated readme

--- README.md
+++ README.md
@@ -1,13 +1,30 @@
 # Mongo API Project
 
-Replace this readme with your own information about your project. 
+First Mongo db.
 
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+## Learning objectives
 
-## The problem
+- How to create routes
+- How to deploy Node projects
+- How to model data in Mongoose
+- How to fetch items from a Mongo database using Mongoose
+- How to seed large amounts of data to a database
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+## Tech
+
+- JavaScript
+- Moongose
+- Express
+- Mongo DB
+- Heroku
+- Postman
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+Available routes:
+/books, /books/:id
+
+Queries available:
+title/author/page/language
+
+[Book API] https://thereselinden-mongo-book-api.herokuapp.com/
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
 //import booksData from './data/books.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
 //_____________create my modules
 //model is a method /function function that takes two argument
@@ -48,44 +48,46 @@ const Book = mongoose.model('Book', {
 
 if (process.env.RESET_DATABASE) {
   const seedDatabase = async () => {
-    await Book.deleteMany() //clear database
-    await booksData.forEach((item) => new Book(item).save()) //single object from json array
-  }
-  seedDatabase()
-}
+    await Book.deleteMany(); //clear database
+    await booksData.forEach((item) => new Book(item).save()); //single object from json array
+  };
+  seedDatabase();
+};
 
-
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
+const listEndpoints = require('express-list-endpoints')
 
-//____________to get an error message if server is down
-// 1 === connected 0,2,3 not connected
+//____________error message in case server is down
 app.use((req, res, next) => {
   if(mongoose.connection.readyState === 1) {
     next() //to execute next get response
   } else {
-    res.status(503).send({ error: "service unavailable"})
-  }
-})
+    res.status(503).send({ error: "service unavailable"});
+  };
+});
 
-// Start defining your routes here
+//____________Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+  if(!res) {
+      res
+      .status(404)
+      .send({ Error: 'Sorry, a problem occured try again later' });
+  }
+  else res.send(listEndpoints(app));
+});
 
-//______________All books
+//_____________All books
 app.get('/books', async (req, res) => {
-  const { title, author, language, page } = req.query
-  const pageNumber = +page || 1 //why does ?page=0 console logged out as 1? 
-  console.log(pageNumber)
+  const { title, author, language, page } = req.query;
+  const pageNumber = +page || 1; //why does ?page=0 console logged out as 1? 
+  const pageSize = 20;
+  const skip = pageSize * (pageNumber -1);
+  //console.log(pageNumber);
   /* is line 87 is shorthand for: ?????
   https://www.sitepoint.com/shorthand-javascript-techniques/
   
@@ -97,10 +99,8 @@ app.get('/books', async (req, res) => {
 
   */
 
-  const pageSize = 20
-  const skip = pageSize * (pageNumber -1)
 
-  //första authors = definering i modell , author = query parameter.
+  //första authors = definering i modell , author = query parameter. - REMOVE
   let books = await Book.find({
     title: new RegExp(title, 'i'),
     authors: new RegExp(author, 'i'),
@@ -110,15 +110,14 @@ app.get('/books', async (req, res) => {
     .limit(pageSize)
     .skip(skip)
 
-
   if (books) {
-    res.json({ currentPage: pageNumber, pageSize: pageSize, skip: skip, results: books })
+    res.json({ currentPage: pageNumber, pageSize: pageSize, skip: skip, results: books });
   } else {
-    res.status(404).send({ error: "No books found"})
-  }
-})
+    res.status(404).send({ error: "No books found"});
+  };
+});
 
-//____________Single book
+//_____________Single book
 app.get('/books/:id', async (req, res) => {
   const { id } = req.params;
 
@@ -128,14 +127,30 @@ app.get('/books/:id', async (req, res) => {
       res.json(singleBook);
     } else res.status(404).send({ error: `No book with id: ${id} found.` });
   } catch {
-    res.status(400).send({ error: `${id} is not a valid bookID`})
-  }
+    res.status(400).send({ error: `${id} is not a valid bookID`});
+  };
 });
 
-// Start the server
+//______________Array of authors
+app.get("/authors", async (req, res) => {
+  const { authors } = req.query;
+  const authorsArray = await Book.find({ authors: {$in: authors}});
+  console.log(authorsArray);
+  //const uniqueAuthorsArray = [...new Set(authorsArray)];
+  
+  if (!authorsArray) {
+    res
+    .status(404)
+    .send({Error: "something went wrong"});
+  };
+  //res.send({authors: uniqueAuthorsArray.length, results: uniqueAuthorsArray});
+  res.send({ results: authorsArray });
+});
+
+//_____________Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
 
 //npm install @babel/helper-compilation-targets 
 // INCASE PROBLEM STARTING NPM RUN DEV
