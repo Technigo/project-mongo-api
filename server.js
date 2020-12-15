1f262e0848a971ede0b9d0c9e037c3964dca3ab1
Olof Lindahl <olindahl@gmail.com>
2020-12-15 23:42:37 +0100

    added pagination

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
-// 
+//
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
 // import booksData from './data/books.json'
-import netflixData from './data/netflix-titles.json'
-console.log(netflixData.length)
+import netflixData from "./data/netflix-titles.json";
+console.log(netflixData.length);
 // import topMusicData from './data/top-music.json'
 
 //const mongoUrl = "mongodb+srv://dbUser:dbUserOlofTechnigo@cluster0.x2ofn.mongodb.net/projectMongo?retryWrites=true&w=majority"
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
-console.log(mongoUrl)
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+console.log(mongoUrl);
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-const Show = mongoose.model('Show', {
+const Show = mongoose.model("Show", {
   show_id: Number,
   title: String,
   director: String,
@@ -31,69 +31,71 @@ const Show = mongoose.model('Show', {
   duration: String,
   listed_in: String,
   description: String,
-  type: String
-})
+  type: String,
+});
 
 // Show.deleteMany().then(() => {
 // netflixData.forEach((showData) => {
 //   new Show(showData).save()
 // })})
 
- if (process.env.RESET_DB) {
-  console.log('resetting database')
- 	const seedDatabase = async () => {
-     await Show.deleteMany({})
+if (process.env.RESET_DB) {
+  console.log("resetting database");
+  const seedDatabase = async () => {
+    await Show.deleteMany({});
 
- 		netflixData.forEach((showData) => {
- 			new Show(showData).save()
- 		})
-   }
+    netflixData.forEach((showData) => {
+      new Show(showData).save();
+    });
+  };
 
-   seedDatabase()
- }
+  seedDatabase();
+}
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  console.log('Test')
-  res.send('Hello world')
-  console.log(mongoUrl)
-})
+app.get("/", (req, res) => {
+  console.log("Test");
+  res.send("Hello world");
+  console.log(mongoUrl);
+});
 
 //All shows
 
-app.get('/shows', async (req, res) => {
-  const shows = await Show.find()
-  res.json(shows)
-})
+app.get("/shows", async (req, res) => {
+  const shows = await Show.find();
+  res.json(shows);
+});
 
 //shows limited to 10 documents, paginated with query "page"
 
-app.get('/shows/limited', async (req, res) => {
-  const page = req.query.page
-  const shows = await Show.find().skip(page * 10 - 10).limit(10)
-  res.json(shows)
-})
+app.get("/shows/limited", async (req, res) => {
+  const page = req.query.page;
+  const shows = await Show.find()
+    .skip((page === 0 ? page : 1) * 10 - 10) //page value 0 will resort to 1 since monbgoDB doesn't accept otherwise.
+    .limit(10);
+  res.json(shows);
+});
 
 //parameter country
 
-app.get('/shows/:country', async (req, res) => {
-  const { country } = req.params
-  const shows = await Show.find( {country: country })
-  res.json(shows)
-})
+app.get("/shows/:country", async (req, res) => {
+  const { country } = req.params;
+  const shows = await Show.find({ country: country });
+  res.json(shows);
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
