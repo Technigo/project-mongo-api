4c48e9bce9805e6f2a91fdd41aaf8fb4b9f92926
Hanna Skeppe <hannaskeppe@gmail.com>
2020-12-15 17:06:53 +0100

    Setting up project. Started on routes.

--- server.js
+++ server.js
@@ -3,16 +3,9 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import netflixData from './data/netflix-titles.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/hannas-project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
@@ -27,9 +20,45 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+// set up models (Schema) (define what type of data)
+const NetflixTitle = new mongoose.model('NetflixTitle', {
+  show_id: Number,
+  title: String,
+  director: String,
+  cast: String,
+  country: String,
+  date_added: String,
+  release_year: Number,
+  rating: String,
+  duration: String,
+  listed_in: String,
+  description: String,
+  type: String
+})
+
+
+//Populate the database
+if (process.env.RESET_DATABASE) {
+  const populateDatabase = async () => {
+    await NetflixTitle.deleteMany(); //main rule: clean database before populating database.
+
+    netflixData.forEach(item => {
+      const newNetflixTitle = new NetflixTitle(item); // item is a single object from the json array.
+      newNetflixTitle.save();
+    })
+  }
+  populateDatabase();
+}
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+app.get('/netflix-titles', async (req, res) => {
+  //res.json(netflixData);
+  const allTitles = await NetflixTitle.find();
+  res.json(allTitles);
+})
+
+app.get('/netflix-titles/:title', async (req, res) => {
+  const singleTitle = await NetflixTitle.findOne({ title: req.params.title })
+  res.json(singleTitle)
 })
 
 // Start the server
