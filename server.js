93bd87001984bc1040c8e7cfdd75c0a61070066d
ClaireCaudwell <clairecaudwell@gmail.com>
2020-12-15 22:21:36 +0100

    Deleted notes on different datasets, added notes for my own use, created the model for the objects that will be stored in my database, seeded database by iterating over the books data set i chose to use and then saving it to the database. Also included deleteMany when the seedDatabase function is called so whatever data already exsists in the database will be deleted and replaced with new data. Created two endpoints. The first that queries different query parameters, author, title, language, averagerating and also if none of those query parameters are true, it returns the entire array of data. And the second one that returns a book when the id is specified in the endpoint.

--- server.js
+++ server.js
@@ -1,38 +1,117 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express, { query } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import bookData from './data/books.json';
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+// Allowing for us to connect to mongodb database
+// Line 10 url to database which we can use in mongodb to access the server data
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/books";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+// Creating the model/structure of the database and what data will be stored there for each array element. This model is based on the books.json data set, but it can be based on any kind of data set. Can be string, number and boolean written as values, but which one it is depends on what the properties of the keys are written as in the initial data set.
+// Changed bookID in the data set to _id so then I could query using findById() to find a specific id of a book
+const Book = mongoose.model('Book', {
+  bookID: Number,
+  title: String,
+  authors: String,
+  average_rating: Number,
+  isbn: String,
+  isbn13: String,
+  language_code: String,
+  num_pages: Number,
+  ratings_count: Number,
+  text_reviews_count: Number,
+});
+
+/* Seeding database: 
+Means that this data is sent to our database when it's first started.
+Mongoose async/await operations - helping with asynchronous handling?
+Creating a new author based on the mongoose model above.
+Then saving this to the mongodb database
+*/
+const seedDatabase = async () => {
+// deleteMany will allow for the data that was stored on the database on the last get request to be deleted so that there are no dupications of data. Then the new Authors will be created again.
+  await Book.deleteMany({});
+
+  bookData.forEach((bookData) => {
+    new Book(bookData).save();
+  })
+};
+
+seedDatabase();
+
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+// Welcome message when you enter the url
+app.get('/', (request, response) => {
+  res.json("Welcome to Claire's book API 🌼 For documentation go to https://books-deployment.herokuapp.com/documentation");
+});
+
+// First endpoint that will return the whole array of books data
+// Async and await are being used as find is asyncronous and might take some time to return the data being fetched from the server?
+app.get('/books', async (request, response) => {
+  const {author, title, language, averagerating } = request.query;
+
+  // RegExp is a regular expression object that is used to match the author string/text added to the query parameter with a pattern. 
+  if(author) {
+    const authorRegexp = new RegExp(author);
+    const queryAuthor = await Book.find({authors: authorRegexp});
+    if(queryAuthor.length === 0){
+      return response.status(404).json({ error: "Sorry that we don't have any data for that author"});
+    }
+    return response.json(queryAuthor);    
+  }
+
+  if(title) {
+    const titleRegexp = new RegExp(title);
+    const queryTitle = await Book.find({title: titleRegexp});
+    if(queryTitle.length === 0){
+      return response.status(404).json({ error: "Sorry that we don't have any data for that title"});
+    }
+    return response.json(queryTitle);    
+  }
+
+  if(language) {
+    const langRegexp = new RegExp(language);
+    const queryLanguage = await Book.find({language_code: langRegexp});
+    if(queryLanguage.length === 0){
+      return response.status(404).json({ error: "Sorry that we don't have any data for that language"});
+    }
+    return response.json(queryLanguage);    
+  }
+
+  if(averagerating) {
+    const queryAveRat = await Book.find({average_rating: averagerating});
+    if(queryAveRat.length === 0){
+      return response.status(404).json({ error: "Sorry that we don't have any data for that average rating"});
+    }
+    return response.json(queryAveRat);    
+  }
+
+  const books = await Book.find();
+  response.json(books);
+})
+
+// Second endpoint where the user can write a bookID and a specific book will be returned.
+app.get('/books/:id', async (request, response) => {
+  const specificBook = await Book.findOne({bookID: request.params.id});
+
+  if(specificBook) {
+    response.json(specificBook);
+  } else {
+    response.status(404).json({ error: "Sorry that we don't have any data for that book"}); 
+  }  
 })
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
-})
+});
