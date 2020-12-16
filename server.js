4f88cc83819070770f83f2a0f6c65f426e50318a
Katsiez <katarinajovanovic.a@gmail.com>
2020-12-16 18:03:05 +0100

    returning an empty array, work in progress, will it work after deployment?

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const Book = mongoose.model('Book', {
   authors: String,
   authors: String,
   average_rating: Number,
-  isbn: Number,
+  isbn: String,
   isbn13: Number,
   language_code: String,
   num_pages: Number,
@@ -23,14 +23,13 @@ const Book = mongoose.model('Book', {
 
 if (process.env.RESET_DATABASE) {
   const seedDatabase = async () => {
-    await Book.deleteMany({});
-
+    await Book.deleteMany({})
     booksData.forEach((bookData) => {
       new Book(bookData).save();
-    });
+    })
   };
   seedDatabase();
-}
+};
 
 const port = process.env.PORT || 2000;
 const app = express();
@@ -38,6 +37,11 @@ const app = express();
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(bodyParser.json());
+
+const myEndpoints = require("express-list-endpoints");
+app.get("/", (request, response) => {
+  response.send(myEndpoints(app));
+});
 
 app.use((req, res, next) => {
   if (mongoose.connection.readyState === 1) {
@@ -47,39 +51,22 @@ app.use((req, res, next) => {
   }
 })
 
-const myEndpoints = require("express-list-endpoints");
-// Start defining your routes here
-app.get("/", (request, response) => {
-  response.send(myEndpoints(app));
+app.get("/books", async (req, res) => {
+  const allBooks = await Book.find();
+    res.json(allBooks);
 });
 
-app.get("/books", async (req, res) => {
-  const { title, author, sort } = req.query
-  const titleRegex = new RegExp(title, 'i')
-  const authorRegex = new RegExp(author, 'i')
+app.get("/books/id/:bookId", async (req, res) => {
+  try {
+    const singleBook = await Book.findOne({ bookID: req.params.bookID });
 
-  const sortQuery = (sort) => {
-    if (sort === 'rating') {
-      return { average_rating: -1 }
+    if (singleBook) {
+      res.json(singleBook);
+    } else {
+      res.status(404).json("Hmm, we can't find that book in our database. Try searching another ID!");
     }
-  }
-
-  const books = await Book.find({
-    title: titleRegex,
-    authors: authorRegex
-  })
-    .sort(sortQuery(sort))
-
-  res.json(books)
-
-})
-
-app.get("/books/:id", async (req, res) => {
-  const book = await Book.findById(req.params.id);
-  if (book) {
-    res.json(book);
-  } else {
-    res.status(404).json({ error: "Book not found" });
+  } catch (err) {
+    res.status(400).json({ error: "No such book ID" });
   }
 });
 
@@ -89,13 +76,21 @@ app.get("/books/:isbn", async (req, res) => { try {
     if (book) {
       res.json(book);
     } else {
-      res.status(404).json({ error: `Could not find book with isbn=${isbn}` });
+      res.status(404).json({ error: `No books found with the following isbn=${isbn}` });
     }
   } catch (err) {
     res.status(400).json({ error: 'invalid request' })
   }
 });
 
+app.get('/books/author/:authors', async (req, res) => {
+  const searchAuthor = req.params.authors;
+  const thisBookAuthor = await Book.find({ authors: { $regex : new RegExp(searchAuthor, "i") } });
+  if (thisBookAuthor.length === 0) {
+    res.status(404).json("No books have been found by such author. Perhaps try searching another author?");
+  }
+  res.json(thisBookAuthor);
+});
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
