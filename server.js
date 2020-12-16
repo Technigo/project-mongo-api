f7368c5eb2155e04531d7f808473087b6a7fbcb9
jonna ru <jonna.ru@icloud.com>
2020-12-16 21:37:59 +0100

    blue level

--- server.js
+++ server.js
@@ -3,22 +3,12 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-//
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'brew install wgetbrew install wget
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import volcanosData from "./data/volcanos.json";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/volcanos";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be
-// overridden when starting the server. For example:
-//
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
@@ -27,33 +17,113 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const Animal = mongoose.model("Animal", {
-  name: String,
-  age: Number,
-  isFurry: Boolean,
+const Volcano = new mongoose.model("Volcano", {
+  Number: Number,
+  Name: String,
+  Country: String,
+  Region: String,
+  Type: String,
 });
 
-Animal.deleteMany().then(() => {
-  new Animal({ name: "Alfons", age: 2, isFurry: true }).save();
-  new Animal({ name: "Lucy", age: 5, isFurry: true }).save();
-  new Animal({ name: "Goldy the goldfish", age: 1, isFurry: false }).save();
-});
+if (process.env.RESET_DATABASE) {
+  const volcanoDatabase = async () => {
+    await Volcano.deleteMany();
+
+    volcanosData.forEach((item) => {
+      const newVolcano = new Volcano(item);
+      newVolcano.save();
+    });
+  };
+  volcanoDatabase();
+}
+
+// const Volcano = mongoose.model("Volcano", {
+//   name: String,
+//   country: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: "Country",
+//   },
+// });
+
+// const Name = mongoose.model("Name", {
+//   name: String,
+// });
+
+// const Country = mongoose.model("Country", {
+//   name: String,
+// });
+
+// if (process.env.RESET_DATABASE) {
+//   const seedDatabase = async () => {
+//     await Name.deleteMany();
+
+//     const larderello = new Name({ name: "Larderello" });
+//     await larderello.save();
+
+//     const dubbi = new Name({ name: "Dubbi" });
+//     await dubbi.save();
+//   };
+//   seedDatabase();
+// }
+
+// Defines the port the app will run on. Defaults to 8080, but can be
+// overridden when starting the server. For example:
+//
+//   PORT=9000 npm start
+
+// Add middlewares to enable cors and json body parsing
+app.use(cors());
+app.use(bodyParser.json());
+
+// const Animal = mongoose.model("Animal", {
+//   name: String,
+//   age: Number,
+//   isFurry: Boolean,
+// });
+
+// Animal.deleteMany().then(() => {
+//   new Animal({ name: "Alfons", age: 2, isFurry: true }).save();
+//   new Animal({ name: "Lucy", age: 5, isFurry: true }).save();
+//   new Animal({ name: "Goldy the goldfish", age: 1, isFurry: false }).save();
+// });
+
+// Start defining your routes here
+
+// app.get("/", (req, res) => {
+//   Animal.find().then((animals) => {
+//     res.json(animals);
+//   });
+// });
+
+// app.get("/:name", (req, res) => {
+//   Animal.findOne({ name: req.params.name }).then((animal) => {
+//     if (animal) {
+//       res.json(animal);
+//     } else {
+//       res.status(404).json({ error: "Not found" });
+//     }
+//   });
+// });
+
+// app.get("volcanos", async (req, res) => {
+//   const volcanos = await Volcano.find();
+//   res.json(volcanos);
+// });
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  Animal.find().then((animals) => {
-    res.json(animals);
-  });
+  res.send("Hello world");
 });
 
-app.get("/:name", (req, res) => {
-  Animal.findOne({ name: req.params.name }).then((animal) => {
-    if (animal) {
-      res.json(animal);
-    } else {
-      res.status(404).json({ error: "Not found" });
-    }
-  });
+app.get("/volcanos", async (req, res) => {
+  const allVolcanos = await Volcano.find();
+  res.json(allVolcanos);
+});
+
+app.get("/volcanos/:name", async (req, res) => {
+  const singleVolcano = await Volcano.findOne({ Name: req.params.name });
+
+  res.json(singleVolcano);
 });
 
 // Start the server
