dc799ac7e6348e2a5f49d93c59617332548b8291
Sara Sundblom <sundblomsara@gmail.com>
2020-12-16 16:09:43 +0100

    started to decide with endpoints to have

--- server.js
+++ server.js
@@ -3,36 +3,49 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import topMusicData from './data/top-music.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+
+// when searching for artists- all artists-
+const Artist = mongoose.model('Artist', {
+  name: String,
+  track: String,
+  energy: Number, 
+  danceAbility: Number
 })
 
-// Start the server
+//being able to search for one artist
+//being able to search for one track
+
+
+// search based on genre? 
+const Genre = mongoose.model('Genre', {
+  name: String,
+  artist: {
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Artist '
+  }
+})
+  
+//able to search for songs based on 'danceability' - up-tempo or slow!  
+
+
+app.get('/', (req, res) => {
+  res.send('Hello surdeg!')
+})
+
+
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
