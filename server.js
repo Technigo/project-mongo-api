4e4e952c745cda031f022b6bd1f5027dcfcb60d5
Moa <63230455+mhammarback@users.noreply.github.com>
2020-12-16 16:03:37 +0100

    indentation check

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
-import dotenv from 'dotenv'
 
 import booksData from './data/books.json'
-
-dotenv.config()
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -42,7 +39,6 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
  
-
 app.get('/', (req, res) => {
   res.send('Welcome!')
 })
@@ -63,9 +59,7 @@ app.get('/books/book/:bookID', async (req, res) => {
 
 app.get('/books/authors/:authorName', async (req, res) => {
   const AuthorName = req.params.authorName
-
   const authorBooks = await Book.find({ authors: { $regex : new RegExp(AuthorName, "i") } })
-  // regex added so the search is non-case-sensitive
 
   if ( authorBooks.length === 0) {
     res.status(404).json('no books found by that author')
