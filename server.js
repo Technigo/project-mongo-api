743ea033c823aad25e524801bcc71a139aec6a4a
caroline birgersson <caroline@birgerochblom.se>
2020-12-16 13:13:42 +0100

    added data

--- server.js
+++ server.js
@@ -1,38 +1,87 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+//import booksData from './data/books.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/books";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+const Author = mongoose.model("Author", {
+  name: String,
+});
+
+const Book = mongoose.model("Book", {
+  title: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Author",
+  },
+});
+
+if (process.env.RESET_DATABASE) {
+  console.log("Resetting database");
+
+  const seedDatabase = async () => {
+    await Author.deleteMany(); // fixes so it does not load more objects with new rendering
+    await Book.deleteMany();
+
+    const tolkien = new Author({ name: "Tolkien" });
+    await tolkien.save();
+
+    const duras = new Author({ name: "Duras" });
+    await duras.save();
+
+    await new Book({ title: "Harry", author: tolkien }).save();
+    await new Book({ title: "Larry", author: duras }).save();
+  };
+  seedDatabase();
+}
+
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get("/", (req, res) => {
+  res.send("API of books");
+});
+
+app.get("/authors", async (req, res) => {
+  //returns all authors
+  const authors = await Author.find();
+  res.json(authors);
+});
+
+app.get("/authors/:id", async (req, res) => {
+  //returns a single author
+  const author = await Author.findById(req.params.id);
+  res.json(author);
+});
+
+app.get("/authors/:id/books", async (req, res) => {
+  //returns the books of an author
+  const author = await Author.findById(req.params.id);
+  const books = await Book.find({ author: mongoose.Types.ObjectId(author.id) });
+  res.json(books);
+});
+
+app.get("/books", async (req, res) => {
+  //returns all the books
+  const books = await Book.find().populate("author");
+  res.json(books);
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
