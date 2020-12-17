09907821c4ec33eec16f7a93c44a6e7a4a819320
Lili <lilihorvath@github.com>
2020-12-17 16:41:52 +0100

    First commit

--- server.js
+++ server.js
@@ -3,36 +3,77 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import booksData from './data/books.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const Book = mongoose.model('Book', {
+  bookID: Number,
+  title: String,
+  authors: String,
+  average_rating: Number,
+  isbn: String,
+  isbn13: Number,
+  language_code: String,
+  num_pages: Number,
+  ratings_count: Number,
+  text_reviews_count: Number
+})
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Book.deleteMany({})
+
+    booksData.forEach((bookData) => {
+      new Book(bookData).save()
+    })
+  }
+  seedDatabase()
+}
+
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
-// Start the server
+app.get('/books', async (req, res) => {
+  const { title, author, sort } = req.query
+  const titleRegex = new RegExp(title, 'i')
+  const authorRegex = new RegExp(author, 'i')
+
+  const sortQuery = (sort) => {
+    if (sort === 'rating') {
+      return { average_rating: -1 }
+    }
+  }
+
+  const books = await Book.find({
+    title: titleRegex,
+    authors: authorRegex
+  })
+    .sort(sortQuery(sort))
+
+  res.json(books)
+
+})
+
+app.get('/books/:id', async (req, res) => {
+  const book = await Book.findById(req.params.id)
+  if (book) {
+    res.json(book)
+  } else {
+    res.status(404).json({ error: 'Book not found' })
+  }
+})
+
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
