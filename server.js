0f5526c165413add26c3d72c7a10edae3867813c
Jamie Cook <jamie.p.cook@outlook.com>
2020-12-17 16:15:32 +0100

    req.query added to endpoint

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 8080;
 const app = express();
 const listEndpoints = require("express-list-endpoints");
 
@@ -43,17 +43,32 @@ app.get("/", (req, res) => {
   res.send(listEndpoints(app));
 });
 
+// app.get("/albums", async (req, res) => {
+//   const { artist, year, label } = req.query;
+
+//   let albums = await Album.find({
+//     artist: new RegExp(artist, "i"),
+//     year: { $eq: year },
+//     label: new RegExp(label, "i"),
+//   });
+//   res.json(albums);
+// });
+
 app.get("/albums", async (req, res) => {
-  const allAlbums = await Album.find();
+  const queryParameters = req.query;
+  console.log(queryParameters);
+
+  const allAlbums = await Album.find(req.query);
   res.json(allAlbums);
 });
 
 app.get("/albums/:album", async (req, res) => {
-  const { album } = req.params;
-  if (album) {
-    const findAlbum = await Album.findOne({ albumName: req.params.album });
-    res.json(findAlbum);
-  }
+  // const { album } = req.params;
+  // if (album) {
+  //   const findAlbum = await Album.findOne({ albumName: req.params.album });
+  //   res.json(findAlbum);}
+  const singleAlbum = await Album.findOne({ albumName: req.params.album });
+  res.json(singleAlbum);
 });
 
 app.get("/albums/artist");
