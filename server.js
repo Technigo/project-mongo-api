3b960a833f902583939f4679d765bf66e9dd6d61
Moa Blomkvist <moa@birgerochblom.se>
2020-12-17 23:11:50 +0100

    2 routes

--- server.js
+++ server.js
@@ -5,24 +5,11 @@ import mongoose from 'mongoose'
 
 import netflixData from './data/netflix-titles.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
-
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/movies"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/productions"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-//const Actor = mongoose.model('Actor', {
-  //name: String
-//})
-
-const Movie = mongoose.model('Movie', {
+const Production = mongoose.model('Production', {
   show_id: Number,
   title: String,
   director: String,
@@ -41,47 +28,17 @@ const Movie = mongoose.model('Movie', {
 if (process.env.RESET_DATABASE) {
   console.log('resetting database!')
   const populateDatabase = async () => {
-    await Movie.deleteMany();
+    await Production.deleteMany();
   
     netflixData.forEach(item => {
-      const newMovie = new Movie(item);
-      newMovie.save();
+      const newProduction = new Production(item);
+      newProduction.save();
     })
   }
   populateDatabase();
 }
 
 
-//Command: RESET_DATABASE=true npm run dev 
-
-//if (process.env.RESET_DATABASE) {
-  //console.log('resetting database!')
-
-  //const seedDatabase = async () => {
-   //await Actor.deleteMany()
-    //await Movie.deleteMany()
-    
-    //const pitt = new Actor({ name: 'Brad Pitt' })
-    //await pitt.save()
-  
-    //const hopkins = new Actor({ name: 'Anthony Hopkins' })
-    //await hopkins.save()
-
-    //await new Movie({ title: 'The Silence of the Lambs', actor: hopkins }).save()
-    //await new Movie({ title: 'Bobby', actor: hopkins }).save()
-    //await new Movie({ title: 'Hannibal', actor: hopkins }).save()
-    //await new Movie({ title: 'Troja', actor: pitt }).save()
-    //await new Movie({ title: 'Mr. & Mrs. Smith ', actor: pitt }).save()
-    //await new Movie({ title: 'Fight Club', actor: pitt }).save()
-  //}
-  //seedDatabase()
-//}
-
-
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -89,6 +46,7 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+// Global Error if problem with the database
 app.use((req, res, next) => {
   if (mongoose.connection.readyState === 1) {
     next()
@@ -97,47 +55,30 @@ app.use((req, res, next) => {
   }
 })
 
-// Start defining your routes here
+// ROUTES
 app.get('/', (req, res) => {
-  Actor.find().then(actors => {
-    res.json(actors)
+    res.send('API NeflixData - Powered by: Moa Blomkvist')
   })
+
+// One array of data
+app.get('/productions', async (req, res) => {
+  const allProductions = await Production.find()
+  res.json(allProductions)
 })
 
-app.get('/actors', async (req, res) => {
-  const actors = await Actor.find()
-  res.json(actors)
-})
-
-app.get('/actors/:id', async (req, res) => {
+// One object of data (findById)
+app.get('/productions/:id', async (req, res) => {
   try {
-    const actor = await Actor.findById(req.params.id)
-    if (actor) {
-      res.json(actor)
+    const production = await Production.findById(req.params.id)
+    if (production) {
+      res.json(production)
     } else {
-      res.status(404).json({ error: 'Actor not found' })
+      res.status(404).json({ error: 'Production not found' })
     }
   } catch (err) {
-    res.status(400).json({ error: 'Invalid user id' })
+    res.status(400).json({ error: 'Invalid production id' })
   }
 })
-
-app.get('/actors/:id/movies', async (req, res) => {
-  const actor = await Actor.findById(req.params.id) 
-  if (actor) {
-    const movies = await Movie.find({ actor: mongoose.Types.ObjectId(actor.id) })
-    res.json(movies)
-  } else {
-    res.status(404).json({ error: 'Actor not found' })
-  }
-})
-
-app.get('/movies', async (req, res) => {
-  const movies = await Movie.find().populate('actor')
-  res.json(movies)
-})
-
-
 
 // Start the server
 app.listen(port, () => {
