a9b24399dc58b296fa79f816af2675253e324070
Sara Sundblom <sundblomsara@gmail.com>
2020-12-17 19:33:46 +0100

    triyng to add querys doesnt got to well

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { query } from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
@@ -24,7 +24,7 @@ const TopSong = mongoose.model('TopSong', {
   genre: String,
   bpm: Number,
   energy: Number,
-  danceability: Number ,
+  danceability: Number,
   loudness: Number,
   liveness: Number,
   valence: Number,
@@ -34,7 +34,8 @@ const TopSong = mongoose.model('TopSong', {
   popularity: Number
 })
 
-if(process.env.RESET_DATABASE){
+
+if (process.env.RESET_DATABASE) {
   console.log('RESETTING DATABASE')
 
   const populateDatabase = async () => {
@@ -45,30 +46,41 @@ if(process.env.RESET_DATABASE){
       newTopSong.save()
     })
   }
-
   populateDatabase()
 }
 
 
 app.get('/', (req, res) => {
-  res.send('Hello surdeg!')
+  res.send('Hello sourdough!')
 })
 
 app.get('/topsongs', async (req, res) => {
-  const topsongs = await TopSong.find()
-  res.json(topsongs)
+  const queryParameters = req.query
+  console.log(queryParameters)
+
+  const allTopsongs = await TopSong.find()
+  res.json(allTopsongs)
 })
 
+
+
+
+app.get('/topsongs/:artist', async (req, res) => {
+  const queryParameters = req.query
+  const findArtist = await TopSong.find({ artistName: req.params.artist })
+  res.json(findArtist)
+})
+
+// app.get('/topsongs/track/:track', async (req, res) => {
+//   const findTrack = await TopSong.find({ trackName: req.query.track })
+//   res.json(findTrack)
+// })
+
+// {being able to search for one artist
+// being able to search for one track
+// able to search for songs based on 'danceability' - up-tempo or slow. 
+// search based on genre? }
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
-
-
-//being able to search for one artist
-//being able to search for one track
-
-// search based on genre? 
-
-  
-//able to search for songs based on 'danceability' - up-tempo or slow!  
