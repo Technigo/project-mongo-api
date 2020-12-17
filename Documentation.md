6cf2ecd978790aa3fa3ef3fca9c6924fe680f9a9
Karin Nordkvist <kontakt@karinnordkvist.se>
2020-12-17 13:19:55 +0100

    Models added, database populated and most routes declared

---
+++ Documentation.md
@@ -0,0 +1,71 @@
+
+# TEDTALKS BY KARIN 
+
+
+## BASE URL
+https://tedtalks-by-karin-mongo.herokuapp.com/
+
+
+## TALKS
+`https://tedtalks-by-karin.herokuapp.com/talks/?page={number of page}`
+
+Listing all talks on pages consisting of 50 talks per page. To query for a specific page, type `page={number of page}`.
+
+#### Query for talks filtered by following:
+
+**Category (category)** 
+
+`https://tedtalks-by-karin.herokuapp.com/talks/?category={category name}&category={second category name}`
+
+Listing all talks in a selected category. Filtering by multiple categories is possible. When using multiple queries the result will be based on talks containing **all** queries. 
+
+
+**Speaker (speaker)**
+
+`https://tedtalks-by-karin.herokuapp.com/talks/?speaker={speaker name}`
+
+Listing all talks made by selected speaker.
+
+
+**Event (event)**
+
+`https://tedtalks-by-karin.herokuapp.com/talks/?event={name of event}`
+
+Listing all talks presented on selected event.
+
+
+## SINGLE TALK
+`https://tedtalks-by-karin.herokuapp.com/talks/{id}`
+
+To list a single talk add the individual ID to the endpoint. 
+
+## SPEAKERS
+`https://tedtalks-by-karin.herokuapp.com/speakers/`
+
+Listing all speakers.
+
+## TALKS BY SPEAKER
+`https://tedtalks-by-karin.herokuapp.com/speakers/{speaker}/talks`
+
+Listing all talks for chosen speaker.
+
+## EVENTS
+`https://tedtalks-by-karin.herokuapp.com/events/`
+
+Listing all events.
+
+## TALKS BY EVENT
+`https://tedtalks-by-karin.herokuapp.com/events/{event}/talks`
+
+Listing all talks for chosen event.
+
+## CATEGORIES
+`https://tedtalks-by-karin.herokuapp.com/categories/`
+
+Listing all categories.
+
+----------------------------------------------------------------
+
+
+##### Dataset source: 
+https://www.kaggle.com/rounakbanik/ted-talks
--- server.js
+++ server.js
@@ -1,38 +1,144 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import tedData from './data/ted.json';
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-mongo';
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const port = process.env.PORT || 8080;
+const app = express();
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+// Middleware --------------------------------
+app.use(cors());
+app.use(bodyParser.json());
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+// Models ------------------------------------
+const Talk = new mongoose.model('Talk', {
+  description: String,
+  event: String,
+  main_speaker: String,
+  name: String,
+  speaker_occupation: String,
+  tags: Array,
+  url: String,
+});
 
-// Start defining your routes here
+const Event = new mongoose.model('Event', {
+  name: String,
+});
+
+// To reset and populate database ------------
+if (process.env.RESET_DATABASE) {
+  const populateDatabase = async () => {
+    await Talk.deleteMany();
+    await Event.deleteMany();
+
+    tedData.forEach((item) => {
+      item.tags = JSON.parse(item.tags);
+      const newTalk = new Talk(item);
+      newTalk.save();
+    });
+
+    // Remove all dublicate events and create a new array
+    const allEvents = Array.from(new Set(tedData.map((talk) => talk.event)));
+    allEvents.forEach((item) => {
+      const newEvent = new Event({ name: item });
+      newEvent.save();
+    });
+  };
+  populateDatabase();
+}
+
+// Routes ----------------------------------------------------------------
 app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+  res.json(
+    'Welcome to TED-talks API - by Karin! Read full documentation here: 👉 https://github.com/karinnordkvist/karinnordkvist-Technigo-17-20-Mongo-API/blob/master/Documentation.md.'
+  );
+});
+
+// TALKS ---------------------------------------
+app.get('/talks', (req, res) => {
+  const queriedTalks = req.query.search;
+  const queryRegex = new RegExp(queriedTalks, 'i');
+  Talk.find({ name: queryRegex }, (err, data) => {
+    if (err) {
+      res.status(400).json({ error: 'Invalid request' });
+    } else {
+      res.json({ 'Amount of talks': data.length, data: data });
+    }
+  });
+});
+
+// TALKS BY ID --------------------------------
+app.get('/talks/:id', (req, res) => {
+  Talk.findById(req.params.id, (err, data) => {
+    if (err) {
+      res.status(404).json({
+        error: `Could not find any talk with the id ${req.params.id}`,
+      });
+    } else {
+      res.json(data);
+    }
+  });
+  //
+});
+// EVENTS ------------------------------------
+app.get('/events', (req, res) => {
+  Event.find({}, (err, data) => {
+    if (err) {
+      res.status(404).json({
+        error: `Error`,
+      });
+    } else {
+      res.json({ 'Amount of events': data.length, data: data });
+    }
+  });
+});
+
+// SPEAKERS --------------------------------
+app.get('/speakers', (req, res) => {
+  Talk.find({}, (err, data) => {
+    if (err) {
+      res.status(404).json({
+        error: `Error`,
+      });
+    } else {
+      const allSpeakers = Array.from(
+        new Set(data.map((talk) => talk.main_speaker))
+      );
+      res.json({ 'Amount of speakers': allSpeakers.length, data: allSpeakers });
+    }
+  });
+});
+
+// CATEGORIES --------------------------------
+app.get('/categories', (req, res) => {
+  Talk.find({}, (err, data) => {
+    if (err) {
+      res.status(404).json({
+        error: `Error`,
+      });
+    } else {
+      const allCategories = Array.from(
+        new Set(
+          [].concat.apply(
+            [],
+            data.map((talk) => talk.tags)
+          )
+        )
+      );
+      res.json({
+        'Amount of categories': allCategories.length,
+        data: allCategories,
+      });
+    }
+  });
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
