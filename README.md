825749d6198ce4050c70ae973e999bbe766922d4
JESSICA PANDITHA <JESSICA.PANDITHA@GMAIL.COM>
2020-12-17 19:41:20 +0100

    completed project and updated readme

--- README.md
+++ README.md
@@ -1,13 +1,36 @@
 # Mongo API Project
 
-Replace this readme with your own information about your project. 
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+This project was an introduction to Mongo DB and Mongoose. This is the first time we stored our data in a database. Then it was about creating RESTful routes to display the data.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+Having imported the data I set up the database in Mongo DB compass. Then the model Movie was created to handle each item in the data array. Different routes were created to show filtered data. Then I added some error handling in case the user entered invalid entries. Models in Mongoose were used for data and also Mongoose methods like find, findOne. Async and await was used to handle asynchronous operations. Path parameters and query parameters were used for the different endpoints.If I had more time I would have created another model and connected them.
+
+## Endpoints
+
+• Root: /
+
+• movies: /movies (This gives data about all the nominated movies at Golden Globe)
+
+• movies: nominee/nominee (Choose nominee to only get that specific nominated movie)
+
+The following endpoints can be combined to filter even more:
+
+• year_film: /movies?year_film=(Choose a film released year to get all the movies released that year)
+
+• year_award: /movies?year_award=(Choose an award year to get all the movies of that award year)
+
+• ceremony: /movies?ceremony=(Choose ceremony number to get all the movies for that specific ceremony)
+
+• category: /movies?category=(Choose category to get all the movies for that specific category)
+
+• nominee: /movies?nominee=(Choose name of nominee to get that specific movie)
+
+• film: /movies?film=(Choose name of movie to get that movie)
+
+• win: /movies?win=(Choose true or false to get movies that won/didn't win)
+
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+https://golden-globes.herokuapp.com/
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
  
-import goldenGlobesData from './data/golden-globes.json'
+import goldenGlobesData from './data/golden-globes.json';
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -16,7 +16,7 @@ mongoose.Promise = Promise;
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+//Middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -30,10 +30,13 @@ const Movie = new mongoose.model('Movie', {
   win: Boolean
 });
 
+//To avoid that the data duplicates everytime you seed the database, we have to do deleteMany before you seed the database.
+//Here we say if we reset the database by doing RESET_DB=true npm run dev, then delete the data and seed the database.
 if (process.env.RESET_DB) {
   const seedDatabase = async () => {
     await Movie.deleteMany();
 
+//Here we take all the data from the whole array and for each item we create a Movie model
     goldenGlobesData.forEach(item => {
       const newMovie = new Movie(item);
       newMovie.save();
@@ -42,29 +45,40 @@ if (process.env.RESET_DB) {
   seedDatabase();
 }
 
-
 // Routes
 app.get('/', (req, res) => {
   res.send('Welcome to Golden Globes API where you can find information about all the movies nominated')
 });
 
-//This returns all movie nominations
+//This returns all movie nominations. Then we have a universal query param to filter based on any property.
+//For example you can search http://localhost:8080/movies?win=true&year_film=2009 and get all the movies that won for films released in 2009
 app.get('/movies', async (req, res) => {
-  const queryParameters = req.query;
   const allMovies = await Movie.find(req.query);
-  res.json(allMovies);
+
+  if (allMovies.length > 0) {
+    res.json(allMovies);
+  } else {
+    res.status(400).json({ error: 'Invalid entry' });
+  }
 });
 
 //This returns one single movie based on the name of the nomineed movie
 app.get('/movies/nominee/:nominee', async (req, res) => {
-  const { nominee } = req.params;
-
   const singleNominee = await Movie.findOne({ nominee: req.params.nominee });
-  
-  res.json(singleNominee);
+
+  if (singleNominee) {
+    res.json(singleNominee);
+  } else {
+    res.status(404).json({ error: 'Invalid nominee'});
+  }
 });
 
 // Starting the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+// For this setup to work, two Config Vars need to be added in Heroku:
+// MONGO_URL should be equal to the connection string generated in MongoDB Cloud Atlas
+// for this project. The PW, the user name & test should be replaced in the link from MongoDB.
+// RESET_DB with value true 
