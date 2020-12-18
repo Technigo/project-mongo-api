7bd56941ad17b15684d6b6698d005a867ed40ba1
Sara Sundblom <sundblomsara@gmail.com>
2020-12-18 17:26:55 +0100

    error message and regexp for case insensitive

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+const ERROR_404 = "Sorry, can't find what you are looking for, please try again"
+
 
 // when searching for artists- all artists-
 const TopSong = mongoose.model('TopSong', {
@@ -63,24 +65,37 @@ app.get('/topsongs', async (req, res) => {
 })
 
 
-
-
-app.get('/topsongs/:artist', async (req, res) => {
-  const queryParameters = req.query
-  const findArtist = await TopSong.find({ artistName: req.params.artist })
-  res.json(findArtist)
+app.get('/topsongs/artist/:artist', async (req, res) => {
+  const artistRegexp = new RegExp(req.params.artist, "i")
+  const findArtist = await TopSong.find({ artistName: artistRegexp })
+  if (findArtist.length === 0) {
+    res.status(404).json(ERROR_404)
+  } else {
+    res.json(findArtist)
+  }
 })
 
-// app.get('/topsongs/track/:track', async (req, res) => {
-//   const findTrack = await TopSong.find({ trackName: req.query.track })
-//   res.json(findTrack)
-// })
+app.get('/topsongs/track/:track', async (req, res) => {
+  const trackRegexp = new RegExp(req.params.track, "i")
+  const findTrack = await TopSong.find({ trackName: trackRegexp })
+  if (findTrack.length === 0) {
+    res.status(404).json(ERROR_404)
+  } else {
+    res.json(findTrack)
+  }
+}) 
+
+// Prio : 1, Make error massage! 2, more endpoints
 
 // {being able to search for one artist
 // being able to search for one track
+
 // able to search for songs based on 'danceability' - up-tempo or slow. 
 // search based on genre? }
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
+
+
+// do a map over all the artist so it returns a list og all artists in the list. 
