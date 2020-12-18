3b45f958f9790ada192cec95f0464b13d60bd519
MindstormingAB <s.elander@mindstorming.se>
2020-12-18 12:52:59 +0100

    Testing endpoints from week 17

--- server2.js
+++ server2.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 
 import spotifyData from "./data/spotify-releases.json";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-spotify"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-spotify-v2"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
@@ -13,7 +13,7 @@ mongoose.Promise = Promise
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 9000
+const port = process.env.PORT || 9100
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
@@ -22,26 +22,47 @@ app.use(bodyParser.json())
 
 const Release = new mongoose.model("Release", {
   album_type: String,
-  artists: [{ external_urls: { spotify: String }, href: String, id: String, name: String, artist_type: String, uri: String }],
-  available_markets: [String],
+  // artists: [{ external_urls: { spotify: String }, name: String }],
+  artists: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Artist"
+    }
+  ],
   external_urls: { spotify: String },
-  href: String,
-  id: String,
   images: [{ height: Number, url: String, width: Number }],
   name: String,
   release_date: String,
-  release_date_precision: String,
-  total_tracks: Number,
-  type: String,
-  uri: String
+  total_tracks: Number
+});
+
+const Artist = new mongoose.model("Artist", {
+  external_urls: { spotify: String },
+  name: String
 });
 
 if (process.env.RESET_DATABASE) {
   const populateDatabase = async () => {
+    await Artist.deleteMany();
     await Release.deleteMany();
 
-    spotifyData.forEach(item => {
-      new Release(item).save();
+    // let artists = [];
+
+    // const spotifyArtists = spotifyData.map(item => item.artists.map(artist =))
+    spotifyData.forEach(async item => {
+      item.artists.forEach(item => {
+        const newArtist = new Artist(item);
+        // artists.push(newArtist);
+        await newArtist.save();
+      })
+    })
+
+    spotifyData.forEach(async releaseItem => {
+      const newRelease = new Release({
+        ...releaseItem,
+        // artists: artists.find(artist => artist.name === releaseItem.)
+      })
+      new newRelease.save();
     })
   }
   populateDatabase();
@@ -53,7 +74,7 @@ const ERROR_RELEASES_NOT_FOUND = { error: "No release matched your request" };
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Welcome to the Spotify Releases API")
-});
+})
 
 // This route will return a collection of releases
 app.get("/releases", async (req, res) => {
@@ -66,35 +87,7 @@ app.get("/releases", async (req, res) => {
       releases: allReleases
     });
   }
-});
-
-// This route will return a single release based on id
-app.get("/releases/:id", async (req, res) => {
-  const release = await Release.findOne({ id: req.params.id });
-  if (!release) {
-    res.status(404).json(ERROR_RELEASES_NOT_FOUND);
-  } else {
-    res.json(release);
-  }
-});
-
-// This route will return a collection of releases for the specified artist
-app.get("/releases/artist/:artist", async (req, res) => {
-  const { artist } = req.params;
-  const singleArtist = await Release.find({ name: artist })
-  console.log(singleArtist);
-  // const filteredReleases = spotifyData.filter(
-  //   (item) => item.artists.map((artist) => artist.name).toString() === artist
-  // );
-  // if (filteredReleases.length === 0) {
-  //   res.status(404).json(ERROR_RELEASES_NOT_FOUND);
-  // } else {
-  //   res.json({
-  //     total: filteredReleases.length,
-  //     releases: filteredReleases
-  //   });
-  // };
-});
+})
 
 // Start the server
 app.listen(port, () => {
