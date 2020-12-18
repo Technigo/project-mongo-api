7361c0c44eeb4d4758e1efd47e28606e67772299
Sara Stjernberg <sara_stjernberg@hotmail.com>
2020-12-18 14:51:51 +0100

    try and catch

--- server.js
+++ server.js
@@ -16,18 +16,25 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body pa rsing
+// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
+//Middleware to handle connections errors
+app.use((req, res, next) => {
+  try {
+    if (mongoose.connection.readyState === 1) {
+      next()
+    } else {
+      res.status(503).json({ error: 'Service unavailable' });
+    }
+  } catch (error) {
+    res.status(400).json({ error: 'Error! Could not access the server.' });
+  }
+})
 
 const Book = new mongoose.model('Book', {
   bookID: Number,
@@ -53,24 +60,49 @@ if (process.env.RESET_DATABASE) {
 }
 
 
-// ROUTES
 app.get('/', (req, res) => {
   res.send('Welcom to my book-API')
 })
 
+//get all books
 app.get('/books', async (req, res) => {
-  const queryParameters =  req.query
-  console.log(queryParameters)
-
-  const allBooks = await Book.find(queryParameters);
+  const allBooks = await Book.find(req.query)
   res.json(allBooks)
 })
 
+//get a book based on id
 app.get('/books/:id', async (req, res) => {
-  const { id } = req.params;
-  const singleBook = await Book.find({ bookID: +id })
-  
-  res.json(singleBook);
+  try {
+    const singleBook = await Book.findOne({ bookID: req.params.id })
+
+    if (singleBook) {
+      res.json(singleBook)
+    } else {
+      res.status(404).json({ error: 'Book not found' })
+    }
+  } catch (error) {
+    res.status(400).json({ error: 'Invalid book-id' })
+  }
+})
+
+//get books from a chosen author
+app.get('/books/authors/:author', async (req, res) => {
+  const booksByAuthor = await Book.find({ authors: req.params.author })
+  if (booksByAuthor.length > 0) {
+    res.json(booksByAuthor)
+  } else {
+    res.status(404).json({ error: "Invalid author" })
+  }
+})
+
+//get a book with a certain title
+app.get('/books/titles/:title', async (req, res) => {
+  const bookTitle = await Book.findOne({title: req.params.title})
+  if (bookTitle) {
+    res.json(bookTitle); 
+  }else{
+    res.status(404).json({ error: "Invalid title" })  
+}
 })
 
 // Start the server
