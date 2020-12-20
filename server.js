ef3b0d99948052ebe7022a92ca495bde2417eaf3
Sara Heijkenskjöld <sara.heijkenskjold@gmail.com>
2020-12-20 23:47:32 +0100

    Added query and endpoint

--- server.js
+++ server.js
@@ -100,13 +100,22 @@ app.get('/', (request, response) => {
 // /eruptions endpoint
 // RETURNS: A collection of all eruptions from MongoDB as an array
 //
+// PARAMETERS:
+// - page (default: 1): a number indicating what page number to show
+//    usage: /eruptions/?page=1
+// - region: a string
+//    usage: /eruptions?region=Antarctica
 app.get('/eruptions', async (request, response) => {
-  const { page } = request.query;
+  const { page, region } = request.query;
   const pageNumber = +page || 1;
   const pageSize = 20;
   const skip = pageSize * (pageNumber - 1);
-  const totalEruptions = await Eruption.find();
-  const allEruptions = await Eruption.find()
+  const totalEruptions = await Eruption.find({
+    region: new RegExp(region, 'i'),
+  });
+  const allEruptions = await Eruption.find({
+    region: new RegExp(region, 'i'),
+  })
     .populate('type')
     .limit(pageSize)
     .skip(skip);
@@ -117,6 +126,7 @@ app.get('/eruptions', async (request, response) => {
     currentPage: `${pageNumber}/${Math.ceil(totalEruptions.length / pageSize)}`,
     results: allEruptions,
   };
+
   if (allEruptions.length === 0) {
     response.status(404).send({ error: 'Not found' });
   } else {
@@ -132,8 +142,6 @@ app.get('/eruptions', async (request, response) => {
 //    usage: /eruptions/390847
 //
 app.get('/eruptions/:id', async (request, response) => {
-  const { id } = request.params;
-
   try {
     const singleVolcano = await Eruption.findById(request.params.id);
     if (singleVolcano) {
@@ -141,19 +149,77 @@ app.get('/eruptions/:id', async (request, response) => {
     } else {
       response
         .status(404)
-        .send({ error: `No volcano with id ${id} was found` });
+        .send({ error: `No volcano with id ${request.params.id} was found` });
     }
   } catch {
-    response.status(400).send({ error: `${id} is not a valid id` });
+    response
+      .status(400)
+      .send({ error: `${request.params.id} is not a valid id` });
   }
 });
 
 // /types endpoint
 // RETURNS: A collection of all types from volcanic-eruptions.json as an array
 //
+// PARAMETERS:
+// - name: a string
+//    usage: /types?type=Stratovolcano
+//
 app.get('/types', async (request, response) => {
-  const allTypes = await Type.find();
-  response.json(allTypes);
+  const types = await Type.find({
+    type: new RegExp(request.query.type, 'i'),
+  });
+
+  if (types) {
+  }
+  response.json({
+    totalNumberOfTypes: types.length,
+    result: types,
+  });
+});
+
+// /types/:id endpoint
+// RETURNS: One unique item
+//
+// PARAMETERS:
+// - id: a unique number
+//    usage: /types/390847
+//
+app.get('/types/:id', async (request, response) => {
+  try {
+    const singleType = await Type.findById(request.params.id);
+    if (singleType) {
+      response.json(singleType);
+    } else {
+      response
+        .status(404)
+        .send({ error: `No type with id ${request.params.id} was found` });
+    }
+  } catch {
+    response
+      .status(400)
+      .send({ error: `${request.params.id} is not a valid id` });
+  }
+});
+
+// /types/:id/eruptions
+app.get('/types/:id/eruptions', async (request, response) => {
+  const type = await Type.findById(request.params.id);
+
+  if (type) {
+    const eruptions = await Eruption.find({
+      type: mongoose.Types.ObjectId(type.id),
+    }).populate('type');
+    response.json({
+      totalNumberOfEruptions: eruptions.length,
+      result: eruptions,
+    });
+  } else {
+    response.status(404).json({
+      error: `No eruptions found for type with id ${request.params.id}`,
+    });
+  }
+  Type.find({ type: type }).populate('type');
 });
 
 // Start the server
