11e8a266d4a1878d30e3d5b1b2ee2bbf071bd837
Sabina de Maré <62811005+Snabbina@users.noreply.github.com>
2020-12-20 09:09:02 +0100

    Made route to show title, and connection to Atlas, also tried to deploy to Heroku.

--- server.js
+++ server.js
@@ -1,97 +1,70 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
 
+import booksData from "./data/books.json";
 
-import booksData from './data/books.json'
+//Author model and connection to database
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-books";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
+const port = process.env.PORT || 8080;
+const app = express();
 
-//Author model and connection to database 
-//database
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-books"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
-
-const port = process.env.PORT || 8080
-const app = express()
-
-
-//defining models
-const Author = mongoose.model('Author', {
+//A model
+const Author = mongoose.model("Author", {
   title: String,
   authors: String,
-  isbn: Number
-})
+  isbn: Number,
+});
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
-
+app.use(cors());
+app.use(bodyParser.json());
 
 if (process.env.RESET_DATABASE) {
+  const seedDatabase = async () => {
+    await Author.deleteMany();
 
-const seedDatabase = async () => {
-  await Author.deleteMany()
-
-
-  booksData.forEach (item => {
-       const newAuthor = new Author(item)
-         newAuthor.save()
-        }) 
- 
-  }
-  seedDatabase()
+    booksData.forEach((item) => {
+      const newAuthor = new Author(item);
+      newAuthor.save();
+    });
+  };
+  seedDatabase();
 }
 
-// Routes here
-app.get('/', (req, res) => {
-  res.send('Welcome, this is the restfull Authors Backend')
-})
+// Routes starts here
+app.get("/", (req, res) => {
+  res.send("Welcome, this is the database full of great authors and books");
+});
 
+//Get all authors
+app.get("/authors", async (req, res) => {
+  const allAuthors = await Author.find(req.query);
+  console.log(allAuthors);
 
-app.get('/authors', async (req, res) => {
-  const allAuthors = await Author.find(req.query)
-   console.log(allAuthors)
-
-   if (allAuthors.length > 0) {
-    res.json(allAuthors)
+  if (allAuthors.length > 0) {
+    res.json(allAuthors);
   } else {
-    res.status(404).json({ error: 'Sorry, could not find the data'})
+    res.status(404).json({ error: "Sorry, could not find the data" });
   }
-  
-})
+});
 
-// //get singel author
-// app.get('/authors/author/:id', async (req, res) => {
-//   const singleAuthor = await Author.findOne({id: req.params.id})
-//   if (singleAuthor) {
-//     res.json(singleAuthor)
-//   } else {
-//     res.status(404).json({ error: 'Sorry, could not find the author' })
-//   }
-  
-})
+//Get a single title for an author
+app.get("/authors/title/:title", async (req, res) => {
+  const singleTitle = await Author.findOne({ title: req.params.title });
 
-// app.get('/books', async (req, res) => {
-//   const books = await Book.find().populate('author')
-//   res.json(books)
-  
-// })
-
-// app.get('/authors/:id/books', async (req, res) => {
-//   const author = await Author.findById(req.params.id)
-//   if (author){
-//     const books = await Book.find({ author: mongoose.Types.ObjectId(author.id)})
-//   res.json(books)
-//   } else {
-//     res.json(404).json({ error })
-//   }
-  
-// })
-
+  if (singleTitle) {
+    res.json(singleTitle);
+  } else {
+    res.status(404).json({ error: "Sorry, could not find the title" });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
