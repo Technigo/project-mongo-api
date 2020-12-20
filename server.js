02a5cbaeccdc29b7c44ce01f63021237b1574b9e
JOHANNA REXIN <johanna.rexin@gmail.com>
2020-12-20 19:49:59 +0100

    First push

--- server.js
+++ server.js
@@ -3,23 +3,41 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import topMusicData from './data/top-music.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const Artist = mongoose.model('Artist', {
+  id: Number,
+  trackName: String,
+  artistName: String,
+  genre: String,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  loudness: Number,
+  liveness: Number,
+  valence: Number,
+  length: Number,
+  acousticness: Number,
+  speechiness: Number,
+  popularity: Number
+})
+
+if (process.env.RESET_DATABASE) {
+  const seedDatabase = () => {
+    Artist.deleteMany()
+  
+    topMusicData.forEach(item => {
+      const newArtist = new Artist(item)
+      newArtist.save()
+    })
+  }
+  seedDatabase()
+}
+
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -27,9 +45,33 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
+
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Hello!')
+})
+
+app.get('/artists', async (req, res) => {
+  const artists = await Artist.find()
+  res.json(artists)
+})
+
+// Endpoint 1, all the data with 50 popular songs on Spotify 
+app.get('/songs', (req, res) => {
+  res.json(topMusicData)
+})
+
+// Endpoint 2, sorting the data on music genre
+app.get('/genre/:genre', (req, res) => {
+  const genre = req.params.genre
+  const songGenre = topMusicData.filter((item) => item.genre === genre)
+  res.json(songGenre)
+})
+
+// Endpoint 3, showing only one song identified by id
+app.get('/songs/:id', (req, res) => {
+  const id = req.params.id
+  const singleSong = topMusicData.find((song) => song.id === +id)
+    res.json(singleSong)
 })
 
 // Start the server
