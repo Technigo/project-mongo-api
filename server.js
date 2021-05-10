300396bca5664c2fdebeb64d103761b5ce173c54
Anna Honkonen <ak.honkonen@gmail.com>
2021-05-10 10:21:17 +0300

    installed depencies, set my own dataset, started working with mongoose by doing a mongoose model and adding one exercise

--- server.js
+++ server.js
@@ -1,35 +1,59 @@
+/* eslint-disable linebreak-style */
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import exercisesData from './data/exercises.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-mongo'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
+const Exercise = mongoose.model('Exercise', {
+  exerciseID: Number,
+  name: String,
+  category: String,
+  muscle_group: String,
+  target_muscle: String,
+  instructions: String,
+  img: String
+})
+
+Exercise.deleteMany().then(() => {
+  new Exercise({
+    exerciseID: 112,
+    name: 'testing',
+    category: 'testing',
+    muscle_group: 'testing',
+    target_muscle: 'testing',
+    instructions: 'testing',
+    img: 'testing'
+  }).save()
+})
+
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  Exercise.find().then((exercises) => {
+    res.json(exercises)
+  })
+})
+
+app.get('/:name', (req, res) => {
+  Exercise.findOne({ name: req.params.name }).then((exercise) => {
+    if (exercise) {
+      res.json(exercise)
+    } else {
+      res.status(404).json({ error: 'Not found' })
+    }
+  })
 })
 
 // Start the server
