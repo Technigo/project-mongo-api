54984ccc7c0d13e43284324e7d8ad5151f3de0ad
Ann-Sofi Jönsson <annsofi.jonsson@hotmail.se>
2021-05-10 15:10:32 +0200

    added seedDatabase, Bookmodel, 2 endpoints

--- server.js
+++ server.js
@@ -1,22 +1,50 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import booksData from './data/books.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// const Author = mongoose.model('Author', {
+//   name: String
+// })
+
+// const bookSchema = new mongoose.Schema({
+//   authors: String,
+//   title: String,
+//   language_code: String,
+//   num_pages: Number,
+//   average_rating: Number,
+//   isbn: Number
+// })
+
+const Book = mongoose.model('Book', {
+  bookID: Number,
+  title: String,
+  authors: String,
+  average_rating: Number,
+  // är isbn en string??
+  // isbn: Number,
+  language_code: String,
+  num_pages: Number,
+  ratings_count: Number,
+  text_reviews_count: Number
+})
+
+if (process.env.RESET_DB) {
+  console.log('resetting database')
+
+  const seedDatabase = async () => {
+    await Book.deleteMany();
+    booksData.forEach((book) => new Book(book).save())
+  }
+  seedDatabase()
+}
+
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
@@ -25,11 +53,27 @@ const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json());
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Hello')
+})
+
+// endpoint to get all books
+app.get('/books', async (req, res) => {
+  const books = await Book.find()
+  res.json(books)
+})
+
+// endpoint to get a single book by id
+app.get('/books/:id', async (req, res) => {
+  const book = await Book.findById(req.params.id)
+  if (book) {
+    res.json(book)
+  } else {
+    res.status(404).json({ error: 'Book not found' })
+  }
 })
 
 // Start the server
