87d30590f7a790afa7cde8d1c7b0b75d73f0ef93
Ann-Sofi Jönsson <annsofi.jonsson@hotmail.se>
2021-05-10 22:53:41 +0200

    Made endpoint for author, textreviews and fixed id endpoint

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import listEndpoints from 'express-list-endpoints'
 
 import booksData from './data/books.json'
 
@@ -26,8 +27,7 @@ const Book = mongoose.model('Book', {
   title: String,
   authors: String,
   average_rating: Number,
-  // är isbn en string??
-  // isbn: Number,
+  isbn: String,
   language_code: String,
   num_pages: Number,
   ratings_count: Number,
@@ -57,7 +57,7 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello')
+  res.send(listEndpoints(app))
 })
 
 // endpoint to get all books
@@ -66,14 +66,60 @@ app.get('/books', async (req, res) => {
   res.json(books)
 })
 
-// endpoint to get a single book by id
+// endpoint to get a single book by id, obs id från compass, ej id från json
 app.get('/books/:id', async (req, res) => {
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    res.status(404).json({ error: 'Invalid id' })
+    return;
+  }
+
   const book = await Book.findById(req.params.id)
   if (book) {
     res.json(book)
   } else {
     res.status(404).json({ error: 'Book not found' })
   }
+})
+
+
+// endpoint to get the 20 books with most textreviews, kommer från förra veckans kod
+// app.get('/textreviews', (req, res) => {
+//   const books = booksData.sort((a, b) => b.text_reviews_count - a.text_reviews_count);
+//   res.json(books.slice(0, 20));
+// })
+
+app.get('/textreviews', async (req, res) => {
+  let books = await Book.find()
+  books = books.sort((a, b) => b.text_reviews_count - a.text_reviews_count);
+  res.json(books.slice(0, 20));
+})
+
+// denna funkar inte
+app.get('/author/:author', async (req, res) => {
+  const { author } = req.params
+  const queryRegex = new RegExp(author, 'i')
+  const books = await Book.find({ authors: queryRegex })
+
+  // let books = await Book.find();
+  // books = books.filter((book) => book.authors.toLowerCase().includes(author.toLowerCase()))
+
+  if (books.length) {
+    res.json(books)
+  } else {
+    res.status(404).json({ error: 'Author not found' })
+  }
+})
+
+app.get('/books', async (req, res) => {
+  const { title, author } = req.query
+  const titleRegex = new RegExp(title, 'i')
+  const authorRegex = new RegExp(author, 'i')
+
+  const books = await Book.find({
+    title: titleRegex,
+    authors: authorRegex
+  })
+  res.json(books)
 })
 
 // Start the server
