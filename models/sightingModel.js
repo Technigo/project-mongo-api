126e64e5847966b2bbd1c4da39007d1493882022
Bellsibub <isabella.billgren@gmail.com>
2021-05-10 16:36:25 +0200

    project setup

---
+++ models/sightingModel.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const Sighting = mongoose.model('Sighting', {
+  date: String,
+  city: String,
+  state: String,
+  country: String,
+  shape: String,
+  duration_seconds: Number,
+  duration_description: String,
+  comments: String,
+  date_posted: String,
+  position: {
+    latitude: Number,
+    longitude: Number
+  }
+});
+
+export default Sighting;
--- server.js
+++ server.js
@@ -1,39 +1,34 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import router from './routes';
+// import data from './data/ufoSightings.json'
+// import Sighting from './models/sightingModel'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/ufo-sightings';
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+// if (process.env.RESET_DB) {
+//   const seedDatabase = async () => {
+//     await Sighting.deleteMany({})
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+//     data.forEach((d) => {
+//       new Sighting(d).save()
+//     })
+//   }
+
+//   seedDatabase()
+// }
+
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
-
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.use(cors());
+app.use(express.json());
+// Add router module
+app.use('/', router);
 
 // Start the server
-app.listen(port, () => {
-  // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+app.listen(port, () => {});
