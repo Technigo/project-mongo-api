07dff5c3c24f7847d390a9d302f2a787b512692f
Sofarz <sofia.wallen@gmail.com>
2021-05-11 19:41:33 +0200

    define first model. Create seedDB, wrap in environment variable. Create first endpoint.

--- server.js
+++ server.js
@@ -1,35 +1,58 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import booksData from './data/books.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+// Defining models 
+const bookSchema = new mongoose.Schema({
+   // bookID: {
+  // },
+  title: String,
+  authors: String,
+  average_rating: Number,
+  isbn: String,
+  language_code: String,
+  num_pages: Number,
+  ratings_count: Number
+})
+
+const Book = mongoose.model('Book', bookSchema)
+
+// The seedDB is going to re-run everytime we start the server, if we don't want that
+// to happen and want to be able to choose when that should happen, we can wrap it
+// in an environment variable
+if (process.env.RESET_DATABASE) {
+  const seedDB = async () => {
+    await Book.deleteMany()
+
+    await booksData.forEach(item => {
+      const newBook = new Book(item)
+      newBook.save()
+    })
+  }
+  seedDB()
+}
+
 const port = process.env.PORT || 8080
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Hello World')
+})
+
+app.get('/books', async (req, res) => {
+  const books = await Book.find()
+  res.json(books)
 })
 
 // Start the server
