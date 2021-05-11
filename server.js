2fc6f289b1ccafb3da81680da14732288c4681ed
Aepera <astridepersson@gmail.com>
2021-05-11 22:56:58 +0200

    Model created. Seed DB with data. Route with all titles

--- server.js
+++ server.js
@@ -1,35 +1,58 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
 // import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
+import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
+const NetflixTitle = mongoose.model('NetflixTitle', {
+  show_id: Number,
+  title: String,
+  director: String,
+  cast: String,
+  country: String,
+  date_added: String,
+  release_year: Number,
+  rating: String,
+  duration: String,
+  listed_in: String,
+  description: String,
+  type: String
+})
+
+if (process.env.RESET_DB) {
+  const seedDB = async () => {
+    await NetflixTitle.deleteMany()
+    await netflixData.forEach(item => {
+      const newTitle = new NetflixTitle(item)
+      newTitle.save()
+    })
+  }
+  seedDB()
+}
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/titles', async (req, res) => {
+  const titles = await NetflixTitle.find()
+  res.json(titles)
 })
 
 // Start the server
