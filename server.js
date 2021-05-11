8aa4810724d80835419f2e1d1cba58c487278375
“[ida <[ida.t.sylven@gmail.com]>
2021-05-11 22:13:06 +0200

    Made my own data.json-file. Created Mongoose model, seedDB and inital endpoint

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import designers from './data/designers.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+
+const Designer = mongoose.model('Designer', {
+  name: String,
+  surname: String,
+  wasBorn: Number,
+  memberOfChambreSyndicaleDeLaHauteCouture: Boolean
+})
+
+if (process.env.RESET_DB) {
+  const seedDB = async () => {
+    await Designer.deleteMany()
+    await designers.forEach(item => {
+      new Designer(item).save()
+    })
+  }
+  seedDB()
+}
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -25,11 +33,16 @@ const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/designers', async (req, res) => {
+  const designers = await Designer.find()
+  res.json(designers)
 })
 
 // Start the server
