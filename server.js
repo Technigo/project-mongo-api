893d8bc2e79835e65950f2cfc1de6a7884914255
Maria Pogosjan <Maria.pogosjan@gmail.com>
2021-05-11 22:57:07 +0200

    added Schema, Model and created routes

--- server.js
+++ server.js
@@ -1,38 +1,91 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import netflixData from './data/netflix-titles.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const movieSchema = new mongoose.Schema({
+  show_id: Number,
+  title: String,
+  director: String,
+  cast: String,
+  country: String, 
+  date_added: String,
+  release_year: Number,
+  rating: String, 
+  duration: String, 
+  listed_in: String,
+  description: String, 
+  type: String
+})
+
+const Movie = mongoose.model('Movie', movieSchema)
+
+// Things to read on: reset_db, new, save, two id:s
+if (process.env.RESET_DB) {
+  const seedDB = async () => {
+    await Movie.deleteMany()
+    await netflixData.forEach((show) => {
+      const newMovie = new Movie(show)
+      newMovie.save()
+    })
+  }
+  seedDB()
+}
+
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
-// Start the server
+// OK
+app.get('/movies', async (req, res) => {
+  const movies = await Movie.find()
+  res.json({ length: movies.length, data: movies })
+})
+
+// OK
+app.get('/titles', async (req, res) => {
+  const title = await Movie.find()
+  const filterTitles = title.map((item) => item.title)
+  const sortTitles = filterTitles.sort()
+
+  res.json({ length: sortTitles.length, data: sortTitles })
+})
+
+// don't get console log in terminal? How can I split?
+app.get('/genres', async (req, res) => {
+  const genre = await Movie.find()
+  const filterGenres = genre.map((item) => item.listed_in)
+  let genresUnique = []
+  
+  filterGenres.forEach((item) => {
+    if (!genresUnique.includes(item)) {
+      genresUnique.push(item)
+    } 
+  })
+  res.json({ length: genresUnique.length, data: genresUnique })
+})
+
+// How to do with two ID:s? Error message dosen't work.
+app.get('/movies/:id', async (req, res) => {
+  const movie = await Movie.findById(req.params.id)
+  if (movie) {
+    res.json({ data: movie })
+  } else {
+    res.status(404).json({ error: 'Not found!' })
+  } 
+})
+
 app.listen(port, () => {
   // eslint-disable-next-line
   console.log(`Server running on http://localhost:${port}`)
