ac00fb8f3585f1b3e9ce480d655de3920e1ed195
Ylva Landoff Lindberg <ylvalandofflindberg@Ylvas-MBP-2.lan>
2021-05-11 16:28:50 +0200

    Added database, mongoose.Shema(model) 2 endpoints

--- server.js
+++ server.js
@@ -1,35 +1,80 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+
+import netflixData from './data/netflix-titles.json'
+
+
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
+const showSchema = new mongoose.Schema({
+  title: String,
+  director: String,
+  release_year: Number,
+  type: String,
+  duration: String,
+  description: String
+})
+
+const Show = mongoose.model('Show', showSchema);
+
+// const newShow = new Show ({
+//   title: 'JASON show',
+//   director: 'Ylva',
+//   release_year: 2021,
+//   type: 'TV Show',
+//   duration: '120 min',
+//   description: 'Best show ever'
+// })
+
+// newShow.save()
+
+//TO USE THIS, TYPE: RESET_DB=true npm run dev IN TERMINAL
+if (process.env.RESET_DB) {
+const seedDB = async () => {
+  await ShowData.deleteMany();
+
+  await netflixData.forEach(item => {
+    const newShow = new Show(item)
+    newShow.save();
+  });
+}
+seedDB();
+}
+
+
+
+
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8082
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+// Add middlewares to enable cors
+app.use(cors()) 
+app.use(express.json())
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+
+// // Start defining your routes here
+// app.get('/', (req, res) => {
+//   Show.find().then(show => {
+//     res.json(show)
+//   })
+// })
+
+//http://localhost:8082/list
+
+app.get('/list', async (req, res) => {
+  const show = await Show.find();
+  res.json(show)
+})
+
+app.get('/list/series', async (req, res) =>{
+  const data = await Show.find({ type: 'TV Show'})
+  res.json(data)
 })
 
 // Start the server
