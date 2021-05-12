d6eb35504c3ceaed4b73489ab4b599f14f86bd75
Åsa Hildebrand <asahildebrand@yahoo.com>
2021-05-12 21:30:28 +0200

    Started to build routes and endpoints

--- server.js
+++ server.js
@@ -1,36 +1,70 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import booksData from './data/books.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const bookSchema = new mongoose.Schema({
+  bookID: Number,
+  title: String,
+  authors: String,
+  average_rating: Number,
+  isbn: Number,
+  isbn13: Number,
+  language_code: String,
+  num_pages: Number,
+  ratings_count: Number,
+  text_reviews_count: Number
+})
+
+const Book = mongoose.model('Book', bookSchema)
+
+if (process.env.RESET_DB) {
+  const seedDataBase = async () => {
+    await Book.deleteMany()
+
+    await booksData.forEach(item => {
+      const newBook = new Book(item)
+      newBook.save()
+    })
+  }
+  seedDataBase()
+}
+
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+
+// Endpoint that shows all books
+app.get('/books', async (req, res) => {
+  const books = await Book.find()
+  res.json(books)
+})
+
+// Endpoint that shows a specific book
+app.get('/books/:id', async (req, res) => {
+  const { id } = req.params
+  const singleBook = await Book.findOne({ bookID: +id })
+
+  if (singleBook) {
+    res.json(singleBook)
+  } else {
+    res.status(404).json({ error: 'Could not find this book'})
+  }
+  
+})
+
 
 // Start the server
 app.listen(port, () => {
