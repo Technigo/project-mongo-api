5ef74cf418126f7cd45acf0a4ecc4719adbb46cc
Caroline Byström <frivridnadagar@gmail.com>
2021-05-12 17:28:51 +0200

    deal with lowercase in databases, add more endpoints

--- server.js
+++ server.js
@@ -15,12 +15,16 @@ mongoose.Promise = Promise
 const titlesSchema = new mongoose.Schema({
   //_id: { type: String, required: true }, // Added --->  make _id to a string. if not want to deal with _id and id?
   show_id: String,
-  type: String,
-  title: {
-    any: mongoose.Schema.Types.Mixed
-  },// can also be a number. --> earlier: String, // now:  ({ any: Schema.Types.Mixed })
-  director: { type: mongoose.Schema.Types.ObjectId, ref: 'Director' }, // []    or possible to add: name: "" ? 
-  cast: String,
+  type: String, 
+  title: String,// can also be a number. ({ any: Schema.Types.Mixed })
+  director: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Director',
+  },
+  cast: {
+    type: String,
+    lowercase: true
+  },
   country: String,
   date_added: String,
   release_year: Number,
@@ -45,19 +49,18 @@ if (process.env.RESET_DB) {
     await Title.deleteMany()
     await Director.deleteMany()
 
-    let directors = [] // or take away {} ?? 
+    let directors = []
 
-    netflixTitles.slice(0,100).forEach(async item => {
-      const director = new Director({"name": item.director}) // make object according to Director schema above
+    netflixTitles.slice(0,100).forEach(async item => {  // remove slice or change to 1000 objects or similar for deployment purpose
+      const director = new Director({"name": item.director}) 
       
       if (item.director != "") {
         directors.push(director)
-        await director.save()
+        await director.save()  
       } 
     })
 
-    // how to save title with type MIXED ?? 
-    netflixTitles.slice(0,100).forEach(async item => {
+    netflixTitles.slice(0,100).forEach(async item => { //remove slice or change to 1000 objects or similar for deployment purpose
       const title = new Title({
         ...item,
         director: directors.find(singleDirector => singleDirector.name === item.director)
@@ -91,31 +94,99 @@ app.get('/', (req, res) => {
 
 // return all titles
 app.get('/titles', async (req, res) => {
-  // v 1 universal version
+  // universal version
   const titles = await Title.find().populate('director') // populate title with director object
-  res.json(titles) // or netflixTitles
+  res.json({ length: titles.length, data: titles })
 })
+
+// query titles by names in cast ---- ? doesn't work right now. * 
+app.get('/titles/cast', async (req, res) => {
+  const { cast } = req.query
+
+  if (cast) {
+    console.log(cast) // cast is undefined
+    const cast = await Title.find({
+      cast: {
+        $regex: new RegExp(cast, "i") // 2 arguments: 1. the string w want to look for 2. i = check fr the string but don't care for case sensitivity
+      }
+    })
+    res.json({ length: cast.length, data: cast })
+    } else {
+      const cast = await Title.find()
+      res.json({ data: cast })           // What would be the else case for this? 
+    }
+  })
 
 // Return the id of one netflix title
 app.get('/titles/:id', async (req, res) => {
-  //console.log(mongoose.isValidObjectId(req.params.id)) // TRUE
-  const title = await Title.findById(req.params.id)
-  res.json(title)
+  const { id } = req.params
+
+  try {
+    const singleTitle = await Title.findById(id)
+    // const directorOfTitle = await Director.findbyId(title.director)
+    res.json({ data: singleTitle }) // , data: directorOfTitle
+  } catch(error) {
+    res.status(404).json({ error: 'ID not found' })
+  }
 })
 
-//60992b36fea8157986d330be
+// query director by director name
+app.get('/directors', async (req, res) => {
+  const { director } = req.query
 
+  if (director) {
+  const directors = await Director.find({
+    name: {
+      $regex: new RegExp(director, "i") // 2 arguments: 1. the string w want to look for 2. i = check for the string but don't care for case sensitivity
+    }
+  })
+  res.json({ length: directors.length, data: directors })
+  } else {
+    const directors = await Director.find()
+    res.json({ data: directors })           // What would be the else case for this? 
+  }
+})
+
+// id
 app.get('/directors/:id', async (req, res) => {
-  console.log(mongoose.isValidObjectId(req.params.id))
-  const director = await Director.findById(req.params.id)
+  // console.log(mongoose.isValidObjectId(req.params.id))
+  const { id } = req.params
 
-  res.json(director)
- /*  const title = await Title.find({ director: mongoose.Types.ObjectId(director.id) })
-  //if (director) {
-    res.json(title) */
-/*   } else {
-    res.status(404).json({ error: 'title not found'})
-  } */
+  try {
+    const oneDirector = await Director.findById(id)
+    res.json({ data: oneDirector })
+  } catch(error) {
+    res.status(404).json({ error: 'Director id not found' })
+  }
+})
+
+// return all titles of a specific director
+app.get('/directors/:id/titles', async (req, res) => {
+  const { id } = req.params
+  const director = await Director.findById(id)
+
+  try {
+    if (director) {
+    const titles = await Title.find({ director: mongoose.Types.ObjectId(director.id) })
+    res.json({ length: titles.length, data: titles })
+    }
+  } catch(error) {
+   res.status(400).json({ error: 'Director not found'})
+ } 
+})
+
+// WHEN it starts to work, destructure it as a query under titles instead
+app.get('/releaseyear', async (req, res) => {
+  const { year } = req.query
+
+  if (year) {
+    console.log(year) // year comes out as undefined ! 
+    const year = await Title.find({year})
+    res.json({ length: year.length, data: year })
+    } else {
+      const year = await Title.find()
+      res.json({ data: year }) 
+    }
 })
 
 // Start the server
