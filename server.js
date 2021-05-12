671ba24598c8d7fbd2a740729d76bb9b5a9f2bed
Erika Andersson Porath <erika.porath@hotmail.com>
2021-05-13 00:37:59 +0200

    added some code from the lecture

--- server.js
+++ server.js
@@ -1,20 +1,54 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import netflixData from './data/netflix-titles.json'
+
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+
+const Show = mongoose.model("Show", {
+  show_id: Number,
+  title: String,
+  director: String,
+  cast: String,
+  country: String,
+  date_added: String,
+  release_year: Number,
+  rating: String,
+  duration: String,
+  listed_in: String,
+  description: String,
+  type: String
+})
+
+if (process.env.RESET_DB) {
+  const seedDB = async () => {
+    await netflixData.forEach(item => {
+      const newShow = new Show(item)
+      newShow.save()
+    })
+  }
+  seedDB()
+}
+
+// const newShow = new Show({
+//   show_id: 81172543,
+//   title: "Pirates of the Caribbean: The Curse of the Black Pearl",
+//   director: "Gore Verbinski",
+//   cast: "Johnny Depp, Geoffrey Rush, Orlando Bloom, Keira Knightley",
+//   country: "United States",
+//   date_added: "",
+//   release_year: 2003,
+//   rating: "PG-13",
+//   duration: "143 min",
+//   listed_in: "Action, Adventure, Fantasy",
+//   description: "Blacksmith Will Turner teams up with eccentric pirate 'Captain' Jack Sparrow to save his love, the governor's daughter, from Jack's former pirate allies, who are now undead.",
+//   type: "Movie"
+// })
+// newShow.save()
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -25,7 +59,7 @@ const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
