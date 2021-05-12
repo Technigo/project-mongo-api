ba5ab94e3f58c9bdc3fa24a553c593cddb27ecac
Polkapetunian <70851837+Polkapetunian@users.noreply.github.com>
2021-05-12 23:27:33 +0200

    Seeded database with data of trees cared for by Umeå kommun

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import trees from './data/trees.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/trees-of-umea"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+
+const Tree = mongoose.model('Tree', {
+  geoPoint: String,
+  scientificName: String,
+  treeType: String,
+  streetOrPark: String
+})
+
+if (process.env.RESET_DB) {
+  console.log('Resetting database!')
+
+  const seedDatabase = async () => {
+    await Tree.deleteMany({})
+
+    trees.forEach((treeData) => {
+      new Tree(treeData).save()
+    })
+  }
+  seedDatabase()
+}
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -25,11 +38,16 @@ const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Hello Bagheera')
+})
+
+app.get('/trees', async (req, res) => {
+  const trees = await Tree.find()
+  res.json(trees)
 })
 
 // Start the server
