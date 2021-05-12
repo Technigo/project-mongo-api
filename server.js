0b5d5cef98bf977bba33dec6ae917dbba8bd5143
Anna Lindgren <annalindgren@MacBook-Pro-som-tillhor-Anna.local>
2021-05-12 09:30:39 +0200

    Added books Schema and book model

--- server.js
+++ server.js
@@ -3,12 +3,9 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
+import booksData from './data/books.json'
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
@@ -16,14 +13,39 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
+// Books schema
+const bookSchema = new mongoose.Schema({
+  bookID: Number,
+  title: String,
+  authors: String,
+  average_rating: Number,
+  isbn: Number,
+  isbn13: Number,
+  language_code: String,
+  num_pages: Number,
+  ratings_count: Number,
+  text_reviews_count: Number
+})
+
+// Book Model (with schema added)
+const Book = mongoose.model('Book', bookSchema)
+
+const newBook = new Book({
+  bookdID: 1465,
+  title: 'Anna Lindgrens Path To Developer',
+  authors: 'Anna Lindgren',
+  average_rating: 5,
+  isbn: 123456,
+  isbn13: 1234567,
+  num_pages: 200,
+  ratings_count: 5,
+  text_reviews_count: 800
+})
+newBook.save()
+
+const port = process.env.PORT || 8091
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
@@ -31,6 +53,11 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+
+// app.get('/books', async (req, res) => {
+//   const books = await Book.find()
+//   res.json(books)
+// })
 
 // Start the server
 app.listen(port, () => {
