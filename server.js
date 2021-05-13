f7a48c221a1af5a721fa236977585e2a4b913328
Bellsibub <isabella.billgren@gmail.com>
2021-05-13 12:53:11 +0200

    removed seed

--- server.js
+++ server.js
@@ -5,26 +5,12 @@ import dotenv from 'dotenv';
 
 import router from './routes';
 import errorHandler from './controllers/errorController';
-import data from './data/ufoSightings.json';
-import Sighting from './models/sightingModel';
 
 dotenv.config();
 
 const mongoUrl = process.env.MONGO_URL || 'mongodb+srv://dbAdmin:GVjqx6dZ7PLl1Hsv@cluster0.ytdmt.mongodb.net/ufo-sightings?retryWrites=true&w=majority';
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
-
-if (process.env.RESET_DB) {
-  const seedDatabase = async () => {
-    await Sighting.deleteMany({});
-
-    data.forEach((d) => {
-      new Sighting(d).save();
-    });
-  };
-
-  seedDatabase();
-}
 
 const port = process.env.PORT || 8080;
 const app = express();
