f9cd011d823454f0d914e3428a25515e1c9fc93c
newmarkdev <newmarkdev@gmail.com>
2021-05-13 16:02:11 +0200

    added more endpoints for each year

--- server.js
+++ server.js
@@ -86,11 +86,65 @@ app.get('/btc-historical-prices/2015', async (req, res) => {
   }
 });
 // 2016
+app.get('/btc-historical-prices/2016', async (req, res) => {
+  const btcData = await BtcHistorical.find();
+  const btc2016 = await btcData.filter((item) => item.SNo === 1099)
+  if (btc2016) {
+    res.status(200).json({ dataMay2016: { closingPrice: btc2016[0].Close, marketCap: btc2016[0].Marketcap } });
+  } else {
+    res.status(404).json({ error: 'Not found' });
+  }
+});
 // 2017
+app.get('/btc-historical-prices/2017', async (req, res) => {
+  const btcData = await BtcHistorical.find();
+  const btc2017 = await btcData.filter((item) => item.SNo === 1464)
+  if (btc2017) {
+    res.status(200).json({ dataMay2017: { closingPrice: btc2017[0].Close, marketCap: btc2017[0].Marketcap } });
+  } else {
+    res.status(404).json({ error: 'Not found' });
+  }
+});
 // 2018
+app.get('/btc-historical-prices/2018', async (req, res) => {
+  const btcData = await BtcHistorical.find();
+  const btc2018 = await btcData.filter((item) => item.SNo === 1844)
+  if (btc2018) {
+    res.status(200).json({ dataMay2018: { closingPrice: btc2018[0].Close, marketCap: btc2018[0].Marketcap } });
+  } else {
+    res.status(404).json({ error: 'Not found' });
+  }
+});
 // 2019
+app.get('/btc-historical-prices/2019', async (req, res) => {
+  const btcData = await BtcHistorical.find();
+  const btc2019 = await btcData.filter((item) => item.SNo === 2196)
+  if (btc2019) {
+    res.status(200).json({ dataMay2019: { closingPrice: btc2019[0].Close, marketCap: btc2019[0].Marketcap } });
+  } else {
+    res.status(404).json({ error: 'Not found' });
+  }
+});
 // 2020
+app.get('/btc-historical-prices/2020', async (req, res) => {
+  const btcData = await BtcHistorical.find();
+  const btc2020 = await btcData.filter((item) => item.SNo === 2561)
+  if (btc2020) {
+    res.status(200).json({ dataMay2020: { closingPrice: btc2020[0].Close, marketCap: btc2020[0].Marketcap } });
+  } else {
+    res.status(404).json({ error: 'Not found' });
+  }
+});
 // 2021
+app.get('/btc-historical-prices/2021', async (req, res) => {
+  const btcData = await BtcHistorical.find();
+  const btc2021 = await btcData.filter((item) => item.SNo === 2859)
+  if (btc2021) {
+    res.status(200).json({ dataFeb2021: { closingPrice: btc2021[0].Close, marketCap: btc2021[0].Marketcap } });
+  } else {
+    res.status(404).json({ error: 'Not found' });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
