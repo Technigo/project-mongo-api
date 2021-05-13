caa8f5f683748678fae0d7f352966d98fec9699e
Maria Pogosjan <Maria.pogosjan@gmail.com>
2021-05-13 23:55:37 +0200

    query operator,  for dubble id and aggregate to find, sort and unset titles

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import listEndpoints from 'express-list-endpoints'
+
 import netflixData from './data/netflix-titles.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
@@ -24,13 +26,16 @@ const movieSchema = new mongoose.Schema({
 
 const Movie = mongoose.model('Movie', movieSchema)
 
-// Things to read on: reset_db, new, save, two id:s
+// Function to start seeding our Database
+// will only run if RESET_DB environment variable is present and is true
 if (process.env.RESET_DB) {
   const seedDB = async () => {
+    // Starts by deleting any pre-existing Movie objects to prevent duplicates when running database
     await Movie.deleteMany()
-    await netflixData.forEach((show) => {
+
+    netflixData.forEach(async (show) => {
       const newMovie = new Movie(show)
-      newMovie.save()
+      await newMovie.save()
     })
   }
   seedDB()
@@ -42,48 +47,124 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-// Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send(listEndpoints(app))
 })
 
-// OK
+// OK 
+// QA: Where do we add await? 
+// Routes only /movies and endpont the logic? 
+// Is this a good way to do more query params? 
+// How to add more in the same search? 
 app.get('/movies', async (req, res) => {
-  const movies = await Movie.find()
-  res.json({ length: movies.length, data: movies })
+  const { country, year, show } = req.query 
+
+  // delete all the id's from json and only show mongooose id
+  await Movie.updateMany(
+    { }, 
+    { 
+      $unset: { show_id: "" }
+    }
+  )
+
+  if (country) {
+    const movies = await Movie.find({ 
+      country: {
+        $regex: new RegExp(country, "i")
+      }
+    }) 
+    res.json({ length: movies.length, data: movies })
+  } else if (year) {
+    const movies = await Movie.find({ release_year: year })  
+    res.json({ length: movies.length, data: movies })
+  } else if (show) { // fix it so it also shows tv
+    const movies = await Movie.find({ $or: [{ type: "Movie" }, { type: "TV-show" }] })
+    res.json({ length: movies.length, data: movies })
+  } else {
+    const movies = await Movie.find()
+    res.json({ length: movies.length, data: movies })
+  }
 })
 
-// OK
+// OK 
+// Is this good way to do routes or should I do /movies/titles? 
 app.get('/titles', async (req, res) => {
-  const title = await Movie.find()
-  const filterTitles = title.map((item) => item.title)
-  const sortTitles = filterTitles.sort()
+  const title = await Movie.aggregate([
+    { $sort: { title: 1 } },
+    { $unset: ["_id", "director", "cast", "country", "date_added", "release_year", "rating", "duration", "listed_in", "description", "type", "__v"] }
+  ])
+  res.json({ length: title.length, data: title })
 
-  res.json({ length: sortTitles.length, data: sortTitles })
+  /* 
+   ****** Filtering and could only find titles but not sort. Solved it with aggregate ******
+    const title = await Movie.find({}, { title: 1, _id: 0 })
+    res.json({ length: title.length, data: title })
+
+    ****** Without map and all the titles apply ******
+    const filterTitles = title.map((item) => item.title)
+    res.json({ length: filterTitles.length, data: filterTitles.sort() })  
+  */
 })
 
-// don't get console log in terminal? How can I split?
+// OK 
+// How can I split for each word and then push in to the new array? 
 app.get('/genres', async (req, res) => {
   const genre = await Movie.find()
   const filterGenres = genre.map((item) => item.listed_in)
+  const test = filterGenres.toString().split(" ")
+  console.log(test) 
   let genresUnique = []
   
   filterGenres.forEach((item) => {
     if (!genresUnique.includes(item)) {
       genresUnique.push(item)
-    } 
+    }  
   })
   res.json({ length: genresUnique.length, data: genresUnique })
 })
 
-// How to do with two ID:s? Error message dosen't work.
-app.get('/movies/:id', async (req, res) => {
-  const movie = await Movie.findById(req.params.id)
-  if (movie) {
+// OK 
+app.get('/years', async (req, res) => {
+  const year = await Movie.find()
+  const filterYear = year.map((item) => item.release_year)
+  let yearUnique = []
+
+  filterYear.forEach((item) => {
+    if (!yearUnique.includes(item)) {
+      yearUnique.push(item)
+    }
+    return yearUnique.sort().reverse()
+  })
+
+  res.json({ length: yearUnique.length, data: yearUnique })
+})
+
+// OK
+// Shows from only 2018 and up with operator and projection on what to show 
+app.get('/movies/latest', async (req, res) => {
+  const genre = await Movie.find(
+    { release_year: 
+      { 
+        $gte: 2018 
+      } }, 
+    { 
+      release_year: 1, title: 1, cast: 1 
+    }
+  )
+ 
+  res.json({ length: genre.length, data: genre })
+})
+
+// Is 404 ok for ID because it is uniq and not found? Or should this also be 400? 
+app.get('/movies/:movieId', async (req, res) => {  
+  const { movieId } = req.params
+
+  try {
+    const movie = await Movie.findById(movieId)
     res.json({ data: movie })
-  } else {
-    res.status(404).json({ error: 'Not found!' })
-  } 
+  } catch (error) {
+    res.status(404).json({ error: 'Not found!', details: error })
+  }
 })
 
 app.listen(port, () => {
