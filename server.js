fdf1ff15422dc69df57b766ee4fc3daab35faff8
newmarkdev <newmarkdev@gmail.com>
2021-05-14 15:29:26 +0200

    added catch and try

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
 import listEndpoints from 'express-list-endpoints'
 
 import btc from './data/btc.json'
 
-dotenv.config()
+// dotenv.config()
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/btc"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -53,17 +53,21 @@ app.get('/', (_, res) => {
 })
 
 app.get('/btc-historical-prices', async (req, res) => {
-  const btcData = await BtcHistorical.find();
-  res.json(btcData);
+  try {
+    const btcData = await BtcHistorical.find();
+    res.status(200).json(btcData);
+  } catch (error) {
+    res.status(404).json({ error: 'Not found' })
+  }
 });
 
 // find / display one closing price at a date close to the start of May 2013 + the market cap on that day 
 app.get('/btc-historical-prices/2013', async (req, res) => {
-  const btcData = await BtcHistorical.find();
-  const btc2013 = await btcData.filter((item) => item.SNo === 3)
-  if (btc2013) {
+  try {
+    const btcData = await BtcHistorical.find();
+    const btc2013 = btcData.filter((item) => item.SNo === 3)
     res.status(200).json({ dataMay2013: { closingPrice: btc2013[0].Close, marketCap: btc2013[0].Marketcap } });
-  } else {
+  } catch {
     res.status(404).json({ error: 'Not found' });
   }
 });
@@ -71,7 +75,7 @@ app.get('/btc-historical-prices/2013', async (req, res) => {
 // Closing Price and Marketcap May 2014
 app.get('/btc-historical-prices/2014', async (req, res) => {
   const btcData = await BtcHistorical.find();
-  const btc2014 = await btcData.filter((item) => item.SNo === 369)
+  const btc2014 = btcData.filter((item) => item.SNo === 369)
   if (btc2014) {
     res.status(200).json({ dataMay2014: { closingPrice: btc2014[0].Close, marketCap: btc2014[0].Marketcap } });
   } else {
@@ -82,7 +86,7 @@ app.get('/btc-historical-prices/2014', async (req, res) => {
 // 2015
 app.get('/btc-historical-prices/2015', async (req, res) => {
   const btcData = await BtcHistorical.find();
-  const btc2015 = await btcData.filter((item) => item.SNo === 733)
+  const btc2015 = btcData.filter((item) => item.SNo === 733)
   if (btc2015) {
     res.status(200).json({ dataMay2015: { closingPrice: btc2015[0].Close, marketCap: btc2015[0].Marketcap } });
   } else {
@@ -92,7 +96,7 @@ app.get('/btc-historical-prices/2015', async (req, res) => {
 // 2016
 app.get('/btc-historical-prices/2016', async (req, res) => {
   const btcData = await BtcHistorical.find();
-  const btc2016 = await btcData.filter((item) => item.SNo === 1099)
+  const btc2016 = btcData.filter((item) => item.SNo === 1099)
   if (btc2016) {
     res.status(200).json({ dataMay2016: { closingPrice: btc2016[0].Close, marketCap: btc2016[0].Marketcap } });
   } else {
@@ -102,7 +106,7 @@ app.get('/btc-historical-prices/2016', async (req, res) => {
 // 2017
 app.get('/btc-historical-prices/2017', async (req, res) => {
   const btcData = await BtcHistorical.find();
-  const btc2017 = await btcData.filter((item) => item.SNo === 1464)
+  const btc2017 = btcData.filter((item) => item.SNo === 1464)
   if (btc2017) {
     res.status(200).json({ dataMay2017: { closingPrice: btc2017[0].Close, marketCap: btc2017[0].Marketcap } });
   } else {
@@ -112,7 +116,7 @@ app.get('/btc-historical-prices/2017', async (req, res) => {
 // 2018
 app.get('/btc-historical-prices/2018', async (req, res) => {
   const btcData = await BtcHistorical.find();
-  const btc2018 = await btcData.filter((item) => item.SNo === 1844)
+  const btc2018 = btcData.filter((item) => item.SNo === 1844)
   if (btc2018) {
     res.status(200).json({ dataMay2018: { closingPrice: btc2018[0].Close, marketCap: btc2018[0].Marketcap } });
   } else {
@@ -122,7 +126,7 @@ app.get('/btc-historical-prices/2018', async (req, res) => {
 // 2019
 app.get('/btc-historical-prices/2019', async (req, res) => {
   const btcData = await BtcHistorical.find();
-  const btc2019 = await btcData.filter((item) => item.SNo === 2196)
+  const btc2019 = btcData.filter((item) => item.SNo === 2196)
   if (btc2019) {
     res.status(200).json({ dataMay2019: { closingPrice: btc2019[0].Close, marketCap: btc2019[0].Marketcap } });
   } else {
@@ -132,7 +136,7 @@ app.get('/btc-historical-prices/2019', async (req, res) => {
 // 2020
 app.get('/btc-historical-prices/2020', async (req, res) => {
   const btcData = await BtcHistorical.find();
-  const btc2020 = await btcData.filter((item) => item.SNo === 2561)
+  const btc2020 = btcData.filter((item) => item.SNo === 2561)
   if (btc2020) {
     res.status(200).json({ dataMay2020: { closingPrice: btc2020[0].Close, marketCap: btc2020[0].Marketcap } });
   } else {
@@ -142,7 +146,7 @@ app.get('/btc-historical-prices/2020', async (req, res) => {
 // 2021
 app.get('/btc-historical-prices/2021', async (req, res) => {
   const btcData = await BtcHistorical.find();
-  const btc2021 = await btcData.filter((item) => item.SNo === 2859)
+  const btc2021 = btcData.filter((item) => item.SNo === 2859)
   if (btc2021) {
     res.status(200).json({ dataFeb2021: { closingPrice: btc2021[0].Close, marketCap: btc2021[0].Marketcap } });
   } else {
