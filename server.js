01797c2cb77879487388a49a69d07dd3638a244b
jess-stack <72085164+jess-stack@users.noreply.github.com>
2021-05-14 16:56:00 +0200

    changed url

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 
 import movieData from './data/netflix-titles.json'
 
-const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-mongo';
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/movieTime';
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
