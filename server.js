b3663ef02e12eaedc8df0a8703503469e85b272a
Laima Duhovnaja <laima@macbook-air.mynet>
2021-05-14 15:57:47 +0200

    added routes, regexp

--- server.js
+++ server.js
@@ -1,39 +1,89 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import songs from "./data/top-music.json";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const songSchema = new mongoose.Schema({
+  id: Number,
+  trackName: String,
+  artistName: String,
+  genre: String,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  loudness: Number,
+  liveness: Number,
+  valence: Number,
+  length: Number,
+  acousticness: Number,
+  speechiness: Number,
+  popularity: Number,
+});
+
+const Song = mongoose.model("Song", songSchema);
+
+if (process.env.RESET_DB) {
+  const seedDB = async () => {
+    await Song.deleteMany();
+
+    songs.forEach((item) => {
+      const newSong = new Song(item);
+      newSong.save();
+    });
+  };
+
+  seedDB();
+}
+
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/songs", async (req, res) => {
+  const { song_name } = req.query;
+
+  if (song_name) {
+    const songs = await Song.find({
+      trackName: {
+        $regex: new RegExp(song_name, "i"),
+      },
+    });
+    res.json(songs);
+  } else {
+    const songs = await Song.find();
+    res.json(songs);
+  }
+});
+
+app.get("/songs/:songId", async (req, res) => {
+  const { songId } = req.params;
+  const singleSong = await Song.findOne({ _id: songId });
+  res.json(singleSong);
+});
+
+app.get("/songs/artist_name/:artistName", async (req, res) => {
+  const { artistName } = req.params;
+
+  const singleSong = await Song.findOne({ artistName: artistName });
+  res.json(singleSong);
+});
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
