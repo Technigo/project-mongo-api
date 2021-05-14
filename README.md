cfed0d5e9c67aec94d93d89bf6cdb797b670642f
Ylva Landoff Lindberg <ylvalandofflindberg@Ylvas-MBP-2.lan>
2021-05-14 16:46:43 +0200

    Finished some endpoints using .then,.map. Added ReadMe

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # Mongo API Project
 
-Replace this readme with your own information about your project. 
+I use MongoDB to store and retrieve data from and use that data to produce a RESTful API.
 
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+I have created several routes and endpoints.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+Mostly just played around and tested diffrent ways.
+
+I have used Mongoose to fetch data from Mongo. And worked in postman.
 
 ## View it live
 
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-import express from 'express'
+import express, { response } from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
 import netflixData from './data/netflix-titles.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-mongo'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
@@ -26,10 +26,6 @@ const showSchema = new mongoose.Schema({
     type: String,
     lowercase: true
   },
-  duration: {
-    type: String,
-    lowercase: true
-  },
   description: {
     type: String,
     lowercase: true
@@ -40,6 +36,7 @@ const Show = mongoose.model('Show', showSchema);
 
 
 //TO USE THIS, TYPE: RESET_DB=true npm run dev IN TERMINAL
+
 if (process.env.RESET_DB) {
 const seedDB = async () => {
   await Show.deleteMany();
@@ -52,100 +49,124 @@ const seedDB = async () => {
 seedDB();
 }
 
-//   PORT=9000 npm start
 const port = process.env.PORT || 8082
 const app = express()
 
-// Add middlewares to enable cors
 app.use(cors()) 
 app.use(express.json())
 
-
-// Start defining your routes here
 app.get('/', (req, res) => {
   Show.find().then(show => {
     res.json('Show most go on!')
   })
 })
 
-// app.get('/list/descriptions', async (req, res) => {
-//   const allDescriptions = await Show.map(item => item.description);
+// All descriptions http://localhost:8082/descriptions
 
-//   res.json({ data:allDescriptions })
-// });
+app.get('/shows/descriptions', async (req, res) => {
+  const allDescriptions =  await Show.find()
+  .then((items) => items.map(item => item.description))
+  res.json({  data: allDescriptions })
+});
 
-//http://localhost:8082/list
+//http://localhost:8082/shows
 
-app.get('/list', async (req, res) => {
+app.get('/shows', async (req, res) => {
   const show = await Show.find();
   res.json(show)
 });
 
-app.get('/list/series', async (req, res) =>{
+http://localhost:8082/shows/series
+app.get('/shows/series', async (req, res) =>{
   const data = await Show.find({ type: 'TV Show'})
   res.json(data)
 });
 
 // //return one show by id
-app.get('/list/:showid', async (req, res) =>{
+
+app.get('/shows/:showid', async (req, res) =>{
   const { showid } = req.params;
 
-  const singleShow = await Show.findById({_id: showid});
-  res.json(singleShow);
+  try {
+    const singleShow = await Show.findById({_id: showid});
+    res.json(singleShow);
+  } catch(error) {
+    res.status(400).json({error: 'sorry', details: error });
+  }
 });
 
-// //return one show by title. Use this way to write async/await code with the catch error
-app.get('/list/show/:showname', async (req, res) =>{
+//return one show by title. Use this way to write async/await code with the catch error
+
+app.get('/shows/show/:showname', async (req, res) =>{
   const { showname } = req.params;
 
     try {
       const singleShow = await Show.findOne({ title: showname });
       res.json(singleShow);
     } catch(error) {
-      res.status(400).json({error: 'sorry', details: error }); 
+      res.status(400).json({error: 'Invalid request', details: error }); 
     }
 })
 
-//http://localhost:8082/list/directors?director=Mati%20Diop
+//http://localhost:8082/shows/directors?director=Mati%20Diop
 // //quary params when its only one, unique. ($reqex: new RegExp(showTitle, 'i' dont care about case sensitive)
-app.get('/list/directors/', async (req, res) => {
+
+app.get('/shows/directors/', async (req, res) => {
   const { director } = req.query;
 
   if (director) {
-    const list = await Show.find({ 
+    const shows = await Show.find({ 
       director:{
         $regex: new RegExp(director, 'i')
       } 
     })
-    res.json(list);
+    res.json(shows);
   } else {
-    const list = await Show.find();
-    res.json(list);
+    const shows = await Show.find();
+    res.json(shows);
   }
 })
 
+//http://localhost:8082/showlist?releaseYear=2018
 
+app.get('/showlist', async (req, res) => {
+  let data
+  const { releaseYear } = req.query;
+  const { country } = req.query;
 
-//http://localhost:8082/list/countries?country=india
+  if (releaseYear) {
+    data = await Show.find({release_year: +releaseYear })
+  } else if (country) {
+    data = await Show.find({
+        country: {
+        $regex: new RegExp(country, "i")
+      }
+    })
+  } else {
+    data = await Show.find()
+  }
+  res.json(data)
+})
 
-app.get('/list/countries', async (req, res) => {
+//http://localhost:8082/shows/countries?country=india
+
+app.get('/shows/countries', async (req, res) => {
   const { country } = req.query;
 
   if (country) {
-    const list = await Show.find({
+    const shows = await Show.find({
       country: {
         $regex: new RegExp(country, "i")
       }
     })
-    res.json(list);
+    res.json(shows);
   } else {
-    const list = await Show.find();
-    res.json(list);
+    const shows = await Show.find();
+    res.json(shows);
   }
 })
 
-// Start the server
 app.listen(port, () => {
-  // eslint-disable-next-line
-  console.log(`Server running on http://localhost:8082`)
+
+  console.log(`Server running on http://localhost:${port}`)
 })
