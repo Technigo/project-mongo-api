e45dd929614c59654adf3d218dddb98afcee4311
Your Name <isabellamarkskog@live.se>
2021-05-15 12:06:57 +0200

    Updated README file and did general clean up of code

--- README.md
+++ README.md
@@ -2,14 +2,20 @@
 
 I've built a RESTful API using a database to store and retrieve data. 
 
-I used MongoDB for the database, mongoose for the modelling and manipulation of data and Express.js (Node.js framework)
+I used MongoDB for the database, mongoose for the modelling of data and Express.js (Node.js framework).
 
 
 ## The problem
 
-The main task was to build a RESTful API and to use a database to store and retrieve data, which I populate the API with. 
+The main task was to build a RESTful API and to use a database to store and retrieve data.
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+I started with setting up the database using MongoDB and built a model for the data that I chose (books data) using mongoose. After that I set up an async function to seed the database using the aforementioned mongoose model with an if statement around it to ensure that the database is only being reseeded when I want it to. With the database in place I began thinking about what kind of endpoints I would like to have for the data. I then started to create the endpoints home, all books, books by top rating (4 or higher), short read and id. I also implemented filters via query parameters to filter, via mongoose, the data returned from endpoints which then return an array of data.
+
+I've also implemented error handling throughout the project. For instance, when searching for a book by id, I've set up an if statement to handle when the specific id doesn't exist and return some useful data in the response instead. I've also implemented try and catch blocks.
+
+I chose to use the npm package express-list-endpoints to list the endpoints in the home endpoint. I continuously tested my API using Postman and checked out my database using MongoDB Compass. 
+
+If I had more time, I would implement a frontend to by API where I display the documentation for my API. I would also like to implement pages to return only a selection of results from the array and to use mongoose's aggregate function.
 
 
 ## View it live
--- server.js
+++ server.js
@@ -18,16 +18,13 @@ const bookSchema = new mongoose.Schema({
   authors: String,
   average_rating: Number,
   isbn: String,
-  language_code: String,
-  num_pages: Number,
-  ratings_count: Number,
-  text_reviews_count: Number
+  num_pages: Number
 });
 
 const Book = mongoose.model('Book', bookSchema);
 
 if (process.env.RESET_DB) {
-  console.log('Resetting database!'); // remove this later?
+  console.log('Resetting database!');
 
   const seedDB = async () => {
     await Book.deleteMany();
@@ -39,10 +36,7 @@ if (process.env.RESET_DB) {
         authors: item.authors,
         average_rating: item.average_rating,
         isbn: item.isbn,
-        language_code: item.language_code,
-        num_pages: item.num_pages,
-        ratings_count: item.ratings_count,
-        text_reviews_count: item.text_reviews_count
+        num_pages: item.num_pages
       });
       await newBook.save();
     });
@@ -79,24 +73,20 @@ app.get('/', (req, res) => {
 // All books
 // Books by author, query path: '/books?author=adams'
 // Books by title, query path: '/books?title=chamber'
-// Books by language, query path: '/books?language=eng' //check this query, it doesn't work properly
 // Books by isbn, query path: '/books?isbn=439785960'
 app.get('/books', async (req, res) => {
-  const { author, title, language, isbn } = req.query;
+  const { author, title, isbn } = req.query;
   let booksToSend = await Book.find();
 
   try {
     if (author) {
-      booksToSend = await Book.find({ authors: new RegExp(author, "i") })
+      booksToSend = await Book.find({ authors: new RegExp(author, "i") });
     } 
     if (title) {
-      booksToSend = await Book.find({ title: new RegExp(title, "i") })
-    }
-    if (language) {
-      booksToSend = await Book.find({ language_code: new RegExp(language, "i") })
+      booksToSend = await Book.find({ title: new RegExp(title, "i") });
     }
     if (isbn) {
-      booksToSend = await Book.find({ isbn: new RegExp(isbn, "i") })
+      booksToSend = await Book.find({ isbn: new RegExp(isbn, "i") });
     }
     if (booksToSend.length === 0) {
       res.status(404).send(`Sorry, could not find any books!`);
@@ -104,14 +94,14 @@ app.get('/books', async (req, res) => {
   
     res.json(booksToSend);
   } catch (err) {
-    res.status(404).send({ error: "Page not found" })
+    res.status(404).send({ error: "Page not found" });
   }
 });
 
 // Books by top rating (4 or higher)
 app.get('/books/toprating', async (req, res) => {
   try {
-    const booksByTopRating = await Book.find({ average_rating: { $gte: 4 } })
+    const booksByTopRating = await Book.find({ average_rating: { $gte: 4 } });
     res.json(booksByTopRating);
   } catch (err) {
     res.status(404).send({ error: "Page not found" });
@@ -140,7 +130,7 @@ app.get('/books/:bookId', async (req, res) => {
       res.status(404).json({ error: 'Book not found' });
     }
   } catch (err) {
-    res.status(400).json({ error: 'Invalid book ID' }); // Bad request
+    res.status(400).json({ error: 'Invalid book ID' });
   }
 });
 
