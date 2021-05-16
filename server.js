23d44500172e6e951be0a2e10422feee1b7e5ff4
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-05-16 01:45:11 -0500

    Connected to my new database of books

--- server.js
+++ server.js
@@ -2,35 +2,35 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
-import ceremonies from './data/golden-globes.json'
+import books from './data/mybooks.json'
 
-//setup to connect to our database
+//Setup to connect to our database
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
 //Fields of our model
-const ceremonySchema = new mongoose.Schema ({
-  year_film: Number,
-  year_award: Number,
-  ceremony: Number,
-  category: String,
-  nominee: String,
-  film: String,
-  win: Boolean
+const booksSchema = new mongoose.Schema ({
+  bookID: Number,
+  title: String,
+  authors: String,
+  num_pages: Number,
+  language: String,
+  sinopsis: String
 })
-//Model from the ceremonySchema
-const Ceremony = mongoose.model('Ceremony', ceremonySchema)
+
+//Model from booksSchema
+const Book = mongoose.model('Book', booksSchema)
 
 //Seeding of our database
 if (process.env.RESET_DB) {
   console.log('SEEDING')
   const seedDB = async () => {
-    await Ceremony.deleteMany()
+    await Book.deleteMany()
 
-    await ceremonies.forEach(item => {
-      const newCeremony = new Ceremony(item)
-      newCeremony.save()
+    await books.forEach(item => {
+      const newBook = new Book(item)
+      newBook.save()
     })
   }    
   seedDB()
@@ -48,12 +48,11 @@ app.get('/', (req, res) => {
   res.send('Hola world')
 })
 
-//Reaching for all the ceremonies
-app.get('/ceremonies', async (req, res) => {
-  const ceremonies = await Ceremony.find();
-  res.json(ceremonies)
+//Reaching for all the books
+app.get('/books', async (req, res) => {
+  const books = await Ceremony.find();
+  res.json(books)
 })
-//looking for one specific ceremony
 
 // Start the server
 app.listen(port, () => {
