d55889962895c9c8c3a0a510aa60a324841cbce2
NoemiSalome <n.s.frischknecht@gmail.com>
2021-05-16 13:11:11 +0200

    dotenv

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
+import dotenv from 'dotenv'
 import listEndpoints from 'express-list-endpoints'
 
 import booksData from './data/books.json'
+
+dotenv.config()
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
