e20cafdd48756d4d15cb50f5df8a22410d049182
Martin <martno15@gmail.com>
2021-05-16 22:09:57 +0200

    Final commit for now

--- server.js
+++ server.js
@@ -1,39 +1,81 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import places from "./data/dream-places.json"
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+const mongoUrl = process.env.MONGO_URL
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const Location = mongoose.model("Location", {
+  name: String,
+  keywords: String,
+  coherence: Number,
+  mainSourceA: String,
+  mainSourceB: String,
+  status: String,
+  governor: String,
+  population: Number,
+  size: String
+})
+
+if (process.env.RESET_DB) {
+  const seedDB = async () => {
+    await Location.deleteMany()
+
+    await places.forEach(place => {
+      const newLocation = new Location(place)
+      newLocation.save()
+    })
+  }
+  seedDB()
+}
+
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
-// Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send("hello there")
 })
 
-// Start the server
+app.get('/locations', async (req, res) => {
+  const { status } = req.query
+  
+  if (status) {
+    const locations = await Location.find({
+      status: {
+        $regex: new RegExp(status, "i")
+      }
+    })
+    res.json(locations)
+  } else {
+    const locations = await Location.find()
+    res.json(locations)
+  }
+})
+
+app.get('/locations/:locationId', async (req, res) => {
+  const { locationId } = req.params
+  try {
+    const singleLocation = await Location.findOne({ _id: locationId })
+    res.json(singleLocation)
+  } catch(error) {
+    res.status(400).json({ error: "something went wrong :(", details: error })
+  }
+})
+
+app.get('/locations/name/:locationName', async (req, res) => {
+  const { locationName } = req.params
+  console.log(locationName)
+  const singleLocation = await Location.findOne({ name: locationName })
+  console.log(singleLocation)
+  res.json(singleLocation)
+})
+
 app.listen(port, () => {
-  // eslint-disable-next-line
   console.log(`Server running on http://localhost:${port}`)
 })
