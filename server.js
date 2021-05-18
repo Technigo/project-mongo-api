3cd9c81f7f94da029ab3352bf47b9afaffc92d6c
Elaine Bergstrom <Elainebergstrom26@gmail.com>
2021-05-18 21:37:46 +0200

    adding seed database and mongoose model

--- server.js
+++ server.js
@@ -1,39 +1,63 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import mlpCharacters from './data/my-little-ponies.json';
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const Character = mongoose.model('Character', {
+  name: String,
+  kind: String,
+  age: String,
+  residence: String,
+  cutieMark: String,
+  coatColor: String,
+  eyeColor: String,
+  maneColor: String,
+  skills: String,
+  maneSix: Boolean
+});
+
+const newCharacter = new Character({
+  name: "Big McIntosh",
+  kind: "Earth Pony",
+  age: "Adult Pony",
+  residence: "Ponyville",
+  cutieMark: "Half a green apple",
+  coatColor: "Red",
+  eyeColor: "Green",
+  maneColor: "Orange",
+  skills: "Farming, Repairs, Singing, Strength, and saying Yup",
+  maneSix: false
+});
+newCharacter.save();
+
+const seedDB = () => {
+  mlpCharacters.forEach(item => {
+    const newCharacter = new Character(item);
+    newCharacter.save();
+  });
+}
+
+seedDB();
+
+const port = process.env.PORT || 8000
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+
+const mongoURL = process.env.MONGO_URL || 'mongodb://localhost/my-little-pony'
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+  res.send('hello ponies')
+});
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
+  console.log(`Server running 🦄 on http://localhost:${port}`)
 })
