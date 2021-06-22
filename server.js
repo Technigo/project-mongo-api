0254295daf473df1b6aa297b4edbe0ab8f3b3be4
liaback <felicia.liaback@gmail.com>
2021-06-22 13:12:37 +0200

    url

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
 import topmusics from "./data/top-music.json";
+
+dotenv.config()
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
+// Defining models
 const topMusicSchema = new mongoose.Schema({
   trackName: {
     type: String,
-    lowercase: true
+    lowercase: true,
   },
   artistName: {
     type: String,
-    lowercase: true
+    lowercase: true,
   },
   genre: String,
   popularity: Number,
@@ -22,16 +27,43 @@ const topMusicSchema = new mongoose.Schema({
 
 const TopMusic = mongoose.model("TopMusic", topMusicSchema);
 
+const artistSchema = new mongoose.Schema({
+  artistName: String,
+});
+
+const Artist = mongoose.model("Artist", artistSchema);
+
+// seedDB
 if (process.env.RESET_DB) {
   const seedDB = async () => {
     await TopMusic.deleteMany();
 
-    await topmusics.forEach((item) => {
-      const newTopMusic = new TopMusic(item);
-      newTopMusic.save();
-  });
-}
-seedDB();
+    // await topmusics.forEach((item) => {
+    //   const newTopMusic = new TopMusic(item);
+    //   newTopMusic.save();
+    // });
+
+    await Artist.deleteMany();
+
+    const artistsArray = [];
+
+    topmusics.forEach(async (item) => {
+      const newArtist = new Artist(item);
+      artistsArray.push(newArtist);
+      await newArtist.save();
+    });
+
+    topmusics.forEach(async (item) => {
+      const newTopMusic = new TopMusic({
+        ...item,
+        artistName: artistsArray.find(
+          (singleArtist) => singleArtist.artistName === item.artistName
+        ),
+      });
+      await newTopMusic.save();
+    });
+  };
+  seedDB();
 }
 
 const port = process.env.PORT || 8080;
@@ -40,29 +72,89 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// endpoint - top music
-app.get('/topmusics', async (req, res) => {
+// check connection
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next();
+  } else {
+    res.status(503).json({ error: "Service not available" });
+  }
+});
+
+// Endpoint to filter artists by name using query params
+app.get("/topmusics", async (req, res) => {
+  const { name } = req.query;
+  try {
+    if (name) {
+      const artist = await TopMusic.find({
+        artistName: {
+          $regex: new RegExp(name, "i"),
+        },
+      });
+      res.json(artist);
+    } else {
+      const artist = await TopMusic.find();
+      res.json(artist);
+    }
+  } catch (error) {
+    res.status(400).json({ error: "invalid request" });
+  }
+});
+
+// Endpoint to get the whole music list
+app.get("/topmusics", async (req, res) => {
   const topmusics = await TopMusic.find();
-  res.json(topmusics)
+  if (topmusics) {
+    res.json(topmusics);
+  } else {
+    res.status(404).json({ errro: "Music list was not found" });
+  }
 });
 
-// params - find a specific element like id
-// query - filtering our colection to catch elements
-
-// Endpoint to find music by id using params
-app.get('/topmusics/:musicId', async (req,res) => {
-  const { musicId } = req.params;
-  const singleMusic = await TopMusic.findById(musicId);
-  res.json(singleMusic)
+// Endpoint to find music by id using path params
+app.get("/topmusics/:musicId", async (req, res) => {
+  try {
+    const { musicId } = req.params;
+    const singleMusic = await TopMusic.findById(musicId);
+    if (singleMusic) {
+      res.json(singleMusic);
+    } else {
+      res.status(404).send({ error: "Id not found" });
+    }
+  } catch (err) {
+    res.status(400).send({ error: "Invalid request" });
+  }
 });
 
-// Edpoint to filter music by track name using query
-app.get('/topmusics/track/:trackName', async (req,res) => {
-  const { trackName } = req.params;
-  const singleTrack = await TopMusic.findOne({ trackName });
-  res.json(singleTrack)
+// Endpoint to find track using path param
+app.get("/topmusics/track/:trackName", async (req, res) => {
+  try {
+    const { trackName } = req.params;
+    const singleTrack = await TopMusic.findOne({ trackName });
+    if (singleTrack) {
+      res.json(singleTrack);
+    } else {
+      res.status(404).send({ error: "track not found" });
+    }
+  } catch (err) {
+    res.status(400).send({ error: "Invalid request" });
+  }
 });
 
+//Endpoint to get all artists
+app.get("/artists/:artistID", async (req, res) => {
+  const { artistID} = req.params
+  try {
+    const artist = await Artist.findById(artistID)
+    if (artist) {
+      res.json({ data: artist });
+    } else {
+      res.status(404).send({ error: `Artist ${artistID} not found` });
+    }
+  } catch (err) {
+    res.status(400).send({ error: "invalid request" });
+  }
+});
 
 app.listen(port, () => {
   // eslint-disable-next-line
