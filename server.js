fb89e72885d66540de03df8b29a0877c15147589
Nazanin Adampira <Nazi.pira88@gmail.com>
2021-07-01 18:51:41 +0200

    backends fix

--- server.js
+++ server.js
@@ -2,23 +2,73 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import data from './data/netflix-titles.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+
+//setup connection to mongodb
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/shows"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
+
+//setup of shows title
+const Show = mongoose.model('Show', {
+  // Properties defined here match the keys from the netflix-title.json file
+  show_id: {
+    type: Number
+  },
+  title: {
+    type: String
+  },
+  director: {
+    type: String
+  },
+  cast: {
+    type: String
+  },
+  country: {
+    type: String
+  },
+  date_added: {
+    type: String
+  },
+  release_year: {
+    type: Number
+
+  },
+  rating: {
+    type: String
+  },
+  duration: {
+    type: String
+  },
+  listed_in: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  type: {
+    type: String
+  },
+})
+
+
+// to prevent duplicating data in the database when server starts we wrap it in an environment variable
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Show.deleteMany({})
+
+    data.forEach((show) => {
+      new Show(show).save()
+    })
+  }
+
+  seedDatabase()
+}
+
+
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
@@ -32,8 +82,63 @@ app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
+
+// Regular expression => display all the shows base on searching word, and the 'i' = uppercase/lowercase
+// http://localhost:8080/shows?year=2018&title=people&listed_in=international
+app.get("/shows", async (req, res) => {
+  let queryObj = {}
+
+  let startIndex, perPage
+  if (req.query.perPage) {
+    perPage = +req.query.perPage
+  }
+  if (req.query.page && req.query.perPage) {
+    startIndex = perPage * (+req.query.page - 1)
+  }
+  if (req.query.listed_in) { queryObj['listed_in'] = new RegExp(req.query.listed_in, 'i') }
+  if (req.query.year) { queryObj['release_year'] = req.query.year }
+  if (req.query.title) { queryObj['title'] = new RegExp(req.query.title, 'i') }
+
+  Show.find(queryObj).sort('title')
+    .then((results) => {
+      let resultsObj = {
+        "total_shows": results.length
+      }
+      // Successful
+      if (req.query.page && req.query.perPage) {
+        resultsObj.shows = results.slice(startIndex, startIndex + perPage)
+        res.json(resultsObj)
+      } else {
+
+        resultsObj.shows = results
+        res.json(resultsObj)
+      }
+
+    }).catch((err) => {
+      //Error - Failure
+      res.json({ message: 'Cannot find this show', err: err })
+    })
+})
+
+
+// path-params to be able to find a specefic show //
+// http://localhost:8080/shows/id/81082007/
+app.get("/shows/id/:id", async (req, res) => {
+  const id = req.params.id
+  Show.findOne({ 'show_id': id })
+    .then((result) => {
+      // Succesfull//
+      res.json(result)
+    }).catch((err) => {
+      // Error - Failure//
+      res.json({ message: 'Cannot find this show', err: err })
+    })
+})
+
+
+
+
 // Start the server
 app.listen(port, () => {
-  // eslint-disable-next-line
   console.log(`Server running on http://localhost:${port}`)
 })
