8d571af613335a5a9171938288b0e6a616941643
Darya <darya.lapata@gmail.com>
2021-12-13 18:47:47 +0100

    set up database

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 import express from 'express'
 import cors from 'cors'
+// mongoose framework for mongodb 
+// express framework for node 
 import mongoose from 'mongoose'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import netflixData from './data/netflix-titles.json'
 
+// setting app the database, where to find database
+// if delployed process.env.MONGO_URL, otherwise localhost 
+// project-mongo is the name of the database
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+// connectiong mangoose 
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
@@ -26,9 +25,49 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+// CREATING MODEL HERE
+// we need to create a model of our data, 
+// this model is how the data should look like in the database
+// specifing model, model() takes two arguments => modelname and an object 
+const Show = mongoose.model("Show", {
+  // the template how it should be stored, key=value, value is the datatype in this case
+  show_id: Number,
+  title: String,
+  director: String,
+  cast: String,
+  country: String,
+  date_added: String,
+  realese_year: String, 
+  rating: String, 
+  duration: String, 
+  listed_in: String, 
+  description: String, 
+  type: String
+})
+
+if (process.env.RESET_DB) {
+  // async awaut is just another method to do .then()
+  const seedDatabase = async () => {
+    // deleting all of the items in the database 
+    await Show.deleteMany({})
+
+    netflixData.forEach((showData) => {
+      // forEach item in data array, we creating new show from the model
+      // and save it with save() that exist in mongoose framework
+      new Show(showData).save()
+    })
+  }
+  // calling for function seedDatabase()
+  seedDatabase()
+}
+
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Hello world! Welcome my documentation!')
+})
+
+app.get('/shows', (req, res) => {
+  res.json(netflixData)
 })
 
 // Start the server
@@ -36,3 +75,11 @@ app.listen(port, () => {
   // eslint-disable-next-line
   console.log(`Server running on http://localhost:${port}`)
 })
+
+// FROM database mongodb compass
+// id is generated automaticaly by mongoose and mongodb
+// _id:61b762abb355c91b662c3f7c
+// name:"Bob"
+// age:36
+// version, not important 
+// __v:0
