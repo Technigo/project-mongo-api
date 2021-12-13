1bdf92471132f5d332bbe9f84337a3df6143d872
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2021-12-13 13:35:15 +0100

    change naming of type in json

--- server.js
+++ server.js
@@ -21,13 +21,207 @@ mongoose.Promise = Promise;
 const port = process.env.PORT || 8080;
 const app = express();
 
+const Title = mongoose.model("Title", {
+  show_id: Number,
+  title: String,
+  director: String,
+  cast: String,
+  country: String,
+  date_added: String,
+  release_year: Number,
+  rating: String,
+  duration: String,
+  listed_in: String,
+  type: String,
+});
+
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Title.deleteMany({});
+    netflixData.forEach((title) => {
+      new Title(title).save();
+    });
+  };
+  seedDatabase();
+}
+
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello world");
+const paginate = (res, page, limit, titlesToSend) => {
+  let pageInt = parseInt(page);
+  let limitInt = parseInt(limit);
+  const next = pageInt + 1;
+  const previous = pageInt - 1;
+  const startIndex = (pageInt - 1) * limitInt;
+  const endIndex = pageInt * limitInt;
+  let nrPages = titlesToSend.length / limitInt;
+  nrPages = Math.ceil(nrPages);
+  const titlesToSendPage = titlesToSend.slice(startIndex, endIndex);
+
+  if (previous === 0) {
+    res.status(200).json({
+      next: next,
+      response: titlesToSendPage,
+      success: true,
+    });
+  } else if (endIndex < titlesToSend.length) {
+    res.status(200).json({
+      next: next,
+      previous: previous,
+      response: titlesToSendPage,
+      success: true,
+    });
+  } else {
+    res.status(200).json({
+      previous: nrPages,
+      response: titlesToSendPage,
+      success: true,
+    });
+  }
+};
+
+// function that takes in path-param, checks if it can find it and returns a proper status and response. If there is a page and limit set, the function pagination is called. The plus-sign before a variable says to convert it from string to number
+const checkPathParam = (
+  param,
+  typeOfTitle,
+  res,
+  typeOfParam,
+  page,
+  limit
+) => {
+  let titlesWithParam = {};
+  let errorResponse = "";
+  if (typeOfParam === "id") {
+    if (typeOfTitle === "Movie") {
+      titlesWithParam = titles.find(
+        (item) => item.show_id === +param && item.type === "Movie"
+      );
+      errorResponse = `there is no movie with the id ${param}`;
+    } else {
+      titlesWithParam = titles.find(
+        (item) => item.show_id === +param && item.type === "TV show"
+      );
+      errorResponse = `there is no tv-show with the id ${param}`;
+    }
+  } else if (typeOfParam === "year") {
+    if (typeOfTitle === "Movie") {
+      titlesWithParam = titles.filter(
+        (item) =>
+          item.release_year === +param && item.type === "Movie"
+      );
+      errorResponse = `there are no movies from ${param}`;
+    } else {
+      titlesWithParam = titles.filter(
+        (item) =>
+          item.release_year === +param && item.type === "TV Show"
+      );
+      errorResponse = `there are no tv-shows from ${param}`;
+    }
+  }
+
+  // if there is no result, return 404. If page and limit exists, paginate the result.
+  if (!titlesWithParam || titlesWithParam.length === 0) {
+    res.status(404).json({
+      response: errorResponse,
+      success: false,
+    });
+  } else if (page && limit) {
+    paginate(res, page, limit, titlesWithParam);
+  } else {
+    res.status(200).json({
+      response: titlesWithParam,
+      success: true,
+    });
+  }
+};
+
+app.get("/netflix-titles", (req, res) => {
+  const { year, country, page, limit } = req.query;
+
+  let titlesToSend = titles;
+
+  if (year) {
+    titlesToSend = titlesToSend.filter(
+      (item) => item.release_year === +year
+    );
+  }
+  if (country) {
+    titlesToSend = titlesToSend.filter(
+      (item) =>
+        item.country.toLowerCase().indexOf(country.toLowerCase()) !==
+        -1
+    );
+  }
+  if (page && limit) {
+    paginate(res, page, limit, titlesToSend);
+  } else {
+    res.status(200).json({
+      response: titlesToSend,
+      success: true,
+    });
+  }
+});
+
+app.get("/netflix-titles/movies", (req, res) => {
+  const { page, limit } = req.query;
+  let titlesToSend = titles.filter((title) => title.type === "Movie");
+  if (page && limit) {
+    paginate(res, page, limit, titlesToSend);
+  } else {
+    res.status(200).json({
+      response: titlesToSend,
+      success: true,
+    });
+  }
+});
+
+app.get("/netflix-titles/movies/:id", (req, res) => {
+  const { id } = req.params;
+  checkPathParam(id, "Movie", res, "id");
+});
+
+app.get("/netflix-titles/movies/year/:year", (req, res) => {
+  const { page, limit } = req.query;
+  const year = req.params.year;
+  checkPathParam(year, "Movie", res, "year", page, limit);
+});
+
+app.get("/netflix-titles/tv-shows", (req, res) => {
+  const { page, limit } = req.query;
+  let titlesToSend = titles.filter(
+    (title) => title.type === "TV Show"
+  );
+  if (page && limit) {
+    paginate(res, page, limit, titlesToSend);
+  } else {
+    res.status(200).json({
+      response: titlesToSend,
+      success: true,
+    });
+  }
+});
+
+app.get("/netflix-titles/tv-shows/:id", (req, res) => {
+  const { id } = req.params;
+  checkPathParam(id, "TV-show", res, "id");
+});
+
+app.get("/netflix-titles/tv-shows/year/:year", (req, res) => {
+  const { page, limit } = req.query;
+  const year = req.params.year;
+  checkPathParam(year, "TV-show", res, "year", page, limit);
+});
+
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get("*", function (req, res) {
+  res.status(404).json({
+    response: "There is no such page",
+    success: false,
+  });
 });
 
 // Start the server
