c78b316ca2fe83a4b158befdafbee113f9907554
JohannaMJ <johanna.mannestal@gmail.com>
2021-12-13 12:31:09 +0100

    project is up and running

--- server.js
+++ server.js
@@ -1,38 +1,65 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
-// 
+//
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
+import booksData from './data/books.json';
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-mongo';
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+const Book = mongoose.model('Book', {
+  bookID: Number,
+  title: String,
+});
+
+Book.deleteMany().then(() => {
+  // data.forEach((Book) => {
+  new Book(booksData).save();
+  // });
+});
+
+// if (process.env.RESET_DB) {
+//   const seedDatabase = async () => {
+//     await booksData.deleteMany({});
+
+//     data.forEach((bookData) => {
+//       new Book(bookData).save();
+//     });
+//   };
+
+//   seedDatabase();
+// }
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+  Book.find().then((books) => {
+    res.json(books);
+  });
+  // res.send('Hello world');
+  // res.json(booksData);
+});
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
