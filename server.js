2bfe811c464190d6464aaa258cd209e056e4bc68
jakobxlindstrom <jake@lindstroem.se>
2021-12-14 17:54:06 +0100

    initial

--- server.js
+++ server.js
@@ -1,24 +1,15 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import listEndpoints from 'express-list-endpoints'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
 // import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import topMusicData from './data/top-music.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/jakob-mongo'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -28,11 +19,31 @@ app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  // res.send('Welcome to my first backend')
+  res.send(listEndpoints(app))
 })
 
-// Start the server
+// app.get('/endpoints', (req, res) => {
+//   res.send(listEndpoints(app))
+// })
+
+app.get('/top', (req, res) => {
+  res.json(topMusicData)
+})
+app.get('/top/:id', (req, res) => {
+  const { id } = req.params
+
+  const trackId = topMusicData.find((trackName) => trackName.id === +id)
+
+  if (!trackId) {
+    res.status(404).send('No artist found')
+  } else {
+    res.json(trackId)
+  }
+})
+
+// Start the server89i
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
+  console.log(`Server running on http://localhost:${port} go go go`)
 })
