8cba22b22f21e7b6d5dd3b7750b29b0b5a2a95f1
Birgit Nehrwein <nehrwein@posteo.de>
2021-12-14 14:27:39 +0100

    set up endpoints, added error messages for 400 and 503

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
-
+import listEndpoints from 'express-list-endpoints'
 import netflixData from './data/netflix-titles.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
@@ -15,10 +15,20 @@ mongoose.Promise = Promise
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
+const movies = netflixData.filter((item) => item.type === "Movie")
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(express.json())
+
+// check via middleware, if we are connected to the database
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: 'Service unavailable' })
+  }
+})
 
 // Making a model of the Netflix-Data for the DB
 const NetflixEntry = mongoose.model('NetflixEntry', {
@@ -47,9 +57,139 @@ if (process.env.RESET_DB) {
   seedDatabase()
 }
 
-// Start defining your routes here
+// Start defining routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send({
+    Welcome: "Welcome to my Netflix API",
+    documentation: "https://documenter.getpostman.com/view/18068162/UVR4PW9m"
+  })
+})
+
+// route provides all endpoints
+app.get('/endpoints', (req, res) => {
+  res.json({
+    response: listEndpoints(app),
+    success: true
+  })
+})
+
+// route provides a sorted list of all countries that are in netflixData
+app.get('/countries', (req, res) => {
+  // a list of all countries in the Netflix-Data without any duplicates. Therefore I am creating a Set (object with unique items). I convert the set into an array afterwards and sort it alphabetically
+  const countries = Array.from(new Set(netflixData.map((item) => item.country))).sort()
+
+  res.json({
+    response: countries,
+    success: true
+  })
+})
+
+// route with all shows (both movies and other) from the provided country
+app.get('/countries/:country', async (req, res) => {
+  // req is an object with properties such as query and params, 
+  // both query and params are empty objects from the beginning 
+  // every query parameter consists of key and value
+  try {
+    const { country } = req.params
+    const contentByCountry = await netflixData.filter((item) => item.country.toLowerCase() === country)
+  
+    if (contentByCountry.length === 0) {
+      res.status(404).json({
+        response: 'Sorry, but there is no content for this country',
+        success: false
+      })
+    } else {
+      res.json({
+        response: contentByCountry,
+        success: true
+      })
+    } 
+  } catch (err) {
+    res.status(400).json({ error: 'Invalid country name' })
+  }
+})
+
+// route provides all movies and has the possibility to query for director, year and actor. You can also do pagination by setting page & limit as query parameters
+app.get('/movies', (req, res) => {
+  const { director, year, actor, page, limit } = req.query
+
+  let filteredMovies = movies
+
+  if (director) {
+    filteredMovies = filteredMovies.filter((item) => item.director.toLowerCase().includes(director.toLowerCase()))
+  }
+
+  if (year) {
+    filteredMovies = filteredMovies.filter((item) => item.release_year === +year)
+  }
+
+  if (actor) {
+    filteredMovies = filteredMovies.filter((item) => item.cast.toLowerCase().includes(actor.toLowerCase()))
+  }
+
+  // PAGINATION
+  // limit = no. of items to show per page 
+  if (page && limit) {
+    // first index-no of item from filteredMovies we want to display
+    const startIndex = (page - 1) * limit
+    // last index-no of item from filteredMovies we want to display
+    const endIndex = page * limit 
+    // slice gives us what is in between startIndex and endIndex
+    const paginationArray = filteredMovies.slice(startIndex, endIndex)
+   
+    res.json(paginationArray)
+  }  
+
+  res.json({
+    response: filteredMovies,
+    success: true
+  })
+})
+
+// route provides one movie by ID
+app.get('/movies/id/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+
+    const movie = movies.find((item) => item.show_id === +id)
+  
+    if (!movie) {
+      res.status(404).json({
+        response: 'No movie found, that matches this ID',
+        success: false
+      })
+    } else {
+      res.json({
+        response: movie,
+        success: true
+      })
+    }
+  } catch (err) {
+    res.status(400).json({ error: 'Invalid movie ID' })
+  }
+})
+
+// route provides movies by name (can return more than one movie, if the provided parts of the title match with several movies)
+app.get('/movies/title/:title', (req, res) => {
+  try {
+    const { title } = req.params
+
+    const movie = movies.filter((item) => item.title.toLowerCase().includes(title.toLowerCase()))
+
+    if (movie.length === 0) {
+      res.status(404).json({
+        response: 'No movie found, that matches this title',
+        success: false
+      })
+    } else {
+      res.json({
+        response: movie,
+        success: true
+      })
+    }
+  } catch (err) {
+    res.status(400).json({ error: 'Invalid movie title' })
+  }
 })
 
 // Start the server
