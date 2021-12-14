a6df52726d52b2b480b8cf7347815ad006825447
themisk84 <efthymios.karakasis@gmail.com>
2021-12-14 15:58:54 +0100

    adding routes

--- server.js
+++ server.js
@@ -1,38 +1,85 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import netflixData from "./data/netflix-titles.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
-
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
+
+const Content = mongoose.model("Content", {
+  title: String,
+  director: String,
+  cast: String,
+  country: String,
+  date_added: String,
+  release_year: Number,
+  rating: String,
+  duration: String,
+  listed_in: String,
+  description: String,
+  type: String,
+});
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next();
+  } else {
+    res.status(503).json({
+      error: "Connection problems",
+    });
+  }
+});
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Content.deleteMany();
+
+    netflixData.forEach((item) => {
+      const newContent = new Content(item);
+      newContent.save();
+    });
+  };
+  seedDatabase();
+}
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get("/", (req, res) => {
+  Content.find().then((item) => {
+    res.json(item);
+  });
+});
+
+app.get("/year/:year", async (req, res) => {
+  const search_year = req.params.year;
+  const findYear = await Content.find({ release_year: search_year });
+  res.json(findYear);
+});
+
+app.get("/type/:type", async (req, res) => {
+  const search_type = req.params.type;
+  const findType = await Content.find({ type: search_type });
+  res.json(findType);
+});
+
+app.get("/titles/:id", async (req, res) => {
+  const title = await Content.findById(req.params.id);
+  res.json(title);
+});
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
