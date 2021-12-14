5b19cb69e685dc168d8d94f176de4a645c633f61
Kaliine <80845418+Kaliine@users.noreply.github.com>
2021-12-14 22:17:24 +0100

    set up database, seeded the database and started to add endpoints

--- server.js
+++ server.js
@@ -2,23 +2,14 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import booksData from './data/books.json'
+import { stringify } from 'nodemon/lib/utils'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+// Defines the port the app will run on. 
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -26,10 +17,72 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+// The Model
+const Book = mongoose.model('Book', {
+  bookID: Number,
+  title: String,
+  authors: String,
+  average_rating: Number,
+  isbn: Number,
+  isbn13: Number,
+  language_code: String,
+  num_pages: Number,
+  ratings_count: Number,
+  text_reviews_count: Number
+})
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Book.deleteMany({}) //async await method to make sure that the data doesn't get duplicated
+
+    // Looping through the array of booksData and
+    // for each object (item), a new book is created with the data that is inside of each object 
+    booksData.forEach(item => {
+      const newBook = new Book(item)
+      newBook.save()
+    })
+  }
+
+  seedDatabase()
+}
+
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  Book.find().then(books => {
+    res.json(books)
+  })
+  /* res.send('Hello world') */
 })
+
+app.get('/:title', (req, res) => {
+  Book.findOne({ title: req.params.title }).then(book => {
+    if (book) {
+      res.json(book)
+    } else {
+      res.status(404).json({ error: 'Page not found' })
+    }
+  })
+})
+
+/* app.get('/:authors', (req, res) => {
+  Book.findOne({ authors: req.params.authors }).then(book => {
+    if (book) {
+      res.json(book)
+    } else {
+      res.status(404).json({ error: 'Page not found' })
+    }
+  })
+})
+
+app.get('/:bookID', (req, res) => {
+  Book.findOne({ bookID: req.params.bookID }).then(book => {
+    if (book) {
+      res.json(book)
+    } else {
+      res.status(404).json({ error: 'Page not found' })
+    }
+  })
+}) */
 
 // Start the server
 app.listen(port, () => {
