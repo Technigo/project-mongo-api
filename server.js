dc9167c517026fb3deb55bd1f69b5fed98a1cea5
Lovisa Carling <lovisa.carling@gmail.com>
2021-12-14 16:25:19 +0100

    added query params for pages and average rating

--- server.js
+++ server.js
@@ -52,9 +52,116 @@ if (process.env.RESET_DB) {
 }
 
 // Start defining your routes here
+//Query param to get book by title: /books?title=enter the title
+//Query param to get book by author: /books?authors=enter the author
 app.get('/books', async (req, res) => {
-  const books = await Book.find()
-  res.json(books)
+  const { title, authors } = req.query
+
+  const allBooks = await Book.find({
+    title: new RegExp(title, 'i'),
+    authors: new RegExp(authors, 'i'),
+  })
+
+  if (!allBooks) {
+    res.status(404).json('Could not find any books..')
+  } else {
+    res.json(allBooks)
+  }
+})
+
+//Endpoint to get a single book by id
+app.get('/books/id/:id', async (req, res) => {
+  const { id } = req.params
+
+  const bookById = await Book.findOne({ bookID: id })
+
+  if (!bookById) {
+    res.status(404).json('No books with that ID found..')
+  } else {
+    res.status(200).json(bookById)
+  }
+})
+
+//Endpoint to get all books with less than 350 pages
+app.get('/books/pages/shortbooks', async (req, res) => {
+  try {
+    const shortBooks = await Book.find({ num_pages: { $lt: 350 } })
+    res.json(shortBooks)
+  } catch (error) {
+    res.status(400).json({
+      error: 'Cannot find',
+    })
+    console.log('error')
+  }
+})
+
+//Endpoint to get all books books with pages between 350 to 800
+app.get('/books/pages/mediumbooks', async (req, res) => {
+  try {
+    const mediumBooks = await Book.find({
+      $and: [{ num_pages: { $gt: 350 } }, { num_pages: { $lt: 800 } }],
+    })
+    res.json(mediumBooks)
+  } catch (error) {
+    res.status(400).json({
+      error: 'Cannot find',
+    })
+    console.log('error')
+  }
+})
+
+//Endpoint to get all books greater than and equal to 800 pages
+app.get('/books/pages/longbooks', async (req, res) => {
+  try {
+    const longBooks = await Book.find({ num_pages: { $gte: 800 } })
+    res.json(longBooks)
+  } catch (error) {
+    res.status(400).json({
+      error: 'Cannot find',
+    })
+    console.log('error')
+  }
+})
+
+//Endpoint for all books with a rating less than 2.5
+app.get('/books/ratings/lowrating', async (req, res) => {
+  try {
+    const lowRating = await Book.find({ average_rating: { $lt: 2.5 } })
+    res.json(lowRating)
+  } catch (error) {
+    res.status(400).json({
+      error: 'Cannot find',
+    })
+  }
+})
+
+//Endpoint for all books with a rating between 2.5 and 3.8
+app.get('/books/ratings/mediumrating', async (req, res) => {
+  try {
+    const mediumRating = await Book.find({
+      $and: [
+        { average_rating: { $gte: 2.5 } },
+        { average_rating: { $lt: 3.7 } },
+      ],
+    })
+    res.json(mediumRating)
+  } catch (error) {
+    res.status(400).json({
+      error: 'Cannot find',
+    })
+  }
+})
+
+//Endpoint for all books with a rating greater than 3.8
+app.get('/books/ratings/highrating', async (req, res) => {
+  try {
+    const highRating = await Book.find({ average_rating: { $gte: 3.8 } })
+    res.json(highRating)
+  } catch (error) {
+    res.status(400).json({
+      error: 'Cannot find',
+    })
+  }
 })
 
 // Start the server
