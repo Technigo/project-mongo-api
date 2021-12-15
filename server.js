091b7d99f20a7fa955cc7cc20c58adaedb07e1a4
Maria Thomasson <81348201+MariaThomasson@users.noreply.github.com>
2021-12-15 06:17:51 +0100

    Save after installing different things in order to get MongoDB to work

--- server.js
+++ server.js
@@ -7,13 +7,42 @@ import mongoose from 'mongoose'
 // 
 // import goldenGlobesData from './data/golden-globes.json'
 // import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
+import booksData from './data/books.json'
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+
+const bookSchema = new mongoose.Schema({
+  bookID: Number,
+  title: {
+    type: String,
+    lowercase: true
+  },
+  authors: {
+    type: String,
+    uppercase: true
+  },
+  average_rating: Number,
+  num_pages: Number,
+  ratings_count: Number
+})
+
+const Book = mongoose.model('Book', bookSchema)
+
+if (process.env.RESET_DB) {
+  const seedDataBase = async () => {
+    await Book.deleteMany()
+
+    booksData.forEach((item) => {
+      const newBook = new Book(item)
+      newBook.save()
+    })
+  }
+  seedDataBase()
+}
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -28,7 +57,7 @@ app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Here are all your books')
 })
 
 // Start the server
