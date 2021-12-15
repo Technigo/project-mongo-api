ca4cc766ad01faae1f5a129dff765b1e2115e7d0
fatigr26 <fatigr2692@gmail.com>
2021-12-15 16:54:59 +0100

    set up

--- server.js
+++ server.js
@@ -1,38 +1,102 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import booksData from "./data/books.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
-
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
-
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+// Defines the port the app will run on. Defaults to 8080, but can be overridden when starting the server. For example: PORT=9000 npm start
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+
+
+
+//mongoURL is the address of my database in my local machine
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1/project-mongo";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
+
+//Model: Must use camel case / model name must be the same as the variable
+const Book = mongoose.model("Book", {
+  bookID: Number,
+  title: String,
+  authors: String,
+  average_rating: Number,
+  isbn: Number,
+  isbn13: Number,
+  language_code: String,
+  num_pages: Number,
+  ratings_count: Number,
+  text_reviews_count: Number
+});
+
+
+//envioroment variable: RESET_DB: RESET_DB=true npm run dev
+//deleteMany: It's a function from mongoose and it delete all the info we have stored in the database before saving it so it does not duplicate.
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Book.deleteMany({});
+
+    
+
+  };
+  
+  seedDatabase();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Endpoints
+app.get("/", (req, res) => {
+  res.send("Welcome to my world!");
+});
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
+
+
+
+
+
+// This code is the model we created instead of using the json data
+// const User = mongoose.model("User", {
+//   name: String,
+//   age: Number,
+// });
+
+// const newUser = new User({
+//   name: "Lola",
+//   age: 35,
+// });
+
+// const newUser2 = new User({
+//   name: "Ana",
+//   age: 27,
+// });
+
+// if (process.env.RESET_DB) {
+//   const seedDatabase = async () => {
+//     await User.deleteMany({});
+
+//     newUser.save();
+//     newUser2.save();
+//   };
+//   seedDatabase();
+// }
