134f0c9419fa494d3ce774cbb4e830f5248a6b04
jakobxlindstrom <jake@lindstroem.se>
2021-12-15 11:56:02 +0100

    update

--- server.js
+++ server.js
@@ -5,12 +5,42 @@ import listEndpoints from 'express-list-endpoints'
 
 import topMusicData from './data/top-music.json'
 
-const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/jakob-mongo'
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/top'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
 const port = process.env.PORT || 8080
 const app = express()
+
+const Music = mongoose.model('Music', {
+  id: Number,
+  trackName: String,
+  artistName: String,
+  genre: String,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  loudness: Number,
+  liveness: Number,
+  valence: Number,
+  length: Number,
+  acousticness: Number,
+  speechiness: Number,
+  popularity: Number,
+  // Properties defined here match the keys
+})
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Music.deleteMany({})
+
+    topMusicData.forEach((item) => {
+      new Music(item).save()
+    })
+  }
+
+  seedDatabase()
+}
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
@@ -22,13 +52,28 @@ app.get('/', (req, res) => {
   res.send(listEndpoints(app))
 })
 
-app.get('/top', (req, res) => {
-  res.json(topMusicData)
+app.get('/top', async (req, res) => {
+  const { trackName, artistName, genre } = req.params
+
+  try {
+    const allMusic = await Music.find({
+      trackName: new RegExp(trackName, 'i'),
+      artistName: new RegExp(artistName, 'i'),
+      genre: new RegExp(genre, 'i'),
+    })
+    res.json(allMusic)
+  } catch (error) {
+    res.status(400).json({
+      error: 'cannot find',
+    })
+  }
 })
-app.get('/top/:id', (req, res) => {
+
+app.get('/top/:id', async (req, res) => {
   const { id } = req.params
 
-  const trackId = topMusicData.find((trackName) => trackName.id === +id)
+  // const trackId = topMusicData.find((trackName) => trackName.id === +id)
+  const trackId = await Music.findOne({ id: id })
 
   if (!trackId) {
     res.status(404).send('No artist found')
@@ -37,7 +82,20 @@ app.get('/top/:id', (req, res) => {
   }
 })
 
-// Start the server89i
+// app.get('/top', (req, res) => {
+//   const { trackName, artistName, genre } = req.params
+
+//   // const trackId = topMusicData.find((trackName) => trackName.id === +id)
+//   const trackId = await Music.findOne({ id: id })
+
+//   if (!trackId) {
+//     res.status(404).send('No artist found')
+//   } else {
+//     res.json(trackId)
+//   }
+// })
+
+// Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
   console.log(`Server running on http://localhost:${port} go go go`)
