b7b27048ba0b7442d4041cada6a0ab5b8a5ff215
JohannaMJ <johanna.mannestal@gmail.com>
2021-12-15 12:25:51 +0100

    cleaning up code

--- server.js
+++ server.js
@@ -1,167 +1,9 @@
-// import express from 'express';
-// import cors from 'cors';
-// import mongoose from 'mongoose';
-// import listEndpoints from 'express-list-endpoints';
-
-// // If you're using one of our datasets, uncomment the appropriate import below
-// // to get started!
-// //
-// // import goldenGlobesData from './data/golden-globes.json'
-// // import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json';
-// // import netflixData from './data/netflix-titles.json'
-// // import topMusicData from './data/top-music.json'
-
-// const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/books';
-// mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-// mongoose.Promise = Promise;
-
-// // Defines the port the app will run on. Defaults to 8080, but can be
-// // overridden when starting the server. For example:
-// //
-// //   PORT=9000 npm start
-// const port = process.env.PORT || 8080;
-// const app = express();
-
-// // Add middlewares to enable cors and json body parsing
-// app.use(cors());
-// app.use(express.json());
-
-// const Book = mongoose.model('Book', {
-//   bookID: Number,
-//   title: String,
-//   authors: String,
-//   average_rating: Number,
-//   isbn: Number,
-//   isbn13: Number,
-//   language_code: String,
-//   num_pages: Number,
-//   ratings_count: Number,
-//   text_reviews_count: Number,
-// });
-
-// if (process.env.RESET_DB) {
-//   const seedDatabase = async () => {
-//     await Book.deleteMany({});
-
-//     booksData.forEach((item) => {
-//       new Book(item).save();
-//     });
-//   };
-
-//   seedDatabase();
-// }
-
-// // Start defining your routes here
-
-// // Lists all endpoints
-// app.get('/', (req, res) => {
-//   res.send(listEndpoints(app));
-// });
-
-// //Query param to get book by title: /books?title=enter the title
-// //Query param to get book by author: /books?authors=enter the author
-// app.get('/books', async (req, res) => {
-//   const { title, authors } = req.query;
-
-//   // const allBooks = await Book.find({
-//   //   title: new RegExp(title, 'i'),
-//   //   authors: new RegExp(authors, 'i'),
-//   // })
-
-//   // if (!allBooks) {
-//   //   res.status(404).json('Could not find any books..')
-//   // } else {
-//   //   res.json(allBooks)
-//   // }
-
-//   try {
-//     const allBooks = await Book.find({
-//       title: new RegExp(title, 'i'),
-//       authors: new RegExp(authors, 'i'),
-//     });
-//     res.json(allBooks);
-//   } catch (error) {
-//     res.status(400).json({
-//       error: 'Cannot find',
-//     });
-//     console.log('error');
-//   }
-// });
-
-// //Endpoint to get a single book by id
-// app.get('/books/id/:id', async (req, res) => {
-//   const { id } = req.params;
-
-//   const bookById = await Book.findOne({ bookID: id });
-
-//   if (!bookById) {
-//     res.status(404).json('No books with that ID found..');
-//   } else {
-//     res.status(200).json(bookById);
-//   }
-// });
-
-// //Endpoint to get all books with less than 350 pages
-// app.get('/books/pages/shortbooks', async (req, res) => {
-//   try {
-//     const shortBooks = await Book.find({ num_pages: { $lt: 350 } });
-//     res.json(shortBooks);
-//   } catch (error) {
-//     res.status(400).json({
-//       error: 'Cannot find',
-//     });
-//     console.log('error');
-//   }
-// });
-
-// //Endpoint to get all books books with pages between 350 to 800
-// app.get('/books/pages/mediumbooks', async (req, res) => {
-//   try {
-//     const mediumBooks = await Book.find({
-//       $and: [{ num_pages: { $gt: 350 } }, { num_pages: { $lt: 800 } }],
-//     });
-//     res.json(mediumBooks);
-//   } catch (error) {
-//     res.status(400).json({
-//       error: 'Cannot find',
-//     });
-//     console.log('error');
-//   }
-// });
-
-// //Endpoint to get all books greater than and equal to 800 pages
-// app.get('/books/pages/longbooks', async (req, res) => {
-//   try {
-//     const longBooks = await Book.find({ num_pages: { $gte: 800 } });
-//     res.json(longBooks);
-//   } catch (error) {
-//     res.status(400).json({
-//       error: 'Cannot find',
-//     });
-//     console.log('error');
-//   }
-// });
-
-// // Start the server
-// app.listen(port, () => {
-//   // eslint-disable-next-line
-//   console.log(`Server running on http://localhost:${port}`);
-// });
-
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import listEndpoints from 'express-list-endpoints';
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-//
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
 import booksData from './data/books.json';
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/books';
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -178,7 +20,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// const bookSchema = new mongoose.Schema({
 const Book = mongoose.model('Book', {
   bookID: Number,
   title: String,
@@ -190,8 +31,6 @@ const Book = mongoose.model('Book', {
   ratings_count: Number,
   text_reviews_count: Number,
 });
-
-// const Book = mongoose.model('Book', bookSchema);
 
 if (process.env.RESET_DB) {
   console.log();
@@ -222,7 +61,7 @@ app.get('/books', async (req, res) => {
     res.json(allBooks);
   } catch (error) {
     res.status(400).json({
-      error: 'Cannot find',
+      error: 'Cannot find any books',
     });
     console.log('error');
   }
@@ -250,7 +89,7 @@ app.get('/books/pages/shortbooks', async (req, res) => {
     res.json(shortBooks);
   } catch (error) {
     res.status(400).json({
-      error: 'Cannot find',
+      error: 'Cannot find any books',
     });
     console.log('error');
   }
@@ -265,7 +104,7 @@ app.get('/books/pages/mediumbooks', async (req, res) => {
     res.json(mediumBooks);
   } catch (error) {
     res.status(400).json({
-      error: 'Cannot find',
+      error: 'Cannot find any books',
     });
     console.log('error');
   }
@@ -278,7 +117,7 @@ app.get('/books/pages/longbooks', async (req, res) => {
     res.json(longBooks);
   } catch (error) {
     res.status(400).json({
-      error: 'Cannot find',
+      error: 'Cannot find any books',
     });
     console.log('error');
   }
@@ -291,7 +130,7 @@ app.get('/books/ratings/low', async (req, res) => {
     res.json(lowRating);
   } catch (error) {
     res.status(400).json({
-      error: 'Cannot find',
+      error: 'Cannot find any books',
     });
   }
 });
@@ -308,7 +147,7 @@ app.get('/books/ratings/medium', async (req, res) => {
     res.json(mediumRating);
   } catch (error) {
     res.status(400).json({
-      error: 'Cannot find',
+      error: 'Cannot find any books',
     });
   }
 });
@@ -320,7 +159,7 @@ app.get('/books/ratings/high', async (req, res) => {
     res.json(highRating);
   } catch (error) {
     res.status(400).json({
-      error: 'Cannot find',
+      error: 'Cannot find any books',
     });
   }
 });
