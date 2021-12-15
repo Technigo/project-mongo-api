4bc459fa8b1867460c4ef29aff97fd34306e89ca
Mattsson717 <83518778+Mattsson717@users.noreply.github.com>
2021-12-15 22:49:45 +0100

    added querys

---
+++ ENDPOINTS.md
@@ -0,0 +1,3 @@
+## Provide good documentation.
+
+Always provide a list of endpoints in the documentation (for example, in the `README`). Don't force users to read your code just to learn how to use the final product.
--- server.js
+++ server.js
@@ -1,38 +1,99 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import booksData from "./data/books.json";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/patrik-books";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+const Book = mongoose.model("Book", {
+  bookID: Number,
+  title: String,
+  authors: String,
+  averageRating: Number,
+  isbn: Number,
+  isbn13: Number,
+  languageCode: String,
+  numPages: Number,
+  ratingsCount: Number,
+  textReviewsCount: Number,
+});
+
+if (process.env.RESET_DATABASE) {
+  const seedDatabase = async () => {
+    await Book.deleteMany({});
+
+    booksData.forEach((item) => {
+      const newBook = new Book(item);
+      newBook.save();
+    });
+  };
+  seedDatabase();
+}
+
+// Our own middleware that checks if the database is connected before going to our endpoints
+// app.use((req, res, next) => {
+//   if (mongoose.connection.readystate === 1) {
+//     next();
+//   } else {
+//     res.status(503).json({
+//       response: "Service unavailable",
+//       succes: false,
+//     });
+//   }
+// });
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get("/", (req, res) => {
+  res.send("Cowabunga");
+});
+
+// Get all the books
+app.get("/books", async (req, res) => {
+  let books = await Book.find(req.query);
+  if (req.query.numPages) {
+    const booksByPages = await Book.find().gt("numPages", req.query.numPages);
+    books = booksByPages;
+  }
+  res.json(books);
+});
+
+app.get("/books/:id", async (req, res) => {
+  try {
+    const bookById = await Book.findById(req.params.id);
+    if (bookById) {
+      res.json({
+        response: bookById,
+        succes: true,
+      });
+    } else {
+      res.status(404).json({
+        response: "Book not found",
+        succes: false,
+      });
+    }
+  } catch (err) {
+    res.status(404).json({
+      response: "Invalid id",
+      success: false,
+    });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
