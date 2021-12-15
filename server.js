028405471a8ac73ef766cee7b78c5cbce397d28a
fatigr26 <fatigr2692@gmail.com>
2021-12-15 21:29:01 +0100

    seeding the database

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
 //mongoURL is the address of my database in my local machine
 const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -33,28 +31,22 @@ const Book = mongoose.model("Book", {
   text_reviews_count: Number
 });
 
-
-//envioroment variable: RESET_DB: RESET_DB=true npm run dev
-//deleteMany: It's a function from mongoose and it delete all the info we have stored in the database before saving it so it does not duplicate.
+//RESET_DB: RESET_DB=true npm run dev
+//deleteMany: It's a function from mongoose and it delete all the info we have stored in the database before 
+//saving it so it does not duplicate and this way, we ensure we don't have anything in the database before we start 
+//Seedind a database:
 if (process.env.RESET_DB) {
   const seedDatabase = async () => {
-    await Book.deleteMany({});
+    await Book.deleteMany({}); 
 
-    
+    booksData.forEach(item => {
+      const newBook = new Book(item)
+      newBook.save()
+    })
 
   };
-  
   seedDatabase();
 }
-
-
-
-
-
-
-
-
-
 
 
 
@@ -75,7 +67,8 @@ app.listen(port, () => {
 
 
 
-// This code is the model we created instead of using the json data
+// This code is the model we created instead of using the json data and that's why doesn't need any looping
+
 // const User = mongoose.model("User", {
 //   name: String,
 //   age: Number,
