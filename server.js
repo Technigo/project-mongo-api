2daecc415a69d9ac164a32c38c6628895c20ed80
Lisa Blomé <81302626+annaester@users.noreply.github.com>
2021-12-15 12:38:59 +0100

    query and endpoints

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.json());
 const Book = mongoose.model("Book", {
   bookID: Number,
   title: String,
-  author: String,
+  authors: String,
   average_rating: Number,
   isbn: Number,
   isbn13: Number,
@@ -38,6 +38,8 @@ const Book = mongoose.model("Book", {
   ratings_count: Number,
   text_reviews_count: Number,
 });
+
+// you need the async and await cause JavaScript doesn't wait for fetches and methods that take longer time.
 
 if (process.env.RESET_DB) {
   const seedDatabase = async () => {
@@ -55,7 +57,61 @@ if (process.env.RESET_DB) {
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello world");
+  //list of endpoints
 });
+// .find() - all of the objects in your data
+// endpoint for all books
+app.get("/books", async (req, res) => {
+  console.log(req.query);
+  let books = await Book.find(req.query);
+
+  if (req.query.num_pages) {
+    const booksByPages = await Book.find().lt("num_pages", req.query.num_pages);
+    books = booksByPages;
+  }
+
+  if (req.query.average_rating) {
+    const booksByRating = await Book.find().gt(
+      "average_rating",
+      req.query.average_rating
+    );
+
+    books = booksByRating;
+  }
+
+  // gt = greater than
+  // lt = less than
+  // .skip - how many to skip in the list .skip(3) will skip the 3 first and give you the rest
+  // .limit() = limit(10) returns only 10
+
+  res.json(books);
+});
+
+app.get("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const bookById = await Book.findById(id);
+
+    if (bookById) {
+      res.json(bookById);
+    } else {
+      res.status(404).json({ error: "No book found" });
+    }
+  } catch (err) {
+    res.status(400).json({ error: "Id not valid" });
+  }
+
+  //where you can get the title, author, rating, language for each book in your frontend
+});
+
+app.get("/books/authors/:authors", async (req, res) => {
+  const authors = await Book.find({ authors });
+  res.send(authors); //need to map
+});
+
+// app.post("/books", {req, res} => {
+
+// })
 
 // Start the server
 app.listen(port, () => {
