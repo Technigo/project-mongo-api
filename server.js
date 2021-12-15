dfabd7510cc2f43508902605244c5204be8e2db0
Emelie <Emelie@Jophees-MacBook-Pro.local>
2021-12-15 17:12:11 +0100

    added error handling with try/catch for index and title routes, added async await to all routes.

--- server.js
+++ server.js
@@ -2,15 +2,6 @@ import express from "express"
 import cors from "cors"
 import mongoose from "mongoose"
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-//
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
-
 import spotifyTopTracks from "./data/top-tracks.json"
 
 const mongoUrl =
@@ -62,6 +53,14 @@ if (process.env.RESET_DB) {
 }
 
 // Start defining your routes here
+// Our own middleware that checks if the database is connected before going forward to our endpoints
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: "Service unavailable" })
+  }
+})
 
 app.get("/", (req, res) => {
   res.send(
@@ -69,60 +68,62 @@ app.get("/", (req, res) => {
   )
 })
 
-app.get("/tracks", (req, res) => {
-  Tracks.find().then((tracks) => {
-    res.json(tracks)
-  })
+app.get("/tracks", async (req, res) => {
+  const tracks = await Tracks.find(req.query)
+  res.json(tracks)
 })
 
-app.get("/tracks/index/:index", (req, res) => {
-  Tracks.findOne({ index: req.params.index }).then((index) => {
+app.get("/tracks/index/:index", async (req, res) => {
+  try {
+    const index = await Tracks.findOne({ index: req.params.index })
     if (index) {
       res.json(index)
     } else {
       res.status(404).json({ error: "no track found with that index number" })
     }
-  })
+  } catch (err) {
+    res.status(400).json({ error: "ERROR" })
+  }
 })
 
-app.get("/tracks/title/:title", (req, res) => {
-  Tracks.findOne({ title: req.params.title }).then((title) => {
+app.get("/tracks/title/:title", async (req, res) => {
+  try {
+    const title = await Tracks.findOne({ title: req.params.title })
     if (title) {
       res.json(title)
     } else {
       res.status(404).json({ error: "title not found" })
     }
-  })
+  } catch (err) {
+    res.status(400).json({ error: "title is invalid" })
+  }
 })
 
-app.get("/tracks/artists/:artist", (req, res) => {
-  Tracks.find({ artist: req.params.artist }).then((artist) => {
-    if (artist) {
-      res.json(artist)
-    } else {
-      res.status(404).json({ error: "no track with that bpm found" })
-    }
-  })
+app.get("/tracks/artists/:artist", async (req, res) => {
+  const artist = await Tracks.find({ artist: req.params.artist })
+  if (artist) {
+    res.json(artist)
+  } else {
+    res.status(404).json({ error: "No artist found" })
+  }
 })
 
-app.get("/tracks/year/:year", (req, res) => {
-  Tracks.find({ year: req.params.year }).then((year) => {
-    if (year) {
-      res.json(year)
-    } else {
-      res.status(404).json({ error: "titles not found on this year" })
-    }
-  })
+app.get("/tracks/year/:year", async (req, res) => {
+  const year = await Tracks.find({ year: req.params.year })
+  if (year) {
+    res.json(year)
+  } else {
+    res.status(404).json({ error: "No titles found on this year" })
+  }
 })
 
-app.get("/tracks/genre/:genre", (req, res) => {
-  Tracks.find({ genre: req.params.genre }).then((genre) => {
-    if (genre) {
-      res.json(genre)
-    } else {
-      res.status(404).json({ error: "Genre not found" })
-    }
-  })
+app.get("/tracks/genre/:genre", async (req, res) => {
+  const genre = await Tracks.find({ genre: req.params.genre })
+  if (genre) {
+    res.json(genre)
+  } else {
+    res.status(404).json({ error: "Genre not found" })
+  }
 })
 
 app.get("/tracks/bpm/:bpm", (req, res) => {
