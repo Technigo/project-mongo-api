e0de63b21e75a1a1bc134f623c96f6089cd55b70
Skrosen <76884712+Skrosen@users.noreply.github.com>
2021-12-15 17:23:05 +0100

    commented in code

--- server.js
+++ server.js
@@ -54,69 +54,69 @@ if (process.env.RESET_DB) {
 app.use(cors());
 app.use(express.json());
 
-// // Available routes/endpoints below
-// app.get("/", (req, res) => {
-// 	res.send(listEndpoints(app));
-// });
+// Available routes/endpoints below
+app.get("/", (req, res) => {
+	res.send(listEndpoints(app));
+});
 
-// app.get("/books", async (req, res) => {
-// 	// const { author, title, language } =
-// 	let filteredBooks = await Book.find(req.query);
-// 	console.log(filteredBooks);
-// 	try {
-// 		if (req.query.author) {
-// 			filteredBooks = await filteredBooks.find("author", req.query.author);
-// 		}
-// 		if (req.query.title) {
-// 			filteredBooks = await filteredBooks.find("title", req.query.title);
-// 		}
-// 		if (req.query.language) {
-// 			filteredBooks = await filteredBooks.find("language", req.query.language);
-// 		}
-// 		res.json(filteredBooks);
-// 	} catch (err) {
-// 		res.status(400).json({
-// 			response: "Search is invalid",
-// 			success: false,
-// 		});
-// 	}
-// });
+app.get("/books", async (req, res) => {
+	// const { author, title, language } =
+	let filteredBooks = await Book.find(req.query);
+	console.log(filteredBooks);
+	try {
+		if (req.query.author) {
+			filteredBooks = await filteredBooks.find("author", req.query.author);
+		}
+		if (req.query.title) {
+			filteredBooks = await filteredBooks.find("title", req.query.title);
+		}
+		if (req.query.language) {
+			filteredBooks = await filteredBooks.find("language", req.query.language);
+		}
+		res.json(filteredBooks);
+	} catch (err) {
+		res.status(400).json({
+			response: "Search is invalid",
+			success: false,
+		});
+	}
+});
 
-// //gt is greater than lt is lower than
-// app.get("/books/rating", async (req, res) => {
-// 	let books = await Book.find(req.query);
+//gt is greater than lt is lower than
+app.get("/books/rating", async (req, res) => {
+	let books = await Book.find(req.query);
 
-// 	if (req.query.average_rating) {
-// 		const booksOnRating = await books
-// 			.find()
-// 			.gt("average_rating", req.query.average_rating);
-// 		books = booksOnRating;
-// 	}
-// 	res.json(books);
-// });
+	if (req.query.average_rating) {
+		const booksOnRating = await books
+			.find()
+			.gt("average_rating", req.query.average_rating);
+		books = booksOnRating;
+	}
+	res.json(books);
+});
 
-// app.get("/books/id/:id", async (req, res) => {
-// 	const id = req.params.id;
-// 	const bookById = await books.findById(id);
-// 	try {
-// 		if (bookById) {
-// 			res.json({
-// 				response: bookById,
-// 				success: true,
-// 			});
-// 		} else {
-// 			res.status(404).json({
-// 				response: "Error: book not found",
-// 				success: false,
-// 			});
-// 		}
-// 	} catch (err) {
-// 		res.status(400).json({
-// 			response: "Id is invalid",
-// 			success: false,
-// 		});
-// 	}
-// });
+app.get("/books/id/:id", async (req, res) => {
+	const id = req.params.id;
+	const bookById = await books.findById(id);
+	try {
+		if (bookById) {
+			res.json({
+				response: bookById,
+				success: true,
+			});
+		} else {
+			res.status(404).json({
+				response: "Error: book not found",
+				success: false,
+			});
+		}
+	} catch (err) {
+		res.status(400).json({
+			response: "Id is invalid",
+			success: false,
+		});
+	}
+});
 
 // Start the server
 app.listen(port, () => {
