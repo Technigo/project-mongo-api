f1d7dbbac07d62ce9b5068e8c99a1107dfdad75c
Rephili <72921631+Rephili@users.noreply.github.com>
2021-12-15 14:54:14 +0100

    set up mongoose model and seeded the database with data

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
+import avocadoSales from './data/avocado-sales.json'
+
+
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // 
@@ -11,7 +14,7 @@ import mongoose from 'mongoose'
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-avocado-sales"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
@@ -24,15 +27,41 @@ const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(express.json())
+app.use(express.json()) 
 
-// Start defining your routes here
+// Set up a mongoose model
+const Sale = mongoose.model('Sale', {
+  id: Number,
+  date: String,
+  averagePrice: Number,
+  totalVolume: Number,
+  totalBagsSold: Number,
+  smallBagsSold: Number,
+  largeBagsSold: Number,
+  xLargeBagsSold: Number,
+  region: String
+})
+
+// Seed the database with the data for the avocado sales
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Sale.deleteMany({})
+
+    avocadoSales.forEach(item => {
+      const newSale = new Sale(item)
+      newSale.save()
+    })
+  }
+
+  seedDatabase()
+}
+
+// This is our first endpoint
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Hello from us!')
 })
 
 // Start the server
 app.listen(port, () => {
-  // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
+  console.log(`Server running on http://localhost:${port} YAY YAY`)
 })
