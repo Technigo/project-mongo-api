2a2a2a03684d565089e4ed5d696dab1cce3a93b1
Naushin Malik <nosheen.malik@gmail.com>
2021-12-16 11:26:12 +0100

    fixing data issue again

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import listEndPoints from 'express-list-endpoints'; // for listing all routes
 import topMusicData from './data/top-music.json';
+
 //----------------* for database connection *--------------------//
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/musicTrack';
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
