0a00ddf49a474fe97d4f4caaf59fd4d7b28cebeb
Jessica Nygren <jessicanygren@C02D54BJMD6R.local>
2021-12-16 23:00:37 +0100

    xx

--- server.js
+++ server.js
@@ -59,6 +59,18 @@ app.use((req, res, next) => {
 //   res.send('50 popular SpotifyTracks')
 // })
 
+app.get('/', (req, res) => {
+  const apiGuide = {
+    Endpoints: [
+      {
+        '/books------------------------->': 'Get all books',
+        '/books/id/--------------------->': 'Get book by Id',
+      },
+    ],
+  };
+  res.send(apiGuide)
+});
+
 // List of endpoints
 app.get('/endpoints', (req, res) => {
   res.send(listEndpoints(app))
