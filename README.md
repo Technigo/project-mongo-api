9a3e94c466d61a5ada420f5bdaa1c2b44ef9295f
Maria Petersson <mariapetersson@Marias-MacBook.lan>
2021-12-17 16:53:15 +0100

    added path params to title, artist, genre. added gt query for bpm. Added comments to code

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # Mongo API Project
 
-Replace this readme with your own information about your project. 
+Replace this readme with your own information about your project.
 
 Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
 
 ## The problem
 
 Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+
+## Provide good documentation.
+
+Always provide a list of endpoints in the documentation (for example, in the `README`). Don't force users to read your code just to learn how to use the final product.
 
 ## View it live
 
--- server.js
+++ server.js
@@ -1,14 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-//
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
+import listEndpoints from 'express-list-endpoints';
 import topMusicData from './data/top-music.json';
 
 const mongoUrl =
@@ -16,12 +9,23 @@ const mongoUrl =
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080;
+// Defines the port the app will run on.
+const port = process.env.PORT || 8088;
 const app = express();
+
+// Add middlewares to enable cors and json body parsing
+app.use(cors());
+app.use(express.json());
+
+// Our own middleware checks if the database is connected before going forward to our endpoints.
+// is the server up and running?
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next();
+  } else {
+    res.status(503).json({ error: 'Service unavailable' });
+  }
+});
 
 const Track = mongoose.model('Track', {
   id: Number,
@@ -40,8 +44,9 @@ const Track = mongoose.model('Track', {
   popularity: Number,
 });
 
+// Resets database. First deletes previous data, then saves new. Prevents duplications.
 if (process.env.RESET_DB) {
-  const seedDatabase = async () => {
+  const seedDataBase = async () => {
     await Track.deleteMany({});
 
     topMusicData.forEach((item) => {
@@ -50,42 +55,38 @@ if (process.env.RESET_DB) {
     });
   };
 
-  seedDatabase();
+  seedDataBase();
 }
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
-
-// Our own middleware that checks if the database is connected before going forward to our endpoints
-app.use((req, res, next) => {
-  if (mongoose.connection.readyState === 1) {
-    next();
-  } else {
-    res.status(503).json({ error: 'Service unavailable' });
-  }
-});
-
-// Start defining your routes here
+// Defining routes starts here
 app.get('/', (req, res) => {
   res.send('Hello musiclover');
 });
-
-// get all the songs. A simple solution.
-app.get('/songs', async (req, res) => {
-  let songs = await Track.find(req.query);
-
-  // filtering songs on bpm. the bpm will be greater that the value we type in.
-  if (req.query.bpm) {
-    const songsByBPM = await Track.find().gt('bpm', req.query.bpm);
-    songs = songsByBPM;
-  }
-
-  res.json(songs);
+// See all possible endpoints
+app.get('/endpoints', (req, res) => {
+  res.send(listEndpoints(app));
 });
 
-// get one song based on id
-app.get('/songs/:id', async (req, res) => {
+// get the songs from the database (not a local file).
+// .find is a mongoose soultion. it can take a while and need async and await.
+app.get('/songs', async (req, res) => {
+  let songs = await Track.find(req.query);
+  if (songs) {
+    // greater than-filter songs by bpm.
+    // Example: songs/?bpm=110 will show all songs with a bom of 10 or higher.
+    if (req.query.bpm) {
+      const songsByBpm = await Track.find().gt('bpm', req.query.bpm);
+      songs = songsByBpm;
+    }
+    res.json(songs);
+  } else {
+    res.status(404).json({ error: 'Song not found' });
+  }
+});
+
+// Get track by id. We use this to find only one result. findById is a mongoose-thing.
+// try and catch will catch unexpected errors and prevent the database from thinking forever.
+app.get('/songs/id/:id', async (req, res) => {
   try {
     const songById = await Track.findById(req.params.id);
     if (songById) {
@@ -95,6 +96,36 @@ app.get('/songs/:id', async (req, res) => {
     }
   } catch (err) {
     res.status(400).json({ error: 'The id is invalid' });
+  }
+});
+
+// get song by title. Write %20 instead of spaces.
+app.get('/songs/title/:title', async (req, res) => {
+  const songByTitle = await Track.findOne({ title: req.params.trackName });
+  if (songByTitle) {
+    res.json(songByTitle);
+  } else {
+    res.status(404).json({ error: 'title not found' });
+  }
+});
+
+// Get artist.
+app.get('/songs/artist/:artist', async (req, res) => {
+  const artist = await Track.find({ artist: req.params.artistName });
+  if (artist) {
+    res.json(artist);
+  } else {
+    res.status(404).json({ error: 'Artist not found' });
+  }
+});
+
+// get a specific genre.
+app.get('/songs/genre/:genre', async (req, res) => {
+  const songByGenre = await Track.find({ genre: req.params.genre });
+  if (songByGenre) {
+    res.json(songByGenre);
+  } else {
+    res.status(404).json({ error: 'Genre not found' });
   }
 });
 
