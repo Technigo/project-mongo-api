71349dabd9d5b4f30a10bb1047f74b660f3d9b0e
Elin Elmvik Diczfalusy <elinelmvik@gmail.com>
2021-12-17 17:27:21 +0100

    added listEndpoints

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import { param } from 'express/lib/request'
 import dotenv from 'dotenv'
+import listEndpoints from 'express-list-endpoints'
 
 dotenv.config()
 
@@ -11,17 +12,6 @@ import netflixData from './data/netflix-titles.json'
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-mongo'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
-
-// Defines the port the app will run on. Defaults to 8080, but can be
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
-
-// Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
 
 const Title = mongoose.model('Title', {
   show_id: Number,
@@ -51,13 +41,24 @@ if (process.env.RESET_DB) {
   seeDatabase()
 }
 
+//   PORT=9000 npm start
+const port = process.env.PORT || 8080
+const app = express()
+
+// Add middlewares to enable cors and json body parsing
+app.use(cors())
+app.use(express.json())
+
 // Start defining your routes here
 app.get('/', async (req, res) => {
-  const netflixTitles = await Title.find().limit(30)
+  res.send(listEndpoints(app))
+})
+
+//all netflix titles here
+app.get('/titles', async (req, res) => {
+  const netflixTitles = await Title.find().limit(3)
 
   res.json(netflixTitles)
-
-  //res.send(process.env.API_KEY)
 })
 
 // Start defining your routes here
