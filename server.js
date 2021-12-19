707b9beecb38c7adf253e02fe0106494c908d032
katiewu1 <83988618+katiewu1@users.noreply.github.com>
2021-12-19 16:25:19 +0100

    add pagination to endpoint '/boardgames/year/:year'

--- server.js
+++ server.js
@@ -189,7 +189,16 @@ app.get('/boardgames/:id', async (req, res) => {
 app.get('/boardgames/year/:year', async (req, res) => {
   try {
     const { year } = req.params
+
+    // pagination: page = 0 and limit = 20, or we can change the value based on the query params
+    const pagination = {
+      page: parseInt(req.query.page, 10) || 0,
+      limit: parseInt(req.query.limit, 10) || 20,
+    }
+
     const boardGameByYear = await BoardGame.find({ year })
+      .skip(pagination.page * pagination.limit)
+      .limit(pagination.limit)
     // console.log(boardGameByYear.length)
     if (boardGameByYear.length > 0) {
       res.status(200).json({
@@ -211,9 +220,9 @@ app.get('/boardgames/year/:year', async (req, res) => {
 })
 
 // sort the board games by rank, from top to bottom
-// http://localhost:8080/ranks
-// http://localhost:8080/ranks?page=1
-// http://localhost:8080/ranks?page=0&limit=5
+// http://localhost:8080/ranked
+// http://localhost:8080/ranked?page=1
+// http://localhost:8080/ranked?page=0&limit=5
 app.get('/ranked', async (req, res) => {
   try {
     // pagination - page = 0 and limit = 20 or we can change the value based on the query params
@@ -227,8 +236,6 @@ app.get('/ranked', async (req, res) => {
       .skip(pagination.page * pagination.limit)
       .limit(pagination.limit)
 
-    // console.log(boardGameRanks.length)
-
     if (boardGameRanks.length > 0) {
       res.status(200).json({
         response: boardGameRanks,
@@ -240,82 +247,6 @@ app.get('/ranked', async (req, res) => {
         success: false,
       })
     }
-  } catch (err) {
-    res.status(404).json({
-      error: 'Page not found',
-      success: false,
-    })
-  }
-})
-
-// use query for sorting instead? /boardgames?order=
-
-// list top 20 ranked board games (according to Bayesian average)
-app.get('/top_20', async (req, res) => {
-  try {
-    const boardGameTop20 = await BoardGame.find().sort({ rank: 1 }).limit(20)
-    if (boardGameTop20.length > 0) {
-      res.status(200).json({
-        response: boardGameTop20,
-        success: true,
-      })
-    } else {
-      res.status(404).json({
-        error: 'Top 20 board games not found',
-        success: false,
-      })
-    }
-  } catch (err) {
-    res.status(404).json({
-      error: 'Page not found',
-      success: false,
-    })
-  }
-})
-
-// list top 20 most rated board games by users
-app.get('/top_20_rated', async (req, res) => {
-  try {
-    const boardGameTopRated = await BoardGame.find()
-      .sort({ usersRated: -1 })
-      .limit(20)
-
-    if (boardGameTopRated.length > 0) {
-      res.status(200).json({
-        response: boardGameTopRated,
-        success: true,
-      })
-    } else {
-      res.status(404).json({
-        error: 'Top 20 most rated board games by users not found',
-        success: false,
-      })
-    }
-  } catch (err) {
-    res.status(404).json({
-      error: 'Page not found',
-      success: false,
-    })
-  }
-})
-
-// sort the board games alphabetically, from characters (# * .) -> number (0-9) -> letters (a-z, other)
-app.get('/alphabet', async (req, res) => {
-  try {
-    const boardGameAlphabet = await BoardGame.find(req.query).sort({ name: 1 })
-
-    if (boardGameAlphabet.length > 0) {
-      res.status(202).json({
-        response: boardGameAlphabet,
-        success: true,
-      })
-    } else {
-      res.status(404).json({
-        response: 'List of board games (alphabetically) not found',
-        success: false,
-      })
-    }
-    res.json(boardGameAlphabet)
   } catch (err) {
     res.status(404).json({
       error: 'Page not found',
