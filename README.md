fefd44bb922941db61bcee2b9bc708a10c13b139
intehon <emelie.soderhielm@gmail.com>
2021-12-21 20:29:54 +0100

    add endpoints, query, readme

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # Mongo API Project
 
-Replace this readme with your own information about your project. 
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+Building an API in mongo db
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+To list all books: /books
+
+To find by query: `/books?numPages={query}` (the number you enter + everything above), `/books?averageRating={query}` (the number you enter + everything above), `/books?ratingsCount={query}`(the number you enter + everything above), `/books?languageCode={query}`
+
+To list all authors: /books/authors (shows in unordered list)
+
+To find by id: `/books/{id}`
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+~coming soon~
--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import listEndpoints from 'express-list-endpoints'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
+import booksData from './data/books.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -19,20 +13,141 @@ mongoose.Promise = Promise
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 9090
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(express.json())
 
+const Book = mongoose.model('Book', {
+  bookID: Number,
+  title: String,
+  authors: String,
+  averageRating: Number,
+  isbn: Number,
+  isbn13: Number,
+  languageCode: String,
+  numPages: Number,
+  ratingsCount: Number,
+  textReviewsCount: Number
+})
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Book.deleteMany({})
+
+    booksData.forEach(item => {
+      const newBook = new Book(item)
+      newBook.save()
+    })
+  }
+  seedDatabase()
+}
+
+// Our own middleware to see if the database is connected before going to our endpoints
+
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({
+      error: 'Service unavailable',
+      success: false
+    })
+  }
+})
+
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send(listEndpoints(app))
 })
+
+//mongoose way of getting endpoints
+//get all the books
+
+app.get('/books', async (req, res) => {
+  let books = await Book.find(req.query)
+
+  // filter by query for pages per book, if greater than or equal to given number
+
+  if (req.query.numPages) {
+    const booksByPages = await Book.find().gt('numPages', req.query.numPages)
+    books = booksByPages
+  }
+
+  // filter by rating
+
+  if (req.query.averageRating) {
+    const booksByRating = await Book.find().gt('averageRating', req.query.averageRating)
+    books = booksByRating
+  }
+
+  // filter by ratings count
+
+  if (req.query.ratingsCount) {
+    const booksByRatingCount = await Book.find().gt('ratingsCount', req.query.ratingsCount)
+    books = booksByRatingCount
+  }
+
+
+  res.json(books)
+})
+
+//get one book based on id
+
+app.get('/books/:id', async (req, res) => {
+  try {
+    const bookById = await Book.findById(req.params.id)
+    if (bookById) {
+      res.json({
+        response: bookById,
+        success: true
+      })
+    } else {
+      res.status(404).json({ 
+        response: 'Book not found', 
+        success: false 
+      })
+    }
+  } catch(err) {
+    res.status(400).json({
+      response: 'Invalid request',
+      success: false
+    })
+  }
+})
+
+
+
+
+// Authors endpoint
+
+app.get('/books/authors', async (req, res) => {
+  try {
+    const bookByAuthors = await Book.distinct('authors')
+    if (bookByAuthors) {
+      res.json({
+        response: bookByAuthors,
+        success: true
+      })
+    } else {
+      res.status(404).json({ 
+        response: 'Authors not found', 
+        success: false 
+      })
+    }
+  } catch (err) {
+    res.status(400).json({
+      response: 'Invalid request',
+      success: false
+    })
+  }
+})
+
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
+  console.log(`Server running on http://localhost:${port} ~(^@_^)~`)
 })
