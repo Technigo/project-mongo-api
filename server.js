dd2463e9cf92fc8d0b7b41ecad8d2c3248770b1a
molbimien <43146046+molbimien@users.noreply.github.com>
2022-01-02 13:22:57 +0100

    added own middleware that checks if the database is connected before going forward to our endpoints

--- server.js
+++ server.js
@@ -3,15 +3,8 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 
 import netflixData from './data/netflix-titles.json'
+import listEndpoints from 'express-list-endpoints'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -27,6 +20,15 @@ const app = express()
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(express.json())
+
+// Our own middleware that checks if the database is connected before going forward to our endpoints
+app.use((req, res, next) => {
+	if (mongoose.connection.readyState === 1) {
+		next();
+	} else {
+		res.status(503).json({ error: "Service unavailable" });
+	}
+});
 
 // Add model for the db
 const NetflixTitle = mongoose.model('NetflixTitle', {
@@ -60,7 +62,36 @@ if (process.env.RESET_DB) {
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  // res.send('Hello from us!')
+  res.send(listEndpoints(app))
+})
+
+// Get all the Netflix movies and shows
+app.get('/netflixtitles', async (req, res) => {
+  const netflixTitles = await NetflixTitle.find(req.query)
+  res.json(netflixTitles)
+})
+
+// Get one Netflix title based on id
+app.get('/netflixtitles/id/:id', async (req, res) => {
+  try {
+    const netflixTitleById = await NetflixTitle.findById(req.params.id)
+    if(!netflixTitleById) {
+      res.status(404).json({
+        response: 'Title not found',
+        success: false,
+      })
+    } else {
+      res.json({
+        response: netflixTitleById,
+        success: true,
+      })
+    }
+  } catch (err) {
+    res.status(400).json({
+      error: 'Id is invalid'
+    })
+  }
 })
 
 // Start the server
