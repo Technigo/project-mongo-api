d93033cf667dc7f3125f398a6ab0f26df554f2c2
Jenny Quach <quach.jen@gmail.com>
2022-01-04 01:41:02 +0100

    added an endpoint /books/:id

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import booksData from './data/books.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// 
-// import goldenGlobesData from './data/golden-globes.json'
-// import avocadoSalesData from './data/avocado-sales.json'
-// import booksData from './data/books.json'
-// import netflixData from './data/netflix-titles.json'
-// import topMusicData from './data/top-music.json'
-
-
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/books"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
 
 // Defines the port the app will run on
 const port = process.env.PORT || 8080
@@ -21,23 +15,61 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/books"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
-
-
-const Author = mongoose.model('Author', {
-  name: String
+//middleware
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: 'Service unavailable' })
+  }
 })
 
-// Start defining your routes here
+const Book = mongoose.model('Book', {
+  id: Number,
+  title: String,
+  author: String,
+  rating: Number,
+  language: String,
+  isbn: Number,
+  pages: Number,
+})
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Book.deleteMany({})
+
+    booksData.forEach(book => {
+      new Book(book).save()
+    })
+  }
+
+  seedDatabase()
+}
+
+// This is our first endpoint
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
-app.get('/authors', async (req, res) => {
-  const authors = await Author.find()
-  res.json(authors)
+// get all the books
+
+app.get('/books', async (req, res) => {
+  const books = await Book.find()
+  res.json(books)
+})
+
+// get a specific book based on id
+app.get('/books/:id', async (req, res) => {
+  try {
+    const bookId = await Book.findById(req.params.id)
+    if (bookId) {
+      res.json(bookId)
+    } else {
+      res.status(404).json({error: 'No book found with that id'})
+    }
+  } catch (err) {
+    res.status(400).json({ error: 'Id is invalid'})
+  }
 })
 
 // Start the server
