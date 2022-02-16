6cd4de36e870d598d16d2fc29f095a5a5d45a9c6
Jsfrulle <Jessicamhermansson@gmail.com>
2022-02-16 12:58:50 +0100

    fix

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import mongoose from "mongoose";
 import musicData from "./data/music.json";
 
-
 const mongoUrl =
   process.env.MONGO_URL || "mongodb://localhost/project-mongo-api";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -16,11 +15,11 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 /* templat för vad som finns i ett objekt   */
 
-const Music = mongoose.model('Music', {
+const Music = mongoose.model("Music", {
   id: Number,
   track: String,
   artist: String,
@@ -37,50 +36,41 @@ const Music = mongoose.model('Music', {
   popularity: Number
 });
 
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Music.deleteMany({});
 
-
-
-if (process.env.RESET_DB) { 
-  const seedDatabase = async () => { 
-    await Music.deleteMany({}) 
-
-    musicData.forEach(item => { 
-      const newMusic = new Music(item) 
-      newMusic.save()
-    })
-  }
-  seedDatabase()
+    musicData.forEach((item) => {
+      const newMusic = new Music(item);
+      newMusic.save();
+    });
+  };
+  seedDatabase();
 }
 
-
-
-app.get("/", async (req, res) => {
-  res.json('hello')
+app.get('/', async (req, res) => {
+  res.json("hello");
 });
 
+app.get('/tracks', async (req, res) => {
+  const allTracks = await Music.find();
+  res.json(allTracks);
+});
 
-app.get("/tracks", async (req, res) => {
-  const allTracks = await Music.find()
-  res.json(allTracks)
-})
-
-
-
-
-app.get("/artists/:artist", async (req, res) => {
+app.get('/artists/:artist', async (req, res) => {
   try {
-    const artist = await Music.find({ artist: req.params.artist })
+    const artist = await Music.find({ artist: req.params.artist });
     if (artist) {
-      res.json(artist)
+      res.json(artist);
     } else {
-      res.status(404).json({ error: "No artist found" })
+      res.status(404).json({ error: "No artist found" });
     }
   } catch (err) {
-    res.status(400).json({ error: "artist is not valid" })
+    res.status(400).json({ error: "artist is not valid" });
   }
-})
+});
 
-app.get("/artists/:id", async (req, res) => {
+app.get('/artists/:id', async (req, res) => {
   try {
     const songId = await Music.findById({ id: req.params.id });
 
