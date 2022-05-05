e9aa539b16c679bb441244b7c379943b96f2b6a4
Rawisara Saman <rawiisara@gmail.com>
2022-05-05 21:46:33 +0200

    data json added to mongo DB

--- server.js
+++ server.js
@@ -1,35 +1,60 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from 'body-parser'
 import mongoose from "mongoose";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import netflixData from "./data/netflix-titles.json";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+require('dotenv/config')
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/world-streaming-entertainment";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+  console.log('connected to Mongo DB')
+});
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
+
+const port = process.env.PORT || 8000;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+const NetflixItem = mongoose.model('NetflixItem', {
+  "show_id": Number,
+  "title": String,
+  "director": String,
+  "cast": String,
+  "country": String,
+  "date_added": String,
+  "release_year": Number,
+  "rating": String,
+  "duration": String,
+  "listed_in": String,
+  "description": String,
+  "type": String
+})
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await NetflixItem.deleteMany({})
+
+    netflixData.forEach((item) => {
+      new NetflixItem(item).save()
+    })
+  }
+
+  seedDatabase()
+}
+
 app.use(cors());
+app.use(bodyParser.json())
 app.use(express.json());
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+
+app.get("/", async (req, res) => {
+  const allNetflixItems = await NetflixItem.find()
+  res.json(allNetflixItems)
 });
 
-// Start the server
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
