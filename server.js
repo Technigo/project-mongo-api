e732011165dacc536f14bd3d34d6e89b5c3513f0
Lisa Bergström <lisa.bergstrom@live.se>
2022-05-11 15:01:38 +0200

    added endpoints

--- server.js
+++ server.js
@@ -8,16 +8,45 @@ import mongoose from "mongoose";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import topMusicData from "./data/top-music.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
+const Song = mongoose.model("Song", {
+  id: Number,
+  trackName: String,
+  artistName: String,
+  genre: String,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  loudness: Number,
+  liveness: Number,
+  valence: Number,
+  length: Number,
+  acousticness: Number,
+  speechiness: Number,
+  popularity: Number,
+});
+
+//If the database is resetting this will happen
+if (process.env.RESET_DB) {
+  const SeedDatabase = async () => {
+    await Song.deleteMany();
+    topMusicData.forEach((singleSong) => {
+      const newSong = new Song(singleSong);
+      newSong.save();
+    });
+  };
+  SeedDatabase();
+}
+
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8090;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
@@ -26,8 +55,53 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Hello!");
 });
+
+app.get("/artists", async (req, res) => {
+  const artists = await Song.find();
+  res.json(artists);
+});
+
+app.get("/tracks", async (req, res) => {
+  const tracks = await Song.find();
+  res.json(tracks);
+});
+
+// app.get("/songs/song", async (req, res) => {
+// //   //this will retrive only the first found song
+// //   const { artistName, trackName, energy } = req.query;
+
+// //   const artistNameRegex = new RegExp(artistName, "i");
+// //   const trackNameRegex = new RegExp(trackName, "i");
+// //   const energyRegex = new RegExp(energy, "i");
+
+// //   const singleSong = await Song.find({
+// //     artistName: artistName,
+// //     trackName: trackName,
+// //     energy: energy,
+// //   });
+// //   res.send(singleSong);
+// // });
+
+app.get("/songs/song/:artistName", async (req, res) => {
+  //this will retrive only the first found song
+  const singleSong = await Song.find({ artistName: req.params.artistName });
+  res.send(singleSong);
+});
+
+//  try {
+//    const artist = await Song.findById(req.params.id);
+//  if (artist) {
+//   res.json(artist)
+// } else {
+// }
+//   res.status(404).json({error: 'User not find!'})
+// } catch (err) {
+//   res.status(400).json({error: 'Invalid user Id'})
+// }
+
+// })
 
 // Start the server
 app.listen(port, () => {
