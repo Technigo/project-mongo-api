52166bbc0aede0bfb15d2214fce8c0451dfc3b21
Isabell Wästfelt <isabell@iMac-som-tillhor-Isabell.local>
2022-05-12 13:54:20 +0200

    installed express-list-endpoints

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import listEndpoints from 'express-list-endpoints'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
 import topMusicData from './data/top-music.json'
 
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-mongo'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -60,10 +52,11 @@ app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello!')
+  res.send(listEndpoints(app))
 })
 
 app.get('/artists', async (req, res) => {
+  //Will show the list of Top Music
   const artists = await Song.find()
   res.json(artists)
 })
@@ -74,11 +67,13 @@ app.get('/tracks', async (req, res) => {
 })
 
 app.get('/songs/song/:artistName', async (req, res) => {
+  //Will retrive only the first found song
   const singleSong = await Song.findOne({ artistName: req.params.artistName })
   res.send(singleSong)
 })
 
 app.get('/songs/genre/:genre', async (req, res) => {
+  //Will retrive the songs that belongs to the genre
   const singleGenre = await Song.find({ genre: req.params.genre })
   res.send(singleGenre)
 })
