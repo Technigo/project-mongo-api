2cb6723ce90c3d60e20a7c1d92d01394db944676
Isabell Wästfelt <isabell@iMac-som-tillhor-Isabell.local>
2022-05-12 15:17:31 +0200

    deleted code

--- server.js
+++ server.js
@@ -82,6 +82,3 @@ app.get('/songs/genre/:genre', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
-
-// const myRegex = /.*/gm
-// artistName: artistName ? artistName : myRegex,
