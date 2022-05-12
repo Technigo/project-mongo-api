6c6c9df2ac1fb149072003776a6cd2c981181b0e
michaelchangdk <michaelchangdk@icloud.com>
2022-05-12 16:46:23 +0200

    added data file

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import doItBeFartin from "./data/doitbefartin.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/doitbefartin";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -24,9 +17,76 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const Animal = mongoose.model("Animal", {
+  name: String,
+  scientificName: String,
+  doesItFart: Boolean,
+  doesItMaybeFart: Boolean,
+  descriptionExists: Boolean,
+  descriptionNotes: String,
+  paperLink: String,
+  verifiedBy: String,
+});
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Animal.deleteMany();
+    doItBeFartin.forEach((animal) => {
+      new Animal(animal).save();
+    });
+  };
+  seedDatabase();
+}
+
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send([
+    { "/": "Home" },
+    { "/animals": "All the data" },
+    { "/animal/:animal": "Find one animal" },
+    { "/description/:boolean": "True or false if there a fart description" },
+    { "/doitbefartin/:boolean": "True or false if it be fartin" },
+    {
+      "/doitmaybebefartin/:boolean": "True or false if it MAYBE be fartin",
+    },
+  ]);
+});
+
+app.get("/animals", (req, res) => {
+  res.status(200).json({
+    data: doItBeFartin,
+    success: true,
+  });
+});
+
+app.get("/animal/:animal", (req, res) => {
+  // const animalSearch = await Animal.find({name: req.params.animal})
+  // res.send(animalSearch)
+  res.status(200).json({
+    data: doItBeFartin,
+    success: true,
+  });
+});
+
+app.get("/description/:boolean", (req, res) => {
+  res.status(200).json({
+    data: doItBeFartin,
+    success: true,
+  });
+});
+
+app.get("/doitbefartin/:boolean", (req, res) => {
+  res.status(200).json({
+    data: doItBeFartin,
+    success: true,
+  });
+});
+
+app.get("/doitmaybebefartin/:boolean", (req, res) => {
+  res.status(200).json({
+    data: doItBeFartin,
+    success: true,
+  });
 });
 
 // Start the server
