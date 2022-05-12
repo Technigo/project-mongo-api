9b7c7c46102634a2de7cfa8dc8742fa5f2893e5c
bostrombundock <sbostrom@ymail.com>
2022-05-12 23:25:17 +0200

    added new endpoints

--- server.js
+++ server.js
@@ -1,32 +1,63 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import topMusicData from "./data/top-music.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+const Song = mongoose.model('Song', {
+  id: Number,
+  trackName: String, 
+  artistName: String,
+  genre: String,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  loudness: Number,
+  liveness: Number, 
+});
+
+if(process.env.RESET_DB) {
+const seedDatabase = async () => {
+await Song.deleteMany();
+topMusicData.forEach(singleSong => { 
+  const newSong = new Song(singleSong);
+  newSong.save();
+})
+}
+seedDatabase();
+}
+
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+
 app.use(cors());
 app.use(express.json());
 
+app.get('/songs/song', async (req, res) => {
+  const {artistName, genre, energy} = req.query;
+const singleSong = await Song.findOne({artistName: artistName, genre: genre, energy: energy});
+res.send(singleSong);
+});
+
+
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+app.get('/', (req, res) => {
+  res.send('Welcome to the Api for the top music!');
+});
+
+app.get('/songs/song/:artistName', async (req, res) => {
+  //Will retrive only the first found song
+  const artistName = await Song.findOne({ artistName: req.params.artistName });
+  res.send(artistName);
+});
+
+app.get('/songs/genre/:genre', async (req, res) => {
+  const singleGenre = await Song.find({ genre: req.params.genre });
+  res.send(singleGenre);
 });
 
 // Start the server
