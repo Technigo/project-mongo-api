d8359756969d11ff4039e603f30a44e0364fa2be
StepbyStepp <91485842+SteppbySteph@users.noreply.github.com>
2022-05-13 15:34:42 +0200

    added another endpoint for title

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # Project Mongo API
 
-Replace this readme with your own information about your project.
+The purpuose of this project is to learn how to use Mongodb to store data, and how to query that data from your API.
+The endpoints you can look for are: 
 
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+../books 
+../books/:id
 
 ## The problem
 
 Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+There have been issues deploying the project with heroku. At the beginning the problem was solved after adding a key/value to the "config var" in Heroku and then I had to sign up to a mongo cloud to be able to upload my new dataset.  
 
 ## View it live
 
 https://booksfor-mongo-api.herokuapp.com/
 
-endpoints:
-../books 
-../books/:id
--- server.js
+++ server.js
@@ -8,9 +8,6 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -56,16 +53,16 @@ app.get("/books", async (req, res) => {
   })
 });
 
-// //adding middleware to check if the database is up or not
-// app.use((req, res, next) => {
-//   if (mongoose.connection.readyState === 1) {
-//     next()
-//   } else {
-//     res.status(503).json({ error: 'Service unavailable'})
-//   }
-// })
+//adding middleware to check if the database is up or not
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: 'Service unavailable'})
+  }
+})
 
-//returning one single object
+//returning one single object by id
 app.get("/books/:id", async (req, res) => {
   try {
     const bookById = await Book.findOne({ bookID: req.params.id})
@@ -86,6 +83,24 @@ app.get("/books/:id", async (req, res) => {
       success: false,
     })
   }
+})
+
+//finding a title using path params
+app.get("/books/title/:title", async (req, res) => {
+  const { title } = req.params;
+  const bookTitle = await Book.find({title: title});
+  
+    if (bookTitle.length !== 0) {
+      res.status(200).json({
+        data: bookTitle,
+        success: true,
+      })
+    } else {
+      res.status(404).json({
+        error: 'title not found',
+        success: false,
+      })
+    }
 })
 
 // Start the server
