306b3c4d366c508448ffdb23047252e5963522f5
Ida Halling <idahalling@Idas-MacBook-Pro.local>
2022-05-13 09:27:27 +0200

    everything

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
 # Project Mongo API
 
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+This week's project was to start using a database with MongoDB to store data and retrieve data from it, and use that data to produce a RESTful API.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+I used a top-music dataset that was included for this express-api project, but "converted" it using MongoDB and mongoose model.
 
 ## View it live
-
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
--- server.js
+++ server.js
@@ -1,35 +1,110 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import getEndpoints from 'express-list-endpoints'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.Promise = Promise;
+import topMusicData from './data/top-music.json'
+import { reset } from 'nodemon'
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
+const port = process.env.PORT || 8080
+const app = express()
+
+const mongoUrl =
+  process.env.MONGO_URL || 'mongodb://localhost/project-mongoMongo'
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+const Song = mongoose.model('Song', {
+  id: Number,
+  trackName: String,
+  artistName: String,
+  genre: String,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  loudness: Number,
+  liveness: Number,
+  valence: Number,
+  length: Number,
+  acousticness: Number,
+  speechiness: Number,
+  popularity: Number
+})
+
+// seed data
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Song.deleteMany()
+    topMusicData.forEach((singleSong) => {
+      const newSong = new Song(singleSong)
+      newSong.save()
+    })
+  }
+  seedDatabase()
+}
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+
+app.get('/', (req, res) => {
+  res.send(getEndpoints(app))
+})
+
+// ----- path params -------
+app.get('/artists', async (req, res) => {
+  //Will show the list of Top Music
+  const artists = await Song.find()
+  res.json(artists)
+})
+
+app.get('/tracks', async (req, res) => {
+  const tracks = await Song.find()
+  res.json(tracks)
+})
+
+app.get('/songs/song/:artistName', async (req, res) => {
+  // this will retrive only the first found song
+  // create a new variabel and Song is our moongose model
+  const singleSong = await Song.find({ artistName: req.params.artistName })
+  res.send(singleSong)
+})
+
+app.get('/songs/genre/:genre', async (req, res) => {
+  //Will retrive the songs that belongs to the genre
+  const singleGenre = await Song.find({ genre: req.params.genre })
+  res.send(singleGenre)
+})
+
+// ----- query params -------
+app.get('/songs/song/', async (req, res) => {
+  const { artistName, trackName } = req.query
+
+  const myRegex = /.*/gm
+
+  if (trackName) {
+    const singleSong = await Song.find({
+      artistName: artistName ? artistName : myRegex,
+      trackName: trackName
+    })
+    res.send(singleSong)
+  } else {
+    const singleSong = await Song.find({ artistName: artistName })
+    res.send(singleSong)
+  }
+})
+
+// http://localhost:8080/songs/song?artistName=Marshmello&trackName=Happier  - en låt från en artist
+// http://localhost:8080/songs/song?trackName=Happier  - en låt med ett givet namn
+// http://localhost:8080/songs/song?artistName=Marshmello- alla låtar från en artist FUNKAR
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+  console.log(`Server running on http://localhost:${port}`)
+})
