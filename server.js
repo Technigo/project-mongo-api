a787f93b3ba330d6f58ef34162aa4025a20b3695
Katarina Elgerus <katarina.elgerus@liv.se>
2022-05-13 09:41:56 +0200

    first setup

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-
+import getEndpoints from "express-list-endpoints";
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // import avocadoSalesData from "./data/avocado-sales.json";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import topMusicData from "./data/top-music.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -20,16 +20,85 @@ mongoose.Promise = Promise;
 const port = process.env.PORT || 8080;
 const app = express();
 
+/*const User = mongoose.model("User",{
+  name: String,
+  age: Number,
+  deceased: Boolean
+})*/
+
+const Song = mongoose.model("Song",{
+  "id": Number,
+    "trackName": String,
+    "artistName": String,
+    "genre": String,
+    "bpm": Number,
+    "energy": Number,
+    "danceability": Number,
+    "loudness": Number,
+    "liveness": Number,
+    "valence": Number,
+    "length": Number,
+    "acousticness":Number,
+    "speechiness": Number,
+    "popularity": Number
+
+})
+
+
+if(process.env.RESET_DB) {
+  const seedDatabase=async () => {
+    await Song.deleteMany()
+    topMusicData.forEach(singleSong=>{
+      const newSong=new Song(singleSong)
+      newSong.save()
+    })
+  }
+  seedDatabase()
+}
+
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send(getEndpoints(app));
 });
+
+app.get("/songs", async (req,res)=>{
+  Song.find().then(songs=>{
+    res.send(songs)
+  })
+})
+
+app.get('/songs/genre/:genre', async (req, res) => {
+  const singleGenre = await Song.find({ genre: req.params.genre })
+  res.send(singleGenre)
+}) 
+
+app.get("/songs/song", async (req, res)=>{
+ const {artistName, trackName}=req.query
+
+//const artistNameRegex= new RegExp(artistName, "i");
+//const trackNameRegex= new RegExp(trackName, "i");
+//const energyRegex= new RegExp(energy, "i");
+
+
+if (trackName){const singleSong= await Song.find({
+  artistName: artistName,
+  trackName: trackName
+})
+  res.send(singleSong)
+}
+
+else { const singleSong= await Song.find({
+  artistName: artistName,
+})
+res.send(singleSong)}
+})
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
