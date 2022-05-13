13bbfe6f61ba91b619d4971b2303b51501e4965e
Jessica Sandler <jessica.sandler@hotmail.com>
2022-05-13 14:35:59 +0200

    Add routes and documentation

--- server.js
+++ server.js
@@ -1,32 +1,33 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import allEndpoints from 'express-list-endpoints'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import players from "./data/football-players.json"
 
-import players from './data/football-players.json'
-
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.Promise = Promise;
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
+const port = process.env.PORT || 8080
+const app = express()
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-const Player = mongoose.model("Player", {
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: 'Service unavailable' })
+  }
+})
+
+const Player = mongoose.model('Player', {
   id: Number,
   name: String,
   position: String,
@@ -46,33 +47,59 @@ if (process.env.RESET_DB) {
     })
   }
   seedDatabase()
-  
+
 }
 
+app.get('/', (req, res) => {
 
+  const documentation = {
+    "About": "This is an open API about the Sweden women's national football team",
+    "Routes": [
+      {
+        "/": "Documentation",
+        "/endpoints": "All endpoints",
+        "/players/all": "Get all players",
+        "/players/club/${club}": "Get all the players in a specific club",
+        "/players/name/${name}": "Get a specific player by name",
+      }
+    ]
+  }
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Project Mongo API!");
+  res.send(documentation);
 });
 
-app.get("/players", (req, res) => {
+app.get('/players/all', (req, res) => {
   Player.find().then(players => {
     res.json(players)
   })
 })
 
-app.get('/:name', (req, res) => {
-  Player.findOne({name: req.params.name}).then(player => {
-    if(player) {
-      res.json(player)
+app.get('/players/club/:club', (req, res) => {
+  Player.find({ club: req.params.club }).then(players => {
+    if (players) {
+      res.json(players)
     } else {
-      res.status(404).json({ error: 'Not found' })
+      res.status(404).json({ error: 'No players in that club was found' })
     }
   })
 })
 
+app.get('/players/name/:name', (req, res) => {
+  Player.findOne({ name: req.params.name }).then(player => {
+    if (player) {
+      res.json(player)
+    } else {
+      res.status(404).json({ error: 'No player with that name was found' })
+    }
+  })
+})
+
+// List all endpoints
+app.get('/endpoints', (req, res) => {
+  res.send(allEndpoints(app))
+})
+
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`)
 });
