c62a68a69de4cc1fc5f863fceebcef34f9f69fc8
idanaslund <idanaslund@users.noreply.github.com>
2022-05-13 09:13:20 +0200

    deploying

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import listEndpoints from "express-list-endpoints"
 import netflixData from "./data/netflix-titles.json";
 
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo-api";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
