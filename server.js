2eb38835c9207878aef480b8eac5b51e5fa36b2b
jenniferwvng <83246937+jenniferwvng@users.noreply.github.com>
2022-05-13 19:31:05 +0200

    routes for finding data in mongodb

--- server.js
+++ server.js
@@ -2,13 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import topMusicData from "./data/top-music.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -18,7 +12,43 @@ mongoose.Promise = Promise;
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
-const app = express();
+const app = express(); 
+
+const Song = mongoose.model('Song', {
+  "id": Number,
+  "trackName": String,
+  "artistName": String,
+  "genre": String,
+  "bpm": Number,
+  "energy": Number,
+  "danceability": Number,
+  "loudness": Number,
+  "liveness": Number,
+  "valence": Number,
+  "length": Number,
+  "acousticness": Number,
+  "speechiness": Number,
+  "popularity": Number
+})
+
+//Deleting everything in our database: an empty pattern - never use in production code, only for educational purposes
+//to prevent adding several duplicates of data objects to database for every reload of server
+//create a function that check if we want to reset our database, which will be dependent on env variable, then will delete everything
+
+//provide RESET_DB=true npm run dev for start in terminal
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    //async will tell us to wait for a piece of code before we do anything else
+    //await for whole collection to be deleted
+    await Song.deleteMany();
+    topMusicData.forEach(singleSong => {
+      const newSong = new Song(singleSong);
+      newSong.save();
+    })
+    //normally when wanting to delete something, specify (requirements)
+  }
+  seedDatabase();
+}
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
@@ -26,8 +56,33 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  const routes = {
+    'GET artistName route': "/songs/song/:artistName",
+    'GET genre route': "/songs/:genre"
+  }
+  res.send(routes);
 });
+
+app.get("/songs/song/:artistName", async (req, res) => {
+  const singleSong = await Song.findOne({artistName: req.params.artistName});
+  
+  if (singleSong) {
+    res.send(singleSong);
+  } else {
+    res.status(404).json({error: 'Not found'})
+  }
+})
+
+app.get("/songs/:genre", async (req, res) => {
+  const songGenres = await Song.find({genre: req.params.genre});
+  
+  if (songGenres) {
+    res.send(songGenres);
+  } else {
+    res.status(404).json({error: 'Not found'})
+  }
+})
+
 
 // Start the server
 app.listen(port, () => {
