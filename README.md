2f1f621acd7103d03b7334ed6af7ed661208a932
Emma Lindell <emma.lindell4@gmail.com>
2022-05-13 20:33:13 +0200

    final version

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
-# Project Mongo API
+# Project Mongo API 🥭
 
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+Second week of backend with Node.js and MongoDB. A lot of things to learn, really exciting and challenging. Looking forward learning more in the final project.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+This week I've learned even more about creating API endpoints and how I use MongoDB and Express.
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+https://emmas-songs.herokuapp.com/
--- server.js
+++ server.js
@@ -1,35 +1,95 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+import listEndpoints from 'express-list-endpoints'
+import topMusicData from "./data/top-music.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.Promise = Promise;
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
+const port = process.env.PORT || 8080
+const app = express()
+
+
+// model a specific set of rules, for how the document will look like
+// every document created should look like this:
+const Song = mongoose.model("Song", {
+id: Number,
+trackName: String,
+artistName: String,
+genre: String,
+bpm: Number,
+energy: Number,
+danceability: Number,
+loudness: Number,
+liveness: Number,
+valence: Number,
+length: Number,
+acousticness: Number,
+speechiness: Number,
+popularity: Number
+})
+
+
+if(process.env.RESET_DB) {
+  const seedDataBase = async () => {
+    await Song.deleteMany()
+    topMusicData.forEach( singleSong => {
+      const newSong = new Song (singleSong)
+      newSong.save()
+    })
+  }
+  seedDataBase()
+}
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
+
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+  res.send(listEndpoints(app))
+})
+
+
+// Get all songs
+app.get("/songs", async (req, res) => {
+
+  try {
+    let { page, size } = req.query
+    if (!page) {
+      page = 1
+    } if (!size) {
+      size = 100
+    }
+
+    const limit = parseInt(size, 100)
+    const skip = (page - 1) * size
+
+    const songs = await Song.find().limit(limit).skip(skip)
+    res.send({ page, size, data: songs})
+  } catch (error) {
+    res.status(400).json({ error: "Error, not a page or size"})
+  }
+})
+
+
+// Artist name
+app.get("/songs/song/:artistName", async (req, res) => {
+  // this will retrieve only the first found song
+  const singleSong = await Song.findOne({artistName: req.params.artistName})
+  res.send(singleSong)
+})
+
+
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+  console.log(`Hello world`)
+  console.log(`Server running on http://localhost:${port}`)
+})
