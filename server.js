f6633e12d01d25b76880e61672a11b344013a030
michaelchangdk <michaelchangdk@icloud.com>
2022-05-13 09:35:46 +0200

    trying to get this to work

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import doItBeFartin from "./data/doitbefartin.json";
+// import doItBeFartin from "./data/doitbefartin.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/doitbefartin";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -29,20 +29,28 @@ const Animal = mongoose.model("Animal", {
 });
 
 // if (process.env.RESET_DB) {
-const seedDatabase = async () => {
-  await Animal.deleteMany();
-  doItBeFartin.forEach((animal) => {
-    new Animal(animal).save();
-  });
-};
-seedDatabase();
+// const seedDatabase = async () => {
+//   await Animal.deleteMany();
+//   doItBeFartin.forEach((animal) => {
+//     new Animal(animal).save();
+//   });
+// };
+// seedDatabase();
 // }
 
-// Start defining your routes here
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next();
+  } else {
+    res.status(503).json({ error: "Service unavailable" });
+  }
+});
+
+// Route explanation
 app.get("/", (req, res) => {
   res.send([
     { "/": "Home" },
-    { "/animals": "All the data" },
+    { "/animals": "All the animals" },
     { "/animal/:animal": "Find one animal" },
     { "/description/:boolean": "True or false if there a fart description" },
     { "/doitbefartin/:boolean": "True or false if it be fartin" },
@@ -52,37 +60,67 @@ app.get("/", (req, res) => {
   ]);
 });
 
-app.get("/animals", (req, res) => {
+// Route for all the animals
+app.get("/animals", async (req, res) => {
+  let animals = await Animal.find();
   res.status(200).json({
-    data: doItBeFartin,
+    data: animals,
     success: true,
   });
 });
 
-app.get("/animal/:animal", (req, res) => {
+app.get("/animal/:animal", async (req, res) => {
   // const animalSearch = await Animal.find({name: req.params.animal})
   // res.send(animalSearch)
+  let animals = await Animal.find();
+
+  // const animal = req.params.animal;
+  let specificAnimal = animals.filter((a) =>
+    a.name.toLowerCase.includes(req.params.animal.toLocaleLowerCase())
+  );
+
+  // if (animal) {
+  //   specificAnimal = animals.filter((a) =>
+  //     a.name.toLowerCase().includes(animal.toLowerCase())
+  //   );
+  // }
+
+  if (specificAnimal) {
+    res.status(200).json({ data: specificAnimal, success: true });
+  } else {
+    res.status(400).json({ error: "Animal not found" });
+  }
+});
+
+app.get("/description/:boolean", async (req, res) => {
+  let animals = await Animal.find();
+
+  let boolean = req.params.descriptionExists;
+
+  const descriptionExists = animals.filter(
+    (a) => a.descriptionExists.toString() === boolean.toString()
+  );
+
+  if (descriptionExists.length > 0) {
+    res.status(200).json({ data: description, success: true });
+  } else {
+    res.status(400).json({ error: "Boolean incorrect. use true or false." });
+  }
+  // if (animals)
+  // res.status(200).json({
+  //   data: doItBeFartin,
+  //   success: true,
+  // });
+});
+
+app.get("/doitbefartin/:boolean", async (req, res) => {
   res.status(200).json({
     data: doItBeFartin,
     success: true,
   });
 });
 
-app.get("/description/:boolean", (req, res) => {
-  res.status(200).json({
-    data: doItBeFartin,
-    success: true,
-  });
-});
-
-app.get("/doitbefartin/:boolean", (req, res) => {
-  res.status(200).json({
-    data: doItBeFartin,
-    success: true,
-  });
-});
-
-app.get("/doitmaybebefartin/:boolean", (req, res) => {
+app.get("/doitmaybebefartin/:boolean", async (req, res) => {
   res.status(200).json({
     data: doItBeFartin,
     success: true,
