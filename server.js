9b149e277b0e73da542ee126d1e83a099fc259ec
Kyparn <93533136+Kyparn@users.noreply.github.com>
2022-05-14 11:42:26 +0200

    added year and id

--- server.js
+++ server.js
@@ -1,35 +1,88 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import listEndpoints from 'express-list-endpoints'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import netflixData from './data/netflix-titles.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.Promise = Promise;
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/myNetflix'
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
+const Stream = mongoose.model('Stream', {
+  show_id: String,
+  title: String,
+  director: String,
+  cast: String,
+  country: String,
+  date_added: String,
+  release_year: Number,
+  rating: String,
+  duration: String,
+  listed_in: String,
+  description: String,
+  type: String,
+})
+
+const seedDatabase = async () => {
+  await Stream.deleteMany()
+  netflixData.forEach((stream) => {
+    new Stream(stream).save()
+  })
+}
+
+seedDatabase()
+
+// Defines the port the app will run on. Defaults to 8080, but can be
+// overridden when starting the server. For example:
+//
+//   PORT=9000 npm start
+const port = process.env.PORT || 8080
+const app = express()
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(bodyParser.json())
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+
+app.get('/', (req, res) => {
+  res.send(listEndpoints(app))
+})
+
+app.get('/myNetflix', async (req, res) => {
+  const mystream = await Stream.find()
+  res.json(mystream)
+})
+
+// Geting route shows with ID
+
+app.get('/myNetflix/shows/:show_id', (reg, res) => {
+  const id = reg.params.show_id
+  Stream.find({ show_id: id })
+    .then((results) => {
+      res.json(results)
+    })
+    .catch((err) => {
+      res.json({ message: 'Cant find query', err: err })
+    })
+})
+
+//Geting route year
+
+app.get('/myNetflix/year/:release_year', async (req, res) => {
+  const relaseDate = await Stream.find({
+    release_year: req.params.release_year,
+  })
+  res.send(relaseDate)
+})
+app.get('/myNetflix/type/:type', async (req, res) => {
+  const movieType = await Stream.find({ type: req.params.type })
+  res.send(movieType)
+})
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+  console.log(`Server running on http://localhost:${port}`)
+})
