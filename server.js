dbd928980b9dd1e67507a44e8f215e72ab69f6fc
toal13 <tomoyo.tomoyo.m@gmail.com>
2022-05-14 22:16:09 +0200

    first push

--- server.js
+++ server.js
@@ -1,35 +1,85 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+import listEndpoints from "express-list-endpoints"
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import netflixData from "./data/netflix-titles.json"
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.Promise = Promise;
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
+const port = process.env.PORT || 8080
+const app = express()
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
+
+const NetflixTitle = mongoose.model("NetflixTitle", {
+  "show_id": Number,
+  "title": String,
+  "director": String,
+  "cast": String,
+  "country": String,
+  "date_added": String,
+  "release_year": String,
+  "rating": String,
+  "duration": String,
+  "listed_in": String,
+  "description": String,
+  "type": String
+})
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await NetflixTitle.deleteMany()
+    netflixData.forEach((item) => {
+      const newNetflixTitle = new NetflixTitle(item)
+      newNetflixTitle.save()
+    })
+  }
+  seedDatabase()
+}
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+app.get('/', (req, res) => {
+  res.send(listEndpoints(app))
+})
+
+// Get all the Netflix movies and shows
+app.get('/netflixtitles', async (req, res) => {
+  const netflixTitles = await NetflixTitle.find(req.query)
+  res.json(netflixTitles)
+})
+
+// Get one Netflix title based on id
+app.get('/netflixtitles/id/:id', async (req, res) => {
+  try {
+    const netflixTitleById = await NetflixTitle.findById(req.params.id)
+    if(!netflixTitleById) {
+      res.status(404).json({
+        response: 'Title not found',
+        success: false,
+      })
+    } else {
+      res.json({
+        response: netflixTitleById,
+        success: true,
+      })
+    }
+  } catch (err) {
+    res.status(400).json({
+      error: 'Id is invalid'
+    })
+  }
+})
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
-});
+})
+
