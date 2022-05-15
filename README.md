be73c038227c43ac070375c7b7d562f1bff43e86
Marianne Ardin <55622908+MarianneArdin@users.noreply.github.com>
2022-05-15 17:58:45 +0200

    updated

--- README.md
+++ README.md
@@ -2,10 +2,13 @@
 
 This was my second backend project and the first using Mongo DB and Mongoose.
 
-## The problem
+## The challenge
 
 I used the basecode from my previous project https://github.com/MarianneArdin/project-express-api
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+Endpoints: /goldenglobes
+/nominations
+
+
--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-//FIX NEDAN//
-//import listEndpoints from 'express-list-endpoints';
-
 import goldenGlobesData from './data/golden-globes.json';
 
 const data = goldenGlobesData;
-//NOT SURE WHERE THIS GOES FROM GUIDE DEPLOYING ON NOTION//
-//mongodb+srv://Spazza76:<Gotland2022>@cluster0.8w8rr.mongodb.net/goldenGlobes?retryWrites=true&w=majority
 
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-mongo';
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -34,7 +29,7 @@ const Award = mongoose.model('Award', {
 //Starts seeding the database
 if (process.env.RESET_DB) {
 	const seedDatabase = async () => {
-		await Award.deleteMany();
+		await Award.deleteMany({});
 		data.forEach((item) => {
 			const newAward = new Award(item);
 			newAward.save();
@@ -59,78 +54,24 @@ app.use((req, res, next) => {
 //RESTful routes/endpoints
 app.get('/', (req, res) => {
 	res.send(
-		'Welcome to the Golden-Globes API. Enter /endpoints to see available endpoints.'
+		'Welcome to the Golden-Globes API. Endpoints: /goldenglobes /nominations'
 	);
 });
 
-//THIS DOES NOT WORK
-app.get('/endpoints', (req, res) => {
-	//this endpoint is going to tell us all possible endpoint we have
-	res.send(listEndpoints(app));
-});
-
-//THIS WORKS
+//this route will display all nominations
 app.get('/nominations', (req, res) => {
-	//this route will display all nominations
 	res.json(data);
 });
-
-app.get('/goldenGlobes/nominee', async (res, req) => {
-	const nominee = await nominee.find().populate('nominee');
-	res.json(nominee);
-	if (!data) {
-		res.status(404).json('Not found');
-	} else {
-		res.status(200).json({ data: nominee, success: true });
-	}
-});
-
-app.get('/goldenGlobes/category', async (res, req) => {
-	const category = await category.find().populate('catagory');
-	res.json(category);
-	if (!data) {
-		res.status(404).json('Not found');
-	} else {
-		res.status(200).json({ data: category, success: true });
-	}
-});
-
-//FRÅN DAMIEN CODE ALONG similar to above by Daniel?//
-const ceremony = mongoose.model('ceremony', {
-	name: String,
-});
-
-const film = mongoose.model('Film', {
-	title: String,
-	nominee: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Nominee',
-	},
-});
-
-//TA BORT?
-// if (process.env.RESET_DATABASE) {
-// 	console.log('Resetting database!');
-// 	const seedDatabase = async () => {
-// 		await film.deleteMany();
-// 		const TheDanishGirl = new film({ name: 'The Danish Girl' });
-// 		await TheDanishGirl.save();
-// 	};
-
-// 	seedDatabase();
-// }
 
 // Start defining your routes here
 app.get('/', (req, res) => {
 	res.send('Golden Globe winners');
 });
 
-//Fetches data from the API FRÅN FÖRRA VECKAN //
 app.get('/goldenGlobes', (req, res) => {
 	res.status(200).json(data);
 });
 
-//THIS DOES NOT WORK
 app.get('/goldenGlobes/year_award/:year_award', (req, res) => {
 	const filmByYear = data.find(
 		(goldenGlobes) => goldenGlobes.year_award === req.params.year_award
@@ -146,14 +87,12 @@ app.get('/goldenGlobes/year_award/:year_award', (req, res) => {
 app.get('/goldenGlobes/win/:showName', (req, res) => {
 	const showName = req.params.showName;
 
-	let goldenGlobesWinners = data.find((item) => item.nominee === showName);
-	res.status(200).json({ data: goldenGlobesWinners.win, success: true });
+	let data = data.find((item) => item.nominee === showName);
+	res.status(200).json({ data: data.win, success: true });
 });
-//här slutar API FRÅN FÖRRA VECKAN //
 
-//FRÅN DAMIEN CODE ALONG endpoints FUNKAR INTE//
-app.get('/goldenGlobes/film', async (res, req) => {
-	const film = await film.find();
+app.get('/film', async (res, req) => {
+	const film = await film.find({});
 	res.json(film);
 });
 
