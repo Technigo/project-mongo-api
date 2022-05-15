3a0f35e4084c7c4abbab1327011e175c6e501fa6
Emma Springs <emmasprings@gmail.com>
2022-05-15 14:04:20 +0200

    added endpoints

--- server.js
+++ server.js
@@ -1,35 +1,84 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+import listEndpoints from 'express-list-endpoints'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import printCollection from './data/print-collection.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.Promise = Promise;
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/mongodb-prints"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
+const port = process.env.PORT || 8080
+const app = express()
+
+// const Prints = mongoose.model("Prints", {
+//   title: String,
+//   price: Number,
+//   media: String,
+//   forSale: Boolean
+// })
+
+
+const Print = mongoose.model("Print", {
+  id: Number,
+  title: String,
+  year: String,
+  media: String,
+  price: String,
+  size: String,
+  url: String
+})
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Print.deleteMany()
+    printCollection.forEach( (singlePrint) => {
+      const newPrint = new Print(singlePrint)
+      newPrint.save()
+    })
+  }
+  seedDatabase()
+}
+
+// const printOne = new Print({title: "Ocean View", price: 300, media: "Digital Photography", forSale: true})
+// printOne.save()
+
+// const printTwo = new Print({title: "Above", price: 400, media: "Digital Photography", forSale: true})
+// printTwo.save()
+
+
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+  res.send(listEndpoints(app))
+})
+
+app.get("/prints", async (req,res) => {
+  const allPrints = await Print.find(req.query)
+  res.send(allPrints)
+})
+
+app.get("/prints/title/:title", async (req,res) => {
+  // this will retrieve only one print
+  const onePrint = await Print.findOne({title: req.params.title.toLowerCase()})
+  res.send(onePrint)
+})
+
+app.get("/prints/year/:year", async (req,res) => {
+  const byYear = await Print.find({year: req.params.year})
+  res.send(byYear)
+})
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+  console.log(`Server running on http://localhost:${port}`)
+})
