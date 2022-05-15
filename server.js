ebc0fd1ff4e33dab2ca0a24e3b94742738d8eaf2
Laura Sjolander <laura.sjolander@gmail.com>
2022-05-15 22:06:32 +0200

    first setup with seedDatabase in Mongodb

--- server.js
+++ server.js
@@ -2,23 +2,53 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import booksData from "./data/books.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+// Port for running app
 const port = process.env.PORT || 8080;
 const app = express();
+
+
+
+const User = mongoose.model("User", {
+  name: String,
+  age: Number,
+  deceased: Boolean,
+});
+
+const Book = mongoose.model("Book", {
+    bookID: Number,
+    title: String,
+    authors: String,
+    average_rating: Number,
+    isbn: Number,
+    isbn13: Number,
+    language_code: String,
+    num_pages: Number,
+    ratings_count: Number,
+    text_reviews_count: Number,
+});
+
+//const secondTestUser = new User({name: "Daniel, age: 27, deceased: false"});
+//secondTestUser.save():
+
+if(process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Book.deleteMany();
+    booksData.forEach( singleBook => {
+      const newBook = new Book(singleBook);
+      newBook.save();
+  })
+  }
+ seedDatabase();
+}
+
+// const testUser = new User({name: "Maksy", age: 28, deceased: false});
+// testUser.save();
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
@@ -26,10 +56,11 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Hello Hello, this is here to show it works!");
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Hello Worlds`);
+  console.log(`Showing in terminal: Server running on http://localhost:${port}`);
 });
