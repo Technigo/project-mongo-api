b6556c7a7232133f6f981c3fd7789016aa7725ec
sofiaannajonsson <84452023+sofiaannajonsson@users.noreply.github.com>
2022-05-16 13:52:45 +0200

    corrected the endpoints

--- server.js
+++ server.js
@@ -48,87 +48,26 @@ app.use(express.json());
 app.get("/", (req, res) => {
   const Main = {
     About:
-      "An API with 50 popular Spotify tracks and some data about the music type",
+      "An API with 50 popular Spotify tracks",
     Routes: [
       {
         "/api/songs": "Get all songs",
-        "/api/genres/{genre}": "Get all songs in a specific genre",
-        "/api/artists/{artist}": "Get songs from a specific artist",
-        "/api/titles/{title}": "Get a song by its title name",
-        "/api/ids/{number}": "Get a songs by its ID",
+        "/api/song/{number}": "Get a songs by its ID",
       },
     ],
   };
   res.send(Main);
 });
 
-app.get("/api/songs", (req, res) => {
-  res.status(200).json(songs)
+app.get("/api/songs", async (req, res) => {
+  const Songs = await Song.find();
+  res.send(Songs)
 })
 
-app.get("/api/genres/:genre", (req, res) => {
-  const { genre } = req.params
-  
-  const songsByGenre = songs.filter(
-    (songs) => songs.genre.toLowerCase() === genre.toLowerCase()
-  );
-  
-  res.status(200).json({
-    data: songsByGenre,
-    success: true,
-  });
-});
-
-app.get("/api/artists/:artist", (req, res) => {
-  const { artist } = req.params
-  
-  const songsByArtist = songs.filter(
-    (songs) => songs.artistName.toLowerCase() === artist.toLowerCase()
-  );
-  
-  res.status(200).json({
-    data: songsByArtist,
-    success: true,
-  });
-});
-
-app.get("/api/titles/:title", (req, res) => {
-  const { title } = req.params
-  const songByTitle = songs.find(
-    (song) => song.trackName.toLowerCase() == title.toLowerCase()
-  );
-
-  if (!songByTitle) {
-    res.status(400).json({
-      data: "Not found",
-      success: false,
-    });
-  } else {
-    res.status(200).json({
-      data: songByTitle,
-      sucess: true,
-    });
-  }
-});
-
-app.get("/api/ids/:id", (req, res) => {
-  const { id } = req.params;
-  const songById = songs.find(
-    (song) => song.id == id
-  );
-
-  if (!songById) {
-    res.status(400).json({
-      data: "Not found",
-      success: false,
-    });
-  } else {
-    res.status(200).json({
-      data: songById,
-      sucess: true,
-    });
-  }
-});
+app.get("/api/song/:id", async (req, res) => {
+  const SongById = await Song.findOne({ id: req.params.id });
+  res.send(SongById)
+})
 
 // Starting the server
 app.listen(port, () => {
