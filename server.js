ddee98f30df668b7c24cf2031097ff3a9176920c
Nabeel Mansour <59266812+NabeelMansour@users.noreply.github.com>
2022-05-16 10:31:56 +0200

    finished the project deployment

--- server.js
+++ server.js
@@ -2,12 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-//import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
+
 import topMusicData from "./data/top-music.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
