d63da78d9ae38e2666b4c8b13c425801616052f6
Jacob <jacob@engd.se>
2022-05-17 21:11:44 +0200

    deploy fix

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import listEndpoints from "express-list-endpoints";
-
-
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // import avocadoSalesData from "./data/avocad§o-sales.json";
