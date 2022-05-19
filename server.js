2887519a450e05e21a4d9a0d8bf2bc2ff9e07fdf
Ida Halling <idahalling@Idas-MacBook-Pro.local>
2022-05-19 13:07:06 +0200

    new mongo connection

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 8080
 const app = express()
 
 const mongoUrl =
-  process.env.MONGO_URL || 'mongodb://localhost/project-mongoMongo'
+  process.env.MONGO_URL || 'mongodb://localhost/project-mongoApigit'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
