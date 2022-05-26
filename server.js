a0834d7d95ef3fc9c985a0ca4934db71f7b36371
Laura Sjolander <laura.sjolander@gmail.com>
2022-05-26 16:21:50 +0200

    Added routes and error messages

--- server.js
+++ server.js
@@ -1,102 +1,175 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-//import topMusicData from "./data/top-music.json";
+import topMusicData from "./data/top-music.json";
 
-import booksData from "./data/books.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 mongoose.Promise = Promise;
 
 // Port for running app
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
-
-
-
-// const User = mongoose.model("User", {
-//   name: String,
-//   age: Number,
-//   deceased: Boolean,
-// });
-
-const Book = mongoose.model("Book", {
-    bookID: Number,
-    title: String,
-    authors: String,
-    average_rating: Number,
-    isbn: Number,
-    isbn13: Number,
-    language_code: String,
-    num_pages: Number,
-    ratings_count: Number,
-    text_reviews_count: Number,
-
-});
-
-
-
-//const secondTestUser = new User({name: "Daniel, age: 27, deceased: false"});
-//secondTestUser.save():
-
-// const Song = mongoose.model("Song", {
-//   id: Number,
-//   trackName: String,
-//   artistName: String,
-//   genre: String,
-//   bpm: Number,
-//   energy: Number,
-//   danceability: Number,
-//   loudness: Number,
-//   liveness: Number,
-//   valence: Number,
-//   length: Number,
-//   acousticness: Number,
-//   speechiness: Number,
-//   popularity: Number
-// });
-
-// if(process.env.RESET_DB) {
-//   const seedDatabase = async () => {
-//     await Song.deleteMany();
-//     topMusicData.forEach( singleSong => {
-//       const newSong = new Song(singleSong);
-//       newSong.save();
-//     })
-//   }
-//   seedDatabase();
-// }
-
-
-if(process.env.RESET_DB) {
-  const seedDatabase = async () => {
-    await Book.deleteMany();
-    booksData.forEach( singleBook => {
-      const newBook = new Book(singleBook);
-      newBook.save();
-  })
-  }
- seedDatabase();
-}
-
-// const testUser = new User({name: "Maksy", age: 28, deceased: false});
-// testUser.save();
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
+//Invoke the upcoming lines by next function, if the database is not connected it will give an error message
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({
+      error: "Service unavailable"
+    })
+  }
+
+})
+
+const Song = mongoose.model("Song", {
+  id: Number,
+  trackName: String,
+  artistName: String,
+  genre: String,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  loudness: Number,
+  liveness: Number,
+  valence: Number,
+  length: Number,
+  acousticness: Number,
+  speechiness: Number,
+  popularity: Number
+});
+
+//This deletes the database and awaits until the deletion is done and seeds it with new songs from the json file topMusicData- (resets)
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Song.deleteMany();
+    topMusicData.forEach(singleSong => {
+      const newSong = new Song(singleSong);
+      newSong.save();
+    })
+  }
+  seedDatabase();
+}
+
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Hello, this is here to show it works!");
+  const routeInfo = {
+    Welcome: "Welcome to this API for songs",
+    Routes: [{
+      "/songs": "See all songs in the database",
+      "/songs/songs/:artistName": "See all songs by a specific artist",
+      "/songs/:id" : "See song with a specifik id",
+      "/songs/genre/:genre": "See all songs of a specific genre such as pop or reggaeton",
+      "songs/song?trackName=": "See the song with a specific trackname",
+      "songs/song?trackName=&artistName=": "See the song with a specific trackname and artistname"
+
+    }, ],
+  }
+  res.send(routeInfo)
 });
+
+app.get("/songs", async (req, res) => {
+  const allSongs = await Song.find()
+  res.json(allSongs)
+})
+
+ 
+ //this will retrieve all songs by an artist
+app.get("/songs/songs/:artistName", async (req, res) => {
+ 
+  const artistsSongs = await Song.find({
+    artistName: req.params.artistName
+  });
+  res.send(artistsSongs);
+});
+
+
+//this gets you the songs in a special genre like pop or reggeaton
+app.get("/songs/genre/:genre", async (req, res) => {
+  try {
+    const singleGenre = await Song.find({
+      genre: req.params.genre
+    });
+    if (singleGenre) {
+      res.json(singleGenre)
+    } else {
+      res.status(404).json({
+        error: "Sorry, can not find the genre you were looking for"
+      })
+    }
+  } catch (err) {
+    res.status(400).json({
+      error: "Oops! That input is invalid for a genre"
+    })
+  }
+});
+
+//this will retrieve a specific artist and/or the track: example: http://localhost:8080/songs/song?artistName=Marshmello&trackName=Happier
+
+app.get("/songs/song/", async (req, res) => {
+  
+  const {
+    artistName,
+    trackName
+  } = req.query
+
+    if (trackName && artistName) {
+    const queryTrackArtist = await Song.find({
+      trackName: trackName,
+      artistName: artistName
+    });
+    res.send(queryTrackArtist);
+  } else if (trackName) {
+    const queryTrack = await Song.find({
+      trackName: trackName
+    });
+    res.send(queryTrack);
+  } else if (artistName) {
+    const queryArtist = await Song.find({
+      artistName: artistName
+    });
+    res.send(queryArtist);
+  }
+});
+
+//this will retrieve the specific song matching the id
+app.get("/songs/:id", async (req, res) => {
+ 
+  try {
+    const songId = parseInt(req.params.id);
+    console.log(songId);
+
+    const songById = await Song.findOne({id:songId});
+    console.dir(songById);
+
+    if (songById) {
+      res.json(songById);
+    } else {
+      res.status(404).json({
+        error: "Can not find song with the id"
+      })
+    }
+  } catch (err) {
+    console.dir(err);
+    res.status(400).json({
+      error: "Input invalid for id"
+    })
+  }
+});
+
+
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Hello Worlds`);
   console.log(`Showing in terminal: Server running on http://localhost:${port}`);
 });
