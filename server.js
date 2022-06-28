83dd682a03d14728c4977e93068f51ac7008f363
Dorothea-B <barre.dorothea@gmail.com>
2022-06-28 11:43:40 +0200

    nmp test

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/dorotheas-project
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
+// Defines the port the app will run on. 
+//Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
 const port = process.env.PORT || 8080
@@ -120,6 +121,7 @@ app.get("/movies", (req, res) => {
 app.get("/release-year", (req, res) => {
 
 })
+
 
 
 // Start the server
