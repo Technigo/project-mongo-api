ef06494cd6a68be7366bee8072c00cbf2b35d7e3
Cecilia Råden <you@example.com>
2022-11-22 18:12:57 +0100

    added routes

--- server.js
+++ server.js
@@ -7,13 +7,48 @@ import mongoose from "mongoose";
 // import avocadoSalesData from "./data/avocado-sales.json";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import netflixData from "./data/netflix-titles.json";
+//import topMusicData from "./data/top-music.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
+const Title = mongoose.model("Title", {
+    title: String,
+    director: String,
+    cast: String,
+    country: String,
+    date_added: String,
+    release_year: Number,
+    rating: String,
+    duration: String,
+    listed_in: String,
+    description: String,
+    type: String
+});
+
+if(process.env.RESET_DB) {
+const seedDatabase = async () => {
+  await Title.deleteMany()
+  const snowwhite = new Title({ title: "Snow White" })
+  await snowwhite.save()
+  const lionking = new Title({ title: "Lion King" })
+  await lionking.save()
+}
+
+seedDatabase()
+
+
+ /* const resetDataBase = async () => {
+    await Title.deleteMany();
+    netflixData.forEach(singleTitle => {
+      const NewTitle = new Title(singleTitle);
+      NewTitle.save()
+    })
+  }
+  resetDataBase(); */
+} 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
@@ -27,6 +62,11 @@ app.use(express.json());
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+app.get("/titles", async (req, res) => {
+ const titles = await Title.find()
+ res.json(titles)
 });
 
 // Start the server
