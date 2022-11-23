49322c370a21a37b7ceb107113380552e61858ff
Fonnt <dballesterfont@gmail.com>
2022-11-23 11:24:50 +0100

    [db] Up and running

--- server.js
+++ server.js
@@ -1,22 +1,67 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // import avocadoSalesData from "./data/avocado-sales.json";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import topMusicData from "./data/top-music.json";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb+srv://Font:@cluster0.8xh88s6.mongodb.net/projectMongo?retryWrites=true&w=majority";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
+
+// This is the first model. It's not putting info into the DB. It specify the types
+const User = mongoose.model("User", {
+  name: String,
+  age: Number,
+  deceased: Boolean
+});
+
+const Song = mongoose.model("Song", {
+  id: Number,
+  trackName: String,
+  artistName: String,
+  genre: String,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  loudness: Number,
+  liveness: Number,
+  valence: Number,
+  length: Number,
+  acousticness: Number,
+  speechiness: Number,
+  popularity: Number
+});
+
+
+// Deleting DB is in this case for educational porpouses.
+if (process.env.RESET_DB) {
+  const resetDataBase = async () => {
+    await Song.deleteMany();
+    topMusicData.forEach(singleSong => {
+      const newSong = new Song(singleSong);
+      newSong.save();
+    })
+
+    // await User.deleteMany();
+    // const testUser = new User({
+    //   name: "David",
+    //   age: 35,
+    //   deceased: false
+    // });
+    // testUser.save();
+  }
+  resetDataBase();
+}
+
 const port = process.env.PORT || 8080;
 const app = express();
 
