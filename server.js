cf4675455ccd680c80e847f372e870f93df0a175
NaghmehOkhovat <99764109+NaghmehOkhovat@users.noreply.github.com>
2022-11-23 16:28:27 +0100

    Added a new endpoint for all songs

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ import mongoose from "mongoose";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
+import topMusicData from "./data/top-music.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -17,6 +18,58 @@ mongoose.Promise = Promise;
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
+
+// const User = mongoose.model("User", {
+// 	name: String,
+// 	age: Number,
+// 	deceased: Boolean
+// });
+
+const Song = mongoose.model("Song", {
+  id: Number,
+  trackName: String,
+  artistName: String,
+  genre: String,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  loudness: Number,
+  liveness: Number,
+  valence: Number,
+  length: Number,
+  acousticness: Number,
+  speechiness: Number,
+  popularity: Number
+});
+
+
+if(process.env.RESET_DB) {
+	//resetDataBase
+  const seedDatabase = async () => {
+    await Song.deleteMany();
+    topMusicData.forEach(singleSong => {
+      const newSong = new Song (singleSong)
+			newSong.save()
+    })
+    // await User.deleteMany();
+    // const testUser = new User({name: "Daniel", age: 28, deceased: false});
+    // testUser.save();
+  }
+  seedDatabase();
+}
+
+// if(process.env.RESET_DB) {
+//   const resetDataBase = async () => {
+// 		await User.deleteMany();
+		//Here Refrencing our variable User. testUser will be equal to new User
+   // new user will accept an argument which will be an object
+  //  const testUser = new User({name: "Daniel", age: 28, deceased: false});
+   //Invoke the testUser
+//    testUser.save();
+// 	}
+// 	resetDataBase();
+// };
+
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -26,8 +79,13 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Top-Music API data!");
 });
+
+app.get("/songs", async (req, res) => {
+	const songs = await Song.find()
+	res.json(songs)
+})
 
 // Start the server
 app.listen(port, () => {
