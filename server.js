792b460925db3ac5970d3611885e85d96dc8c81b
Fredrik Holm <karlfredrikholm@gmail.com>
2022-11-23 16:00:20 +0100

    two routes added, one for all songs, one for a single song by its id

--- server.js
+++ server.js
@@ -3,14 +3,6 @@ import cors from "cors";
 import mongoose from "mongoose";
 import topMusicData from "./data/top-music.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
@@ -18,39 +10,33 @@ mongoose.Promise = Promise;
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const User = mongoose.model("User", {
-  name: String,
-  age: Number,
-  deceased: Boolean 
+
+const songSchema = new mongoose.Schema({
+  id: Number,
+  trackName: String,
+  artistName: String,
+  genre: String,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  loudness: Number,
+  liveness: Number,
+  valence: Number,
+  length: Number,
+  acousticness: Number,
+  speechiness: Number,
+  popularity: Number
 });
 
-const Song = mongoose.model("Song", {
-    id: Number,
-    trackName: String,
-    artistName: String,
-    genre: String,
-    bpm: Number,
-    energy: Number,
-    danceability: Number,
-    loudness: Number,
-    liveness: Number,
-    valence: Number,
-    length: Number,
-    acousticness: Number,
-    speechiness: Number,
-    popularity: Number
-});
+const Song = mongoose.model("Song", songSchema);
 
 if (process.env.RESET_DB) {
   const resetDatabase = async () => {
     await Song.deleteMany();
+
     topMusicData.forEach(song => {
-      const newSong = new Song({song});
-      newSong.save();
+      const newSong = new Song(song).save();
     });
-/*     await User.deleteMany();
-    const testUser = new User({name: "Fredrik", age: 36, deceased: false});
-    testUser.save(); */
   }
   resetDatabase();
 }
@@ -65,6 +51,26 @@ app.use(express.json());
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Baaaaaa!");
+});
+
+app.get("/songs", async (req, res) => {
+  const songs = await Song.find();
+  const { bpm, genre } = req.query;
+
+  if (bpm) {
+
+  }
+
+  if (genre) {
+    
+  }
+  
+  res.json(songs);
+});
+
+app.get("/songs/:id", async (req, res) => {
+  const songById = await Song.findOne({id: req.params.id});
+  res.send(songById);
 });
 
 // Start the server
