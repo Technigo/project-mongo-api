2d34b73a4c3b4bab9eb20bd91510dd716739348b
fannycardell <fannycardell@gmail.com>
2022-11-24 14:11:13 +0100

    added thursday code along

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import topMusicData from "./data/top-music.json";
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -8,9 +9,6 @@ import mongoose from "mongoose";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-import topMusicData from "./data/top-music.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -20,39 +18,41 @@ mongoose.Promise = Promise;
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
 
-const User =mongoose.model("User", {
+/* const User = mongoose.model("User", {
   name: String,
   age: Number,
   deceased: Boolean
-});
+}); */
 
 const Song = mongoose.model("Song", {
-    id: Number,
-    trackName: String,
-    artistName: String,
-    genre: String,
-    bpm: Number,
-    energy: Number
+  id: Number,
+  trackName: String,
+  artistName: String,
+  genre: String,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  loudness: Number,
+  liveness: Number,
+  valence: Number,
+  length: Number,
+  acousticness: Number,
+  speechiness: Number,
+  popularity: Number
 });
 
-if(process.env.RESET_DB) {
+
+if (true) {
   const resetDataBase = async () => {
     await Song.deleteMany();
     topMusicData.forEach(singleSong => {
-      const newSong =new Song(singleSong);
+      const newSong = new Song(singleSong);
       newSong.save();
     })
   }
-}
-
-if(process.env.RESET_DB) {
-  const resetDataBase = async () => {
-    await User.deleteMany();
-    // const testUser = new User({name: "Fanny", age: 37, deceased: false})
-    // testUser.save();
-  }
   resetDataBase();
 }
+
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -63,10 +63,92 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Hello Technigoooooo heyhooooo!");
 });
 
+app.get("/songs", async (req, res) => {
+  const allTheSongs = await Song.find({});
+   res.status(200).json({
+    success: true,     
+    body: allTheSongs
+     });
+ });
+
+app.get("/songs/id/:id", async (req, res) => {
+  try {
+    const singleSong = await Song.findById(req.params.id);
+    if (singleSong) {
+      res.status(200).json({
+        success: true,
+        body: singleSong
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        body: {
+          message: "Could not find the song"
+        }
+      });
+    }
+  } catch(error) {
+    res.status(400).json({
+      success: false,
+      body: {
+        message: "Invalid id"
+      }
+    });
+  }
+  
+});
+// app.get("/songs/genre/:genre/danceability/:danceability", async (req, res) => {
+app.get("/songs/", async (req, res) => {
+
+  const {genre, danceability} = req.query;
+  const response = {
+    success: true,
+    body: {}
+  }
+  const matchAllRegex = new RegExp(".*");
+  const genreQuery = genre ? genre : matchAllRegex;
+  const danceabilityQuery = danceability ? danceability : /.*/;
+
+  try {
+    // if ( req.params.genre && req.params.danceability) {
+      response.body = await Song.find({genre: genreQuery, danceability: danceabilityQuery}).limit(2).sort({energy: 1}).select({trackName: 1, artistName: 1})
+      //.exec() => to explore if you're curious enough :P
+    // } else if (req.params.genre && !req.params.danceability) {
+    //   response.body = await Song.find({genre: req.params.genre});
+    // } else if (!req.params.genre && req.params.danceability) {
+    //   response.body = await Song.find({danceability: req.params.danceability});
+    // }
+    res.status(200).json({
+      success: true,
+      body: response
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      body: {
+        message: error
+      }
+    });
+  }
+
+});
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+//RESET_DB=true npm run dev
+// Go here:
+//https://github.com/coreybutler/nvm-windows/releases
+// downlaod nvm-setup.exe
+// run as admin
+// open cmd as admin
+// type nvm install v16.18.1
+//https://mongoosejs.com/docs/queries
+
+//https://regex101.com/
+
+// /yourWodOfChoice/gm - regex to match yourWordOfChoice
+// /.*/gm - regex to match every character in a string
