9a176f6602e6ef66d8a4c86d56798c81ed2af36a
Agnes Ahlman <ahlman.agnes@gmail.com>
2022-11-24 17:27:47 +0100

    setup mongoose models

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 import express from "express";
 import cors from "cors";
+import BooksData from "./data/books.json";
+import { Book } from "./lib/mongodb";
 import mongoose from "mongoose";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+if (process.env.RESET_DB) {
+  const resetDataBase = async () => {
+    await Book.deleteMany();
+    BooksData.forEach((singlebook) => {
+      const newBook = new Book(singlebook);
+      newBook.save();
+    });
+  };
+  resetDataBase();
+}
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.Promise = Promise;
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
