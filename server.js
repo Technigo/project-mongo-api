17bde792b5a4adfcfdd0f76a6705f1a404976558
KajaWilbik <wilbik.kaja@gmail.com>
2022-11-24 12:31:40 +0100

    changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import goldenGlobesData from "./data/golden-globes.json"
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -14,10 +15,48 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
+const Nomination = mongoose.model("Nomination", {
+  "year_film": Number,
+    "year_award": Number,
+    "ceremony": Number,
+    "category": String,
+    "nominee": String,
+    "win": Boolean
+
+}); 
+
+// reseting database is to educational purposes, to not have duplicates
+// in production we should not use it without special order from menagers
+if (process.env.RESET_DB) {
+  const resetDataBase = async () => {
+  //  await addedNominate.deleteMany(); // waiting to delete all data before we create a new object
+    const addedNominate = new Nomination({
+      year_film: 2044, 
+      year_award: 2045,
+      ceremony: 165,
+      category: "best tragic comedy",
+      nominee: "smurf",
+      win: false
+      
+    
+    });
+    addedNominate.save() // we need to put it in the reset function because if is after the deleteMany happens before and we do not update the DB
+
+  }
+  resetDataBase();
+}
+
+
+
+
+
+
+
+
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8090;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
