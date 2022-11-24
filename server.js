2f47be8fc080ca04c06ba4240f9e1e8046828eaa
Petra Eriksson <eriksson.petra@gmail.com>
2022-11-24 13:10:00 +0100

    created a model for our DB, data collection songs

--- server.js
+++ server.js
@@ -1,18 +1,51 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import topMusicData from "./data/top-music.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"; 
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }); // connection. Using the method connect, passing on the url and then the parameter object
 mongoose.Promise = Promise;
+
+const User = mongoose.model("User", {
+  name: String,
+  age: Number,
+  deceased: Boolean
+});
+
+//model how your data set will look like
+const Song = mongoose.model("Song", {
+  "id": Number,
+  "trackName": String,
+  "artistName": String,
+  "genre": String,
+  "bpm": Number,
+  "energy": Number,
+  "danceability": Number,
+  "loudness": Number,
+  "liveness": Number,
+  "valence": Number,
+  "length": Number,
+  "acousticness": Number,
+  "speechiness": Number,
+  "popularity": Number
+});
+
+
+//map out all data from our json into our db
+if (process.env.RESET_DB) {
+    const resetDataBase = async () => {
+    await Song.deleteMany();
+    topMusicData.forEach(singleSong => {
+    const newSong = new Song (singleSong);
+    newSong.save();
+   })
+    // await User.deleteMany();
+    // const testUser = new User ({name: "Lisa", age: 53, deceased: false });
+    // testUser.save();
+  }
+  resetDataBase();
+}
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
