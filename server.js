7fc95c0df7ae861c89c8e833b670cbb3b79248f1
joelohman <joel.ohman@entryevent.se>
2022-11-24 14:27:14 +0100

    Initial commit, some endpoints

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -8,9 +8,9 @@ import mongoose from "mongoose";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import topMusicData from './data/top-music.json';
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-mongo';
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -24,9 +24,113 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const Song = mongoose.model('Song', {
+  id: Number,
+  trackName: String,
+  artistName: String,
+  genre: String,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  loudness: Number,
+  liveness: Number,
+  valence: Number,
+  length: Number,
+  acousticness: Number,
+  speechiness: Number,
+  popularity: Number
+});
+
+if (process.env.RESET_DB) {
+  const resetDataBase = async () => {
+    await Song.deleteMany();
+    topMusicData.forEach((singleSong) => {
+      const newSong = new Song(singleSong);
+      newSong.save();
+    });
+  };
+
+  resetDataBase();
+  console.log('database dropped and recreated');
+}
+
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+app.get('/', (req, res) => {
+  res.send('Hello Technigo!');
+});
+
+// app.get('/songs', async (req, res) => {
+//   const allTheSongs = await Song.find({});
+//   res.status(200).json({
+//     success: true,
+//     body: allTheSongs
+//   });
+// });
+app.get('/songs/id/:id', async (req, res) => {
+  try {
+    const singleSong = await Song.findById(req.params.id);
+    console.log(singleSong);
+
+    if (singleSong) {
+      res.status(200).json({
+        success: true,
+        body: singleSong
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        body: {
+          message: 'Song was not found'
+        }
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      body: {
+        message: 'Invalid id'
+      }
+    });
+  }
+});
+
+// app.get('/songs/genre/:genre/danceability/:danceability', async (req, res) => {
+app.get('/songs/', async (req, res) => {
+  const matchAllRegex = new RegExp('.*');
+  const { genre, danceability } = req.query;
+  const genreQuery = genre ? genre : matchAllRegex;
+  const danceabilityQuery = danceability ? danceability : matchAllRegex;
+  try {
+    let allMatchingSongs = {};
+    // if (req.params.genre && req.params.danceability) {
+    allMatchingSongs = await Song.find({
+      genre: genreQuery,
+      danceability: danceabilityQuery
+    })
+      .limit(2)
+      .sort({ energy: 1 })
+      .select({ artistName: true, trackName: false });
+    // } else if (req.params.genre && !req.params.danceability) {
+    //   allMatchingSongs = await Song.find({
+    //     genre: req.params.genre
+    //   });
+    // } else if (!req.params.genre && req.params.danceability) {
+    //   allMatchingSongs = await Song.find({
+    //     danceability: req.params.danceability
+    //   });
+    // }
+    res.status(200).json({
+      success: true,
+      body: allMatchingSongs
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      body: {
+        message: error
+      }
+    });
+  }
 });
 
 // Start the server
