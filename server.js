bf4fe9842904766a0c8faf86ce63db63cf4bab47
Cecilia Råden <you@example.com>
2022-11-24 14:48:57 +0100

    added endpoints

--- server.js
+++ server.js
@@ -7,51 +7,48 @@ import mongoose from "mongoose";
 // import avocadoSalesData from "./data/avocado-sales.json";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
-import netflixData from "./data/netflix-titles.json";
-//import topMusicData from "./data/top-music.json";
+// import netflixData from "./data/netflix-titles.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-const Title = mongoose.model("Title", {
-    title: String,
-    director: String,
-    cast: String,
-    country: String,
-    date_added: String,
-    release_year: Number,
-    rating: String,
-    duration: String,
-    listed_in: String,
-    description: String,
-    type: String
-});
-
-if(process.env.RESET_DB) {
-const seedDatabase = async () => {
-  await Title.deleteMany()
-  const snowwhite = new Title({ title: "Snow White" })
-  await snowwhite.save()
-  const lionking = new Title({ title: "Lion King" })
-  await lionking.save()
-}
-
-seedDatabase()
-
-
- /* const resetDataBase = async () => {
-    await Title.deleteMany();
-    netflixData.forEach(singleTitle => {
-      const NewTitle = new Title(singleTitle);
-      NewTitle.save()
-    })
-  }
-  resetDataBase(); */
-} 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
+/*
+const User = mongoose.model("User", {
+  name: String,
+  age: Number,
+  deceased: Boolean
+});
+ */
+const Avocado = mongoose.model("Avocado", {
+  id: Number,
+  date: String,
+  averagePrice: Number,
+  totalVolume: Number,
+  totalBagsSold: Number,
+  smallBagsSold: Number,
+  largeBagsSold: Number,
+  xLargeBagsSold: Number,
+  region: String
+});
+
+if(process.env.RESET_DB) {
+  const resetDataBase = async () => {
+    await Avocado.deleteMany();
+    avocadoSalesData.forEach(singleAvocado => {
+      const newAvocado = new Avocado(singleAvocado);
+      newAvocado.save();
+    })
+    // await User.deleteMany();
+    // const testUser = new User({name: "Daniel", age: 28, deceased: false});
+    // testUser.save();
+  }
+  resetDataBase();
+}
+
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -60,16 +57,127 @@ app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+app.get("/avocados", async (req, res) => {
+const EveryAvocado = await Avocado.find();
+res.status(200).json({
+  success: true, 
+  body: EveryAvocado
+})
 });
 
-app.get("/titles", async (req, res) => {
- const titles = await Title.find()
- res.json(titles)
+app.get("/id/:id", async (req, res) => {
+  try {
+    const SingleAvocado = await Avocado.findById(req.params.id);
+    if (SingleAvocado) {
+      res.status(200).json({
+        success: true,
+        body: SingleAvocado
+      })
+    } else {
+      res.status(404).json({
+        success: false,
+        body: {
+          message: "Could not find the region of the avocado"
+        }
+      });
+    }
+  } catch(error) {
+    res.status(400).json({
+      success: false,
+      body: {
+        message: "Invalid id"
+      }
+    });
+  }
+  
 });
+/*
+app.get("/songs", async (req, res) => {
+const allTheSongs = await Song.find({});
+res.status(200).json({
+success: true,
+body: allTheSongs
+});
+}); 
 
+app.get("/songs/id/:id", async (req, res) => {
+  try {
+    const singleSong = await Song.findById(req.params.id);
+    if (singleSong) {
+      res.status(200).json({
+        success: true,
+        body: singleSong
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        body: {
+          message: "Could not find the song"
+        }
+      });
+    }
+  } catch(error) {
+    res.status(400).json({
+      success: false,
+      body: {
+        message: "Invalid id"
+      }
+    });
+  }
+  
+});
+/*
+// app.get("/songs/genre/:genre/danceability/:danceability", async (req, res) => {
+app.get("/songs/", async (req, res) => {
+
+  const {genre, danceability} = req.query;
+  const response = {
+    success: true,
+    body: {}
+  }
+  const matchAllRegex = new RegExp(".*");
+  const genreQuery = genre ? genre : matchAllRegex;
+  const danceabilityQuery = danceability ? danceability : /.;
+
+  try {
+    // if ( req.params.genre && req.params.danceability) {
+      response.body = await Song.find({genre: genreQuery, danceability: danceabilityQuery}).limit(2).sort({energy: 1}).select({trackName: 1, artistName: 1})
+      //.exec() => to explore if you're curious enough :P
+    // } else if (req.params.genre && !req.params.danceability) {
+    //   response.body = await Song.find({genre: req.params.genre});
+    // } else if (!req.params.genre && req.params.danceability) {
+    //   response.body = await Song.find({danceability: req.params.danceability});
+    // }
+    res.status(200).json({
+      success: true,
+      body: response
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      body: {
+        message: error
+      }
+    });
+  }
+
+}); */
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+//RESET_DB=true npm run dev
+// Go here:
+//https://github.com/coreybutler/nvm-windows/releases
+// downlaod nvm-setup.exe
+// run as admin
+// open cmd as admin
+// type nvm install v16.18.1
+//https://mongoosejs.com/docs/queries
+
+//https://regex101.com/
+
+// /yourWodOfChoice/gm - regex to match yourWordOfChoice
+// /.*/gm - regex to match every character in a string 
+
+
