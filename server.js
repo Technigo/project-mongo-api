84c07b80c4eb71b97d65ad7c28b8580e4281055f
fannycardell <fannycardell@gmail.com>
2022-11-25 12:10:32 +0100

    got code along to work

--- server.js
+++ server.js
@@ -3,22 +3,16 @@ import cors from "cors";
 import mongoose from "mongoose";
 import topMusicData from "./data/top-music.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
+
+// Add middlewares to enable cors and json body parsing
+app.use(cors());
+app.use(express.json());
 
 const Song = mongoose.model("Song", {
   id: Number,
@@ -49,22 +43,18 @@ if (true) {
   resetDataBase();
 }
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
-
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigoooooo heyhooooo!");
 });
 
-app.get("/songs", async (req, res) => {
+/*app.get("/songs", async (req, res) => {
   const allTheSongs = await Song.find({});
   res.status(200).json({
     success: true,     
     body: allTheSongs
    });
- });
+ }); */
 
 app.get("/songs/id/:id", async (req, res) => {
   try {
@@ -92,21 +82,21 @@ app.get("/songs/id/:id", async (req, res) => {
   }
   
 });
-// app.get("/songs/genre/:genre/danceability/:danceability", async (req, res) => {
-app.get("/songs/", async (req, res) => {
 
+app.get("/songs/", async (req, res) => {
   const {genre, danceability} = req.query;
   const response = {
     success: true,
     body: {}
   }
   const matchAllRegex = new RegExp(".*");
-  const genreQuery = genre ? genre : matchAllRegex;
-  const danceabilityQuery = danceability ? danceability : /.*/;
+  const matchAllNumeric = new RegExp("[0-9]");
+  const genreQuery = genre ? genre : {$regex: matchAllRegex,  $options: 'i' };
+  const danceabilityQuery = danceability ? danceability : {$gt: 0, $lt: 100};
 
   try {
     // if ( req.params.genre && req.params.danceability) {
-      response.body = await Song.find({genre: genreQuery, danceability: danceabilityQuery}).limit(2).sort({energy: 1}).select({trackName: 1, artistName: 1})
+      response.body = await Song.find({genre: genreQuery, danceability:danceabilityQuery}).limit(2).sort({energy: 1}).select({trackName: 1, artistName: 1})
       //.exec() => to explore if you're curious enough :P
     // } else if (req.params.genre && !req.params.danceability) {
     //   response.body = await Song.find({genre: req.params.genre});
@@ -131,16 +121,3 @@ app.get("/songs/", async (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-//RESET_DB=true npm run dev
-// Go here:
-//https://github.com/coreybutler/nvm-windows/releases
-// downlaod nvm-setup.exe
-// run as admin
-// open cmd as admin
-// type nvm install v16.18.1
-//https://mongoosejs.com/docs/queries
-
-//https://regex101.com/
-
-// /yourWodOfChoice/gm - regex to match yourWordOfChoice
-// /.*/gm - regex to match every character in a string
