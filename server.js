fc591e296e5ebdc1cd6f8f9fa4084bcfc787943c
Lisa Johansson <lisajoh@icloud.com>
2022-11-25 13:36:43 +0100

    Set upp two endpoints to start with

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import mongoose from "mongoose";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import topMusicData from "./data/top-music.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -17,6 +17,45 @@ mongoose.Promise = Promise;
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
+
+const User = mongoose.model("User", {
+  name: String,
+  age: Number,
+  deceased: Boolean
+});
+
+const Song = mongoose.model("Song", {
+  id: Number,
+  trackName: String,
+  artistName: String,
+  genre: String,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  loudness: Number,
+  liveness: Number,
+  valence: Number,
+  length: Number,
+  acousticness: Number,
+  speechiness: Number,
+  popularity: Number
+});
+
+if(process.env.RESET_DB) {
+  const resetDataBase = async () => {
+    await Song.deleteMany();
+    topMusicData.forEach(singleSong => {
+      const newSong = new Song(singleSong);
+      newSong.save();
+    })
+    //await User.deleteMany();
+   // const testUser = new User({name: "Daniel", age: 28, deceased: false});
+   // testUser.save();
+  }
+  resetDataBase();
+}
+
+
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -27,6 +66,23 @@ app.use(express.json());
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+// endpoint shows all songs
+app.get("/music", async (req, res)=> {
+  const music = await Song.find()
+  res.send(music)
+});
+
+//endpoint shows a single song
+app.get("/music/:id", async (req, res) => {
+  const singleSong = await Song.findById(req.params.id)
+  if (singleSong){
+    res.json(singleSong)
+  } else {
+    res.status(404).json("Error: Song not found")
+  }
+  //res.json(singleSong)
 });
 
 // Start the server
