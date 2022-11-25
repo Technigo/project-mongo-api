84d9e69bba0784c1e061d640706b07e74e0d4b82
MariaWellander <maria.wellander@gmail.com>
2022-11-25 15:22:07 +0100

    accomplished the base - yay

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// The connection string that I will need:
+// mongodb+srv://Maria:<password>@cluster0.0cj9ggy.mongodb.net/?retryWrites=true&w=majority
+
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
@@ -8,7 +11,7 @@ import mongoose from "mongoose";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import topMusicData from "./data/top-music.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -17,8 +20,48 @@ mongoose.Promise = Promise;
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
+
+const User = mongoose.model("User", {
+  name: String,
+  age: Number,
+  deceased: Boolean
+});
+
+const Song = mongoose.model("Song", {
+  id: Number,
+  trackName: String,
+  artistName: String,
+  genre: String,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  loudness: Number,
+  liveness: Number,
+  valence: Number,
+  length: Number,
+  acousticness: Number,
+  speechiness: Number,
+  popularity: Number
+});
+
+if(process.env.RESET_DB) {
+  const resetDataBase = async () => {
+    await Song.deleteMany();
+    topMusicData.forEach(singleSong => {
+      const newSong = new Song(singleSong);
+      newSong.save();
+    })
+    // await User.deleteMany();
+    // const testUser = new User({name: "Daniel", age: 28, deceased: false});
+    // testUser.save();
+  }
+  resetDataBase();
+}
+
+
+
 const port = process.env.PORT || 8080;
-const app = express();
+const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
