6c3a3ad3eb794db4a7ac4237adb1a2ffe8829314
Cecilia Råden <you@example.com>
2022-11-25 09:32:20 +0100

    cleaned code

--- server.js
+++ server.js
@@ -2,23 +2,12 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
 const port = process.env.PORT || 8080;
 const app = express();
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 
 const Avocado = mongoose.model("Avocado", {
   id: Number,
@@ -44,13 +33,10 @@ if(process.env.RESET_DB) {
   resetDataBase();
 }
 
-
-
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+// Routes
 app.get("/", async (req, res) => {
 const EveryAvocado = await Avocado.find();
 res.status(200).json({
