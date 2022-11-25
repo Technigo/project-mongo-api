95cfdad2e83c56e70c8e21d71ce0610d8f9ea024
dmnich <daniel@technigo.io>
2022-11-25 16:20:38 +0100

    test deploy to gcp

--- server.js
+++ server.js
@@ -1,148 +1,148 @@
-// import express from "express";
-// import cors from "cors";
-// import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
-// // If you're using one of our datasets, uncomment the appropriate import below
-// // to get started!
+// If you're using one of our datasets, uncomment the appropriate import below
+// to get started!
 import avocadoSalesData from "./data/avocado-sales.json";
-// // import booksData from "./data/books.json";
-// // import goldenGlobesData from "./data/golden-globes.json";
-// // import netflixData from "./data/netflix-titles.json";
+// import booksData from "./data/books.json";
+// import goldenGlobesData from "./data/golden-globes.json";
+// import netflixData from "./data/netflix-titles.json";
+import topMusicData from "./data/top-music.json";
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1/project-mongo";
 
-// const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1/project-mongo";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-// mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-// mongoose.Promise = Promise;
+// Defines the port the app will run on. Defaults to 8080, but can be overridden
+// when starting the server. Example command to overwrite PORT env variable value:
+// PORT=9000 npm start
 
-// // Defines the port the app will run on. Defaults to 8080, but can be overridden
-// // when starting the server. Example command to overwrite PORT env variable value:
-// // PORT=9000 npm start
+const User = mongoose.model("User", {
+  name: String,
+  age: Number,
+  deceased: Boolean
+});
 
-// const User = mongoose.model("User", {
-//   name: String,
-//   age: Number,
-//   deceased: Boolean
-// });
-
-// const Song = mongoose.model("Song", {
-//   id: Number,
-//   trackName: String,
-//   artistName: String,
-//   genre: String,
-//   bpm: Number,
-//   energy: Number,
-//   danceability: Number,
-//   loudness: Number,
-//   liveness: Number,
-//   valence: Number,
-//   length: Number,
-//   acousticness: Number,
-//   speechiness: Number,
-//   popularity: Number
-// });
-
-
-// if(process.env.RESET_DB) {
-//   const resetDataBase = async () => {
-//     await Song.deleteMany();
-//     topMusicData.forEach(singleSong => {
-//       const newSong = new Song(singleSong);
-//       newSong.save();
-//     })
-//     // await User.deleteMany();
-//     // const testUser = new User({name: "Daniel", age: 28, deceased: false});
-//     // testUser.save();
-//   }
-//   resetDataBase();
-// }
+const Song = mongoose.model("Song", {
+  id: Number,
+  trackName: String,
+  artistName: String,
+  genre: String,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  loudness: Number,
+  liveness: Number,
+  valence: Number,
+  length: Number,
+  acousticness: Number,
+  speechiness: Number,
+  popularity: Number
+});
 
 
-// const port = process.env.PORT || 8080;
-// const app = express();
+if(process.env.RESET_DB) {
+  const resetDataBase = async () => {
+    await Song.deleteMany();
+    topMusicData.forEach(singleSong => {
+      const newSong = new Song(singleSong);
+      newSong.save();
+    })
+    // await User.deleteMany();
+    // const testUser = new User({name: "Daniel", age: 28, deceased: false});
+    // testUser.save();
+  }
+  resetDataBase();
+}
 
-// // Add middlewares to enable cors and json body parsing
-// app.use(cors());
-// app.use(express.json());
 
-// // Start defining your routes here
-// app.get("/", (req, res) => {
-//   res.send("Hello Technigo!");
-// });
-// // app.get("/songs", async (req, res) => {
-// //   const allTheSongs = await Song.find({});
-// //   res.status(200).json({
-// //     success: true,
-// //     body: allTheSongs
-// //   });
-// // });
+const port = process.env.PORT || 8080;
+const app = express();
 
-// app.get("/songs/id/:id", async (req, res) => {
-//   try {
-//     const singleSong = await Song.findById(req.params.id);
-//     if (singleSong) {
-//       res.status(200).json({
-//         success: true,
-//         body: singleSong
-//       });
-//     } else {
-//       res.status(404).json({
-//         success: false,
-//         body: {
-//           message: "Could not find the song"
-//         }
-//       });
-//     }
-//   } catch(error) {
-//     res.status(400).json({
-//       success: false,
-//       body: {
-//         message: "Invalid id"
-//       }
-//     });
-//   }
-  
-// });
-// // app.get("/songs/genre/:genre/danceability/:danceability", async (req, res) => {
-// app.get("/songs/", async (req, res) => {
+// Add middlewares to enable cors and json body parsing
+app.use(cors());
+app.use(express.json());
 
-//   const {genre, danceability} = req.query;
-//   const response = {
+// Start defining your routes here
+app.get("/", (req, res) => {
+  res.send("Hello Technigo!");
+});
+// app.get("/songs", async (req, res) => {
+//   const allTheSongs = await Song.find({});
+//   res.status(200).json({
 //     success: true,
-//     body: {}
-//   }
-//   const matchAllRegex = new RegExp(".*");
-//   const matchAllNumeric = new RegExp("[0-9]");
-//   const genreQuery = genre ? genre : {$regex: matchAllRegex,  $options: 'i' };
-//   const danceabilityQuery = danceability ? danceability : {$gt: 0, $lt: 100};
-
-//   try {
-//     // 
-//     // if ( req.params.genre && req.params.danceability) {
-//       response.body = await Song.find({genre: genreQuery, danceability:danceabilityQuery}).limit(2).sort({energy: 1}).select({trackName: 1, artistName: 1})
-//       //.exec() => to explore if you're curious enough :P
-//     // } else if (req.params.genre && !req.params.danceability) {
-//     //   response.body = await Song.find({genre: req.params.genre});
-//     // } else if (!req.params.genre && req.params.danceability) {
-//     //   response.body = await Song.find({danceability: req.params.danceability});
-//     // }
-//     res.status(200).json({
-//       success: true,
-//       body: response
-//     });
-//   } catch (error) {
-//     res.status(400).json({
-//       success: false,
-//       body: {
-//         message: error
-//       }
-//     });
-//   }
-
+//     body: allTheSongs
+//   });
 // });
-// // Start the server
-// app.listen(port, () => {
-//   console.log(`Server running on http://localhost:${port}`);
-// });
+
+app.get("/songs/id/:id", async (req, res) => {
+  try {
+    const singleSong = await Song.findById(req.params.id);
+    if (singleSong) {
+      res.status(200).json({
+        success: true,
+        body: singleSong
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        body: {
+          message: "Could not find the song"
+        }
+      });
+    }
+  } catch(error) {
+    res.status(400).json({
+      success: false,
+      body: {
+        message: "Invalid id"
+      }
+    });
+  }
+  
+});
+// app.get("/songs/genre/:genre/danceability/:danceability", async (req, res) => {
+app.get("/songs/", async (req, res) => {
+
+  const {genre, danceability} = req.query;
+  const response = {
+    success: true,
+    body: {}
+  }
+  const matchAllRegex = new RegExp(".*");
+  const matchAllNumeric = new RegExp("[0-9]");
+  const genreQuery = genre ? genre : {$regex: matchAllRegex,  $options: 'i' };
+  const danceabilityQuery = danceability ? danceability : {$gt: 0, $lt: 100};
+
+  try {
+    // 
+    // if ( req.params.genre && req.params.danceability) {
+      response.body = await Song.find({genre: genreQuery, danceability:danceabilityQuery}).limit(2).sort({energy: 1}).select({trackName: 1, artistName: 1})
+      //.exec() => to explore if you're curious enough :P
+    // } else if (req.params.genre && !req.params.danceability) {
+    //   response.body = await Song.find({genre: req.params.genre});
+    // } else if (!req.params.genre && req.params.danceability) {
+    //   response.body = await Song.find({danceability: req.params.danceability});
+    // }
+    res.status(200).json({
+      success: true,
+      body: response
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      body: {
+        message: error
+      }
+    });
+  }
+
+});
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
 // //RESET_DB=true npm run dev
 // // Go here:
 // //https://github.com/coreybutler/nvm-windows/releases
@@ -156,71 +156,74 @@ import avocadoSalesData from "./data/avocado-sales.json";
 
 // // /yourWodOfChoice/gm - regex to match yourWordOfChoice
 // // /.*/gm - regex to match every character in a string
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.Promise = Promise;
-const port = process.env.PORT || 8080;
-const app = express();
-const Avocado = mongoose.model("Avocado", {
-  id: Number,
-  date: String,
-  averagePrice: Number,
-  totalVolume: Number,
-  totalBagsSold: Number,
-  smallBagsSold: Number,
-  largeBagsSold: Number,
-  xLargeBagsSold: Number,
-  region: String
-});
-if(process.env.RESET_DB) {
-  const resetDataBase = async () => {
-    await Avocado.deleteMany();
-    avocadoSalesData.forEach(singleAvocado => {
-      const newAvocado = new Avocado(singleAvocado);
-      newAvocado.save();
-    })
-  }
-  resetDataBase();
-}
-app.use(cors());
-app.use(express.json());
-// Routes
-app.get("/", async (req, res) => {
-const EveryAvocado = await Avocado.find({});
-res.status(200).json({
-  success: true,
-  body: EveryAvocado
-})
-});
-app.get("/id/:id", async (req, res) => {
-  try {
-    const SingleAvocado = await Avocado.findById(req.params.id);
-    if (SingleAvocado) {
-      res.status(200).json({
-        success: true,
-        body: SingleAvocado
-      })
-    } else {
-      res.status(404).json({
-        success: false,
-        body: {
-          message: "Could not find the region of the avocado"
-        }
-      });
-    }
-  } catch(error) {
-    res.status(400).json({
-      success: false,
-      body: {
-        message: "Invalid id"
-      }
-    });
-  }
-});
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+// import express from "express";
+// import cors from "cors";
+// import mongoose from "mongoose";
+// const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+// mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.Promise = Promise;
+// const port = process.env.PORT || 8080;
+// const app = express();
+// const Avocado = mongoose.model("Avocado", {
+//   id: Number,
+//   date: String,
+//   averagePrice: Number,
+//   totalVolume: Number,
+//   totalBagsSold: Number,
+//   smallBagsSold: Number,
+//   largeBagsSold: Number,
+//   xLargeBagsSold: Number,
+//   region: String
+// });
+// if(process.env.RESET_DB) {
+//   const resetDataBase = async () => {
+//     await Avocado.deleteMany();
+//     avocadoSalesData.forEach(singleAvocado => {
+//       const newAvocado = new Avocado(singleAvocado);
+//       newAvocado.save();
+//     })
+//   }
+//   resetDataBase();
+// }
+// app.use(cors());
+// app.use(express.json());
+// // Routes
+// app.get("/", async (req, res) => {
+// const EveryAvocado = await Avocado.find({});
+// res.status(200).json({
+//   success: true,
+//   body: EveryAvocado
+// })
+// });
+// app.get("/id/:id", async (req, res) => {
+//   try {
+//     const SingleAvocado = await Avocado.findById(req.params.id);
+//     if (SingleAvocado) {
+//       res.status(200).json({
+//         success: true,
+//         body: SingleAvocado
+//       })
+//     } else {
+//       res.status(404).json({
+//         success: false,
+//         body: {
+//           message: "Could not find the region of the avocado"
+//         }
+//       });
+//     }
+//   } catch(error) {
+//     res.status(400).json({
+//       success: false,
+//       body: {
+//         message: "Invalid id"
+//       }
+//     });
+//   }
+// });
+// // Start the server
+// app.listen(port, () => {
+//   console.log(`Server running on http://localhost:${port}`);
+// });
+
+
+// mongodb+srv://daniel:xYJWItQks7uGELgy@cluster0.foghorf.mongodb.net/?retryWrites=true&w=majority
