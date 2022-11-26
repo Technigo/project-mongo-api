b5b5dc302ccc0a814b6613786465817923ef3dbe
Mine Mine Mine <MasterNina@NBs-MacBook-Pro.local>
2022-11-26 01:01:13 +0100

    new array

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 // import dotenv from "dotenv"
-import tedTalkData from "./data/ted-talks.json";
+import tedTalkData from "./data/ted-talksMod.json";
 
 // dotenv.config()
 
@@ -52,15 +52,6 @@ if (process.env.RESET_DB) {
     seedDataBase()
   }
 
-// Gör så att DB rensas och bara de inom funktionen skapas vid refresh
-// Test.deleteMany().then(() => {
-//   new Test({ name: 'J.R.R Tolkien' }).save()
-//   new Test({ name: 'N.S.Berggren' }).save()
-//   new Test({ name: 'J.K. Rowling' }).save()
-// })
-
-
-
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
@@ -80,8 +71,9 @@ app.use((req, res, next) => {
   }
 }) 
 
+
 // Start defining your routes here
-// Route 1 visar vår model
+// Route 1 show introductions to endpoints
 app.get("/", (req, res) => {
   res.send("Hello Everybody and welcome!");
   // TedTalk.find().then(tedTalks => {
@@ -89,10 +81,85 @@ app.get("/", (req, res) => {
   // })
 });
 
-// Route 1 - collection of results (array of elements)
-app.get("/allTedTalks", (req, res) => {
-  res.status(200).json(tedTalkData); 
+// Route # - List of all TED Talks (array of elements)
+app.get("/allTedTalks", async (req, res) => {
+  // res.status(200).json(tedTalkData); 
+  const allTedTalks = await TedTalk.find({});
+    res.status(200).json({
+      success: true,
+      body: allTedTalks
+    });
+    console.log(tedTalkData.length)
 });
+
+// ROUTE #: Filter on a specific TED TAlk with id.
+ // req.params.id refers to given _id
+app.get("/tedTalk/id/:id", async (req, res) => {
+  try {
+    const singleTedTalk = await TedTalk.findById(req.params.id);
+     
+    if (singleTedTalk) {
+      res.status(200).json({
+        success: true,
+        body: singleTedTalk
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        body: {
+          message: `No TED Talk whit this id ${req.params.id}, please try again`
+        }
+      });
+    }
+  } catch(error) {
+    res.status(400).json({
+      success: false,
+      body: {
+        message: "Invalid id"
+      }
+    });
+  }
+  
+});
+
+app.get("/talks/", async (req, res) => {
+
+  const {event, speaker, views} = req.query;
+  const response = {
+    success: true,
+    body: {}
+  }
+  const matchAllRegex = new RegExp(".*");
+  const eventQuery = event ? event : matchAllRegex;
+  const speakerQuery = speaker ?speaker : matchAllRegex;
+  const viewsQuery = views ? views : {$gt:0, $lt:3000000};
+
+  try {
+    // if ( req.params.genre && req.params.danceability) {
+      response.body = await TedTalk.find({event: eventQuery,speaker: speakerQuery, views: viewsQuery}).limit(5)
+      // response.body = await TedTalk.find({event: speakerQuery,speaker: speakerQuery, views: viewsQuery}).limit(2).sort({energy: 1}).select({trackName: 1, artistName: 1})
+      //.exec() => to explore if you're curious enough :P
+    // } else if (req.params.genre && !req.params.danceability) {
+    //   response.body = await Song.find({genre: req.params.genre});
+    // } else if (!req.params.genre && req.params.danceability) {
+    //   response.body = await Song.find({danceability: req.params.danceability});
+    // }
+    res.status(200).json({
+      success: true,
+      body: response
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      body: {
+        message: error
+      }
+    });
+  }
+
+});
+
+
 
 // :name, is case sensetive need to be exact
 // app.get('/:speaker', (req, res) => {
