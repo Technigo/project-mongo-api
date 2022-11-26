0d50101c25bb40270fb6f154ae06730884ae0b8b
MariaWellander <maria.wellander@gmail.com>
2022-11-26 12:26:57 +0100

    added another json, names, deleted comments

--- server.js
+++ server.js
@@ -2,60 +2,32 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-
-const User = mongoose.model("User", {
-  name: String,
-  age: Number,
-  deceased: Boolean
-});
-
-const Song = mongoose.model("Song", {
+const Sale = mongoose.model("Sale", {
   id: Number,
-  trackName: String,
-  artistName: String,
-  genre: String,
-  bpm: Number,
-  energy: Number,
-  danceability: Number,
-  loudness: Number,
-  liveness: Number,
-  valence: Number,
-  length: Number,
-  acousticness: Number,
-  speechiness: Number,
-  popularity: Number
+  date: Number,
+  averagePrice: Number,
+  totalVolume: Number,
+  totalBagsSold: Number,
+  smallBagsSold: Number,
+  largeBagsSold: Number,
+  xLargeBagsSold: Number,
+  popularity: String
 });
 
 if(process.env.RESET_DB) {
   const resetDataBase = async () => {
-    await Song.deleteMany();
-    topMusicData.forEach(singleSong => {
-      const newSong = new Song(singleSong);
-      newSong.save();
+    await Sale.deleteMany();
+    avocadoSalesData.forEach(singleSale => {
+      const newSale = new Sale(singleSale);
+      newSale.save();
     })
-    // await User.deleteMany();
-    // const testUser = new User({name: "Daniel", age: 28, deceased: false});
-    // testUser.save();
   }
   resetDataBase();
 }
-
-
 
 const port = process.env.PORT || 8080;
 const app = express()
@@ -65,32 +37,26 @@ app.use(cors());
 app.use(express.json());
 
 // --- Start of ROUTES ---
+
 // 1st route, the base path.
 app.get("/", (req, res) => {
   res.send("Hello Avocado Sales Lover!");
 });
-// To get all songs.
-// app.get("/songs", async (req, res) => {
-//  const allTheSongs = await Song.find({});
-//  res.status(200).json({
-//    success: true,
-//    body: allTheSongs
-//  })
-// });
-// 2nd route. To get a specific song, using try-catch code block and the _id.
-app.get("/songs/id/:id", async (req, res) => {
+
+// 2nd route. To get a specific sale, using try-catch code block and the _id. The _id is in the MongoDB Compass.
+app.get("/sales/id/:id", async (req, res) => {
   try {
-    const singleSong = await Song.findById(req.params.id);
-    if (singleSong) {
+    const singleSale = await Sale.findById(req.params.id);
+    if (singleSale) {
       res.status(200).json({
         success: true,
-        body: singleSong
+        body: singleSale
       });
     } else {
       res.status(404).json({
         success: false,
         body: {
-          message: "Could not find the song"
+          message: "Could not find the sale"
         }
       });
     }
@@ -102,30 +68,20 @@ app.get("/songs/id/:id", async (req, res) => {
       }
     });
   }
-
 });
-// 3rd route. To get a song/songs with specific parameters, using try-catch. The params are; genre & danceability.
-// app.get("/songs/genre/:genre/danceability/:danceability", async (req, res) => {
-app.get("/songs/", async (req, res) => {
 
-  const {genre, danceability} = req.query;
+// 3rd route. To get a sale/sales with specific parameters, using try-catch. The params are; region & averagePrice.
+app.get("/sales/", async (req, res) => {
+  const {region, averagePrice} = req.query;
   const response = {
     success: true,
     body: {}
   }
   const matchAllRegex = new RegExp(".*");
-  const genreQuery = genre ? genre : matchAllRegex;
-  const danceabilityQuery = danceability ? danceability : matchAllRegex;
-
+  const regionQuery = region ? region : matchAllRegex;
+  const averagePriceQuery = averagePrice ? averagePrice : matchAllRegex;
   try {
-    //if (req.params.genre && req.params.danceability) {
-      response.body = await Song.find({genre: genreQuery, danceability: danceabilityQuery}).limit(2).sort({energy: 1}).select({trackName: 1, artistName: 1})
-      // .exec() => to explore if you're curious enough :P
-    // } else if (req.params.genre && !req.params.danceability) {
-    //  response.body = await Song.find({genre: req.params.genre});
-    // } else if (!req.params.genre && req.params.danceability) {
-    //  response.body = await Song.find({danceability: req.params.danceability});
-    // }
+      response.body = await Sale.find({region: regionQuery, averagePrice: averagePriceQuery}).limit(3).sort({totalVolume: 1}).select({totalBagsSold: 1, largeBagsSold: 1})
     res.status(200).json({
       success: true,
       body: response
@@ -138,15 +94,14 @@ app.get("/songs/", async (req, res) => {
       }
     });
   }
-  
 });
 // --- End of ROUTES ---
-
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
 // The connection string that I will need:
 // mongodb+srv://Maria:<password>@cluster0.0cj9ggy.mongodb.net/?retryWrites=true&w=majority
 
