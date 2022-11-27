09071e1818ba8b247712aae0691ae4cab2faf0ff
Julia Ostedt <julia.ostedt@gmail.com>
2022-11-27 22:25:27 +0100

    removed comments

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
 import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+
+dotenv.config()
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -42,20 +36,18 @@ if(process.env.RESET_DB) {
   resetDataBase();
 }
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
 app.get("/", (req, res) => {
   const endpoints = {
     Welcome: "Hi! This is an open API about Golden Globes",
     Routes: [
       {
-        "/goldenglobes": "Get an array of all Golden Globes objects in the array",
+        "/goldenglobes": "An array of all Golden Globes objects",
         "/goldenglobes/awardyear/:year_award": "Gives back an array with objects based on the year that is typed",
         "/goldenglobes/nominees/:nominee": "Gives back an array with objects based on the name of the nominee",
-        "/goldenglobes/winners/:win": "Gives back an array of all the winners or the ones who did not win",
+        "/goldenglobes/winners/:win": "Gives back an array of all the winners or the ones who did not win. True/False",
       },
     ],
   }
@@ -126,20 +118,8 @@ app.get("/goldenglobes/winners/:win", async (req, res) => {
 })
 
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
 //RESET_DB=true npm run dev
-// Go here:
-//https://github.com/coreybutler/nvm-windows/releases
-// downlaod nvm-setup.exe
-// run as admin
-// open cmd as admin
-// type nvm install v16.18.1
-//https://mongoosejs.com/docs/queries
-
-//https://regex101.com/
-
-// /yourWodOfChoice/gm - regex to match yourWordOfChoice
-// /.*/gm - regex to match every character in a string
