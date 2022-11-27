686f397fa547298af01b46adc50c56c4d5c5b1b0
Cecilia Råden <you@example.com>
2022-11-27 16:32:16 +0100

    removed commas

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
-import netflixData from "./data/netflix-titles.json";
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+import netflixData from "./data/netflix-titles.json"
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.Promise = Promise;
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
 
-const port = process.env.PORT || 8080;
-const app = express();
+const port = process.env.PORT || 8080
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-const Title = mongoose.model("Title", {
+const Title = mongoose.model('Title', {
   show_id: Number,
   title: String,
   director: String, 
@@ -25,35 +25,35 @@ const Title = mongoose.model("Title", {
   duration: String,
   listed_in: String,
   description: String,
-  type: String,
+  type: String
 })
 
 if(true) {
   const resetDataBase = async () => {
    netflixData.forEach(singleTitle => {
       const newTitle = new Title(singleTitle)
-      newTitle.save();
+      newTitle.save()
     })
 
   }
-  resetDataBase();
+  resetDataBase()
 }
 
 // Routes
 
 app.get("/", (req, res) => {
-  res.send("This is an api for Netflix-titles.");
-  });
+  res.send("This is an api for Netflix-titles.")
+  })
 
 app.get("/titles", async (req, res) => {
-  const allTheTitles = await Title.find({});
+  const allTheTitles = await Title.find({})
   res.status(200).json({
     success: true,
     body: allTheTitles
-  });
-});
+  })
+})
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+  console.log(`Server running on http://localhost:${port}`)
+})
