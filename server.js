da60efb1ae71f03f6c8f47c0fe9b6fbb35116468
Elin <elin.segelov@gmail.com>
2022-11-27 15:36:43 +0100

    Add id and genre endpoints

--- server.js
+++ server.js
@@ -1,28 +1,15 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv"
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
+dotenv.config()
+
 import topMusicData from "./data/top-music.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-
-const User = mongoose.model("User", {
-  name: String,
-  age: Number,
-  deceased: Boolean
-})
 
 const Song = mongoose.model("Song", {
   "id": Number,
@@ -42,25 +29,16 @@ const Song = mongoose.model("Song", {
 } 
 )
 
-//User.deleteMany();
 if(process.env.RESET_DB) {
   const resetDataBase = async () => {
     await Song.deleteMany();
-    topMusicData.forEach((song) => {
-      const newTrack = new Song(song)
-      newTrack.save()
+    topMusicData.forEach(singleSong => {
+      const newSong = new Song(singleSong);
+      newSong.save();
     })
-    /* topMusicData.forEach(singleSong = {
-      const newSong = new Song(singleSong)
-      newSong.save()
-    }) */
-    //await User.deleteMany()
-    /* const testUser = new User({name:"Elin", age: 33, deceased: false});
-    testUser.save(); */
   }
   resetDataBase()
 }
-
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -78,12 +56,14 @@ app.get("/songs", async (req, res) => {
   const allSongs = await Song.find({})
   res.status(200).json({
     success: true,
-    body:allSongs
+    body: allSongs
   })
 });
+
 app.get("/songs/id/:id", async (req, res) => {
+  console.log(req.params.id)
   try {
-    const singleSong = await Song.findById(req.params.id)
+    const singleSong = await Song.findById(req.params.id).exec();
     if (singleSong) {
       res.status(200).json({
         success: true,
@@ -105,10 +85,34 @@ app.get("/songs/id/:id", async (req, res) => {
       }
     })
   }
-
-
 });
 
+app.get("/songs/genre/:genre", async (req, res) => {
+  console.log(req.params.genre)
+  try {
+    const genreCollection = await Song.find({genre: req.params.genre}).exec();
+    if (genreCollection) {
+      res.status(200).json({
+        success: true,
+        body: genreCollection
+      })
+    } else {
+      res.status(404).json({
+        success: false,
+        body: {
+          message: "Could not find the song. Check the id and/or try another one"
+        }
+      })
+    }
+  } catch(error) {
+    res.status(400).json({
+      success: false,
+      body: {
+        message: "Invalid genre"
+      }
+    })
+  }
+});
 
 // Start the server
 app.listen(port, () => {
