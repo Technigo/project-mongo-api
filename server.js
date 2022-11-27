5f99b3475c6866fbf11f1fde3f74ee399e5e57c8
Linus Eriksson <erilinus@gmail.com>
2022-11-27 19:23:31 +0100

    finished project with all queries available

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import mongoose from "mongoose";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import topMusicData from "./data/top-music.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -17,6 +17,44 @@ mongoose.Promise = Promise;
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
+
+const User = mongoose.model("user", {
+  name: String,
+  age: Number,
+  deceased: Boolean
+});
+
+const Song = mongoose.model("Song", {
+  id: Number,
+  trackName: String,
+  artistName: String,
+  genre: String,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  loudness: Number,
+  liveness: Number,
+  valence: Number,
+  length: Number,
+  acousticness: Number,
+  speechiness: Number,
+  popularity: Number
+});
+
+if(process.env.RESET_DB) {
+  const resetDataBase = async () => {
+    await Song.deleteMany();
+    topMusicData.forEach(singleSong => {
+      const newSong = new Song(singleSong);
+      newSong.save()
+    })
+    // await User.deleteMany();
+    // const testUser = new User({ name: "Linus", age: 29, deceased: false });
+    // testUser.save();
+  }
+  resetDataBase();
+}
+
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -28,6 +66,110 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+
+// app.get("/songs", async (req, res) => {
+//   const allTheSongs = await Song.find({})
+//   res.status(200).json({
+//     success: true,
+//     body: allTheSongs
+//   });
+// });
+
+app.get("/songs/id/:id", async (req, res) => {
+  try {
+    const singleSong = await Song.findById(req.params.id)
+    if (singleSong) {
+      res.status(200).json({
+        success: true,
+        body: singleSong
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        body: {
+          message: "Could not find the song"
+        }
+      });
+    }
+  } catch(error) {
+    res.status(400).json({
+      success: false,
+      body: {
+        message: "Invalid id"
+      }
+    });
+  }
+});
+
+app.get("/songs/", async (req, res) => {
+
+  const {
+    genre, 
+    trackName, 
+    artistName,
+    bpm, 
+    energy,
+    danceability,
+    loudness,
+    liveness,
+    valence,
+    length,
+    acousticness,
+    speechiness,
+    popularity
+  } = req.query;
+  
+  const response = {
+    success: true,
+    body: {}
+  }
+
+  const trackNameQuery = trackName ? trackName : /.*/;
+  const genreQuery = genre ? genre : /.*/;
+  const artistNameQuery = artistName ? artistName : /.*/;
+  const bpmQuery = bpm ? bpm : {$gt: 0, $lt: 300};
+  const energyQuery = energy ? energy : {$gt: 0, $lt: 100};
+  const danceabilityQuery = danceability ? danceability : {$gt: 0, $lt: 100};
+  const loudnessQuery = loudness ? loudness : {$gt: -100, $lt: 1};
+  const livenessQuery = liveness ? liveness : {$gt: 0, $lt: 100};
+  const valenceQuery = valence ? valence : {$gt: 0, $lt: 100};
+  const lengthQuery = length ? length : {$gt: 0, $lt: 1000};
+  const acousticnessQuery = acousticness ? acousticness : {$gt: 0, $lt: 100};
+  const speechinessQuery = speechiness ? speechiness : {$gt: 0, $lt: 100};
+  const popularityQuery = popularity ? popularity : {$gt: 0, $lt: 100};
+
+  try {
+    response.body = await Song.find({
+      trackName: trackNameQuery,
+      artistName: artistNameQuery,
+      genre: genreQuery,
+      bpm: bpmQuery,
+      energy: energyQuery,
+      danceability: danceabilityQuery,
+      loudness: loudnessQuery,
+      liveness: livenessQuery,
+      valence: valenceQuery,
+      length: lengthQuery,
+      acousticness: acousticnessQuery,
+      speechiness: speechinessQuery,
+      popularity: popularityQuery
+    });
+
+    res.status(200).json({
+      success: true,
+      body: response
+    });
+
+  } catch (error) {
+      res.status(400).json({
+        success: false,
+        body: {
+          message: error
+        }
+      });
+    }
+});
+
 
 // Start the server
 app.listen(port, () => {
