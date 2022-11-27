03fbc5412cea2745b467f56a2fe7ed22b5ee8756
ViktorSvenssonN <viktor.tommy.svensson@gmail.com>
2022-11-27 18:25:02 +0100

    cleaned up code

--- server.js
+++ server.js
@@ -2,12 +2,6 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
 import theOffice from "./data/the-Office.json"
 import topMusicData from "./data/top-music.json";
 
@@ -15,11 +9,8 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
- 
 const Song = mongoose.model("Song", {
+  _id: Number,
   id: Number,
   trackName: String,
   artistName: String,
@@ -53,9 +44,6 @@ if(process.env.RESET_DB) {
       const newSong = new Song(singleSong);
       newSong.save();
     })
-    // await User.deleteMany();
-    // const testUser = new User({name: "Daniel", age: 28, deceased: false});
-    // testUser.save();
   }
   resetDataBase();
 }
@@ -67,9 +55,6 @@ if(process.env.RESET_OFFICE) {
       const newQuote = new Office(singleQuote);
       newQuote.save();
     })
-    // await User.deleteMany();
-    // const testUser = new User({name: "Daniel", age: 28, deceased: false});
-    // testUser.save();
   }
   resetDataBase();
 }
@@ -78,21 +63,21 @@ if(process.env.RESET_OFFICE) {
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
 app.get("/", (req, res) => {
   res.send([
     {"/": "Start page"},
-    {"/songs/all": "Displays all songs"}
+    {"/songs/all": "Displays all songs"},
+    {"/songs/id/:id": "Displays one song with specific ID. example songs/id/637cebbb8d84e049f7380e06"},
+    {"/songs?danceability": "To find max 2 songs with a danceability. example: http://localhost:8080/songs?danceability=76"},
+    {"/songs?genre": "To find max 2 songs with a danceability. example: http://localhost:8080/songs?genre=country rap"},
+    {"/office/all": "display all office quotes"},
   ]);
 });
 
  app.get("/songs/:all", async (req, res) => {
-    // const allTheSongs = await Song.find({}); all songs 
-  // --- OBS! försök alltid att göra try & catch, för att skydda -----!
     try {
       const allTheSongs = await Song.find({});
       if (allTheSongs) {
@@ -119,8 +104,6 @@ app.get("/", (req, res) => {
   }) 
 
   app.get("/office", async (req, res) => {
-    // const allTheSongs = await Song.find({}); all songs 
-  // --- OBS! försök alltid att göra try & catch, för att skydda -----!
     try {
       const allQuotes = await Office.find({});
       if (allQuotes) {
@@ -147,10 +130,8 @@ app.get("/", (req, res) => {
   }) 
 
 app.get("/songs/id/:id", async (req, res) => {
-/*   const allTheSongs = await Song.find({}); all songs */
-// --- OBS! försök alltid att göra try & catch, för att skydda -----!
   try {
-    const singleSong = await Song.findById(req.params.id); // one song leta genom ID
+    const singleSong = await Song.findById(req.params.id); 
     if (singleSong) {
     res.status(200).json({
       success: true,
@@ -174,46 +155,38 @@ app.get("/songs/id/:id", async (req, res) => {
 }
 })
 
-app.get("/songs/", async (req, res) => {
-
-  const {genre, danceability} = req.query;
-  const response = {
-    success: true,
-    body: {}
-  }
- /*  const matchAllRegex = new RegExp(".*"); ***** är ett sätt att göra det. annars senenadnför
-  const genreQuery = genre ? genre : matchAllRegex; */
-  const genreQuery = genre ? genre : /.*/;
-  const danceabilityQuery = danceability ? danceability : /.*/;
-
-  try {
-    // if ( req.params.genre && req.params.danceability) {
-      response.body = await Song.find({genre: genreQuery, danceability: danceabilityQuery}).limit(2).sort({energy: 1}).select({trackName: 1, artistName: 1})
-      //.exec() => to explore if you're curious enough :P
-    // } else if (req.params.genre && !req.params.danceability) {
-    //   response.body = await Song.find({genre: req.params.genre});
-    // } else if (!req.params.genre && req.params.danceability) {
-    //   response.body = await Song.find({danceability: req.params.danceability});
-    // }
-    res.status(200).json({
+  app.get("/songs/", async (req, res) => {
+  
+    const {genre, danceability} = req.query;
+    const response = {
       success: true,
-      body: response
-    });
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-      body: {
-        message: error
-      }
-    });
-  }
+      body: {}
+    }
+    const matchAllRegex = new RegExp(".*");
+    const genreQuery = genre ? genre : {$regex: matchAllRegex,  $options: 'i' };
+    const danceabilityQuery = danceability ? danceability : {$gt: 0, $lt: 100};
+  
+    try {
+      response.body = await Song.find({genre: genreQuery, danceability:danceabilityQuery}).limit(2).sort({energy: 1}).select({trackName: 1, artistName: 1})
 
-});
+      res.status(200).json({
+        success: true,
+        body: response
+      });
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        body: {
+          message: error
+        }
+      });
+    }
+  
+  });
 
-// Start the server
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
 
-//användbara sidor: https://regex101.com/
-// 
+
