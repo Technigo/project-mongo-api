d5e24572b34bdb76f0c4952ac9363bb0edb26a1b
a-well <amandaemkwellington@gmail.com>
2022-11-28 17:14:15 +0100

    Clean up code

--- server.js
+++ server.js
@@ -1,28 +1,11 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+import topMusicData from "./data/top-music.json"
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started! 
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-import topMusicData from "./data/top-music.json";
-
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.Promise = Promise;
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-
-const User = mongoose.model("User", {
-  name: String,
-  age: Number,
-  deceased: Boolean
-});
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
 
 const Song = mongoose.model("Song", {
   id: Number,
@@ -43,54 +26,41 @@ const Song = mongoose.model("Song", {
 
 if(process.env.RESET_DB) {
   const resetDataBase = async () => {
-    await Song.deleteMany();
+    await Song.deleteMany()
     topMusicData.forEach(singleSong => {
-      const newSong = new Song(singleSong);
-      newSong.save();
+      const newSong = new Song(singleSong)
+      newSong.save()
     })
-    // await User.deleteMany();
-    // const testUser = new User({name: "Amanda", age: 31, deceased: false});
-    // testUser.save();
   }
-  resetDataBase();
+  resetDataBase()
 }
 
 
-const port = process.env.PORT || 8080;
-const app = express();
+const port = process.env.PORT || 8080
+const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
-app.use(express.json());
+app.use(express.json())
 
-// Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo! :D");
-});
-
-app.get("/songs", async (req, res) => {
-  const allTheSongs = await Song.find({});
-  res.status(200).json({
-    success: true,
-    body: allTheSongs
-  })
-});
+  res.send("See README for how to use")
+})
 
 app.get("/songs/:id", async (req, res) => {
   try {
-    const singleSong = await Song.findById(req.params.id);
+    const singleSong = await Song.findById(req.params.id)
     if (singleSong) {
       res.status(200).json({
         success: true,
         body: singleSong
-      });
+      })
     } else {
       res.status(404).json({
         success: false,
         body: {
           message: "Could not find the song"
         }
-      });
+      })
     }
   } catch(error) {
     res.status(400).json({
@@ -98,47 +68,46 @@ app.get("/songs/:id", async (req, res) => {
       body: { 
         message: "Invalid id" 
       }
-    });
+    })
   }
 
-});
-app.get("/songs/genre/:genre/danceability/:danceability", async (req, res) => {
+})
+
+app.get("/songs/", async (req, res) => {
+  const {genre, danceability} = req.query
   const response = {
     success: true,
     body: {}
   }
+  const genreQuery = genre ? genre : /.*/
+  const danceabilityQuery = danceability ? danceability : /.*/
   try {
-    if (req.params.genre && req.params.danceability) {
-      const allMatchingSongs = await Song.find({genre: req.params.genre, danceability: req.params.danceability});
-      res.status(200).json({
-        success: true,
-        body: allMatchingSongs
-      });
-    } else if (req.params.genre && !req.params.danceability) {
-        const allMatchingSongs = await Song.find({genre: req.params.genre});
-        res.status(200).json({
-          success: true,
-          body: allMatchingSongs
-        });
-    } else if (!req.params.genre && req.params.danceability) {
-        const allMatchingSongs = await Song.find({danceability: req.params.danceability});
-        res.status(200).json({
-          success: true,
-          body: allMatchingSongs
-        });
-    }
+      response.body = await Song.find({genre: genreQuery, danceability: danceabilityQuery}).limit(10).sort({energy: 1}).select({trackName: 1, artistName: 1, genre: 1})
+    res.status(200).json({
+      success: true,
+      body: response
+    })
   } catch (error) {
     res.status(400).json({
       success: false,
       body: { 
         message: error 
       }
-    });
+    })
   }
+})
 
-});
-
-// Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+  console.log(`Server running on http://localhost:${port}`)
+})
+
+
+
+//RESET_DB=true npm run dev
+
+
+
+//https://regex101.com/
+
+// /yourWodOfChoice/gm - regex to match yourWordOfChoice
+// /.*/gm - regex to match every character in a string
