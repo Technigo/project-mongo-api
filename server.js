28fd8b16ef4d864875612e0c9751a12da12dd49b
Andreas Axelsson <pererikandreas@gmail.com>
2023-05-09 15:49:46 +0200

    added paths

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+// //////////////// IMPORT //////// //
+
+// "express" is a popular web framework for building web applications in Node.js. It provides a set of features for creating web servers, routing requests, handling HTTP requests and responses, and more.
+// "cors" is a middleware package that allows cross-origin resource sharing, which means it enables a server to allow requests from another domain.
+// "mongoose" is an Object Data Modeling (ODM) library that provides a higher-level abstraction over MongoDB. It allows developers to define schemas for their data and provides a more intuitive API for interacting with MongoDB.
+
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
+// ////////////////////////////////// //
+
+// This code block sets up the connection between the Node.js application and the MongoDB database, 
+// allowing the application to read and write data to the database using Mongoose.
 // import topMusicData from "./data/top-music.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
@@ -27,7 +31,7 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Hello and welcome to this API. Here are the endspoints.");
 });
 const { Schema } = mongoose;
 const userSchema = new Schema ({
@@ -57,8 +61,6 @@ const songSchema = new Schema({
 
 const Song = mongoose.model("Song", songSchema);
 
-
-
 app.get("/songs/id/:id", async (req, res) => {
   try {
     const singleSong = await Song.findById(req.params.id);
@@ -82,6 +84,42 @@ app.get("/songs/id/:id", async (req, res) => {
         message: e
       }
     })
+  }
+});
+
+app.get("/songs", async (req, res) => {
+  try {
+    const allSongs = await Song.find();
+    res.status(200).json({
+      success: true,
+      body: allSongs
+    })
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      body: {
+        message: e
+      }
+    })
+  }
+});
+
+app.get("/songsdevided", async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.pageSize) || 10;
+    const allsongsdevided = await Song.find().skip((page - 1) * pageSize).limit(pageSize);
+    res.status(200).json({
+      success: true,
+      body: allsongsdevided
+    });
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      body: {
+        message: e
+      }
+    });
   }
 });
 
