7cbdf02737d165de726c6f18395e9603082efd70
Joanna Philips <66673587+Joanna-Philips@users.noreply.github.com>
2023-05-10 13:30:18 +0200

    Added endpoint for song by name

--- server.js
+++ server.js
@@ -2,43 +2,27 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
 //C:\Program Files\MongoDB\Server\6.0\bin\mongod.exe" --dbpath="c:\data\db
-
 //import topMusicData from "./data/top-music.json";
-//127.0.0.1:27017
+
 const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+// Define the port the app will run on
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+// Middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+// Defining routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 const { Schema } = mongoose;
-const userSchema = new Schema ({
-  name: String,
-  age: Number,
-  alive: Boolean
-});
-
-const User = mongoose.model("User", userSchema);
 
 const songSchema = new Schema({
     id: Number,
@@ -58,51 +42,65 @@ const songSchema = new Schema({
 })
 
 
-
 const Song = mongoose.model("Song", songSchema);
 
-app.get("/songs", async (req, res) => {
-  try {
-    const songList =  await Song.find(req.params);
-    if (songList) {
-      res.status(200).json({
-        success: true,
-        body: {
-          songList: songList
-        } 
-      })
-    } else {
-      res.status(404).json({
-        success: false,
-        body: {
-          message: "No songs in the list"
-        }
-      })
-    }
-  } catch(e) {
-    res.status(500).json({
-      success: false,
-      body: {
-        message: e
-      }
-    })
-  }
-});
-// app.get("/artists", async (req, res) => {
+// Get all songs in the dataset with paging
+// app.get("/songs", async (req, res) => {
 //   try {
-//     const allArtists =  (await Song.find()).projection({ artistName: 1 });
-//     if (allArtists) {
+//     // Parse query parameters to integers or default to 1 page/10 entries limit
+//     const page = parseInt(req.query.page) || 1;
+//     const limit = parseInt(req.query.limit) || 10;
+
+//     // Calculate skip value - how many entries to skip from the beginning of the dataset
+//     const skip = (page - 1) * limit;
+
+//     // Query database with skip and limit
+//     const songList = await Song.find(req.params).skip(skip).limit(limit);
+
+//     if (songList.length > 0) {
 //       res.status(200).json({
 //         success: true,
 //         body: {
-//           allArtists: allArtists
+//           songList: songList,
+//           currentPage: page,
+//           totalPages: Math.ceil(await Song.countDocuments() / limit), // Eg: 50 / 10 = 5 pages
+//           totalSongRecords: await Song.countDocuments() // Counts total number of records or items in the DB
+//         }
+//       });
+//     } else {
+//       res.status(404).json({
+//         success: false,
+//         body: {
+//           message: "No songs in the list"
+//         }
+//       });
+//     }
+//   } catch(e) {
+//     res.status(500).json({
+//       success: false,
+//       body: {
+//         message: e
+//       }
+//     });
+//   }
+// });
+
+// //Get all songs in the dataset
+// app.get("/songs", async (req, res) => {
+//   try {
+//     const songList =  await Song.find(req.params);
+//     if (songList) {
+//       res.status(200).json({
+//         success: true,
+//         body: {
+//           songList: songList
 //         } 
 //       })
 //     } else {
 //       res.status(404).json({
 //         success: false,
 //         body: {
-//           message: "No artists in the list"
+//           message: "No songs in the list"
 //         }
 //       })
 //     }
@@ -116,7 +114,71 @@ app.get("/songs", async (req, res) => {
 //   }
 // });
 
-app.get("/songs/artists", async (req, res) => {
+// Get song by id
+app.get("/songs/id/:id", async (req, res) => {
+  try {
+    const singleSong = await Song.findById(req.params.id);
+    if (singleSong) {
+      res.status(200).json({
+        success: true,
+        body: singleSong
+      })
+    } else {
+      res.status(404).json({
+        success: false,
+        body: {
+          message: "Song not found"
+        }
+      })
+    }
+  } catch(e) {
+    res.status(500).json({
+      success: false,
+      body: {
+        message: e
+      }
+    })
+  }
+});
+
+// Get songs by name
+app.get("/songs/:name", async (req, res) => {
+  try {
+    // regex to remove spaces and to make the param case insensitive
+    let regExpression = "";
+    let trackNameNoSpace = req.params.name.replace(" ", "")
+    for(const char of trackNameNoSpace) {
+      regExpression += `(((\\s)*)(${char})((\\s)*))`;
+    }
+
+    const tracks = await Song.find({ trackName: { '$regex' : new RegExp(`(.*)(${regExpression})(.*)`), '$options' : 'ix'} });
+
+    if (tracks.length > 0) {
+      res.status(200).json({
+        success: true,
+        body: tracks
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        body: {
+          message: `No songs with ${req.params.name}, try again!`
+        }
+      });
+    }
+  } catch(e) {
+    res.status(500).json({
+      success: false,
+      body: {
+        message: e.message
+      }
+    });
+  }
+});
+
+
+//Get all artists in the dataset
+app.get("/artists", async (req, res) => {
   try {
     const artistList = await Song.find({},{ artistName: 1, _id: 0 });
     if (artistList) {
@@ -142,8 +204,10 @@ app.get("/songs/artists", async (req, res) => {
   }
 });
 
-app.get("/songs/artists/:name", async (req, res) => {
+// Get songs by a specific artist
+app.get("/artists/:name", async (req, res) => {
   try {
+    // regex to remove spaces and to make the param case insensitive
     let regExpression = "";
     let artistNameNoSpace = req.params.name.replace(" ", "")
     for(const char of artistNameNoSpace) {
@@ -153,7 +217,7 @@ app.get("/songs/artists/:name", async (req, res) => {
 
     const artistDiscography = await Song.find({ artistName: { '$regex' : new RegExp(`(.*)(${regExpression})(.*)`), '$options' : 'ix'} });
     console.log(artistDiscography);
-    
+
     if (artistDiscography.length > 0) {
       res.status(200).json({
         success: true,
@@ -174,35 +238,6 @@ app.get("/songs/artists/:name", async (req, res) => {
         message: e.message
       }
     });
-  }
-});
-
-
-
-
-app.get("/songs/id/:id", async (req, res) => {
-  try {
-    const singleSong = await Song.findById(req.params.id);
-    if (singleSong) {
-      res.status(200).json({
-        success: true,
-        body: singleSong
-      })
-    } else {
-      res.status(404).json({
-        success: false,
-        body: {
-          message: "Song not found"
-        }
-      })
-    }
-  } catch(e) {
-    res.status(500).json({
-      success: false,
-      body: {
-        message: e
-      }
-    })
   }
 });
 
