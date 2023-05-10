723860ca687673032edfeae1b43e9edef37d401f
Jonas Jakobson <jonas.jakobson@gmail.com>
2023-05-10 12:02:55 +0200

    set up database and get for whole list

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from 'express'
+import cors from 'cors';
+import mongoose from 'mongoose';
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/ig-noble-prize";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
+
+const { Schema } = mongoose;
+
+const prizeSchema = new Schema ({
+  _id: String,
+  Year: Number,
+  Subject: String,
+  Description: String
+});
+
+const Prize = mongoose.model("Prize", prizeSchema);
+
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -24,9 +28,66 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+// Welcome message
+app.get('/', (req, res) => {
+  res.send('Search IG Noble Prize by year, category or description (free text)')
+});
+
+// Full list of Ig Noble Prizes
+app.get('/prizes', async (req, res) => {
+  try {
+    const prizeList = await Prize.find();
+
+    if (prizeList.length > 0) {
+      res.status(200).json({
+        success: true,
+        message: `Found ${prizeList.length} prizes`,
+        body: {
+          prizelist: prizeList,
+        },
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        message: 'No prizes found',
+        body: {},
+      });
+    }
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      message: e,
+      body: {},
+    });
+  }
+});
+
+// Not working properly
+app.get("/prizes/:id", async (req, res) => {
+  try {
+    const singlePrizeByID = await Prize.findById(req.params.id);
+
+    if (singlePrizeByID) {
+      res.status(200).json({
+        success: true,
+        body: singlePrizeByID
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        body: {
+          message: 'Prize not found'
+        }
+      });
+    }
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      body: {
+        message: e
+      }
+    });
+  }
 });
 
 // Start the server
