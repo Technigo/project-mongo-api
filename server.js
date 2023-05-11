64fff4c56021bd870dbde0f11c1a06066702c768
Andreas Axelsson <pererikandreas@gmail.com>
2023-05-11 11:53:14 +0200

    added endpoint and deleated jsons

--- server.js
+++ server.js
@@ -1,48 +1,28 @@
-// //////////////// IMPORT //////// //
+// Import required modules/packages
+import express from "express"; // Web framework for Node.js
+import cors from "cors"; // Middleware for enabling cross-origin resource sharing
+import mongoose from "mongoose"; // Object Data Modeling (ODM) library for MongoDB
 
-// "express" is a popular web framework for building web applications in Node.js. It provides a set of features for creating web servers, routing requests, handling HTTP requests and responses, and more.
-// "cors" is a middleware package that allows cross-origin resource sharing, which means it enables a server to allow requests from another domain.
-// "mongoose" is an Object Data Modeling (ODM) library that provides a higher-level abstraction over MongoDB. It allows developers to define schemas for their data and provides a more intuitive API for interacting with MongoDB.
-
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
-
-// ////////////////////////////////// //
-
-// This code block sets up the connection between the Node.js application and the MongoDB database, 
-// allowing the application to read and write data to the database using Mongoose.
-// import topMusicData from "./data/top-music.json";
-
+// Set up the connection to MongoDB using Mongoose
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Set the default Promise implementation for Mongoose
 mongoose.Promise = Promise;
 
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+// Set the port for the application to run on
 const port = process.env.PORT || 8080;
+
+// Create an instance of the Express application
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+// Add middleware to enable CORS and JSON body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send({
-    Hello: "Welcome to this top-music API!",
-    Endpoints: [
-      { "/": "Startpage / Api Info" },
-      { "/singlesongs/id/:id": "Single song. Just change the :id to a number between 1-50" },
-      { "/allsongs10by10?page=1&pageSize=10": "All songs (10 per page). Change the url to show a specific page number or change the number of songs displayd ber page." },
-    ]
-  })
-});
 
+// Define the schema for the song data
 const { Schema } = mongoose;
-
 const songSchema = new Schema({
     id: Number,
     trackName: String,
@@ -58,10 +38,33 @@ const songSchema = new Schema({
     acousticness: Number,
     speechiness: Number,
     popularity: Number
-})
+});
 
+// Create a model for the Song collection in MongoDB
 const Song = mongoose.model("Song", songSchema);
 
+if (process.env.RESET_DB) {
+  const resetDatabase = async () => {
+  await Song.deleteMany();
+}
+resetDatabase();
+}
+
+// Define the root route that provides API information
+app.get("/", (req, res) => {
+  res.send({
+    Hello: "Welcome to this Music API!",
+    Endpoints: [
+      { "/": "Startpage / Api Info" },
+      { "/singlesongs/id/:id": "Single song. Just change the :id to a number between 1-50" },
+      { "/allsongs10by10?page=1&pageSize=10": "All songs (10 per page). Change the url to show a specific page number or change the number of songs displayed per page." },
+      { "/songsdanceabilityover80": "Displays all songs with a danceability over 80. Gets the party started!" },
+
+    ]
+  });
+});
+
+// Define a route to get a single song by its ID
 app.get("/singlesongs/id/:id", async (req, res) => {
   try {
     const singleSong = await Song.findOne({ id: req.params.id });
@@ -69,14 +72,14 @@ app.get("/singlesongs/id/:id", async (req, res) => {
       res.status(200).json({
         success: true,
         body: singleSong
-      })
+      });
     } else {
       res.status(404).json({
         success: false,
         body: {
           message: "Song not found"
         }
-      })
+      });
     }
   } catch(e) {
     res.status(500).json({
@@ -84,18 +87,19 @@ app.get("/singlesongs/id/:id", async (req, res) => {
       body: {
         message: e
       }
-    })
+    });
   }
 });
 
+// Define a route to get all songs in a paginated manner
 app.get("/allsongs10by10", async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
     const pageSize = parseInt(req.query.pageSize) || 10;
-    const allsongsdevided = await Song.find().skip((page - 1) * pageSize).limit(pageSize);
+    const allSongsDivided = await Song.find().skip((page - 1) * pageSize).limit(pageSize);
     res.status(200).json({
       success: true,
-      body: allsongsdevided
+      body: allSongsDivided
     });
   } catch (e) {
     res.status(500).json({
@@ -104,6 +108,30 @@ app.get("/allsongs10by10", async (req, res) => {
         message: e
       }
     });
+  }
+});
+
+app.get("/songsdanceabilityover80", async (req, res) => {
+  const {genre, danceability } = req.query;
+  const response = {
+    success: true,
+    body:{}
+  }
+  // Regex only for strings
+  const genreRegex = new RegExp(genre);
+  const danceabilityQuery =  { $gt: 80 }
+
+  try {
+    const searchResultFromDB = await Song.find({genre: genreRegex, danceability: danceabilityQuery})
+    if (searchResultFromDB) {
+      response.body = searchResultFromDB
+      res.status(200).json(response)
+    } else {
+      response.success = false,
+      res.status(500).json(response)
+    }
+  } catch(e) {
+    res.status(500).json(response)
   }
 });
 
