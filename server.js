91ec64d9969d956b26cf3b9125a079079ea72ab7
Malin <malin@MacBook-Pro-4.local>
2023-05-11 10:39:59 +0200

    added dataset

--- server.js
+++ server.js
@@ -1,32 +1,61 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import data from './data/yale-ceo-russia-sanctions.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+// Info about dataset: 
+// All rights belong to the authors (Jeffrey Sonnenfeld et al.), please attirbute to original authors.
+// The list below is updated continuously by Jeffrey Sonnenfeld and his team of experts, research fellows, 
+// and students at the Yale Chief Executive Leadership Institute to reflect new announcements from companies in as close to real time as possible.
+// Over 1,000 companies have publicly announced they are voluntarily curtailing operations in Russia to some degree beyond the bare minimum 
+// legally required by international sanctions — but some companies have continued to operate in Russia undeterred.
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
+
+const Company = mongoose.model('Company', {
+  Name: String,
+  Action: String,
+  Industry: String,
+  Country: String,
+  Grade: String
+})
+
+if (process.env.RESET_DATABASE) {
+  const seedDatabase = async () => {
+    await Company.deleteMany({})
+
+    data.forEach((companyData) => {
+      new Company(companyData).save()
+    })
+  }
+  seedDatabase()
+}
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
+const listEndpoints = require('express-list-endpoints')
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: 'Service unavailable' })
+  }
+})
+
 
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+  res.json(listEndpoints(app))
 });
 
 // Start the server
