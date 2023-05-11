e65c4e106cecc8f4ea8576c6552f466628c1d075
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-05-11 18:10:33 +0200

    Add seedDatabase()

--- server.js
+++ server.js
@@ -31,6 +31,20 @@ const Director = mongoose.model('Director', {
   }
 })
 
+if (process.env.RESET_DATABASE) {
+  console.log('Resetting database');
+  
+  const seedDatabase = async () => {
+    await Title.deleteMany()
+
+    const iLostMyBody = new Title({ title: 'I Lost My Body' })
+    await iLostMyBody.save()
+
+    await new Title({ title: iLostMyBody, director: "Jérémy Clapin" }).save()
+  }
+  seedDatabase();
+}
+
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
