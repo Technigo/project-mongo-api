b6420f49b3da74daa821dab02209108f3c3f0bfc
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-05-11 10:40:03 +0200

    movies show in compass

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import netflixData from "./data/netflix-titles.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -20,14 +13,58 @@ mongoose.Promise = Promise;
 const port = process.env.PORT || 8080;
 const app = express();
 
+const Movie = mongoose.model('Movie', {
+  title: String,
+  show_id: Number,
+  director: String,
+  cast: String,
+  country: String,
+  release_year: Number,
+  listed_in: String
+});
+
+// const tvShow = mongoose.model('tvShow', {
+//   title: String,
+//   show_id: Number,
+//   director: String,
+//   cast: String,
+//   country: String,
+//   release_year: Number,
+//   listed_in: String
+// });
+
+  if (process.env.RESET_DB) {
+    const seedDatabase = async () => {
+      await Movie.deleteMany({})
+      // await tvShow.deleteMany({})
+
+      netflixData.forEach((movieData) => {
+      new Movie(movieData).save();
+      
+      })
+    }
+    seedDatabase()
+  }
+
+
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Hello Frida!");
 });
+
+// app.get('/movies', async (req, res) => {
+//   const allMovies = await Movie.find({})
+//   res.json(allMovies)
+// }) RESET_DB
+
+// app.get('/tvShows', async (req, res) => {
+// const tvShows = await tvShow.find()
+// res.json(tvShows)
+// })
 
 // Start the server
 app.listen(port, () => {
