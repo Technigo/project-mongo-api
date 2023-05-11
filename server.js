7f5cf8dd48c6ed5b2d05ea9365fe2a078807e1de
theresBL <tbrannberglendt@gmail.com>
2023-05-11 23:19:45 +0200

    blue level complete

--- server.js
+++ server.js
@@ -1,35 +1,179 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import netflixData from './data/netflix-titles.json'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.Promise = Promise;
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
+// Defines the port the app will run on. 
+const port = process.env.PORT || 8080
+const app = express()
+const listEndpoints = require(("express-list-endpoints"))
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-// Start defining your routes here
+const { Schema } = mongoose
+
+// const userSchema = new Schema({
+//   name: String,
+//   age: Number,
+//   alive: Boolean
+// })
+// const User = mongoose.model('User', userSchema)
+
+const titleSchema = new Schema({
+  show_id: Number,
+  title: String,
+  director: String,
+  cast: String,
+  country: String,
+  date_added: String,
+  release_year: Number,
+  rating: String,
+  duration: String,
+  listed_in: String,
+  description: String,
+  type: String
+})
+const Title = mongoose.model("Title", titleSchema)
+const Type = mongoose.model("Type", titleSchema)
+
+if (process.env.RESET_DATABASE) {
+  const resetDataBase = async () => {
+    await Title.deleteMany()
+    netflixData.forEach((singleTitle) => {
+      const newTitle = new Title(singleTitle)
+      newTitle.save()
+    })
+  }
+  // call a function while declaring it
+  resetDataBase()
+}
+// Routes
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+  res.json(listEndpoints(app))
+  // res.send("Mongoose Bonanza!!!!!")
+})
+
+app.get("/titles", async (req, res) => {
+  const allTitles = await Title.find()
+  res.json(allTitles)
+})
+
+app.get("/country/:country", async (req, res) => {
+  const { country } = req.params;
+  const response = {
+    success: true,
+    body: {},
+  };
+  const countryRegex = new RegExp(country, "i");
+
+  try {
+    const searchResultFromDB = await Title.find({ country: countryRegex });
+    if (searchResultFromDB.length > 0) {
+      response.body = searchResultFromDB;
+      res.status(200).json(response)
+    } else {
+      response.success = false
+      res.status(404).json(response)
+    }
+  } catch (error) {
+    response.success = false;
+    res.status(500).json(response)
+  }
+})
+
+app.get("/type/:category", async (req, res) => {
+  const { category } = req.params
+  const response = {
+    success: true,
+    body: {}
+  };
+
+  try {
+    let titles = []
+
+    if (category === "tvshow") {
+      titles = await Title.find({ type: "TV Show" })
+    } else if (category === "movies") {
+      titles = await Title.find({ type: "Movie" })
+    } else {
+      response.success = false
+      response.body.message = "Invalid category"
+      return res.status(400).json(response);
+    }
+
+    if (titles.length > 0) {
+      response.body = titles
+      res.status(200).json(response)
+    } else {
+      response.success = false
+      res.status(404).json(response)
+    }
+  } catch (error) {
+    response.success = false
+    response.body.message = error.message
+    res.status(500).json(response)
+  }
+})
+
+// app.get("/movies", async (req, res) => {
+//   try {
+//     const movies = await Title.find({ type: "Movie" })
+//     if (movies.length > 0) {
+//       res.status(200).json({
+//         success: true,
+//         body: movies
+//       })
+//     } else {
+//       res.status(404).json({
+//         success: false,
+//         body: {
+//           message: 'No movies found'
+//         }
+//       });
+//     }
+//   } catch (error) {
+//     res.status(500).json({
+//       success: false,
+//       body: {
+//         message: 'Internal server error.'
+//       }
+//     })
+//   }
+// })
+
+app.get("/titles/id/:id", async (req, res) => {
+  try {
+    const singleTitle = await Title.findOne({ show_id: req.params.id })
+    if (singleTitle) {
+      res.status(200).json({
+        success: true,
+        body: singleTitle
+      })
+    } else {
+      res.status(404).json({
+        success: false,
+        body: {
+          message: 'Title not found'
+        }
+      })
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      body: {
+        message: error
+      }
+    })
+  }
+})
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
-});
+})
