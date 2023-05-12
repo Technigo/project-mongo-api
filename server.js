1321d68a1bf7cf5655f2f6c16041e534fab02077
Nina <121503742+ninastroyer@users.noreply.github.com>
2023-05-12 10:39:19 +0200

    update server.js

--- server.js
+++ server.js
@@ -1,29 +1,17 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
-import netflixData from "./data/netflix-titles.json";
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+import netflixData from "./data/netflix-titles.json"
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
 
-// import topMusicData from "./data/top-music.json";
+const port = process.env.PORT || 8080
+const app = express()
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.Promise = Promise;
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
-
-// Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 const Title = mongoose.model("Title", {
   show_id: Number,
@@ -52,31 +40,21 @@ if(process.env.RESET_DB) {
   resetDataBase()
 }
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  const netflixGuide = {
-    Routes: [
-      {
-        '/movies': 'Get all movies',
-        '/movies/:title': 'All movietitles',
-        '/cast/:cast': 'Get movie by castmembers',
-        '/country/:country': 'Get movie by country',
-        '/director/:director': 'Get movie by director'
-      }
-    ]
-  }
-  res.json({responseMessage: netflixGuide});
-});
+// Routes
 
-app.get("/movies", async (req, res) => {
-  const allTheMovies = await movies.find()
+app.get("/", (req, res) => {
+  res.send("This is an api for Netflix-titles. Go to /titles to see all titles, and then add the _id of the title in /titles/ to see a specific title")
+  })
+
+app.get("/titles", async (req, res) => {
+  const allTheTitles = await Title.find({})
   res.status(200).json ({
     success: true,
-    body: allTheMovies
+    body: allTheTitles
   })
-});
+})
 
-app.get("/movies/:titles", async (req, res) => {
+app.get("/titles/:id", async (req, res) => {
   try {
     const SingleTitle = await Title.findById(req.params.id)
     if (SingleTitle) {
@@ -102,7 +80,8 @@ app.get("/movies/:titles", async (req, res) => {
   }
 })
 
+
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+  console.log(`Server running on http://localhost:${port}`)
+})
