3d68d70ab9d292144ea54ea1472399e32f9bdc87
cpz-manifold <carol.pinzon.ardila@gmail.com>
2023-05-12 17:47:29 +0200

    front end basic

--- frontend_mongo/src/App.js
+++ frontend_mongo/src/App.js
@@ -1,9 +1,35 @@
-
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [route, setRoute] = useState('/');
+  const API_URL = 'https://project-mongo-api-7ammel5u7a-lz.a.run.app/'; 
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${API_URL}${route}`);
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
+  }, [route]);
+
+  const handleClick = (route) => {
+    setRoute(route);
+  }
+
   return (
     <div className="App">
+      <button onClick={() => handleClick('/')}>Home</button>
+      <button onClick={() => handleClick('/songs')}>Songs</button>
+      <div>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
     </div>
   );
 }
