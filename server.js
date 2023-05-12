73bb8135562279b8c028fdf269e9d0646dd0798e
Nina <121503742+ninastroyer@users.noreply.github.com>
2023-05-12 12:05:28 +0200

    strictquery

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose"
 import netflixData from "./data/netflix-titles.json"
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+mongoose.set('strictQuery', false);
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
