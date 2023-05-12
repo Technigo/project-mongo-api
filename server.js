805d50aa23d9c093f316fac64f4c011c4724c7e9
Bridget Mailley <bmailley42@gmail.com>
2023-05-12 22:32:58 +0200

    set up restful endpoints

--- server.js
+++ server.js
@@ -21,6 +21,86 @@ app.get("/", (req, res) => {
   res.send("Nintendo Games!");
 });
 
+const { Schema } = mongoose;
+
+const gameSchema = new Schema ({
+  title: String,
+  platform: String,
+  unix_timestamp: Number,
+  esrb_rating: String,
+  developers: Array,
+  genres: Array,
+})
+
+const Game = mongoose.model("Game", gameSchema)
+
+app.get("/games", async (req, res) => {
+  try {
+  const { platform, rating } = req.query
+  let games = await Game.find();
+
+  if (platform) {
+    games=games.filter((game) => {
+      return game.platform.toLowerCase() === platform.toLowerCase()
+    })
+  }
+
+  if (rating) {
+    games=games.filter((game) => {
+      return game.esrb_rating.toLowerCase() === rating.toLowerCase()
+    })
+  }
+
+  if (games) {
+    res.status(200).json({
+      success: true,
+      body: games
+    })
+  } else {
+    res.status(404).json({
+      success: false,
+      body: {
+        message: "Games not found"
+      }
+    })
+  }
+} catch (error) {
+  res.status(500).json({
+    success:false,
+    body: {
+      message: error
+    }
+  })
+}
+})
+
+app.get('/games/id/:id', async (req, res) => {
+  try {
+    const singleGame = await Game.findById(req.params.id);
+    if (singleGame) {
+      res.status(200).json({
+        success: true,
+        body: singleGame
+      })
+    } else {
+      res.status(404).json({
+        success: false,
+        body: {
+          message: "Game not found"
+        }
+      })
+    }
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        body: {
+          message: e
+        }
+      })
+    }
+})
+
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
