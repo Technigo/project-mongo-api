5123caeef2b92eee938f8841922cf9c8adb7e482
cecavi <100417732+cecavi@users.noreply.github.com>
2023-05-14 19:43:52 +0200

    done

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # Project Mongo API
 
-Replace this readme with your own information about your project.
+This weeks project was to create a backend using MongoDB database to store and retrieve data from, with a RESTful API.
 
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+Endpoints created: 
 
-## The problem
+/songs?genre=pop to list all songs in genre pop
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+/songs?genre=pop&danceability=76 shows songs in gnre pop and with danceability greater than 76
 
-## View it live
+/songs/id/645f77ae555b7244afb95eaf shows a specific justin Bieber song
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+The problem
+I based the code on a code along example and live session from Technigo. If I had more time I would connect the MongoDB database in Atlas with Google Cloud API platform.
+I have som problems with my log in at the moment.
+
+View it live
+Google CLoud API platform:
+
+(not deployed yet).
--- server.js
+++ server.js
@@ -8,11 +8,12 @@ import mongoose from "mongoose";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import topMusicData from "./data/top-music.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
+
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -24,9 +25,107 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const { Schema } = mongoose;
+// const userSchema = new Schema ({
+//   name: String,
+//   age: Number,
+//   alive: Boolean
+// });
+
+// const User = mongoose.model("User", userSchema);
+
+const songSchema = new Schema({
+  id: Number,
+  trackName: String,
+  artistName: String,
+  genre: String,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  loudness: Number,
+  liveness: Number,
+  valence: Number,
+  length: Number,
+  acousticness: Number,
+  speechiness: Number,
+  popularity: Number
+})
+
+const Song = mongoose.model("Song", songSchema);
+
+// delete.Many() resets the whole database bu deleting all data
+if (process.env.RESET_DB) {
+  const resetDatabase = async () => {
+    await Song.deleteMany();
+    topMusicData.forEach((singleSong) => {
+      const newSong = new Song(singleSong);
+      newSong.save() // saves all the songs to the DB
+    }) 
+  }
+  resetDatabase();
+}
+
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+// http://localhost:8080/songs?genre=pop to list all songs in genre pop
+app.get("/songs", async (req, res) => {
+  const {genre, danceability} = req.query;
+  const response = {
+    success: true,
+    body: {}
+  }
+  // Regex - Regularexpresseion to get all the pop genres such as canadian pop etc. 
+  // RegEx only works on string
+  // http://localhost:8080/songs?genre=pop&danceability=76 shows songs in gnre pop and with danceability greater than 76
+  const genreRegex = new RegExp(genre);
+  const danceabilityQuery = { $gt: danceability ? danceability : 0 } // gt = greater then lt = lower then
+  try {
+    const searchResultFromDB = await Song.find({genre: genreRegex, danceability: danceabilityQuery}) 
+    if (searchResultFromDB) {
+      response.body = searchResultFromDB
+      res.status(200).json(response)
+    } else {
+      response.success = false,
+      res.status(500).json(response)
+    }
+  } catch(e) {
+    res.status(500).json({
+      success: false,
+      body: {
+        message: e
+      }
+    })
+  }
+});
+
+// http://localhost:8080/songs/id/645f77ae555b7244afb95eaf shows a specific justin Bieber song
+app.get("/songs/id/:id", async (req, res) => {
+  try {
+    const singleSong = await Song.findById(req.params.id);
+    if (singleSong) {
+      res.status(200).json({
+        success: true,
+        body: singleSong
+      })
+    } else {
+      res.status(404).json({
+        success: false,
+        body: {
+          message: "Song not found"
+        }
+      })
+    }
+  } catch(e) {
+    res.status(500).json({
+      success: false,
+      body: {
+        message: e
+      }
+    })
+  }
 });
 
 // Start the server
