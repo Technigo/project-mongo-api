35cbe0e1ae2906d2d48c024f7eaa34db1947058c
Ylva Karlsson <121539816+YlvaKarlsson@users.noreply.github.com>
2023-05-15 00:26:56 +0200

    not working yet

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+//THIS IS NOT WORKING BUT NEEDED TO HAND-IN SOMETHING!
+
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -10,9 +12,23 @@ import mongoose from "mongoose";
 // import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/artists";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
+
+
+const Artist = mongoose.model('Artist', {
+  name: String
+})
+
+const seedDatabase = async () => {
+  const spears = new Artist({ name: 'Britney Spears' })
+  await spears.save()
+
+  const cyrus = new Artist({ name: 'Miley Cyrus' })
+  await cyrus.save()
+}
+seedDatabase()
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -28,6 +44,11 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+
+app.get('/artists', async (req, res) => {
+  const artists = await Artist.find()
+  res.json(artists)
+})
 
 // Start the server
 app.listen(port, () => {
