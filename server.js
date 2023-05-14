dadedc04219bf60dec22646ef5069f78f6eb2889
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2023-05-14 19:39:31 +0200

    one route added coding along with the tuesday live session

--- server.js
+++ server.js
@@ -38,7 +38,17 @@ const userSchema = new Schema ({
   alive: Boolean
 }); 
 
-const User = mongoose.model("User", userSchema);
+const User= mongoose.model("User", userSchema); 
+
+const errorSchema = new Schema ({
+  errorId: Number,
+  code: Number,
+  message: String,
+  length: Number,
+  displayMessage: String
+})
+
+const Error = mongoose.model("Error", userSchema);
 
 //also possible without schema but less proff, just like this
 //const User2 = mongoose.model("User", {
@@ -46,6 +56,32 @@ const User = mongoose.model("User", userSchema);
   //age: Number,
   //alive: Boolean
 //}); 
+
+app.get("/errors/id/:id", async (req, res) => {
+  try {
+    const singleError = await Error.findById(req.params.id);
+    if (singleError) {
+      res.status(200).json({
+        success: true, 
+        body: singleError
+      })
+    } else {
+      res.status(404).json({
+        success: false, 
+        body: {
+          message: "Error not found"
+        }
+      })
+    }
+  } catch(e) {
+    res.status(500).json({
+      success: false, 
+      body: {
+        message: "Error not found"
+      }
+    })
+  }
+});
 
 
 // Start the server
