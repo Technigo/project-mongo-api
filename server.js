621b0bc5c85de4a0ebfc9243d8d48a8db6b803b6
Frid Matilda <matilda.x.frid@scania.com>
2023-05-15 16:17:03 +0200

    start

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import netflixData from "./data/netflix-titles.json";
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -24,9 +25,70 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+// In MongoDB, a schema defines the structure of a document, including the field names, data types, and any constraints or validations. 
+// It serves as a blueprint for how the data should be organized and is used to ensure consistency in the data
+// Schema is basically a (mongoose) skeleton
+const { Schema } = mongoose;
+
+const userSchema = new Schema ({
+  name: String,
+  age: Number,
+  alive: Boolean
+})
+
+// On the other hand, a model in MongoDB is an object that represents a collection of documents in the database. 
+// It acts as an interface between the application and the database, providing methods to perform CRUD operations on the data.
+
+// A model can be created using a schema as a blueprint to ensure that the documents conform to a particular structure and set of rules.
+
+// This is the user model, which makes it possible to create actual data
+const User = mongoose.model("User", userSchema)
+
+
+const titleSchema = new Schema ({
+  show_id: Number,
+  title: String,
+  director: String,
+  cast: String,
+  country: String,
+  date_added: String,
+  release_year: Number,
+  rating: String,
+  duration: String,
+  listed_in: String,
+  description: String,
+  type: String
+})
+
+const Title = mongoose.model("Title", titleSchema)
+
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+app.get("/titles/id/:id", async (req, res) => {
+  res.send("Hello sunshine!");
+  try {
+    const singleTitle = await Title.findOne({ show_id: req.params.id });
+    if (singleTitle) {
+      res.status(200).json({
+        success: true,
+        body: singleTitle
+      })
+    } else {
+      res.status(404).json({
+        success: false,
+        body: {
+          message: "Title is not found"
+        }
+      })
+    }
+  } catch(e) {
+    res.status(500).json({
+      success: false,
+      body: {
+        message: e
+      }
+    })
+  }
 });
 
 // Start the server
