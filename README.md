8ed792485c35bedcb5801e718346f2432bc8a960
Ylva Karlsson <121539816+YlvaKarlsson@users.noreply.github.com>
2023-05-17 18:14:06 +0200

    Fixed some endpoints and commented out some code

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 # Project Mongo API
 
-I will write something here when I get MongoDB to run on my Windows-computer...
+Endpoints:
+
+/songs/all - get all songs
+
+/songs/ - get all songs divided into 20 per page
+
+/songs/:id - delete the ":id" and replace it with number 1-50 to find song by it's id
+
+then I tried to make one for randomly choosing a song - but I can't get it to work...
+/songs/random
+If you see the issue here - please let me know :D
 
 ## The problem
 
-SO MANY... Will update this when I get MongoDB to work on my computer. :/
+Mostly issues with deploying, the endpoints worked but I had issues with connecting and deploying.
+This week I focused on the API itself and therefor did no frontend for this.
 
 ## View it live
 
-Not deployed yet due to not working!
+https://mongo-api-w14-y3rdgxvsjq-ew.a.run.app/
--- server.js
+++ server.js
@@ -2,16 +2,24 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import topMusicData from "./data/top-music.json";
-const listEndpoints = require('express-list-endpoints')
 
-//Should I use docker for below issue or what do I need to do to solve it?
-//I couldn't make the MONGO_URL work with the localhost-version - when I did that it stopped working locally
-const mongoUrl = process.env.MONGO_URL || "mongodb+srv://ylva87:hHwxpsqpR3V62DS9@cluster0.hsexulf.mongodb.net/?retryWrites=true&w=majority";
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
+// Defines the port the app will run on. Defaults to 8080, but can be overridden
+// when starting the server. Example command to overwrite PORT env variable value:
+// PORT=9000 npm start
+const port = process.env.PORT || 8080;
+const app = express();
+const listEndpoints = require('express-list-endpoints')
 
-const Song = mongoose.model('Song', {
+// Add middlewares to enable cors and json body parsing
+app.use(cors());
+app.use(express.json());
+
+const { Schema } = mongoose;
+const dataSchema = new Schema ({
   id: Number,
   trackName: String,
   artistName: String,
@@ -27,39 +35,61 @@ const Song = mongoose.model('Song', {
   speechiness: Number,
   popularity: Number
 })
+// const Song = mongoose.model('Song', {
+//   id: Number,
+//   trackName: String,
+//   artistName: String,
+//   genre: String,
+//   bpm: Number,
+//   energy: Number,
+//   danceability: Number,
+//   loudness: Number,
+//   liveness: Number,
+//   valence: Number,
+//   length: Number,
+//   acousticness: Number,
+//   speechiness: Number,
+//   popularity: Number
+// })
 
+const TrackName = mongoose.model('Trackname', dataSchema)
 const seedDatabase = async () => {
   const data = topMusicData
   Song.collection.insertMany(data, function(err, r) {
   })
 }
 // seedDatabase()
-
-//switch to cover error-types and generate response
-const handleErrors = (err, res) => {
-  switch (err) {
-    case err instanceof mongoose.Error.DocumentNotFoundError:
-      res.status(404).json({ message: "Document not found" })
-      break;
-    case err instanceof mongoose.Error.CastError:
-      res.status(400).json({ message: "Value cast error" })
-      break;
-    default:
-      console.log(err.messages)
-      // res.status(500)
-      break;
-  }
+//might be resetDatabase instead of seedDatabase below, will check
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await TrackName.deleteMany();
+    topMusicData.forEach((trackName) => {
+      const newTrackName = new TrackName(trackName)
+      newTrackName.save();
+    })
+    //const data = topMusicData
+    //Song.collection.insertMany(data, function(err, r) {
+    }
+  seedDatabase();
 }
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
-
-// Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+//I tried to make a more reusable function for handling errors but can't make it to work so the code is quite repetitive...
+//switch to cover error-types and generate response
+//commented out below for now as I've made error-handlers for each endpoint instead - which I got to work
+// const handleErrors = (err, res) => {
+//   switch (err) {
+//     case err instanceof mongoose.Error.DocumentNotFoundError:
+//       res.status(404).json({ message: "Document not found" })
+//       break;
+//     case err instanceof mongoose.Error.CastError:
+//       res.status(400).json({ message: "Value cast error" })
+//       break;
+//     default:
+//       console.log(err.messages)
+//       // res.status(500)
+//       break;
+//   }
+// }
 
 // Start defining your routes here
 app.get("/", (req, res) => {
@@ -67,22 +97,97 @@ app.get("/", (req, res) => {
 });
 
 // for all songs
-app.get('/songs', async (req, res) => {
-  const songs = await Song.find()
-  res.json(songs)
+app.get('/songs/all', async (req, res) => {
+  try {
+  const songs = await TrackName.find();
+  if (songs) {
+  res.status(200).json({
+    success: true,
+    body: songs
 })
-// app.get('/songs', async (req, res) => {
-//   const songs = await Song.find().exec((err, response) =>  {
-//     console.log(err)
-//     if (err !== "null") handleErrors(err, res)} )
-//   res.json(songs)
-// })
+  } else {
+    res.status(404).json({
+      success: false,
+      body: { message: "Songs not found" },
+    })
+  }
+}
+catch(err) {res.status(500).json({
+  success: false,
+  body: { message: "Error: " + err }
+});}
+})
+
+//songs divided into 20 per page
+app.get("/songs/", async (req, res) => {
+  const page = req.query.page || 0;
+  const perPage = req.query.perPage || 20;
+  try {
+    const songs = await TrackName.find().limit(perPage).skip(page * perPage);
+    if (songs) {
+      res.status(200).json({
+        success: true,
+        body: songs
+    })
+      } else {
+        res.status(404).json({
+          success: false,
+          body: { message: "Songs not found" },
+        })
+      }
+    }
+    catch(err) {res.status(500).json({
+      success: false,
+      body: { message: "Error: " + err }
+    });}
+    })
 
 // for individual songs and their id
 app.get('/songs/:id', async (req, res) => {
-  const song = await Song.find({id:req.params.id})
-  res.json(song)
+  try {
+    const singleTrackName = await TrackName.findById(req.params.id)
+    if (singleTrackName) {
+      res.status(200).json({
+success: true,
+body: singleTrackName
+    })
+  } else {
+    res.status(404).json({
+      success: false,
+      body: { message: "No such song" }
+    })
+  }
+}
+catch(err) {
+  res.status(500).json({
+    success: false,
+    body: { message: "Error: " + err }
+  })
+}
 })
+
+//this random doesn't seem to work 100%
+app.get("/songs/random", async (req, res) => {
+  const randomTrackName = await TrackName.aggregate([{$sample:{size: 1}}])
+  try {
+    if (randomTrackName) {
+      res.status(200).json({
+        success: true,
+        body: randomTrackName});
+      } else {
+        res.status(404).json({
+          success: false,
+          body: { message: "Random song not found"}
+        })
+      }
+    }
+    catch(err) {
+      res.status(500).json({
+        success: false,
+        body: { message: "Error: " + err}
+      });
+    }
+  })
 
 // app.get('/songs/:id', async (req, res) => {
 //   const song = await Song.find({id:req.params.id}).exec()
