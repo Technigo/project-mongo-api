df7342e813c6982c9d0f73ae36b2e568fd877902
Sara <sara@MBP-di-Sara.homenet.telecomitalia.it>
2023-05-21 16:17:04 +0200

    one endpoint added

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import inequality from './data/inequality.json';
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -28,6 +21,55 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+
+const { Schema } = mongoose;
+const songSchema = new Schema({
+  id: Number,
+  trackName: String,
+  artistName: String,
+  genre: String,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  loudness: Number,
+  liveness: Number,
+  valence: Number,
+  length: Number,
+  acousticness: Number,
+  speechiness: Number,
+  popularity: Number
+})
+
+const Song = mongoose.model("Song", songSchema);
+
+
+app.get("/songs/id/:id", async (req, res) => {
+try {
+  const singleSong = await Song.findById(req.params.id);
+  if (singleSong) {
+    res.status(200).json({
+      success: true,
+      body: singleSong
+    })
+  } else {
+    res.status(404).json({
+      success: false,
+      body: {
+        message: "Song not found"
+      }
+    })
+  }
+} catch(e) {
+  res.status(500).json({
+    success: false,
+    body: {
+      message: e
+    }
+  })
+}
+});
+
+
 
 // Start the server
 app.listen(port, () => {
