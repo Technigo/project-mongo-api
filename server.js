5de790c4ce2fd26d09c1a9b405f404d49cc7abe7
Emma Engvall <emma.engvall@ica.se>
2023-06-29 22:25:57 +0200

    created schema for songs and queries for genre and danceability

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import topMusicData from "./data/top-music.json";
+import { getRequireSource } from "@babel/preset-env/lib/polyfills/utils";
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -8,7 +10,6 @@ import mongoose from "mongoose";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -24,10 +25,110 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const { Schema } = mongoose;
+const userSchema = new Schema ({
+  name: String,
+  age: Number,
+  married: Boolean
+})
+
+const User = mongoose.model("User", userSchema)
+
+
+const songSchema = new Schema ({
+  id: Number,
+  trackName: String,
+  artistName: String,
+  genre: String,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  loudness: Number,
+  liveness: Number,
+  valence: Number,
+  length: Number,
+  acousticness: Number,
+  speechiness: Number,
+  popularity: Number
+})
+
+const Song = mongoose.model("Song", songSchema)
+
+// Delete data in database and then import data from json-file to database
+if (process.env.RESET_DB) {
+  const resetDatabase = async () => {
+    await Song.deleteMany();
+    topMusicData.forEach((singleSong) => {
+      const newSong = new Song(singleSong);
+      newSong.save()
+    })
+  }
+  resetDatabase();
+}
+
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+
+
+app.get("/songs/", async (req, res) => {
+  const {genre, danceability} = req.query;
+  const response = {
+    success: true,
+    body: {}
+  }
+  const genreRegex = new RegExp(genre);
+  const danceabilityQuery = { $gt: danceability ?
+  danceability : 0 }
+
+  try {
+    response.body = await Song.find({genre: genreRegex, danceability: danceabilityQuery})
+    if (true) {
+      res.status(200).json(response)
+    } else {
+      res.status(404).json({
+        success: false,
+        body: {
+          message: "Song not found"
+        }
+     })
+    } 
+  } catch(e) {
+    res.status(500).json(response)
+  }
+});
+
+
+
+// Fetch a song
+app.get("/songs/id/:id", async (req, res) => {
+  //res.send("Hello Technigo!");
+  try {
+    const singleSong = await Song.findById(req.params.id)
+    if (singleSong) {
+      res.status(200).json({
+        success: true,
+        body: singleSong
+      })
+    } else {
+      res.status(404).json({
+        success: false,
+        body: {
+          message: "Song not found"
+        }
+     })
+    } 
+  } catch(e) {
+    res.status(500).json({
+      success: false,
+      body: {
+        message: e
+      }
+    })
+  }
+});
+
 
 // Start the server
 app.listen(port, () => {
