0aaafdd70dc0d0adc8679d663b85ad6dc4115c3c
Laura Lyckholm <137819100+LauraLyckholm@users.noreply.github.com>
2023-11-19 18:35:04 +0100

    Made more endpoints

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
 // Creates a model named Movie
-const Movie = mongoose.model("Movie", {
+const Movies = mongoose.model("Movie", {
   show_id: Number,
   title: String,
   director: String,
@@ -27,11 +27,11 @@ const Movie = mongoose.model("Movie", {
 
 if (process.env.RESET_DB) {
   const seedDatabase = async () => {
-    await Movie.deleteMany({})
+    await Movies.deleteMany({})
 
     netflixData.forEach((netflixItem) => {
-      new Movie(netflixItem).save()
-    })
+      new Movies(netflixItem).save()
+    });
   }
   seedDatabase()
 }
@@ -62,10 +62,64 @@ app.get("/", (req, res) => {
 });
 
 app.get("/movies", (req, res) => {
-  Movie.find().then(movies => {
-    res.json(movies)
+  Movies.find()
+    .then(movies => {
+      if (movies.length > 0) {
+        res.json(movies);
+      } else {
+        res.status(404).json({ error: "No movies found" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: "Something went wrong, please try again." });
+    });
+});
+
+// Gets an individual movie based on its id. 
+app.get("/movies/:id", (req, res) => {
+  const movieID = parseInt(req.params.id); // Gets the id from the params
+  Movies.findOne({ show_id: movieID }).then(movie => { // Checks if the id from the param is the same as the show_id, if it then it's displayed in json. Findone, because I only want one result.
+    if (movie) {
+      res.json(movie);
+    } else {
+      res.status(404).json({ error: `Movie with id ${movieID} not found. Having troubles finding the movie? Make sure you switch out ':id' for the id you wish to base your query on` });
+    }
   })
-})
+    .catch(error => {
+      res.status(500).json({ error: "Something went wrong, please try again." });
+    });
+});
+
+// Gets all movies with the release year searched for in the querystring. 
+app.get("/movies/year/:year", (req, res) => {
+  const releaseYear = parseInt(req.params.year); // Gets the id from the params
+  Movies.find({ release_year: releaseYear }).then(year => { // Checks if the id from the param is the same as the show_id, if it then it's displayed in json
+    if (year.length > 0) {
+      res.json(year);
+    } else {
+      res.status(404).json({ error: `Movie with the release date ${releaseYear} wasn't found` });
+    }
+  })
+    .catch(error => {
+      res.status(500).json({ error: "Internal Server Error" });
+    });
+});
+
+// Makes it possible to query on a string included in the title
+app.get("/movies/title/:title", (req, res) => {
+  const titleQuery = req.params.title.toLowerCase();
+  Movies.find({ title: { $regex: titleQuery, $options: "i" } }) // Query-filter. Search is performed on "title", titleQuery is the regex pattern searched for, options: "i" makes the search case-insensitive
+    .then(movies => {
+      if (movies) {
+        res.json(movies);
+      } else {
+        res.status(404).json({ error: `No movies found with the word '${titleQuery}' in the title` });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: "Internal Server Error" });
+    });
+});
 
 // Start the server
 app.listen(port, () => {
