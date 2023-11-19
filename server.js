4f07c1e9d18d83a4e5e619f323ca7aa56c3e1267
So Youn Choi <manghatuhyo@gmail.com>
2023-11-19 10:36:48 +0100

    env not working

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
