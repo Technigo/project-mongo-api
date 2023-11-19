d7949f1a47318401ac4408a81fea131d39f43c23
Laura Lyckholm <137819100+LauraLyckholm@users.noreply.github.com>
2023-11-19 16:32:05 +0100

    Started up project by getting all data to show using models

--- server.js
+++ server.js
@@ -7,18 +7,54 @@ import mongoose from "mongoose";
 // import avocadoSalesData from "./data/avocado-sales.json";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
+import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
+// Creates a model named Movie
+const Movie = mongoose.model("Movie", {
+  show_id: Number,
+  title: String,
+  director: String,
+  cast: String,
+  country: String,
+  date_added: String,
+  release_year: Number,
+  rating: String,
+  duration: String,
+  listed_in: String,
+  description: String,
+  type: String
+})
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Movie.deleteMany({})
+
+    netflixData.forEach((netflixItem) => {
+      new Movie(netflixItem).save()
+    })
+  }
+  seedDatabase()
+}
+
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
+
+// Middleware to handle error if service is down
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next();
+  } else {
+    res.status("503").json({ error: "Service unavailable" })
+  }
+})
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
@@ -28,6 +64,12 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+
+app.get("/movies", (req, res) => {
+  Movie.find().then(movies => {
+    res.json(movies)
+  })
+})
 
 // Start the server
 app.listen(port, () => {
