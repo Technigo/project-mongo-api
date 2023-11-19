afc4af3f84681d8a33e9f47532053dad829d1e5e
Andrej <72137459+andrkavr@users.noreply.github.com>
2023-11-19 14:42:32 +0100

    initial commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import mongoose from "mongoose";
 // import avocadoSalesData from "./data/avocado-sales.json";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
+import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
@@ -23,6 +23,22 @@ const app = express();
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+
+// Mongoose object models
+const Title = mongoose.model("Title", {
+  show_id: Number,
+  title: String,
+  director: String,
+  cast: String,
+  country: String,
+  date_added: String,
+  release_year: Number,
+  rating: String,
+  duration: String,
+  listed_in: String,
+  description: String,
+  type: String,
+});
 
 // Start defining your routes here
 app.get("/", (req, res) => {
