fd441dac7bcdc4944b0f260cf83772d520b508f2
Your Name alexandrameija@hotmail.com <alexandrameija@hotmail.com.com>
2023-11-21 17:15:19 +0100

    -m

--- server.js
+++ server.js
@@ -6,19 +6,16 @@ import listEndpoints from 'express-list-endpoints';
 import Book from './models/book';
 import booksData from './data/books.json'; 
 
+// Load environment variables
 dotenv.config();
 
+// MongoDB Connection with retry logic
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 const connectWithRetry = () => {
-  mongoose
-    .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-      console.log('MongoDB connected');
-      mongoose.set('strictQuery', false); // Set strictQuery option to false
-    })
-    .catch((err) => {
+  mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => {
       console.error('MongoDB connection error:', err);
-      console.log('Retrying connection in 5 seconds...');
       setTimeout(connectWithRetry, 5000); // Retry after 5 seconds
     });
 };
@@ -26,19 +23,24 @@ const connectWithRetry = () => {
 // Call the connection function
 connectWithRetry();
 
+// Set Mongoose to use ES6 Promises and address deprecation warning
 mongoose.Promise = Promise;
+mongoose.set('strictQuery', true); // Add this line to address the deprecation warning
 
+// Initialize Express app
 const app = express();
 
+// Apply middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Root route showing available endpoints
 app.get('/', (req, res) => {
-  const endpoints = listEndpoints(app);
-  res.json({ endpoints });
+  res.json({ endpoints: listEndpoints(app) });
 });
 
+// Books routes
 app.get('/books', async (req, res) => {
   try {
     const books = await Book.find();
@@ -49,18 +51,20 @@ app.get('/books', async (req, res) => {
 });
 
 app.get('/books/:id', async (req, res) => {
-  const { id } = req.params;
-  const book = await Book.findOne({ bookID: id });
-
-  if (book) {
-    res.json(book);
-  } else {
-    res.status(404).json({ error: 'Book not found' });
+  try {
+    const book = await Book.findOne({ bookID: req.params.id });
+    if (book) {
+      res.json(book);
+    } else {
+      res.status(404).json({ error: 'Book not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
 app.post('/books', async (req, res) => {
-  const newBook = new Book(req.body); // Ensure body contains the required fields
+  const newBook = new Book(req.body);
   try {
     const savedBook = await newBook.save();
     res.status(201).json(savedBook);
@@ -69,20 +73,26 @@ app.post('/books', async (req, res) => {
   }
 });
 
+// Seed the database (execute cautiously)
 
+seedDatabase();
+
+// Start the server
 const port = process.env.PORT || 8080;
-
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-// Data Seeding
-const seedDatabase = async () => {
-  await Book.deleteMany({}); // Clears the existing books collection
 
-  booksData.forEach(async (bookData) => {
-    const newBook = new Book(bookData);
-    await newBook.save();
-  });
-
-  console.log('Database has been seeded!');
-};
+// Function to seed the database with error handling
+async function seedDatabase() {
+  try {
+    await Book.deleteMany({});
+    await Promise.all(booksData.map(async (bookData) => {
+      const newBook = new Book(bookData);
+      await newBook.save();
+    }));
+    console.log('Database has been seeded!');
+  } catch (err) {
+    console.error('Error during database seeding:', err);
+  }
+}
