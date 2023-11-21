06192939c71ae9c36dff995bee74a8057630edfd
Lisa Dahlkar <lisa.dahlkar@gmail.com>
2023-11-21 17:25:33 +0100

    trying out the netflix-data

--- server.js
+++ server.js
@@ -1,35 +1,74 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import expressListEndpoints from 'express-list-endpoints';
+import netflixData from './data/netflix-titles.json';
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-mongo';
+mongoose.connect(mongoUrl);
+mongoose.set('strictQuery', false);
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+const NetflixTitle = mongoose.model('NetflixTitle', {
+  title: String,
+  genre: String,
+  releaseYear: Number,
+  // other properties...
 });
 
-// Start the server
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    try {
+      await NetflixTitle.deleteMany({});
+      await Promise.all(netflixData.map(titleData => new NetflixTitle(titleData).save()));
+
+      console.log('Database seeded successfully.');
+    } catch (error) {
+      console.error('Error seeding database:', error.message);
+    }
+  };
+
+  seedDatabase();
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello Technigo!');
+});
+
+app.get('/netflix-titles', async (req, res) => {
+  try {
+    const titles = await NetflixTitle.find();
+    res.json(titles);
+  } catch (error) {
+    console.error('Error fetching Netflix titles:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+app.get('/netflix-titles/:id', async (req, res) => {
+  try {
+    const title = await NetflixTitle.findById(req.params.id);
+    if (!title) {
+      return res.status(404).json({ message: 'Title not found' });
+    }
+    res.json(title);
+  } catch (error) {
+    console.error('Error fetching Netflix title by ID:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+app.get('/documentation', (req, res) => {
+  const endpoints = expressListEndpoints(app);
+  res.json(endpoints);
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
