bf3ded274c4dac375e15ec74c30312106933f217
Your Name alexandrameija@hotmail.com <alexandrameija@hotmail.com.com>
2023-11-21 13:40:11 +0100

    -m

--- server.js
+++ server.js
@@ -3,30 +3,33 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import listEndpoints from 'express-list-endpoints';
-import Book from './models/book'; // Adjust the path to your Book model
+import Book from './models/book';
+import booksData from './data/books.json'; 
 
-dotenv.config()
+dotenv.config();
 
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/8080";
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      console.log('MongoDB connected');
+      mongoose.set('strictQuery', false); // Set strictQuery option to false
+    })
+    .catch((err) => {
+      console.error('MongoDB connection error:', err);
+      console.log('Retrying connection in 5 seconds...');
+      setTimeout(connectWithRetry, 5000); // Retry after 5 seconds
+    });
+};
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('MongoDB connected');
-    mongoose.set('strictQuery', false); // Set strictQuery option to false
-  })
-  .catch(err => console.error('MongoDB connection error:', err));
+// Call the connection function
+connectWithRetry();
 
 mongoose.Promise = Promise;
 
-
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
@@ -35,8 +38,6 @@ app.get('/', (req, res) => {
   res.json({ endpoints });
 });
 
-
-// Get all books
 app.get('/books', async (req, res) => {
   try {
     const books = await Book.find();
@@ -46,7 +47,6 @@ app.get('/books', async (req, res) => {
   }
 });
 
-// Get a single book by bookID
 app.get('/books/:id', async (req, res) => {
   const { id } = req.params;
   const book = await Book.findOne({ bookID: id });
@@ -58,7 +58,6 @@ app.get('/books/:id', async (req, res) => {
   }
 });
 
-// Add a new book
 app.post('/books', async (req, res) => {
   const newBook = new Book(req.body); // Ensure body contains the required fields
   try {
@@ -69,7 +68,28 @@ app.post('/books', async (req, res) => {
   }
 });
 
-// Start the server
+// Data Seeding
+const seedDatabase = async () => {
+  await Book.deleteMany({}); // Clears the existing books collection
+
+  for (const bookData of booksData) {
+    const newBook = new Book(bookData);
+    try {
+      await newBook.save();
+    } catch (error) {
+      console.error('Error seeding book:', error);
+    }
+  }
+
+  console.log('Database has been seeded!');
+};
+
+seedDatabase().then(() => {
+  mongoose.connection.close();
+});
+
+const port = process.env.PORT || 8080;
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
