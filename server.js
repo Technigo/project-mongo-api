72b5519c584bc4e58adf2875152cd93488950c9f
So Youn Choi <manghatuhyo@gmail.com>
2023-11-22 18:31:14 +0100

    added env variable with resetdatabase true with render so i could get the result from books route

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ if (process.env.RESET_DATABASE == 'true') {
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+// PORT=9000 npm start..
 const port = process.env.PORT || 8080;
 const app = express();
 
