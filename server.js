053785578b365ee54a7fdfb373a8b0453fdb235c
Frida <139373483+FridaKristian@users.noreply.github.com>
2023-11-22 14:27:14 +0100

    Created a mongoDB + mongoose API with data about 50 songs. I have four routes atm, all songs a specific song (from its ID), all songs of a specific genre and all songs with a danceability-score over 70

--- server.js
+++ server.js
@@ -1,33 +1,92 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+import data from "./data/top-music.json"
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.Promise = Promise;
+// The port the app will run on.
+const port = process.env.PORT || 8080
+const app = express()
+const listEndpoints = require("express-list-endpoints")
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
-
-// Add middlewares to enable cors and json body parsing
+// Add middlewares
 app.use(cors());
 app.use(express.json());
+//Check if the database is available/connected (readyState = 1)
+app.use((req, res, next)=>{
+  if(mongoose.connection.readyState === 1){
+    next()
+  } else {
+    res.status(503).json({error: "service unavailable"})
+  }
+})
 
-// Start defining your routes here
+const ASong = mongoose.model("ASong", {
+  trackName: String,
+  artistName: String,
+  genre: String,
+  length: Number,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  popularity: Number
+})
+
+if (process.env.RESET_DB) {
+  const seedDataBase = async () => {
+    await ASong.deleteMany()
+    data.forEach(song => {
+      new ASong(song).save()
+    })
+  }
+  seedDataBase()
+}
+
+// Main route
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send(listEndpoints(app));
+})
+
+//Route to all songs
+app.get("/songs", (req, res) => {
+  ASong.find().then(song => res.json(song))
 });
+
+// Route to one song
+app.get("/songs/:songId", async (req, res) => {
+  try {
+    const song = await ASong.findById(req.params.songId)
+    if (song) res.json(song)
+    else res.status(404).json({error: "song not found"})
+  } catch (error) {
+    res.status(400).json({error: "invalid song id"})
+  }
+})
+
+//Route to a specific genre
+app.get("/genres/:specificGenre", async (req, res) => {
+  try {
+    const song = await ASong.find({genre: req.params.specificGenre})
+    if (song) res.json(song)
+    else res.status(404).json({error: "There are no songs of that genre"})
+  } catch (error) {
+    res.status(400).json({error: "invalid genre"})
+  }
+})
+
+//Route to songs with danceability over 70
+app.get("/danceable", (req, res) => {
+  ASong.find({ danceability: {$gte: 70} })
+    .then(songs => {
+      if (songs.length > 0) {
+        res.json(songs)
+      } else res.status(404).json({error: "There are no dancable songs"})
+    }).catch(err => {
+      res.status(500).json({error: `Internal server error: $(err)`})})
+})
 
 // Start the server
 app.listen(port, () => {
