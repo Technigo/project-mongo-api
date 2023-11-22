b1bd50adf2c8b5b083903beaa63a48fd55d96e9c
Klaudia Wróblewska <kwroblewska0410@gmail.com>
2023-11-22 17:40:26 +0100

    added 2 endpoints

--- server.js
+++ server.js
@@ -1,32 +1,91 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import topMusicData from './data/top-music.json';
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-mongo';
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
+
+const Song = mongoose.model('Song', {
+  id: Number,
+  trackName: String,
+  artistName: String,
+  genre: String,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  loudness: Number,
+  liveness: Number,
+  valence: Number,
+  length: Number,
+  acousticness: Number,
+  speechiness: Number,
+  popularity: Number,
+});
+
+// if (process.env.RESET_DB) {
+const seedDatabase = async () => {
+  await Song.deleteMany({});
+
+  topMusicData.forEach((topMusicData) => {
+    new Song(topMusicData).save();
+  });
+};
+seedDatabase();
+// }
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
+const listEndpoints = require('express-list-endpoints');
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+app.get('/', (req, res) => {
+  res.send(listEndpoints(app));
+});
+
+app.get('/songs', (req, res) => {
+  Song.find()
+    .then((songs) => {
+      if (songs.length > 0) {
+        res.json(songs);
+      } else {
+        res.status(404).json({ error: 'NO songs found' });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'Something went wrong, please try again.' });
+    });
+});
+
+//get individual song
+
+app.get('/songs/:id', (req, res) => {
+  const songID = parseInt(req.params.id);
+  Song.find({ id: songID })
+    .then((song) => {
+      if (song) {
+        res.json(song);
+      } else {
+        res.status(404).json({ error: `Song with id ${movieID} not found.` });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'Something went wrong, please try again.' });
+    });
+});
+
+//get song
+
+app.get('/songs/artist/:artist', (req, res) => {
+  const artistName = req.params.artist.toLowerCase();
 });
 
 // Start the server
