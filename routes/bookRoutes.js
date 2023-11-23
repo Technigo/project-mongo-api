c5e3f1ca02684343e5171c3bd82325a9761cb5e1
nguyenvuhang@gmail.com <nguyenvuhang@gmail.com>
2023-11-23 16:15:21 +0100

    change to try catch in all routes

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -25,9 +25,12 @@ router.get("/books", async (req, res) => {
 router.get("/books/:id", async (req, res) => {
     const { id } = req.params;
     // Use the BookModel to find all the books in the database
-    await BookModel.findById({bookID: id})
-        .then(result => res.json(result))
-        .catch(error => res.json(error));
+    try {
+        let result = await BookModel.findById(id)
+        res.json(result);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 })
 
 // Define a route to add a new book
@@ -43,20 +46,23 @@ router.post("/add", async (req, res) => {
     const ratings_count = req.body.ratings_count;
     const text_reviews_count = req.body.text_reviews_count;
 
-    await BookModel.create({ 
-        bookID: bookID,
-        title: title,
-        authors: authors,
-        language_code: language_code,
-        average_rating: average_rating,
-        isbn: isbn,
-        isbn13: isbn13,
-        num_pages: num_pages, 
-        ratings_count: ratings_count,
-        text_reviews_count: text_reviews_count
-    })
-        .then(result => res.json(result))
-        .catch(error => res.json(error));
+    try {
+        let result = await BookModel.create({ 
+            bookID: bookID,
+            title: title,
+            authors: authors,
+            language_code: language_code,
+            average_rating: average_rating,
+            isbn: isbn,
+            isbn13: isbn13,
+            num_pages: num_pages, 
+            ratings_count: ratings_count,
+            text_reviews_count: text_reviews_count
+        })
+        res.json(result);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 })
 
 // Define a route to update for example rating of a book by its ID
@@ -64,27 +70,31 @@ router.put("/update/:id", async (req, res) => {
     const { id } = req.params;
     const average_rating = req.body.average_rating;
 
-    await BookModel.findByIdAndUpdate(id, {average_rating: average_rating})
-        .then(result => res.json(result))
-        .catch(error => res.json(error));
+    try {
+        let result = await BookModel.findByIdAndUpdate(id, {average_rating: average_rating});
+        res.json(result);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 })
 
 // Define a route to delete a book by its ID
 router.delete("/delete/:id", async (req, res) => {
     const { id } = req.params;
 
-    await BookModel.findByIdAndDelete(id)
-        .then(result => {
-            if (result) {
-                res.json({
-                    message: `Book with ID ${id} deleted successfully`,
-                    deletedBook: result        
-                });
-            } else {
-                res.status(404).json({ error: `Book with ID ${id} cannot be found`})
-            }
-        })
-        .catch(error => res.status(500).json(error));
+    try {
+        let result = await BookModel.findByIdAndDelete(id);
+        if (result) {
+            res.json({
+                message: `Book with ID ${id} deleted successfully`,
+                deletedBook: result        
+            });
+        } else {
+            res.status(404).json({ error: `Book with ID ${id} cannot be found`})
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 })
 
 export default router;
