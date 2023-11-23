a11f6752e4750c437d6f42dcf119de9375b7a1d5
Malin <malinlundhe@hotmail.com>
2023-11-23 20:10:54 +0100

    started with installing packages, and added some code to server.js

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import expressListEndpoints from "express-list-endpoints";
+import booksData from "./data/books.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -24,12 +18,56 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const Book = mongoose.model('Book', {
+  bookID: Number, 
+  title: String, 
+  authors: String, 
+  average_rating: Number, 
+  isbn: Number, 
+  isbn13: Number, 
+  language_code: String, 
+  num_pages: Number, 
+  ratings_count: Number, 
+  ratings_count: Number, 
+  text_reviews_count: Number,
+});
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Book.deleteMany({});
+
+    books.Data.forEach((bookData) => {
+      new Book(bookData).save();
+    });
+  };
+
+  seedDatabase();
+}
+
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+app.get("/endpoints", (req, res) => {
+  const endpoints = expressListEndpoints(app);
+  res.json(endpoints);
+});
+
+app.get('/books', async (req, res) => {
+  const allBooks = await Book.find();
+  res.json(books);
+});
+
+app.get('/books/:id', async (req, res) => {
+  const book = await Book.findById(req.params.id);
+  res.json(book);
 });
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+
+
