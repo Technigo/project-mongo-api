0098e95e05121c5f33a0988d1310cb756d7b8f89
BeckieMorton <beckam11@gmail.com>
2023-11-23 11:59:26 +0100

    first try setting up and seeding database using json file

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import whlSites from "./data/whlSites.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/whlSites";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
+const Site = mongoose.model("Site", {
+  //Properties defined here match the keys from the whlSites.json file
+});
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await whlSites.deleteMany({});
+
+    data.forEach((siteData) => {
+      new Site(siteData).save();
+    });
+  };
+  seedDatabase();
+}
+
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -27,6 +33,11 @@ app.use(express.json());
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+app.get("/names", async (req, res) => {
+  const names = await Site.find();
+  res.json(names);
 });
 
 // Start the server
