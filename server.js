b97d876eacfffd1a9031be5c4f5c37d9cdd64419
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2023-11-23 18:44:58 +0100

    Created database, data model

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import booksData from "./data/books.json";
+import listEndpoints from "express-list-endpoints";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
+
+// Define our book model
+const Book = mongoose.model('Book', {
+  title: String,
+  authors: String,
+});
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Book.deleteMany({});
+
+    booksData.forEach(async (bookData) => {
+      await new Book({
+        title: bookData.title,
+        authors: bookData.authors,
+      }).save();
+    })
+  }
+
+  seedDatabase();
+}
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -26,7 +42,36 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.json(listEndpoints(app));
+});
+
+app.get("/books", async (req, res) => {
+  const title = req.query.title;
+  const authors = req.query.authors;
+
+  const query = {};
+
+  if (title) {
+    query.title = { "$regex": title, "$options": "i" };
+  }
+
+  if (authors) {
+    query.authors = { "$regex": authors, "$options": "i" };
+  }
+
+  const books = await Book.find(query);
+  res.json(books);
+});
+
+app.get("/books/:bookId", async (req, res) => {
+  const bookId = req.params.bookId;
+  const book = await Book.findById(bookId);
+
+  if (book) {
+    res.json(book);
+  } else {
+    res.status(404).json({ message: "Book not found" });
+  }
 });
 
 // Start the server
