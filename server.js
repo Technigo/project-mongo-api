91f181524cec5967f4c04a038529250531587bfd
stenlisuryadinata <elim_berkat@yahoo.co.id>
2023-11-23 17:11:50 +0700

    update server.js , package.json
    update server.js , package.json
    -populate the mongodb database

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import listEndpoints from 'express-list-endpoints';
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // import avocadoSalesData from "./data/avocado-sales.json";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
+import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
@@ -24,9 +25,89 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const Cast = mongoose.model('Cast', {
+  name: String,
+});
+
+const Movie = mongoose.model('Movie', {
+  title: String,
+  cast: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Cast',
+  }],
+});
+
+const seedDatabase = async () => {
+  await Cast.deleteMany();
+  await Movie.deleteMany();
+
+  const castMap = new Map(); // To track existing cast to avoid duplicates
+
+  // Populate Cast collection
+  netflixData.forEach(item => {
+    const castList = item.cast.split(', ');
+
+    castList.forEach(async name => {
+      // Deduplicate cast names
+      if (!castMap.has(name)) {
+        const cast = new Cast({ name });
+        await cast.save();
+        castMap.set(name, cast._id);
+      }
+    });
+  });
+
+  // Populate Movie collection
+  const movies = [];
+  netflixData.forEach(item => {
+    const movie = new Movie({
+      title: item.title,
+      cast: item.cast.split(', ').map(name => castMap.get(name)),
+    });
+    movies.push(movie);
+  });
+
+  // Save all movies to avoid CastError
+  await Promise.all(movies.map(async movie => {
+    await movie.save();
+  }));
+};
+
+seedDatabase();
+
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send(listEndpoints(app));
+  
+});
+
+app.get("/movies", async (req, res) => {
+  // Retrieve movies with populated cast information
+  const movies = await Movie.find().populate('cast');
+  res.json(movies);
+});
+
+app.get('/movies/cast', async (req, res) => {
+  const cast = await Cast.find();
+  res.json(cast);
+});
+
+app.get('/movies/cast/:id', async (req, res) => {
+  const cast = await Cast.findById(req.params.id);
+  if (cast) {
+    res.json(cast);
+  } else {
+    res.status(404).json({ error: 'Cast not found' });
+  }
+});
+
+app.get('/movies/:id', async (req, res) => {
+  const movie = await Movie.findById(req.params.id).populate('cast');
+  if (movie) {
+    res.json(movie);
+  } else {
+    res.status(404).json({ error: 'Movie not found' });
+  }
 });
 
 // Start the server
