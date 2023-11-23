90435cb7a0b03989880d8477989bdec914e1a218
Anna Robertsson <robertsson_anna@hotmail.com>
2023-11-24 00:37:44 +0100

    sorted out routes

--- routes/songRoutes.js
+++ routes/songRoutes.js
@@ -1,10 +1,32 @@
 import express from "express";
+import listEndpoints from "express-list-endpoints";
 import { MetallicaSongModel } from "../models/MetallicaSongModel";
 
 const router = express.Router()
 
+// ------- Routes -------
+// Show all endpoints in a json format
+router.get("/", (req, res) => {
+   const endpoints = listEndpoints(router)
+   res.json(endpoints);
+});
+
+// Show all Metallica songs
 router.get("/songs", async (req, res) => {
     await MetallicaSongModel.find()
     .then((result) => res.json(result))
     .catch((error) => res.json(error))
 })
+
+// router.get("/metallica-songs", async (req, res) => {
+//   try {
+//   const metallicaSongs = await MetallicaSongModel.find({})
+//   res.json(metallicaSongs)
+//   } catch (err) {
+//     res.status(500).json({ error: err.message })
+//   }
+// })
+
+// Export the router for use in the main application
+//export default router
+module.exports = router;
--- server.js
+++ server.js
@@ -2,9 +2,10 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv"
-import listEndpoints from "express-list-endpoints";
-import { MetallicaSongModel } from "./models/MetallicaSongModel";
-import MetallicaSongs from "./data/MetallicaSongs.json"
+//import { MetallicaSongModel } from "./models/MetallicaSongModel";
+//import MetallicaSongs from "./data/MetallicaSongs.json"
+import songRoutes from "./routes/songRoutes"
+//const songRoutes = require("./routes/songRoutes");
 dotenv.config() //Load environment variables from the .env file
 
 // If you're using one of our datasets, uncomment the appropriate import below to get started!
@@ -20,42 +21,37 @@ mongoose.Promise = Promise;
 const port = process.env.PORT || 8080;
 const app = express();
 
+// Imported routes in the app
+app.use(songRoutes);
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })) // Parse URL-encoded data
+//Ha med??
+// app.use((req, res, next) => {
+//   if (mongoose.connection.readyState === 1) {
+//     next();
+//   } else {
+//     res.status("503").json({ error: "Service unavailable" })
+//   }
+// })
 
-if (process.env.RESET_DB) {
-  const seedDatabase = async () => {
-    try {
-      console.log('Seeding database...');
-    await MetallicaSongModel.deleteMany({})
-await MetallicaSongModel.insertMany(MetallicaSongs);
-            console.log("Database seeded successfully!");
-        } catch (error) {
-            console.error("Error seeding database:", error);
-    }
-  }
+// if (process.env.RESET_DB) {
+//   const seedDatabase = async () => {
+//     try {
+//       console.log('Seeding database...');
+//     await MetallicaSongModel.deleteMany({})
+// await MetallicaSongModel.insertMany(MetallicaSongs);
+//             console.log("Database seeded successfully!");
+//         } catch (error) {
+//             console.error("Error seeding database:", error);
+//     }
+//   }
 
-  seedDatabase()
- } else {
-   console.log('RESET_DB is not true. Database will not be reset.');
- }
-
-// Start defining your routes here
-app.get("/", (req, res) => {
-   const endpoints = listEndpoints(app)
-   res.json(endpoints);
-});
-
-app.get("/metallica-songs", async (req, res) => {
-  try {
-  const metallicaSongs = await MetallicaSongModel.find({})
-  res.json(metallicaSongs)
-  } catch (err) {
-    res.status(500).json({ error: err.message })
-  }
-})
+//   seedDatabase()
+//  } else {
+//    console.log('RESET_DB is not true. Database will not be reset.');
+//  }
 
 // Start the server
 app.listen(port, () => {
