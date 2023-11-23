2c83e6d48df242cd6a9ce87c673aea3382978444
Emmy Dieden <emmydieden@Emmys-MacBook-Pro.local>
2023-11-23 21:16:59 +0100

    made routes, added endpoints

---
+++ routes/bookRoutes.js
@@ -0,0 +1,72 @@
+import express from "express";
+const listEndPoints = require("express-list-endpoints");
+import { BookModel } from "../models/Book";
+
+// Create an instance of the Express router
+// The router method in this code is like setting up a map or a blueprint for handling different kinds of requests in a web application. It helps organize and define how the application should respond when someone visits different URLs. Think of it as creating a list of instructions for the app to follow when it receives specific requests, like "show me all tasks" or "register a new user." This makes the code neat and helps the app know what to do when someone interacts with it.
+const router = express.Router();
+
+//Route to list all the endpoints
+router.get("/", async (req, res) => {
+  const endpoints = listEndPoints(req.app);
+  res.json(endpoints);
+});
+
+//Route for GETing all the books
+router.get("/books", async (req, res) => {
+  //Use the BookModel to find all books in the database
+  // Mongoose Method: TaskModel.find()
+  // Description: This route handles HTTP GET requests and uses the TaskModel to find all tasks in the database. The find() method is a Mongoose method that retrieves all documents from the specified collection (in this case, the "tasks" collection). The found tasks are then responded to the client in JSON format.
+  try {
+    const books = await BookModel.find();
+    res.json(books);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
+//A route to return a single result (one book based on id)
+router.get("/books/:id", async (req, res) => {
+  const id = req.params.id
+
+  //Error message if user gives input that is NaN
+  if(isNaN(id)) {
+    res.status(400).json({ error: "You must enter a number to get the book ID"})
+    return
+  }
+
+  try {
+    //Matching the id from param with a book with same bookID
+    const book = await BookModel.findOne({ bookID: parseInt(id)})
+
+    //Error message when a book with required id does not exost
+    if(!book) {
+      return res.status(404).json({error: "A book with the required id does not exist. Try again."})
+    }
+    res.json(book)
+  } catch (error) {
+    res.json({error: error.message})
+  }
+})
+
+//A route for handling post request (adding a book, with only a title)
+router.post("/add", async (req, res) => {
+  const title = req.body.title;
+
+  //Check if the title is provided:
+  //Checking that the "title" is truthy (not "undefined", "null", or an empty string:)
+  if(!title) {
+    return res.status(400).json({error: "Title is required to add a book"})
+  }
+
+  try {
+    const newBook = await BookModel.create({ title: title });
+    res.json(newBook);
+  } catch (error) {
+    //500 response genereally indicates a server-side error
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+export default router;
--- server.js
+++ server.js
@@ -1,28 +1,51 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config() // Load environment variables from the .env file
+import { BookModel } from "./models/Book";
+import bookRoutes from "./routes/bookRoutes"
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
+import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+//Connection to the database through Mongoose (for local development)
+const mongoUrl = process.env.MONGO_URL //Get the MongoDB connection URL from env variable
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
+const port = process.env.PORT || 8080 //Set the port number for the server
+const app = express() //Create an instance of the express application
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+app.use(cors()); //Enable CORS (Cross-Origin Resource sharing)
+app.use(express.json()); //Parse incoming JSON data
+app.use(express.urlencoded({ extended: false})) //Cool method according to Diego
+
+//Seeding the database
+//RESET_DB=true npm run dev
+if (process.env.RESET_DB) {
+  console.log("Resetting database!")
+
+  const seedDatabase = async () => {
+    await BookModel.deleteMany({})
+    booksData.forEach((book) => {
+      new BookModel(book).save()
+    })
+  }
+  seedDatabase()
+}
+
+//Getting hold of the get/post-methods (routes) through the bookRoutes
+app.use(bookRoutes)
 
 // Start defining your routes here
 app.get("/", (req, res) => {
