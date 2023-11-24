0104e21765c5337e2f5a7f873946b5e89f930afa
Elin Olausson <140396742+Soygirt@users.noreply.github.com>
2023-11-24 15:28:59 +0100

    updates - endpoints working, code still needs cleaning

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import listEndpoints from "express-list-endpoints";
+ import dotenv from "dotenv";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+ dotenv.config();
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+console.log(process.env);
+
+ mongoose.set("strictQuery", false);
+ 
+const mongoUrl = //process.env.MONGO_URL || "mongodb://localhost/project-mongo";//
+process.env.MONGO_URL || "mongodb://127.0.0.1:27017/MongoProject";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -23,11 +24,85 @@ const app = express();
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: "Service unavailable" });
+  }
+});
+
+//import and define the schema
+
+const Book = mongoose.model("Book",
+{
+  bookID: Number,
+  title: String,
+  author: String,
+  average_rating: Number,
+  isbn: Number,
+  isbn13: Number,
+  language_code: String,
+  num_pages: Number,
+  ratings_count: Number,
+  text_reviews_count: Number,
+});
+
+//Reset the database
+/*if (process.env.RESET_DB) {
+  console.log("Resetting database");
+
+  const seedDatabase = async () => {
+    await Book.deleteMany();
+    booksData.forEach((book) => {
+      new Book(book).save();
+    });
+  };
+
+  seedDatabase();
+}*/
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  const endpoints = listEndpoints(app);
+  res.json(endpoints);
 });
+
+app.get("/books", async (req, res) => {
+  try {
+      const books = await Book.find();
+      res.json(books);
+  } catch (error) {
+    console.log(error)
+  };
+  
+});
+
+
+app.get("/books/:id", async (req, res) => {
+  const bookID = req.params.id;
+  Book.find({ bookID: bookID }).then((bookID) => {
+    if (bookID) {
+      res.json(bookID);
+    } else {
+      res.status(404).json({ error: `Book not found` });
+    }
+  });
+});
+
+app.get("/books/rating/:rating", async (req, res) => {
+  const bookRating = req.params.rating;
+  Book.find({ average_rating: bookRating }).then((average_rating) => {
+    if (average_rating) {
+      res.json(average_rating);
+    } else {
+      res.status(404).json({ error: `Book not found` });
+    }
+  });
+});
+
+
+
 
 // Start the server
 app.listen(port, () => {
