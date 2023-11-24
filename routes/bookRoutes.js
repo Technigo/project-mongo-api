da855c0fe17776cc57194b2d3eea45151875706e
BeckieMorton <beckam11@gmail.com>
2023-11-24 13:37:32 +0100

    refactor code in server

--- routes/bookRoutes.js
+++
@@ -1,4 +0,0 @@
-// const express = require("express");
-// const router = express.Router();
-// const { BookModel } = require("../models/BookModel");
-// import listEndpoints from "express-list-endpoints";
--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import booksData from "./data/books.json";
 import siteData from "./data/sites.json";
 import listEndpoints from "express-list-endpoints";
-//const dotenv = require("dotenv");
-//dotenv.config({ path: "./config.env" });
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 
+//------Connect to database------//
 const mongoUrl =
   process.env.MONGO_URL || "mongodb://127.0.0.1:27017/MongoProject";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
+//-----Check connection is working----//
 mongoose.connection.on("connected", () => {
   console.log("Connected to MongoDB");
 });
 
+//-----Define mongoose model based on Site database----//
 const Site = mongoose.model("Site", {
   name: String,
   description: String,
@@ -27,8 +29,7 @@ const Site = mongoose.model("Site", {
   region: String,
 });
 
-//I imported my database through Compass
-
+//I imported my database through Compass and didn't use this code
 // if (process.env.RESET_DB) {
 //   const seedDatabase = async () => {
 //     await Book.deleteMany({});
@@ -39,15 +40,17 @@ const Site = mongoose.model("Site", {
 //   seedDatabase();
 // }
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
+//-------Defines the port the app will run on-----//
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+//--------Add middlewares-------//
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+//---------END POINTS---------//
+
+//lists all end points
 app.get("/", (req, res) => {
   res.send(listEndpoints(app));
 });
@@ -59,7 +62,6 @@ app.get("/sites", async (req, res) => {
 });
 
 //----- End point for all sites from date inscribed-----//
-
 app.get("/date/:dateinscribed", async (req, res) => {
   const siteDate = req.params.dateinscribed;
   Site.find({ dateinscribed: siteDate }).then((date) => {
@@ -86,7 +88,7 @@ app.get("/country/:country", async (req, res) => {
 
 //End point for name of site (one result) eg. (must include spaces, havent added any handling for this)
 app.get("/name/:name", async (req, res) => {
-  // const siteName = req.params.name.toLowerCase().replaceAll(" ", "");
+  // const siteName = req.params.name.toLowerCase().replaceAll(" ", ""); //this doesn't work, found solution with regex through google
   const siteName = req.params.name;
   const regex2 = new RegExp("^" + siteName + "$", "i");
   Site.find({ name: { $regex: regex2 } }).then((name) => {
