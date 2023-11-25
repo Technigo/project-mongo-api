913f3450466436e015f7821a34d071c141c5cd99
sheryll.poo-cheong <shey_pc@gmail.com>
2023-11-25 22:14:42 +0100

    transfrerring models for 2 collections

--- models/NetflixTitle.js
+++ models/NetflixTitle.js
@@ -46,29 +46,6 @@ export const netflixTitleSchema = new Schema({
     }
 })
 
-export const actorSchema = new Schema({
-    name:{
-        type: String
-    },
-/*     title:{
-        type: [mongoose.Schema.Types.ObjectId],
-        ref: "netflixTitles"
-    } */     
-    })
-
-export const countrySchema = new Schema({
-        name:{
-            type: String
-        },      
-        })
-
-// This model is used to interact with the collections in the MongoDB database. It allows you to perform CRUD operations on documents in that collection and provides methods for data validation based on the schema.
-
 // Create a Mongoose model named 'NetflixTitleModel' based on the 'netflixTitleSchema' for the 'netflixTitles' collection
 export const NetflixTitleModel = mongoose.model("netflixTitles", netflixTitleSchema)
 
-// Create a Mongoose model named 'ActorModel' based on the 'actorSchema' for the 'actors' collection
-export const ActorModel = mongoose.model("actors", actorSchema)
-
-// Create a Mongoose model named 'CountryModel' based on the 'countrySchema' for the 'countries' collection
-export const CountryModel = mongoose.model("countries", countrySchema)
