852b9fbb0f3d4d771c9e6abb4abe516037912f06
sheryll.poo-cheong <shey_pc@gmail.com>
2023-11-25 22:40:48 +0100

    retransferring list endpoints to routes

--- routes/netflixTitleRoutes.js
+++ routes/netflixTitleRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import listEndpoints from "express-list-endpoints";
 import {NetflixTitleModel} from "../models/NetflixTitle";
 import { ActorModel } from "../models/Actor";
 import { CountryModel } from "../models/Country";
@@ -6,15 +7,14 @@ import { CountryModel } from "../models/Country";
 // Creating an instance of the Express router
 // The router method in this code is like setting up a map or a blueprint for handling different kinds of requests in a web application. It helps organize and define how the application should respond when someone visits different URLs. Think of it as creating a list of instructions for the app to follow when it receives specific requests, like "show me all titles" or "register a new user." This makes the code neat and helps the app know what to do when someone interacts with it.
 const router = express.Router();
-// const listEndpoints = require('express-list-endpoints')
 
 // -------Defining routes for handling GET requests-------
 
-// //---- Documentation of API ----
-// router.get("/", (req, res) => {
-//     const endpoints = listEndpoints(app);
-//     res.json(endpoints)
-//   });
+//---- Documentation of API ----
+router.get("/", (req, res) => {
+    const endpoints = listEndpoints(router);
+    res.json(endpoints)
+  });
 
 // --- *NetflixTitles collection* ---
 
