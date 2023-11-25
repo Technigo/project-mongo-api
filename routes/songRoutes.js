38fa3cc4eea7c5c58d6e479b10d678e64a0e70ec
Anna Robertsson <robertsson_anna@hotmail.com>
2023-11-26 00:34:11 +0100

    updated readme, commented out seedDatabase, updated server.js and songRoutes

--- routes/songRoutes.js
+++ routes/songRoutes.js
@@ -4,89 +4,153 @@ import listEndpoints from "express-list-endpoints";
 import { MetallicaSongModel } from "../models/MetallicaSongModel";
 
 // Creating an instance of the Express router
-const router = express.Router()
+const router = express.Router();
 
 // ------- The routes -------
 
+// ------- All endpoints -------
 // Show all endpoints available in a JSON format
 router.get("/", (req, res) => {
-   const endpoints = listEndpoints(router)
-   res.json(endpoints);
+  const endpoints = listEndpoints(router);
+  res.json(endpoints);
 });
 
+// ------- All Metallica songs -------
 // Show all Metallica songs
 router.get("/songs", async (req, res) => {
-    // Asynchronously retrieve all Metallica songs from the database
-    await MetallicaSongModel.find()
+  // Asynchronously retrieve all Metallica songs from the database
+  await MetallicaSongModel.find()
     // If the retrieval is successful, handle the result
     .then((result) => res.json(result))
     // If an error occurs during the retrieval, handle the error
-    .catch((error) => res.json(error))
-})
+    .catch((error) => res.json(error));
+});
 
+// ------- One Metallica song based on id -------
 // Show a specific Metallica song based on its MongoDB-generated _id
 router.get("/songs/:id", (req, res) => {
-    const songId = req.params.id;
+  const songId = req.params.id;
 
-    // Find a specific song by its MongoDB-generated _id using the Mongoose model
-    MetallicaSongModel.findOne({ _id: songId })
-        .then((song) => {
-            // If the song is not found, send a 404 error response
-            if (!song) {
-                return res.status(404).json({ message: 'Song not found, please try another id' });
-            }
+  // Find a specific song by its MongoDB-generated _id using the Mongoose model's findById method
+  MetallicaSongModel.findById(songId)
+    .then((song) => {
+      // If the song is not found, send a 404 error response
+      if (!song) {
+        return res
+          .status(404)
+          .json({ message: "Song not found, please try another id" });
+      }
 
-            // Send the retrieved song as a JSON response
-            res.json(song);
-        })
-        .catch((error) => {
-            // If an error occurs during the database query, send an error response
-            res.status(500).json({ message: 'Internal server error', error: error.message });
-        });
+      // Send the retrieved song as a JSON response
+      res.json(song);
+    })
+    .catch((error) => {
+      // If an error occurs during the database query, send an error response
+      res
+        .status(500)
+        .json({ message: "Internal server error", error: error.message });
+    });
 });
 
-// // Show a specific Metallica song based on its MongoDB-generated _id
-// router.get("/songs/:id", async (req, res) => {
-//     const songId = req.params.id
-//     try {
-// // Find a specific song by its MongoDB-generated _id using the Mongoose model
-// const song = await MetallicaSongModel.findOne({ _id: songId })
+// ------- One Metallica song based on title -------
+// Show a specific Metallica song based on the song title (case-insensitive and partial match)
+router.get("/song-name/:title", (req, res) => {
+  const songTitle = req.params.title.toLowerCase(); // Convert to lowercase for case-insensitive search
 
-// // If the song is not found, send a 404 error response
-// if (!song) {
-//     //.then((result) => res.json(result))
-// return res.status(404).json({ message: 'Song not found, please try another id' })    
-// }
+  // Find a song by its title using the Mongoose model, allowing partial matches
+  MetallicaSongModel.find({ name: { $regex: songTitle, $options: "i" } })
+    .then((songs) => {
+      // If no song titles are found, send a 404 error response
+      if (!songs || songs.length === 0) {
+        return res
+          .status(404)
+          .json({ message: "Song not found, please try another song title" });
+      }
 
-// // Send the retrieved song as a JSON response
-// res.json(song)
-//  } catch (error) {
+      // Send the retrieved song title(s) as a JSON response
+      res.json(songs);
+    })
+    .catch((error) => {
+      // If an error occurs during the database query, send an error response
+      res
+        .status(500)
+        .json({ message: "Internal server error", error: error.message });
+    });
+});
+
+// ------- One Metallica album -------
+// Show a specific Metallica album based on the album title (case-insensitive and partial match). Also has the possibillity to check the popularity of the songs within an album with query param
+router.get("/albums/:album", (req, res) => {
+  const requestedAlbum = req.params.album.toLowerCase(); // Convert to lowercase for case-insensitive search
+  const popularity = parseInt(req.query.popularity, 10); // Parse the 'popularity' query parameter as an integer
+
+  // Find the album by title in the database
+  MetallicaSongModel.find({ album: { $regex: requestedAlbum, $options: "i" } })
+    .then((foundAlbum) => {
+      // If no album found, send a 404 error response
+      if (!foundAlbum || foundAlbum.length === 0) {
+        return res
+          .status(404)
+          .json({ message: "Album not found, please try another album title" });
+      }
+
+      // If popularity query parameter exists and is valid, filter songs by popularity
+      if (!isNaN(popularity) && popularity >= 0) {
+        // Filter songs within the album by popularity
+        const songsWithDesiredPopularity = foundAlbum.filter(
+          (song) => song.popularity >= popularity
+        );
+
+        // If no songs match the popularity criterion, send an error response
+        if (songsWithDesiredPopularity.length === 0) {
+          return res.status(404).json({
+            message:
+              "No songs found with this popularity or more in this album",
+          });
+        }
+
+        // Send the filtered songs within the album by popularity as a JSON response
+        return res.json(songsWithDesiredPopularity);
+      }
+      // Send the retrieved album as a JSON response without filtering by popularity
+      res.json(foundAlbum);
+    })
+    .catch((error) => {
+      // If an error occurs during the database query, send an error response
+      res
+        .status(500)
+        .json({ message: "Internal server error", error: error.message });
+    });
+});
+
+// ------- Dummy endpoint -------
+// Show all Metallica songs based on the year it was released
+// router.get("/songs/release", async (req, res) => {
+//   const releaseYear = req.query.release
+
+//   try {
+//     // Find a song by its release year using the Mongoose model, allowing partial matches
+//     const songs = await MetallicaSongModel.find({
+//       release_date: { $regex: "^${releaseYear}", $options: "i" },
+//     });
+
+//     // If no songs from that year is found, send a 404 error response
+//     if (!songs || songs.length === 0) {
+//       return res.status(404).json({
+//         message:
+//           "Song not found for the specified release year, please try another release year",
+//       });
+//     }
+
+//     // Send the retrieved songs as a JSON response
+//     res.json(songs);
+//   } catch (error) {
 //     // If an error occurs during the database query, send an error response
-//  res.status(500).json({ message: 'Internal server error', error: error.message })
-// }
-// })
-
-// Show a specific Metallica album based on the album title
-router.get("/albums/:title", (req, res) => {
-    const albumTitle = req.params.title;
-
-    // Find an album by its title using the Mongoose model
-    MetallicaSongModel.find({ album: albumTitle })
-        .then((album) => {
-            // If the album is not found, send a 404 error response
-            if (!album || album.length === 0) {
-                return res.status(404).json({ message: 'Album not found, please try another album title' });
-            }
-
-            // Send the retrieved album as a JSON response
-            res.json(album);
-        })
-        .catch((error) => {
-            // If an error occurs during the database query, send an error response
-            res.status(500).json({ message: 'Internal server error', error: error.message });
-        });
-});
+//     res
+//       .status(500)
+//       .json({ message: "Internal server error", error: error.message });
+//   }
+// });
 
 // Export the router for use in the main application
-//export default router
 module.exports = router;
--- server.js
+++ server.js
@@ -2,16 +2,15 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import dotenv from "dotenv" // Import dotenv for environment variables
-import MetallicaSongs from "./data/MetallicaSongs.json"
+import dotenv from "dotenv"; // Import dotenv for environment variables
+import MetallicaSongs from "./data/MetallicaSongs.json";
 import { MetallicaSongModel } from "./models/MetallicaSongModel";
-import songRoutes from "./routes/songRoutes" // Import routes for handling song-related endpoints
-//const songRoutes = require("./routes/songRoutes");
+import songRoutes from "./routes/songRoutes"; // Import routes for handling song-related endpoints
 
-dotenv.config() //Load environment variables from the .env file
+dotenv.config(); //Load environment variables from the .env file
 
 // Connection to MongoDB using environment variable for URL
-const mongoUrl = process.env.MONGO_URL
+const mongoUrl = process.env.MONGO_URL;
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -25,25 +24,21 @@ app.use(songRoutes); // Mounting song-related routes in the Express app
 // Middleware setup
 app.use(cors()); // Enable Cross-Origin Resource Sharing (CORS)
 app.use(express.json()); // Parse incoming JSON data
-app.use(express.urlencoded({ extended: false })) // Parse URL-encoded data
+app.use(express.urlencoded({ extended: false })); // Parse URL-encoded data
 
 // Conditional database seeding based on environment variable, commented out since data is in MongoDB Compass
-//if (process.env.RESET_DB) {
-  const seedDatabase = async () => {
-    try {
-      console.log('Seeding database...');
-    await MetallicaSongModel.deleteMany({})
-await MetallicaSongModel.insertMany(MetallicaSongs);
-          console.log("Database seeded successfully!");
-        } catch (error) {
-            console.error("Error seeding database:", error);
-    }
-  }
+// const seedDatabase = async () => {
+//   try {
+//     console.log("Seeding database...");
+//     await MetallicaSongModel.deleteMany({});
+//     await MetallicaSongModel.insertMany(MetallicaSongs);
+//     console.log("Database seeded successfully!");
+//   } catch (error) {
+//     console.error("Error seeding database:", error);
+//   }
+// };
 
-  seedDatabase()
-  // } else {
-  //   console.log('RESET_DB is not true. Database will not be reset.');
-  // }
+// seedDatabase();
 
 // Start the server and listen on the specified port
 app.listen(port, () => {
