2f0b3a352ea0e8d02fcdc168e619cd410886cf96
mariateresepettersson <mariateresepettersson>
2023-11-26 23:07:24 +0100

    Change data to correct file

---
+++ models/Netflix.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+export const netflixSchema = new Schema({
+    // Complex Object Config
+    show_id: { type: Number, },
+    title: { type: String },
+    director: { type: String },
+    cast: { type: String },
+    country: { type: String },
+    date_added: { type: String },
+    release_year: { type: Number },
+    rating: { type: String },
+    duration: { type: String },
+    listed_in: { type: String },
+    description: { type: String },
+    type: { type: String }
+});
+
+// Create a Mongoose model named 'MovieModel' based on the 'netlfixSchema' for the 'Netflix' collection
+// This model is used to interact with the "Netflix" collection in the MongoDB database. It allows you to perform CRUD operations on documents in that collection and provides methods for data validation based on the schema.
+export const MovieModel = mongoose.model("Movie", netflixSchema);
