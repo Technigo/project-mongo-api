49b5e2636af2e3ee13b06f6d641378b9ed8d6ce2
mariateresepettersson <mariateresepettersson>
2023-11-26 23:06:19 +0100

    Created a model and schema

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv"; // Import dotenv for environment variables
+dotenv.config(); // Load environment variables from the .env file
+import movieRoutes from "./routes/movieRoutes"; //Import Routes
+import listEndpoints from "express-list-endpoints";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+//import netflixData from "./data/netflix-titles.json";
 
+// Connection to the database through Mongoose (for local development)
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080; //Set the port number of the server
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+app.use(cors()); // Enable CORS (Cross-Origin Resource Sharing)
+app.use(express.json()); // Parse incoming JSON data
+app.use(express.urlencoded({ extended: false })); // Parse URL-encoded data
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+// Use the routes for handling the API REquests!
+//Route to list all the endpoints
+app.get('/', (req, res) => {
+  res.json({ endpoints: listEndpoints(app) });
 });
+app.use(movieRoutes);
 
-// Start the server
+// Start the server and listen for incoming requests on the specified port
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`); // Display a message when the server is successfully started
 });
