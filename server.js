4dba605fb5afb424db69c0c1a04c84a8adca3df6
Carolina Luna <kroluna@gmail.com>
2023-11-27 12:24:08 +0100

    started all over again, had problems with errors

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import listEndpoints from "express-list-endpoints";
+import bodyParser from "body-parser";
 
-import routes from "./Routes/Routes.js";
+import booksData from "./data/books.json" assert { type: "json" };
 
 dotenv.config();
 
@@ -27,23 +29,81 @@ app.use((req, res, next) => {
   }
 });
 
-app.use(routes);
-
 const mongoUrl =
-  process.env.MONGO_URL || "mongodb://localhost/project-mongo-api";
+  process.env.MONGO_URL || "mongodb://127.0.0.1:27017/project-mongo-api";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// if (process.env.RESET_DB) {
-//   const seedDatabase = async () => {
-//     await Book.deleteMany({});
-//     data.forEach((data) => {
-//       new Book(data).save();
-//     });
-//   };
+const BookDataModel = mongoose.model("BookData", {
+  bookID: { type: Number },
+  title: { type: String },
+  authors: { type: String },
+  average_rating: { type: Number },
+  isbn: { type: Number },
+  isbn13: { type: Number },
+  language_code: { type: String },
+  num_pages: { type: Number },
+  ratings_count: { type: Number },
+  text_reviews_count: { type: Number },
+});
 
-//   seedDatabase();
-// }
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    console.log("Resetting database!");
+
+    await BookDataModel.deleteMany({});
+
+    booksData.forEach(async (book) => {
+      await new BookDataModel(book).save();
+    });
+  };
+  seedDatabase();
+}
+
+app.get("/", (req, res) => {
+  res.json(listEndpoints(app));
+});
+
+app.get("/books", async (req, res) => {
+  try {
+    const books = await BookDataModel.find();
+    res.json(booksData);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ error: "Data not found" });
+  }
+});
+
+app.get("/books/:bookID", async (req, res) => {
+  const { bookID } = req.params;
+
+  try {
+    const book = await BookDataModel.findOne({ bookID: +bookID });
+
+    if (book) {
+      res.json(book);
+    } else {
+      res.status(404).json({ error: "Book not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/books/author", async (req, res) => {
+  const authorName = req.query.author;
+  if (!authorName) {
+    return res.status(400).json({ error: "Author name is required" });
+  }
+
+  try {
+    const booksByAuthor = await BookDataModel.find({ authors: authorName });
+    res.json(booksByAuthor);
+  } catch (err) {
+    res.status(400).json({ error: "Error fetching author's books" });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
