435ace6f3cb8bbcb01c9b78f9f0ebc1fdb130db2
Laura Lyckholm <137819100+LauraLyckholm@users.noreply.github.com>
2023-11-27 18:39:29 +0100

    Added deploylink to readme

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.use("/", movieRoutes);
 // Middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false })); // LOOK THIS UP
 // Middleware to handle error if service is down
 app.use((req, res, next) => {
   if (mongoose.connection.readyState === 1) {
