e9b71d76a2551318749737b9f7749a8729861129
Linnea Johansson <Linneajoh@gmail.com>
2023-12-09 21:09:53 +0100

    route to get books

--- server.js
+++ server.js
@@ -38,6 +38,27 @@ app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
+app.get('/books', async (req, res) => {
+  try {
+    const books = await Book.find({});
+    res.json(books);
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+app.get('/books/:id', async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+    res.json(book);
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
