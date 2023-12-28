b10610e06878fc97e1d58c9b18dc43c466a9ac22
Olga Lepistö <olga.lepisto@gmail.com>
2023-12-28 22:48:38 +0200

    created models and endpoints

--- server.js
+++ server.js
@@ -1,22 +1,45 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import booksData from "./data/books.json";
+import dotenv from "dotenv"
+dotenv.config();
+import listEndpoints from "express-list-endpoints";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+mongoose.set('strictQuery', false);
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo-api";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+const Book = mongoose.model('Book', {
+  bookID: Number,
+  title: String,
+  authors: String,
+  average_rating: Number,
+  isbn: Number,
+  isbn13: Number,
+  language_code: String,
+  num_pages: Number,
+  ratings_count: Number,
+  text_reviews_count: Number
+})
+
+// Seed the database
+// I previously had this inside an if statement with the RESET_DB,
+// but for some reason when I run "RESET_DB=true npm run dev" nothing happens and the database doesn't seed,
+// so I took it out and now it works...
+  const seedDatabase = async () => {
+    await Book.deleteMany({})
+
+    booksData.forEach((book) => {
+      new Book(book).save()
+    })
+  }
+
+  seedDatabase()
+
+// Defines the port the app will run on
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -26,10 +49,31 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+  res.send(listEndpoints(app));
+  });
+
+app.get("/books", async (req, res) => {
+  const books = await Book.find()
+  res.json(books)
+})
+
+app.get("/books/:id", async (req, res) => {
+  const bookID = await Book.findOne({ bookID: req.params.id })
+  res.json(bookID)
+})
+
+app.get("/books/author/:author", async (req, res) => {
+  const author = await Book.find({ authors: req.params.author })
+  res.json(author)
+})
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+const startServer = async () => {
+    await seedDatabase();
+  }
+
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
+
+startServer();
