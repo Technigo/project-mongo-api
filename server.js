9c26ad358a00bd187a50e5e829caec0b36cdd058
isaackamran <isaackamran@hotmail.com>
2024-01-18 12:42:43 +0100

    help from lemons2

--- server.js
+++ server.js
@@ -40,7 +40,12 @@ app.use(router);
 
 // seedDatabase();
 
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-
