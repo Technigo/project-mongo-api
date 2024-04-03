fb46830d0d7b25cdc3ced6185a843a3337543643
hamdiolad <oladhamdi@gmail.com>
2024-04-03 14:28:58 +0200

    added code

--- server.js
+++ server.js
@@ -1,35 +1,73 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+import listEndpoints from "express-list-endpoints";
+import booksData from "./data/books.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+dotenv.config();
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
+const Books = mongoose.model("Book", {
+  bookID: Number,
+  title: String,
+  authors: String,
+  average_rating: Number,
+  isbn: Number,
+  isbn13: Number,
+  language_code: String,
+  num_pages: Number,
+  ratings_count: Number,
+  text_review_count: Number,
+});
 
-// Add middlewares to enable cors and json body parsing
+const seedDatabase = async () => {
+  try {
+    await Books.deleteMany({});
+    await Books.insertMany(booksData);
+    console.log("Database seeded successfully.");
+  } catch (error) {
+    console.error("Error seeding database:", error);
+  }
+};
+seedDatabase();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
-// Start the server
+app.get("/books", async (req, res) => {
+  try {
+    const allBooks = await Books.find();
+    res.json(allBooks);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/books/:bookID", async (req, res) => {
+  try {
+    const { bookID } = req.params;
+    const singleBook = await Books.findOne({ bookID });
+    if (singleBook) {
+      res.json(singleBook);
+    } else {
+      res.status(404).json({ error: "Book not found, try another number" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
