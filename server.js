5bc36683100837d73c216116627e71c1a16b0437
Sofia Johansson <153744881+sofia32057@users.noreply.github.com>
2024-04-30 22:16:55 +0200

    Inital commit - basic setup

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose";
+import mongoose, { Mongoose, mongo } from "mongoose";
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
+import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.Promise = Promise;
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
@@ -23,6 +18,26 @@ const app = express();
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+
+const mongoURL = process.env.MONGO_URL || "mongodb://localhost/library";
+mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
+
+// Mongoose model
+const Book = mongoose.model("Book", {
+  // Properties defined here match the keys from the books.json file
+});
+
+// if (process.env.RESET_DB) {
+const seedDatabase = async () => {
+  await Book.deleteMany({});
+
+  booksData.forEach(book => {
+    new Book(book).save();
+  });
+};
+seedDatabase();
+// }
 
 // Start defining your routes here
 app.get("/", (req, res) => {
