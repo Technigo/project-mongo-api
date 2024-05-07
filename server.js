0996df76313e6be12daa827c803c76c7442fe6ab
SofieFerrari <sofiestrahl@gmail.com>
2024-05-07 10:05:20 +0200

    added schema and model

--- server.js
+++ server.js
@@ -1,35 +1,38 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-mongo'
+mongoose.connect(mongoUrl)
+mongoose.Promise = Promise
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl);
-mongoose.Promise = Promise;
+const port = process.env.PORT || 8080
+const app = express()
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
+const { Schema } = mongoose
+const cheeseSchema = new Schema({
+	id: Number,
+	name: String,
+	animal: String,
+	region: String,
+	taste: String,
+	wine_pairing: String,
+	cheese_rating: Number,
+	wine_pairing_rating: Number,
+})
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+//models
+const Cheese = mongoose.model('Cheese', cheeseSchema)
+
+app.use(cors())
+app.use(express.json())
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+app.get('/', (req, res) => {
+	res.send('Hello Technigo!')
+})
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+	console.log(`Server running on http://localhost:${port}`)
+})
