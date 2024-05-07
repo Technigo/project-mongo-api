57d2ddf9070ec1ea2e1077a9f37241a8fc257dfc
SofieFerrari <sofiestrahl@gmail.com>
2024-05-07 12:53:23 +0200

    added endpoint documentation and first cheese route

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import cheeses from './data/cheeses.json'
+import expressListEndpoints from 'express-list-endpoints'
 
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-mongo'
 mongoose.connect(mongoUrl)
@@ -9,9 +11,11 @@ mongoose.Promise = Promise
 const port = process.env.PORT || 8080
 const app = express()
 
+app.use(cors())
+app.use(express.json())
+
 const { Schema } = mongoose
 const cheeseSchema = new Schema({
-	id: Number,
 	name: String,
 	animal: String,
 	region: String,
@@ -24,12 +28,14 @@ const cheeseSchema = new Schema({
 //models
 const Cheese = mongoose.model('Cheese', cheeseSchema)
 
-app.use(cors())
-app.use(express.json())
-
 // Start defining your routes here
 app.get('/', (req, res) => {
-	res.send('Hello Technigo!')
+	const endpoints = expressListEndpoints(app)
+	res.json(endpoints)
+})
+
+app.get('/cheeses', (req, res) => {
+	res.json(cheeses)
 })
 
 // Start the server
