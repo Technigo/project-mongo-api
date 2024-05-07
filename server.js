77dcf43abc9b05b4dd58a24ea1d87dd6b2fbec8b
Maria-source-cell <manuelabirnat@gmail.com>
2024-05-07 14:12:13 +0200

    getting all movie, in progress

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import express from "express";
 import cors from "cors";
+import express from "express";
 import mongoose from "mongoose";
 
 // If you're using one of our datasets, uncomment the appropriate import below
@@ -7,17 +7,32 @@ import mongoose from "mongoose";
 // import avocadoSalesData from "./data/avocado-sales.json";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
+
+import netflixData from "./data/netflix-titles.json";
+import Movie from "./model/Movie.js";
+
 // import topMusicData from "./data/top-music.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl);
 mongoose.Promise = Promise;
 
+if (process.env.RESET_DB) {
+  const seedDatabased = async () => {
+    await Movie.deleteMany({});
+
+    netflixData.forEach((movieData) => {
+      new Movie(movieData).save();
+    });
+  };
+
+  seedDatabased();
+}
+
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
@@ -27,6 +42,16 @@ app.use(express.json());
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+app.get("/movies", async (req, res) => {
+  const allMovies = await Movie.find();
+
+  if (allMovies.length > 0) {
+    res.json(allMovies);
+  } else {
+    res.status(404).send("No movie was found");
+  }
 });
 
 // Start the server
