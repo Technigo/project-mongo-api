451243fc3858660d2e15922ee39594641509f6e9
unknown <linda.frischknecht@bluewin.ch>
2024-05-07 14:27:04 +0200

    setting up endpoints and dataseeding

--- server.js
+++ server.js
@@ -1,23 +1,50 @@
-import express from "express";
 import cors from "cors";
+import express from "express";
+import expressListEndpoints from "express-list-endpoints";
 import mongoose from "mongoose";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl =
+  process.env.MONGO_URL || "mongodb://localhost/project-mongo-bookdata";
 mongoose.connect(mongoUrl);
 mongoose.Promise = Promise;
+
+// Doing the Schema for the book
+const { Schema } = mongoose;
+const BookSchema = new Schema({
+  bookID: Number,
+  title: String,
+  authors: String,
+  average_rating: Number,
+  isbn: Number,
+  isgn13: Number,
+  language_code: String,
+  num_pages: Number,
+  ratings_count: Number,
+  text_reviews_count: Number,
+});
+
+// Doing the Book Model with the Schema
+const Book = mongoose.model("Book", BookSchema);
+
+// Import the Data
+import booksData from "./data/books.json";
+
+// Seed the database with the books
+const seedDatabase = async () => {
+  console.log("Reseading database");
+  await Book.deleteMany({});
+
+  booksData.forEach((bookData) => {
+    new Book(bookData).save();
+  });
+};
+
+seedDatabase();
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 9090;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
@@ -26,8 +53,15 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.json(endpoints);
 });
+
+app.get("/books", (req, res) => {
+  res.json(booksData);
+});
+
+// Getting all the endpoints
+const endpoints = expressListEndpoints(app);
 
 // Start the server
 app.listen(port, () => {
