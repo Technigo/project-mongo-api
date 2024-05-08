defc34e80deeddde329527510be45923b0f5018c
Alma Herrstrom <almaherrstrom@hotmail.com>
2024-05-08 22:24:13 +0200

    added endpoint for cuisine type

--- server.js
+++ server.js
@@ -25,14 +25,13 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-// Start defining your routes here
-
+// Endpoint listing
 app.get("/", (req, res) => {
   const endpoints = expressListEndpoints(app)
   res.json(endpoints)
 })
 
-//Get all restos
+// All restaurants
 app.get("/restaurants", async (req, res) => {
   const allRestaurants = await Restaurant.find()
 
@@ -42,6 +41,46 @@ app.get("/restaurants", async (req, res) => {
     res.status(404).send("No restaurants with this criteria")
   }
 })
+
+// All unique cuisines
+app.get("/cuisines", async (req, res) => {
+  try {
+    const uniqueCuisines = await Restaurant.distinct("cuisine")
+    res.json(uniqueCuisines)
+  } catch (error) {
+    console.error("Error fetching unique cuisines", error)
+    res.status(500).send("Internal Server Error")
+  }
+})
+
+// Filter on cuisine
+app.get("/cuisines/:cuisine", async (req, res) => {
+  const { cuisine } = req.params
+
+  try {
+    //Find cuisines matching user input using mongoDB's regex operator to make the word in database case insensitive
+    const restaurantsByCuisine = await Restaurant.find({
+      cuisine: { $regex: new RegExp("^" + cuisine, "i") },
+    })
+
+    if (restaurantsByCuisine.length > 0) {
+      res.json(restaurantsByCuisine)
+    } else {
+      res
+        .status(404)
+        .send(
+          "No restaurants found within this cuisine, try searching for a cuisine found in the endpoint /cuisine. Add '%20' instead of spacing, for example 'African,%20Creative'"
+        )
+    }
+  } catch (error) {
+    console.error("Error fetching restaurants by cuisine", error)
+    res.status(500).send("Internal Server Error")
+  }
+})
+
+// Filter on name
+// Filter on id
+// Filter on location
 
 // Start the server
 app.listen(port, () => {
