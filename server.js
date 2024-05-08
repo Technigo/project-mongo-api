c0365ec7efa1971dc3027402b0982c81ed886a0e
Viktoria Olofsson <viktoriaolofsson@gmail.com>
2024-05-08 16:12:36 +0200

    basics

--- server.js
+++ server.js
@@ -1,22 +1,54 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import expressListEndpoints from "express-list-endpoints";
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
 // import avocadoSalesData from "./data/avocado-sales.json";
 // import booksData from "./data/books.json";
 // import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
+import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl);
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+// Schema
+const { Schema } = mongoose;
+
+const movieSchema = new Schema({
+  show_id: Number,
+  title: String,
+  director: String,
+  cast: String,
+  country: String,
+  date_added: String,
+  release_year: Number,
+  rating: String,
+  duration: String,
+  listed_in: String,
+  description: String,
+  type: String,
+});
+
+// Model
+const Movie = mongoose.model("Movie", movieSchema);
+
+// Seed the database
+
+const seedDatabase = async () => {
+  console.log("Resetting and seeding");
+  await Movie.deleteMany();
+
+  netflixData.forEach((movie) => {
+    new Movie(movie).save();
+  });
+};
+seedDatabase();
+
+// Defines the port the app will run on.
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -24,9 +56,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+// Routes
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  const endpoints = expressListEndpoints(app);
+  res.json(endpoints);
+});
+
+app.get("/movies", async (req, res) => {
+  const allMovies = await Movie.find();
+
+  if (allMovies.length > 0) {
+    res.json(allMovies);
+  } else {
+    res.status(404).send("No movie found based on filters");
+  }
 });
 
 // Start the server
