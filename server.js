124484b5e6ff3eec0402e46c24393aaeaee7e8e8
schouenkes <m.schouenke@gmail.com>
2024-05-08 20:27:04 +0200

    connected database and added the json data to it

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import booksData from "./data/books.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/technigo-w14-project-mongo";
 mongoose.connect(mongoUrl);
 mongoose.Promise = Promise;
+
+const Book = mongoose.model("Book", {
+  bookID: Number,
+  title: String,
+  authors: String,
+  average_rating: Number,
+  isbn: Number,
+  isbn13: Number,
+  language_code: String,
+  num_pages: Number,
+  ratings_count: Number,
+  text_reviews_count: Number,
+});
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Book.deleteMany({});
+
+    booksData.forEach((book) => {
+      new Book(book).save();
+    });
+  };
+  console.log("seeded the database");
+  seedDatabase();
+}
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
