0a6c1cfd1319596850ff3011133fe5bc852ae296
Josephineoderland <153084500+Josephineoderland@users.noreply.github.com>
2024-05-09 11:04:32 +0200

    soon

--- server.js
+++ server.js
@@ -1,35 +1,38 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+import booksRouter from "./routes/books.js"
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.Promise = Promise;
+const apiRoutes = [
+  {
+    path: "/",
+    methods: ["GET"],
+  },
+  {
+    path: "/books",
+    methods: ["GET"],
+  },
+]
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
+mongoose
+  .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err))
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+const port = process.env.PORT || 8080
+const app = express()
 
-// Start defining your routes here
+app.use(cors())
+app.use(express.json())
+
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+  res.json(apiRoutes)
+})
 
-// Start the server
+app.use("/books", booksRouter)
+
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+  console.log(`Server running on http://localhost:${port}`)
+})
---
+++ server/importBooks.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import Book from "./models/Book.js";
+import booksData from "./data/books.json" assert { type: "json" };
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+ .then(() => console.log("MongoDB connected..."))
+ .catch((err) => console.log(err));
+
+booksData.forEach(async (bookData) => {
+ try {
+    const book = new Book(bookData);
+    await book.save();
+    console.log(`Book saved: ${book.title}`);
+ } catch (error) {
+    console.error(`Error saving book: ${error.message}`);
+ }
+});
