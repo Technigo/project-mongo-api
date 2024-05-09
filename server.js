f29918ab463227dd134d31538a0b4fe8c1ba9f7f
Your El1an3 <eliane_w@yahoo.com>
2024-05-09 18:07:11 +0200

    setting up basics

--- server.js
+++ server.js
@@ -1,32 +1,57 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import expressListEndpoints from "express-list-endpoints";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+//import data
+import whoData from "./data/doctorwho.json";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+//establish connection to MongoDB database using Mongoose
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/doctorwho-data";
 mongoose.connect(mongoUrl);
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+//define schema
+const { Schema } = mongoose;
+
+const episodeSchema = new Schema({
+  episode_title: String,
+  doctor_actor: String,
+  companion: String,
+  summary: String,
+  air_date: String,
+});
+
+//define model
+const Episode = mongoose.model("Episode", episodeSchema);
+
+//seed the database
+const seedDatabase = () => {
+  whoData.forEach((episode) => {
+    new Episode(episode).save();
+  });
+};
+seedDatabase();
+
+//defines the port the app will run on
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+//add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+//routes
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  const endpoints = expressListEndpoints(app);
+  res.json(endpoints);
+});
+
+//1️⃣ endpoint that returns all episodes
+//example: http://localhost:8080/episodes
+app.get("/episodes", async (req, res) => {
+  const episodes = await Episodes.find();
+  res.json(episodes);
 });
 
 // Start the server
