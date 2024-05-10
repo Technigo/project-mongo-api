fcd766ed8179b0898928a9b19f9459a591a2ee0d
Kathinka Sewell <4355167+kathinka@users.noreply.github.com>
2024-05-10 23:24:59 +0200

    added more error handling and logging with morgan and winston to my project

--- routes/Routes.js
+++ routes/Routes.js
@@ -1,8 +1,333 @@
 import express from "express";
 import Book from "../models/Book.js";
 import listEndpoints from "express-list-endpoints";
-
 const router = express.Router();
+
+//filter with query parameters
+router.get("/filterbooks", async (req, res) => {
+  try {
+    const {
+      title,
+      authors,
+      average_rating,
+      num_pages,
+      ratings_count,
+      text_reviews_count,
+      language_code,
+    } = req.query;
+
+    const query = {};
+
+    if (title) {
+      //find the title with the query parameter
+      query.title = { $regex: new RegExp(title, "i") };
+    }
+    if (authors) {
+      query.authors = { $regex: new RegExp(authors, "i") };
+    }
+    if (average_rating) {
+      query.average_rating = {
+        $gte: Number(average_rating),
+        $lt: Number(average_rating) + 1,
+      };
+    }
+    if (num_pages) {
+      query.num_pages = {
+        $gte: Number(num_pages),
+        $lt: Number(num_pages) + 100,
+      };
+    }
+    if (ratings_count) {
+      query.ratings_count = {
+        $gte: Number(ratings_count),
+        $lt: Number(ratings_count) + 100,
+      };
+    }
+    if (text_reviews_count) {
+      query.text_reviews_count = {
+        $gte: Number(text_reviews_count),
+        $lt: Number(text_reviews_count) + 100,
+      };
+    }
+    if (language_code) {
+      query.language_code = language_code;
+    }
+
+    const books = await Book.find(query).limit(100).exec(); // Limit the number of results to 100
+    res.json(books);
+  } catch (error) {
+    // If an error occurred, create a new error with a custom message
+    const err = new Error(`Error fetching filtered books : ${error}`);
+    // Pass the error to the next middleware
+    next(err);
+  }
+});
+
+router.get("/books/", async (req, res) => {
+  try {
+    const { title } = req.query;
+    const queryRegex = { $regex: new RegExp(title, "i") };
+    const books = await Book.find({ title: queryRegex }).sort({ title: 1 });
+    res.json(books);
+  } catch (error) {
+    // If an error occurred, create a new error with a custom message
+    const err = new Error(`Error fetching books: ${error}`);
+    // Pass the error to the next middleware
+    next(err);
+  }
+});
+
+router.get("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findById(id).exec();
+    if (book) {
+      res.json(book);
+    } else {
+      res.status(404).json({ error: `ID:${id} Not found` });
+    }
+  } catch (error) {
+    // If an error occurred, create a new error with a custom message
+    const err = new Error(`Error fetching books with id ${id} : ${error}`);
+    // Pass the error to the next middleware
+    next(err);
+  }
+});
+
+router.get("/books/authors/:authors", async (req, res) => {
+  try {
+    const authors = req.params.authors;
+    const queryRegex = new RegExp(authors, "i"); // Create a case-insensitive regex to match authors
+    const book = await Book.find({ authors: { $regex: queryRegex } });
+    if (book.length > 0) {
+      res.json(book);
+    } else {
+      res.status(404).json({ error: `Author: ${authors} not found` });
+    }
+  } catch (error) {
+    // If an error occurred, create a new error with a custom message
+    const err = new Error(
+      `Error fetching books with authors: ${authors}: ${error}`
+    );
+    // Pass the error to the next middleware
+    next(err);
+  }
+});
+
+router.get("/books/average_rating/:average_rating", async (req, res) => {
+  try {
+    const average_rating = Number(req.params.average_rating);
+    const book = await Book.find({
+      average_rating: {
+        $gte: average_rating,
+        $lt: average_rating + 1,
+      },
+    });
+    if (book.length > 0) {
+      res.json(book);
+    } else {
+      res
+        .status(404)
+        .json({ error: `No results for this rating: ${average_rating}` });
+    }
+  } catch (error) {
+    // If an error occurred, create a new error with a custom message
+    const err = new Error(
+      `Error fetching books with ${average_rating} rating: ${error}`
+    );
+    // Pass the error to the next middleware
+    next(err);
+  }
+});
+
+router.get("/books/pages_asc/:num_pages", async (req, res) => {
+  try {
+    const num_pages = Number(req.params.num_pages);
+    const book = await Book.find({
+      num_pages: {
+        $gte: num_pages,
+        $lt: num_pages + 50,
+      },
+    }).sort({ num_pages: 1 });
+    if (book.length > 0) {
+      res.json(book);
+    } else {
+      res.status(404).json({ error: `Book with ${num_pages} pages not found` });
+    }
+  } catch (error) {
+    // If an error occurred, create a new error with a custom message
+    const err = new Error(
+      `Error fetching books with ${num_pages} pages: ${error}`
+    );
+    // Pass the error to the next middleware
+    next(err);
+  }
+});
+
+router.get("/books/pages_desc/:num_pages", async (req, res) => {
+  try {
+    const num_pages = Number(req.params.num_pages);
+    const book = await Book.find({
+      num_pages: {
+        $gte: num_pages,
+        $lt: num_pages + 50,
+      },
+    }).sort({ num_pages: -1 });
+    if (book.length > 0) {
+      res.json(book);
+    } else {
+      res.status(404).json({ error: `Book with ${num_pages} pages not found` });
+    }
+  } catch (error) {
+    // If an error occurred, create a new error with a custom message
+    const err = new Error(
+      `Error fetching books with ${num_pages} pages: ${error}`
+    );
+    // Pass the error to the next middleware
+    next(err);
+  }
+});
+
+router.get("/books/ratings_count/:ratings_count", async (req, res) => {
+  try {
+    const ratings_count = Number(req.params.ratings_count);
+    const book = await Book.find({
+      ratings_count: {
+        $gte: ratings_count,
+        $lt: ratings_count + 50,
+      },
+    });
+    if (book.length > 0) {
+      res.json(book);
+    } else {
+      res
+        .status(404)
+        .json({ error: `Rating count ${ratings_count} not found` });
+    }
+  } catch (error) {
+    // If an error occurred, create a new error with a custom message
+    const err = new Error(`Error fetching rating:${ratings_count}  : ${error}`);
+    // Pass the error to the next middleware
+    next(err);
+  }
+});
+
+router.get(
+  "/books/text_reviews_count/:text_reviews_count",
+  async (req, res) => {
+    try {
+      const text_reviews_count = Number(req.params.text_reviews_count);
+      const book = await Book.find({
+        text_reviews_count: {
+          $gte: text_reviews_count,
+          $lt: text_reviews_count + 50,
+        },
+      });
+      if (book.length > 0) {
+        res.json(book);
+      } else {
+        res
+          .status(404)
+          .json({
+            error: `Books with ${text_reviews_count} text reviews not found`,
+          });
+      }
+    } catch (error) {
+      // If an error occurred, create a new error with a custom message
+      const err = new Error(
+        `Error fetching books with ${text_reviews_count} text reviews: ${error}`
+      );
+      // Pass the error to the next middleware
+      next(err);
+    }
+  }
+);
+
+router.get("/books/language_code/:language_code", async (req, res) => {
+  const language_code = req.params.language_code;
+  try {
+    const book = await Book.find({ language_code: language_code });
+    if (book.length > 0) {
+      res.json(book);
+    } else {
+      res
+        .status(404)
+        .json({ error: `${language_code} Language code not found` });
+    }
+  } catch (error) {
+    // If an error occurred, create a new error with a custom message
+    const err = new Error(
+      `Error fetching book with language code ${language_code} : ${error}`
+    );
+    // Pass the error to the next middleware
+    next(err);
+  }
+});
+
+//add new book
+router.post("/books/add/", async (req, res, next) => {
+  try {
+    const newBook = await new Book(req.body).save();
+    res.status(201).json(newBook);
+  } catch (error) {
+    // If an error occurred, create a new error with a custom message
+    const err = new Error(`Error adding new book: ${error}`);
+    // Pass the error to the next middleware
+    next(err);
+  }
+});
+
+//update book
+router.put("/books/update/:id", async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (book) {
+      // Update the fields
+      const fieldsToUpdate = [
+        "title",
+        "authors",
+        "average_rating",
+        "isbn",
+        "isbn13",
+        "language_code",
+        "num_pages",
+        "ratings_count",
+        "text_reviews_count",
+      ];
+      fieldsToUpdate.forEach((field) => {
+        book[field] = req.body[field] || book[field];
+      });
+      // Save the updated book
+      const updatedBook = await book.save();
+
+      res.json(updatedBook);
+    } else {
+      res.status(404).json({ error: "Book not found" });
+    }
+  } catch (error) {
+    // If an error occurred, create a new error with a custom message
+    const err = new Error(`Error updating book: ${error}`);
+    // Pass the error to the next middleware
+    next(err);
+  }
+});
+
+//delete book
+router.delete("/books/delete/:id", async (req, res) => {
+  try {
+    const book = await Book.findByIdAndDelete(req.params.id);
+    if (book) {
+      res.json({ message: "Book deleted" });
+    } else {
+      res.status(404).json({ error: "Book not found" });
+    }
+  } catch (error) {
+    // If an error occurred, create a new error with a custom message
+    const err = new Error(`Error deleting book: ${error}`);
+    // Pass the error to the next middleware
+    next(err);
+  }
+});
 
 // get updated documentation
 router.get("/", (req, res) => {
@@ -59,276 +384,10 @@ router.get("/", (req, res) => {
     });
     res.json(updatedEndpoints);
   } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-//filter with query parameters
-router.get("/filterbooks", async (req, res) => {
-  try {
-    const {
-      title,
-      authors,
-      average_rating,
-      num_pages,
-      ratings_count,
-      text_reviews_count,
-      language_code,
-    } = req.query;
-
-    const query = {};
-
-    if (title) {
-      //find the title with the query parameter
-      query.title = { $regex: new RegExp(title, "i") };
-    }
-    if (authors) {
-      query.authors = { $regex: new RegExp(authors, "i") };
-    }
-    if (average_rating) {
-      query.average_rating = {
-        $gte: Number(average_rating),
-        $lt: Number(average_rating) + 1,
-      };
-    }
-    if (num_pages) {
-      query.num_pages = {
-        $gte: Number(num_pages),
-        $lt: Number(num_pages) + 100,
-      };
-    }
-    if (ratings_count) {
-      query.ratings_count = {
-        $gte: Number(ratings_count),
-        $lt: Number(ratings_count) + 100,
-      };
-    }
-    if (text_reviews_count) {
-      query.text_reviews_count = {
-        $gte: Number(text_reviews_count),
-        $lt: Number(text_reviews_count) + 100,
-      };
-    }
-    if (language_code) {
-      query.language_code = language_code;
-    }
-
-    const books = await Book.find(query).limit(100).exec(); // Limit the number of results to 100
-    res.json(books);
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-router.get("/books/", async (req, res) => {
-  try {
-    const { title } = req.query;
-    const queryRegex = { $regex: new RegExp(title, "i") };
-    const books = await Book.find({ title: queryRegex }).sort({ title: 1 });
-    res.json(books);
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-router.get("/books/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const book = await Book.findById(id).exec();
-    if (book) {
-      res.json(book);
-    } else {
-      res.status(404).json({ error: "ID Not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-router.get("/books/authors/:authors", async (req, res) => {
-  try {
-    const authors = req.params.authors;
-    const queryRegex = new RegExp(authors, "i"); // Create a case-insensitive regex to match authors
-    const book = await Book.find({ authors: { $regex: queryRegex } });
-    if (book.length > 0) {
-      res.json(book);
-    } else {
-      res.status(404).json({ error: "Author not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-router.get("/books/average_rating/:average_rating", async (req, res) => {
-  try {
-    const average_rating = Number(req.params.average_rating);
-    const book = await Book.find({
-      average_rating: {
-        $gte: average_rating,
-        $lt: average_rating + 1,
-      },
-    });
-    if (book.length > 0) {
-      res.json(book);
-    } else {
-      res.status(404).json({ error: "No results for this rating" });
-    }
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-router.get("/books/pages_asc/:num_pages", async (req, res) => {
-  try {
-    const num_pages = Number(req.params.num_pages);
-    const book = await Book.find({
-      num_pages: {
-        $gte: num_pages,
-        $lt: num_pages + 50,
-      },
-    }).sort({ num_pages: 1 });
-    if (book.length > 0) {
-      res.json(book);
-    } else {
-      res
-        .status(404)
-        .json({ error: "Book with this amount of pages not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-router.get("/books/pages_desc/:num_pages", async (req, res) => {
-  try {
-    const num_pages = Number(req.params.num_pages);
-    const book = await Book.find({
-      num_pages: {
-        $gte: num_pages,
-        $lt: num_pages + 50,
-      },
-    }).sort({ num_pages: -1 });
-    if (book.length > 0) {
-      res.json(book);
-    } else {
-      res
-        .status(404)
-        .json({ error: "Book with this amount of pages not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-router.get("/books/ratings_count/:ratings_count", async (req, res) => {
-  try {
-    const ratings_count = Number(req.params.ratings_count);
-    const book = await Book.find({
-      ratings_count: {
-        $gte: ratings_count,
-        $lt: ratings_count + 50,
-      },
-    });
-    if (book.length > 0) {
-      res.json(book);
-    } else {
-      res.status(404).json({ error: "Rating count not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-router.get(
-  "/books/text_reviews_count/:text_reviews_count",
-  async (req, res) => {
-    try {
-      const text_reviews_count = Number(req.params.text_reviews_count);
-      const book = await Book.find({
-        text_reviews_count: {
-          $gte: text_reviews_count,
-          $lt: text_reviews_count + 50,
-        },
-      });
-      if (book.length > 0) {
-        res.json(book);
-      } else {
-        res.status(404).json({ error: "Text reviews not found" });
-      }
-    } catch (error) {
-      res.status(500).json({ error: "Internal server error" });
-    }
-  }
-);
-
-router.get("/books/language_code/:language_code", async (req, res) => {
-  const language_code = req.params.language_code;
-  try {
-    const book = await Book.find({ language_code: language_code });
-    if (book.length > 0) {
-      res.json(book);
-    } else {
-      res.status(404).json({ error: "Language code not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-//add new book
-router.post("/books/add/", async (req, res) => {
-  try {
-    const newBook = await new Book(req.body).save();
-    res.status(201).json(newBook);
-  } catch (error) {
-    res.status(400).json({ error: "Invalid request" });
-  }
-});
-
-//update book
-router.put("/books/update/:id", async (req, res) => {
-  try {
-    const book = await Book.findById(req.params.id);
-    if (book) {
-      // Update the fields
-      const fieldsToUpdate = [
-        "title",
-        "authors",
-        "average_rating",
-        "isbn",
-        "isbn13",
-        "language_code",
-        "num_pages",
-        "ratings_count",
-        "text_reviews_count",
-      ];
-      fieldsToUpdate.forEach((field) => {
-        book[field] = req.body[field] || book[field];
-      });
-      // Save the updated book
-      const updatedBook = await book.save();
-
-      res.json(updatedBook);
-    } else {
-      res.status(404).json({ error: "Book not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ error: `Error updating book: ${error}` });
-  }
-});
-
-//delete book
-router.delete("/books/delete/:id", async (req, res) => {
-  try {
-    const book = await Book.findByIdAndDelete(req.params.id);
-    if (book) {
-      res.json({ message: "Book deleted" });
-    } else {
-      res.status(404).json({ error: "Book not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ error: `Error deleting book: ${error}` });
+    // If an error occurred, create a new error with a custom message
+    const err = new Error(`Error updating endpoints: ${error}`);
+    // Pass the error to the next middleware
+    next(err);
   }
 });
 
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import winston from "winston";
+import DailyRotateFile from "winston-daily-rotate-file";
+import morgan from "morgan";
 import router from "./routes/Routes.js";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl);
@@ -17,6 +24,73 @@ app.use("/", router);
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+
+// Define __filename and __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+// Define logs directory
+const logs = path.join(__dirname, "logs");
+// Create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(path.join(logs, "access.log"), {
+  flags: "a",
+});
+
+// Set up winston with DailyRotateFile transport to avoid log file size growing indefinitely
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "book-api-service" },
+  transports: [
+    // Rotate log files daily
+    new DailyRotateFile({
+      filename: path.join(logs, "error-%DATE%.log"),
+      datePattern: "YYYY-MM-DD",
+      zippedArchive: true,
+      maxSize: "20m",
+      maxFiles: "14d",
+      level: "error",
+    }),
+    new DailyRotateFile({
+      filename: path.join(logs, "combined-%DATE%.log"),
+      datePattern: "YYYY-MM-DD",
+      zippedArchive: true,
+      maxSize: "20m",
+      maxFiles: "14d",
+    }),
+    // Log to the console
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+  ],
+});
+
+// Create a write stream for morgan and write Morgan logs to winston
+const morganStream = {
+  write: (message) => {
+    logger.info(message);
+  },
+};
+
+// Log remote address, remote user, date, HTTP method, URL, HTTP version, status code, content length, referrer, and user-agent.
+app.use(morgan("combined", { stream: morganStream }));
+
+// Error handling middleware
+router.use((err, req, res, next) => {
+  console.error(err.stack); // Log error stack trace
+  res.status(500).send({ error: err.message }); // Send error message to client
+});
+
+// Handle uncaught exceptions
+process.on("uncaughtException", (err) => {
+  logger.error(err);
+  process.exit(1);
+});
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (reason, promise) => {
+  logger.error("Unhandled Rejection at:", promise, "reason:", reason);
+  process.exit(1);
+});
 
 // Start the server
 app.listen(port, () => {
