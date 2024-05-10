89b6550e51789445afa69f4fe61894ff5be32102
Frida Svensson <fridamarisvensson@gmail.com>
2024-05-10 21:16:56 +0200

    Install dotenv and express-list-endpoints, configure dotenv. Remove data that will not be used in this project.

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+import expressListEndpoints from "express-list-endpoints";
+import booksData from "./data/books.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+// Configure dotenv
+require("dotenv").config();
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl);
