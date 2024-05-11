236cac7d22cef6984553d32e33caaac8c8ae2dab
Citronskal <yiaswift@gmail.com>
2024-05-11 22:46:29 +0200

    updated server.js

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ import mongoose from "mongoose";
 import listEndpoints from "express-list-endpoints";
 import topMusicData from "./data/top-music.json";
 
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+
 const songSchema = new mongoose.Schema({
   trackName: String,
   artistName: String,
@@ -34,17 +43,6 @@ const seedDatabase = async () => {
 };
 
 seedDatabase();
-
-
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl);
-mongoose.Promise = Promise;
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
