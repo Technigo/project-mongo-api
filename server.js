7f72c58c56796186884c2355e789c5657b3aa612
Lovisa <lovisa.aaberg@gmail.com>
2024-05-11 11:40:11 +0200

    Endpoint to fetch sneakers by brand

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 import express from "express"
 import cors from "cors"
 import mongoose from "mongoose"
+import dotenv from "dotenv"
 import expressListEndpoints from "express-list-endpoints"
-import Sneaker from "./models/Sneaker"
 
 //Import the data
 import sneakerData from "./data/sneakers.json"
+import Sneaker from "./models/Sneaker"
 
+//.env
+dotenv.config()
+
+//Setting up the MongoDB connection using Mongoose, 
+//retrieves the MongoDB connection URL from the environment variables.
 const mongoUrl =
   process.env.MONGO_URL || "mongodb://localhost/project-mongo-api"
 mongoose.connect(mongoUrl)
@@ -34,19 +40,12 @@ app.use(cors())
 app.use(express.json())
 
 // Route handler
-//http://localhost:8080/
 app.get("/", (req, res) => {
   const endpoints = expressListEndpoints(app)
-
-  const endpointsList = endpoints.map((endpoint) => {
-    if (endpoint.path === "/sneakers") {
-      endpoint.query = ["name", "brand", "color", "price"]
-    } return endpoint
-  })
-  res.json(endpointsList)
+  res.json(endpoints)
 })
 
-//Get all sneakers
+//Endpoint to fetch all sneakers
 app.get("/sneakers", async (req, res) => {
   const allSneakers = await Sneaker.find()
 
@@ -57,7 +56,7 @@ app.get("/sneakers", async (req, res) => {
   }
 })
 
-//Fetch all sneakers in-stock
+//Endpoint to fetch sneakers in-stock
 app.get("/sneakers/in-stock", async (req, res) => {
   const sneakersInStock = await Sneaker.find({ inStock: true }).exec()
 
@@ -68,7 +67,8 @@ app.get("/sneakers/in-stock", async (req, res) => {
   }
 })
 
-//Get only one sneaker
+
+//Endpoint to fetch sneaker by ID
 app.get("/sneakers/:sneakerId", async (req, res) => {
   const { sneakerId } = req.params
   const sneaker = await Sneaker.findById(sneakerId).exec()
@@ -78,6 +78,19 @@ app.get("/sneakers/:sneakerId", async (req, res) => {
   } else {
     res.status(404).send("No sneaker was found, please try again.")
   }
+})
+
+//Endpoint to fetch sneakers by brand
+app.get("/sneakers/brand/:brand", async (req, res) => {
+  const { brand } = req.params
+  const sneakersByBrand = await  Sneaker.find({ brand: brand }).exec()
+  
+  if (sneakersByBrand.length > 0) {
+    res.json(sneakersByBrand)
+  } else {
+    res.status(404).send(`No sneakers were found for this brand: ${brand}, please try another brand.`)
+  }
+  
 })
 
 // Start the server
