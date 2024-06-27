411bd9f6fb859ec127811e738901274734794eef
Looziolooz <lorenzo.dastoli@gmail.com>
2024-06-27 12:35:24 +0200

    update

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ import mongoose from "mongoose";
 import { Book } from "./models/BookModel";
 import listEndpoints from "express-list-endpoints";
 
-// Define the port
+// Defining the port
 const port = process.env.PORT || 8000;
 const app = express();
 
-// Add middlewares to enable CORS and JSON body parsing
+// Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  // Check if connection to the database is stable
+  // Check if conntection is stable
   if (mongoose.connection.readyState === 1) {
     next();
   } else {
@@ -22,18 +22,17 @@ app.use((req, res, next) => {
   }
 });
 
-// Define routes
+//Defining routes____________//
 
-// Root route listing all endpoints
 app.get("/", (req, res) => {
   const endpoints = listEndpoints(app);
   res.status(200).json(endpoints);
 });
 
-// Route for saving a new book
+// Route for save a new book
 app.post("/books", async (req, res) => {
   try {
-    // Check if the new data has the required properties
+    // Check if new data has following properties
     if (!req.body.title || !req.body.author || !req.body.publishYear) {
       return res.status(400).json({
         message: "Send all required fields: title, author, publishYear",
@@ -45,13 +44,8 @@ app.post("/books", async (req, res) => {
       title: req.body.title,
       author: req.body.author,
       publishYear: req.body.publishYear,
-      language: req.body.language,
-      average_rating: req.body.average_rating,
-      num_pages: req.body.num_pages,
-      ratings_count: req.body.ratings_count,
     };
-
-    // Create a new instance
+    // Create a new instans
     const book = await Book.create(newBook);
     // Send the new book to the client
     return res.status(201).json(book);
@@ -61,35 +55,10 @@ app.post("/books", async (req, res) => {
   }
 });
 
-// Route for getting all books from the database
+// Route for Get All Books from DB
 app.get("/books", async (req, res) => {
   try {
-    const { author, language, publishYear, average_rating, num_pages } = req.query;
-    let query = {};
-
-    if (author) {
-      query.author = author;
-    }
-
-    if (language) {
-      query.language = language;
-    }
-
-    if (publishYear) {
-      query.publishYear = publishYear;
-    }
-
-    if (average_rating) {
-      query.average_rating = { $gte: average_rating }; // Find books with rating equal or greater than specified
-    }
-
-    if (num_pages) {
-      query.num_pages = { $gte: num_pages }; // Find books with number of pages equal or greater than specified
-    }
-
-    console.log("Query:", query); // Log the query for debugging
-
-    const books = await Book.find(query);
+    const books = await Book.find({});
 
     return res.status(200).json({
       count: books.length,
@@ -101,10 +70,12 @@ app.get("/books", async (req, res) => {
   }
 });
 
-// Route for getting one book from the database using ID
+// Route for Geting One Book from DB using ID
 app.get("/books/:id", async (req, res) => {
   try {
+    // Destructed the id from the param
     const { id } = req.params;
+    // Find book by ID
     const book = await Book.findById(id);
 
     return res.status(200).json(book);
@@ -114,7 +85,7 @@ app.get("/books/:id", async (req, res) => {
   }
 });
 
-// Route for updating a book
+// Route for Updating a Book
 app.put("/books/:id", async (req, res) => {
   try {
     if (!req.body.title || !req.body.author || !req.body.publishYear) {
@@ -123,7 +94,10 @@ app.put("/books/:id", async (req, res) => {
       });
     }
 
+    // Destructed the id from the param
     const { id } = req.params;
+
+    // Find book by ID and Update
     const result = await Book.findByIdAndUpdate(id, req.body);
 
     if (!result) {
@@ -137,10 +111,11 @@ app.put("/books/:id", async (req, res) => {
   }
 });
 
-// Route for deleting a book
+// Route for Deleting Book
 app.delete("/books/:id", async (req, res) => {
   try {
     const { id } = req.params;
+
     const result = await Book.findByIdAndDelete(id);
 
     if (!result) {
@@ -158,8 +133,8 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/bookStore";
 mongoose
   .connect(mongoUrl)
   .then(() => {
-    console.log("App is connected to the database");
-    // Start the server when connected to the database
+    console.log("App is conntected to db");
+    // Start the server when conntected to db
     app.listen(port, () => {
       console.log(`Server running on http://localhost:${port}`);
     });
@@ -167,5 +142,4 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
 mongoose.Promise = Promise;
