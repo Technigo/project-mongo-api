34c72f0db96464f1b433634071dd4694979e4eb6
Looziolooz <lorenzo.dastoli@gmail.com>
2024-06-27 12:44:21 +0200

    update

--- models/bookModel.js
+++ models/bookModel.js
@@ -17,22 +17,6 @@ const bookSchema = new Schema(
       type: Number,
       required: true,
     },
-    language: {
-      type: String,
-      required: false,
-    },
-    average_rating: {
-      type: Number,
-      required: false,
-    },
-    num_pages: {
-      type: Number,
-      required: false,
-    },
-    ratings_count: {
-      type: Number,
-      required: false,
-    },
   },
   {
     timestamps: true,
