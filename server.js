f9a9333175ef6f3efd8f1d0d3478c239444799e2
Mika <mika@sanalabs.com>
2024-12-11 16:38:10 +0100

    seeded database, set up mongoose model, and added some basic paths

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import topMusicData from "./data/top-music.json";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+const Song = mongoose.model("Song", {
+  // Properties defined here match the keys from the people.json file
+  id: Number,
+  trackName: String,
+  artistName: String,
+  genre: String,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  loudness: Number,
+  liveness: Number,
+  valence: Number,
+  length: Number,
+  acousticness: Number,
+  speechiness: Number,
+  popularity: Number,
+});
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/top-music";
 mongoose.connect(mongoUrl);
+console.log("Connected to MongoDB");
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 9000;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
@@ -27,6 +36,48 @@ app.use(express.json());
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+app.get("/songs", async (req, res) => {
+  const { popularity, genre } = req.query;
+
+  let songs = await Song.find();
+
+  if (popularity) {
+    if (popularity !== "most popular" && popularity !== "least popular") {
+      res.status(400).json({
+        message:
+          "Invalid popularity value. Sort by 'most popular' or 'least popular'.",
+      });
+    }
+    if (popularity === "most popular") {
+      songs.sort((a, b) => b.popularity - a.popularity);
+    } else if (popularity === "least popular") {
+      songs.sort((a, b) => a.popularity - b.popularity);
+    }
+  }
+
+  if (genre) {
+    const searchTerms = genre.toLowerCase().split(" ");
+    songs = songs.filter((song) =>
+      searchTerms.every((term) => song.genre.toLowerCase().includes(term))
+    );
+  }
+
+  if (songs.length === 0) {
+    res.status(404).json({ message: "No songs found" });
+  }
+
+  res.json(songs);
+});
+
+app.get("/songs/:id", (req, res) => {
+  const songId = req.params.id;
+  const song = topMusicData.find((song) => song.id === Number(songId));
+  if (!song) {
+    res.status(404).json({ message: "Song not found" });
+  }
+  res.json(song);
 });
 
 // Start the server
