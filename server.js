eaeeb9c6dcd540d8a85437f567468e8e84316cbd
Zoe <zoe.lindqvist@gmail.com>
2024-12-11 23:11:24 +0100

    initial commit

--- server.js
+++ server.js
@@ -1,35 +1,89 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import bodyParser from "body-parser";
+//import dotenv from "dotenv";
+//import books from "./data/books.json" assert { type: "json" };
+import { Book } from "./models/Book.js";
+import listEndpoints from "express-list-endpoints";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
-
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl);
-mongoose.Promise = Promise;
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
+// Initialize the app **before using it**
 const app = express();
+// Defines the port the app will run on
+const port = process.env.PORT || 9000;
 
-// Add middlewares to enable cors and json body parsing
+// Add middlewares to enable CORS and JSON body parsing
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+app.use((req, res, next) => {
+  // Check if the connection is stable
+  if (mongoose.connection.readyState === 1) {
+    next();
+  } else {
+    res.status(503).json({ error: "Service unavailable" });
+  }
 });
 
-// Start the server
+// ----------------Define routes------------------ //
+app.get("/", (req, res) => {
+  const endpoints = listEndpoints(app);
+  res.status(200).json(endpoints);
+});
+
+// app.get("/authors", async (req, res) => {
+//   const authors = await Author.find();
+//   res.json(authors);
+// });
+
+
+// Route for a new book
+app.get("/books", async (req, res) => {
+  const books = await Book.find().populate("author");
+  res.json(books);
+});
+
+
+
+
+
+
+// Start the server **after initialization**
+// node server.js
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+
+
+
+
+ const Author = mongoose.model("Author", { name: String });
+
+//RESET_DATABASE=true npm run dev
+if (process.env.RESET_DATABASE) {
+  const seedDatabase = async () => {
+    await Author.deleteMany();
+
+    const tolkien = new Author({ name: "J.R.R Tolkien" });
+    await tolkien.save();
+  };
+  seedDatabase();
+}
+
+// MongoDB connection setup
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/books";
+mongoose.connect(mongoUrl);
+
+.then(() => {
+  console.log("App is conntected to db");
+  // Start the server when conntected to db
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
+})
+.catch((err) => {
+  console.log(err);
+});
+mongoose.Promise = Promise;
+
