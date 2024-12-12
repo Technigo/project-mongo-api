a51f7c75144282480c2ce03924d488c4b3cfd663
Emelie Nyberg <emelie.kedert@gmail.com>
2024-12-12 16:06:55 +0100

    was a bit scared of sending to github since I did not have the .env file, but now it should be working

--- server.js
+++ server.js
@@ -1,32 +1,100 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import nintendoGames from "./data/nintendoswitch-games.json";
+import dotenv from "dotenv";
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+// To use .env, also checked if my .env file was in .gitignore
+dotenv.config();
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+// Added this to my .env file: MONGO_URL="mongodb+srv://<username>:<password>@cluster0.mongodb.net/<dbname>?retryWrites=true&w=majority"
+// To make it more secure
+const mongoUrl = process.env.MONGO_URL
+// const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"; // If connected to compass an Local host
 mongoose.connect(mongoUrl);
 mongoose.Promise = Promise;
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
+// Defines mongoose model
+const Game = mongoose.model('Game', {
+  id: Number,
+  name: String,
+  category: String,
+  release_year: Number,
+  rating: Number
+});
+
+// Seed database
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Game.deleteMany({});
+    nintendoGames.forEach(async (gameData) => {
+      const game = new Game(gameData);
+      await game.save();
+    });
+    console.log("Database seeded!");
+  };
+
+  seedDatabase();
+}
+
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Hello Technigo!")
+});
+
+// Get all videogames http://localhost:3000/videogames
+app.get("/videogames", async (req, res) => {
+  const { sorted, category, release_year } = req.query;
+
+  // Sorted rating from highest to lowest http://localhost:3000/videogames?sorted=true
+  if (sorted) {
+    const sortedGames = await Game.find().sort({ rating: -1 }); // Sort after rating, highest first
+    return res.json(sortedGames);
+  }
+
+  // Dynamic query-object so that you can filter on both category and release_year
+  // For example http://localhost:3000/videogames?category=racing&release_year=2017
+  const query = {};
+
+  // For example http://localhost:3000/videogames?category=racing
+  if (category) {
+    query.category = { $regex: category, $options: "i" }; // Case-insensitive search on category. for example its ok if database says "Racing" and url says "racing"
+  }
+
+  // For example http://localhost:3000/videogames?release_year=2017
+  if (release_year) {
+    query.release_year = +release_year; // Filtrera på release_year
+  }
+
+  // Get games depending on the dynamic query-object
+  const videogames = await Game.find(query);
+
+  // Send result as JSON
+  res.json(videogames);
+
+});
+
+// Endpoint that returns one single videogame
+// For example with id=3 http://localhost:3000/videogames/3
+app.get("/videogames/:id", async (req, res) => {
+  const id = req.params.id;
+  const videogame = await Game.findOne({ id: +id });
+
+  if (videogame) {
+    res.json(videogame);
+  } else {
+    res.status(404).json({ error: "No videogame found with that ID" });
+  }
 });
 
 // Start the server
