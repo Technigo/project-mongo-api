f57bba1265533a103fdfe43de7e63b747c4baf56
Elina Eriksson Hult <elinaerikssonhult@gmail.com>
2024-12-13 15:45:41 +0100

    Installed dependencies, added secret env. file and connected project to mongo atlas

--- server.js
+++ server.js
@@ -1,23 +1,42 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
+if (!process.env.MONGO_URL) {
+  console.error("Error: MONGO_URL is not defined in the .env file.");
+  process.exit(1); // Stop the app if MONGO_URL is not defined
+} else {
+  console.log("Mongo URL loaded successfully!");
+}
+
 // import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import topMusicData from "./data/top-music.json";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl);
+
+
+// Connecting to mongo Atlas using the secret data from env. file
+const mongoUrl = process.env.MONGO_URL
+console.log('Mongo URL:', mongoUrl);
+
+mongoose.connect(mongoUrl)
+  .then(() => {
+    console.log("Successfully connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+    // Exit if the connection fails
+    process.exit(1);
+  });
+
 mongoose.Promise = Promise;
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
+console.log(process.env.PORT);
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
