e42020e0f48c3c5f7ceb61d892034e2cbd61baec
HeleneWestrin <helene.westrin@alphadev.se>
2024-12-13 15:03:01 +0100

    Vercel updates

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDatabase = async () => {
-  const mongoUrl = process.env.MONOG_URL || "mongodb://localhost/fantasy-world";
+  const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/fantasy-world";
 
   try {
     await mongoose.connect(mongoUrl);
--- server.js
+++ server.js
@@ -6,29 +6,21 @@ import { configDotenv } from "dotenv";
 // Load environment variables
 configDotenv();
 
-// Set the port
-const port = process.env.PORT || 8080;
-
-const startServer = async () => {
+// Connect to the database and optionally seed it
+(async () => {
   try {
-    // Connect to the database
     await connectDatabase();
 
-    // Seed the database if the environment variable is set
     if (process.env.SEED_DATABASE === "true") {
       console.log("Seeding database...");
       await seedDatabase();
       console.log("Database seeding completed");
     }
-
-    // Start the server
-    app.listen(port, () => {
-      console.log(`Server running on http://localhost:${port}`);
-    });
   } catch (error) {
-    console.error("Error starting the server:", error);
-    process.exit(1); // Exit the process with failure
+    console.error("Error connecting to the database or seeding:", error);
+    process.exit(1);
   }
-};
+})();
 
-startServer();
+// Export the app
+export default app;
