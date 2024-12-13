ddb267ccbb53118c1a5c261b2110cfdb80705cf6
Elina Eriksson Hult <elinaerikssonhult@gmail.com>
2024-12-13 19:09:42 +0100

    Added routes, endpoints and express list endpoints

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import listEndpoints from "express-list-endpoints";
 import booksData from "./data/books.json";
 
 dotenv.config();
@@ -55,7 +56,6 @@ if (process.env.RESET_DB) {
   console.log("Database seeded with book data!");
 
   };
-
   seedDatabase()
 }
 
@@ -64,9 +64,55 @@ app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
+
+// Home page listing all endpoints
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  const endpoints = listEndpoints(app)
+  res.json({
+    message: "Hello and welcome to the libray! Here are all the endpoints:",
+    endpoints: endpoints
+  });
 });
+
+// Route to fetch all books
+app.get("/books", async (req, res) => {
+  const books = await Book.find()
+  res.json(books)
+});
+
+// Route to fetch details for a single book
+app.get("/books/:bookID", async (req, res) => {
+  const { bookID } = req.params;
+  
+  try {
+    const book = await Book.findOne({ bookID: bookID });
+
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    res.json(book);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  };
+});
+
+// Route to get book by author
+app.get("/books/authors/:author", async (req, res) => {
+  const { author } = req.params;
+  try {
+    const books = await Book.find({ authors: new RegExp(author, "i") });
+
+    if (books.length === 0) {
+      return res.status(404).send("No books found for this author");
+    }
+    res.json(books);
+  } catch(error) {
+    console.error("Error retrieving books by author", error);
+    res.status(500).send("Server error");
+  }
+});
+
 
 // Start the server
 app.listen(port, () => {
