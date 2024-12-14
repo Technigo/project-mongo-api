9cdb2345a774dc04dead714a67d391ee871038c4
xingyin2024 <xingyin.thu@gmail.com>
2024-12-14 23:03:46 +0100

    add controller

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -1,10 +1,37 @@
-const existingTrip = await Trip.findOne({
-  userID: userId,
-  $or: [
-    { "tripDate.startDate": { $lt: newEndDate, $gte: newStartDate } },
-    { "tripDate.endDate": { $gt: newStartDate, $lte: newEndDate } },
-  ],
-});
-if (existingTrip) {
-  throw new Error("Trip dates overlap with an existing trip.");
-}
+import { Trip } from "../models/tripModel.js";
+
+// GET all trips
+export const getTrips = async (req, res) => {
+  try {
+    const trips = await Trip.find();
+    res.status(200).json(trips);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// POST a new trip
+export const createTrip = async (req, res) => {
+  try {
+    const trip = new Trip(req.body);
+    await trip.save();
+    res.status(201).json(trip);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// PUT (update) an existing trip
+export const updateTrip = async (req, res) => {
+  try {
+    const trip = await Trip.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!trip) {
+      return res.status(404).json({ message: "Trip not found." });
+    }
+    res.status(200).json(trip);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
